2a7cae774f603aa043c44aee77edd81a39a85aca
Ken Weiner <kweiner@gmail.com>
2000-05-16 20:50:41 +0000

    Applets with no parameters can be used now.
    git-svn-id: https://source.jasig.org/uPortal/trunk@3290 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/channels/CApplet.java
+++ source/org/jasig/portal/channels/CApplet.java
@@ -13,60 +13,62 @@ import java.net.*;
 
 
 /**
- * Displays and applet. You have to pass in the appropriate parameters.
+ * Displays an applet. You have to pass in the appropriate parameters.
  * Applet parameters are currently passed in as a concatenated string with
  * format: name1>value1^name2>value2^name3>value3 ...
  * It is possible that '>' and '^' are characters that may actually appear
  * in applet parameters.  If this is true, this method will have to be
  * rewritten.
- * 
+ *
  * @author Ken Weiner
  * @version $Revision$
  */
-public class CApplet implements org.jasig.portal.IChannel                   
+public class CApplet implements org.jasig.portal.IChannel
 {
-  private Hashtable chConfig = null;
-  
+  private ChannelConfig chConfig = null;
+
   public void init (ChannelConfig chConfig) {this.chConfig = chConfig;}
   public String getName () {return (String) chConfig.get ("name");}
   public boolean isMinimizable () {return true;}
   public boolean isDetachable () {return true;}
   public boolean isRemovable () {return true;}
-  public boolean isEditable () {return false;}  
-  public boolean hasHelp () {return false;}  
-  
+  public boolean isEditable () {return false;}
+  public boolean hasHelp () {return false;}
+
   public int getDefaultDetachWidth () {return 250;}
   public int getDefaultDetachHeight () {return 350;}
-  
+
   public void render (HttpServletRequest req, HttpServletResponse res, JspWriter out)
-  {    
-    try 
+  {
+    try
     {
-      // Change station parameter to reflect local area.  
-      // Station guide at http://www.abcnews.go.com/local/
       out.println ("<center>");
       out.println ("<applet CODE=\"" + chConfig.get ("code") + "\" CODEBASE=\"" + chConfig.get ("codeBase") + "\" WIDTH=\"" + chConfig.get ("width") + "\" HEIGHT=\"" + chConfig.get ("height") + "\" ALIGN=top border=0 archive=\"" + chConfig.get ("archive") + "\">");
-  	  
+
   	  // Split "params" string into name-value pairs
   	  String sParams = (String) chConfig.get ("params");
-  	  StringTokenizer stParams = new StringTokenizer (sParams, "^");
-  	   
-  	  // For each "param" string, parse to get name and value
-  	  String sParamName, sParamValue;
-  	  
-  	  while (stParams.hasMoreTokens()) 
+
+  	  if (sParams != null)
   	  {
-  	    sParamName="";
-  	    sParamValue="";
-  	    StringTokenizer stNameValue = new StringTokenizer (stParams.nextToken (), ">");
-  	    
-  	    if (stNameValue.hasMoreTokens ())
-  	      sParamName = stNameValue.nextToken ();
-  	    
-  	    if (stNameValue.hasMoreTokens ())
-  	      sParamValue = stNameValue.nextToken ();
-        
-        out.println ("<param name=\"" + sParamName + "\" value=\"" + sParamValue + "\">");
+  	    StringTokenizer stParams = new StringTokenizer (sParams, "^");
+
+  	    // For each "param" string, parse to get name and value
+  	    String sParamName, sParamValue;
+
+  	    while (stParams.hasMoreTokens())
+  	    {
+  	      sParamName="";
+  	      sParamValue="";
+  	      StringTokenizer stNameValue = new StringTokenizer (stParams.nextToken (), ">");
+
+  	      if (stNameValue.hasMoreTokens ())
+  	        sParamName = stNameValue.nextToken ();
+
+  	      if (stNameValue.hasMoreTokens ())
+  	        sParamValue = stNameValue.nextToken ();
+
+          out.println ("<param name=\"" + sParamName + "\" value=\"" + sParamValue + "\">");
+        }
       }
 
       out.println ("</applet>");
@@ -81,7 +83,7 @@ public class CApplet implements org.jasig.portal.IChannel
   {
     // This channel is not editable
   }
-  
+
   public void help (HttpServletRequest req, HttpServletResponse res, JspWriter out)
   {
     // This channel has no help
