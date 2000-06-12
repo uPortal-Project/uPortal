1f323471c8077e29c270b3497905818c0a63cd25
shawn.bayern@committers.jasig.org <shawn.bayern@committers.jasig.org>
2000-06-12 20:56:25 +0000

    Bug fixes, better relative image/link handling, and support for parameters that direct elimination of stuff inside <script> tags and OUTSIDE <portal> tag.
    git-svn-id: https://source.jasig.org/uPortal/trunk@3329 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/channels/CPageRenderer.java
+++ source/org/jasig/portal/channels/CPageRenderer.java
@@ -7,11 +7,11 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.util.*;
 import com.objectspace.xml.*;
+import com.oroinc.text.regex.*;
 import org.jasig.portal.*;
 import org.jasig.portal.layout.*;
 
 import java.net.*;
-
 
 /**
  * This is a user-defined channel for rendering a web page.
@@ -22,6 +22,9 @@ import java.net.*;
 public class CPageRenderer implements org.jasig.portal.IChannel                            
 { 
   protected String m_sUrl = null;
+  protected String m_sUrlBaseDir = null;
+  protected String m_sUrlServer = null;
+
   private ChannelConfig chConfig = null;
   
   public void init (ChannelConfig chConfig) {this.chConfig = chConfig;}
@@ -40,6 +43,11 @@ public class CPageRenderer implements org.jasig.portal.IChannel
     try 
     {
       m_sUrl = (String) chConfig.get ("url");
+      m_sUrlBaseDir =
+	  new String(m_sUrl.substring(0, m_sUrl.lastIndexOf("/")));
+      m_sUrlServer = 
+	  new String(m_sUrlBaseDir.substring(7,    // 7 to skip "http://"
+	      m_sUrlBaseDir.substring(7).indexOf("/")+7));
       String sLine = null;
       URL url = new URL (m_sUrl);
       HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
@@ -48,17 +56,21 @@ public class CPageRenderer implements org.jasig.portal.IChannel
       StringBuffer sbHTML = new StringBuffer (1024);
       
       while ((sLine = theHTML.readLine()) != null)
-        sbHTML.append (sLine);
+        sbHTML.append (sLine + "\n");
       
       // Filter out HTML between body tags
       String sHTML = grabHtmlBody (sbHTML.toString ());
       
       if (sHTML != null)
       {
-        // Fix relative images sources
         sHTML = replaceRelativeImages (sHTML);
-        
-        // Replace links
+        sHTML = replaceRelativeLinks (sHTML);
+	if (chConfig.get("eliminateScript") != null &&
+	    chConfig.get("eliminateScript").equals("true"))
+	  sHTML = eliminateScript (sHTML);
+	if (chConfig.get("portalAware") != null &&
+	    chConfig.get("portalAware").equals("true"))
+	  sHTML = eliminateIrrelevant (sHTML);
                     
         out.println (sHTML);
       }
@@ -93,6 +105,8 @@ public class CPageRenderer implements org.jasig.portal.IChannel
     // This channel has no help
   }
   
+  // TODO: should be updated to use ORO regex matching instead of
+  // simple String operations
   protected String grabHtmlBody (String sHTML)
   {    
     try 
@@ -119,48 +133,141 @@ public class CPageRenderer implements org.jasig.portal.IChannel
     return null;
   }
   
+  // replace relative SRC attributes in <IMG> tags with absolute references
   protected String replaceRelativeImages (String sHTML)
   {    
-    try 
-    {
-      int iStartAt = 0;
-      
-      while (iStartAt >= 0)
-      {
-        int iSrcAt;
-        
-        int iNextSRC = sHTML.indexOf ("SRC", iStartAt);
-        int iNextSrc = sHTML.indexOf ("src", iStartAt);
-        
-        if (iNextSRC < 0 && iNextSrc < 0)
-          iSrcAt = -1;
-        else
-          iSrcAt = iNextSRC < iNextSrc ? iNextSRC : iNextSrc;
-          
-        if (iSrcAt >= 0)
-        { 
-          if (! (sHTML.substring (iSrcAt + 4).startsWith ("http://") || sHTML.substring (iSrcAt + 5).startsWith ("http://")))
-          {
-            if (sHTML.charAt (iSrcAt + 4) == '\"')
-              sHTML = sHTML.substring (0, iSrcAt + 5) + m_sUrl + ((sHTML.charAt (iSrcAt + 5)) == '/' ? "" : "/") + sHTML.substring (iSrcAt + 5);
-            else
-              sHTML = sHTML.substring (0, iSrcAt + 4) + m_sUrl + ((sHTML.charAt (iSrcAt + 4)) == '/' ? "" : "/") + sHTML.substring (iSrcAt + 4);
-          }
-          
-          iStartAt = iSrcAt + 1;
-        }
-        else
-        {
-          iStartAt = -1;
-        }
-      }
-      
+    // uses the ORO regular-expression engine (Perl5-style regexes)
+    String r1 = "<img\\s+(.*?)src=\"/(.*?)\"(.*?)>";
+    String t1 = "<img $1src=\"http://" + m_sUrlServer + "/$2\"$3>";
+
+    String r2 = "<img\\s+(.*?)src=\"([^:]*?)\"(.*?)>";
+    String t2 = "<img $1src=\"" + m_sUrlBaseDir + "/$2\"$3>";
+
+    Pattern p1 = null;
+    Pattern p2 = null;
+
+    // Create Perl5Compiler and Perl5Matcher instances.
+    PatternCompiler compiler = new Perl5Compiler();
+    PatternMatcher matcher  = new Perl5Matcher();
+
+    // Attempt to compile the pattern.  If the pattern is not valid,
+    // report the error and exit.
+    try {
+      // for relative links with full pathname
+      p1 = compiler.compile(r1,	Perl5Compiler.CASE_INSENSITIVE_MASK
+	   | Perl5Compiler.SINGLELINE_MASK);
+      // for truly relative links
+      p2 = compiler.compile(r2, Perl5Compiler.CASE_INSENSITIVE_MASK
+	   | Perl5Compiler.SINGLELINE_MASK);
+    } catch(MalformedPatternException e){
+      Logger.log(Logger.ERROR, e);
       return sHTML;
     }
-    catch (Exception e)
-    {
-      Logger.log (Logger.ERROR, e);
+
+    String o1 = Util.substitute(matcher, p1, t1, sHTML,
+		    Util.SUBSTITUTE_ALL, Util.INTERPOLATE_ALL);
+    String o2 = Util.substitute(matcher, p2, t2, o1,
+		    Util.SUBSTITUTE_ALL, Util.INTERPOLATE_ALL);
+    return o2;
+  }
+
+  /** like repaceRelativeImages but with HREF instead of SRC... 
+   *  coule be integrated later to use the same logic for both */
+  protected String replaceRelativeLinks (String sHTML)
+  {
+    // uses the ORO regular-expression engine (Perl5-style regexes)
+    String r1 = "<a\\s+(.*?)href=\"/(.*?)\"(.*?)>";
+    String t1 = "<a $1href=\"http://" + m_sUrlServer + "/$2\"$3>";
+
+    String r2 = "<a\\s+(.*?)href=\"([^:]*?)\"(.*?)>";
+    String t2 = "<a $1href=\"" + m_sUrlBaseDir + "/$2\"$3>";
+
+    Pattern p1 = null;
+    Pattern p2 = null;
+
+    // Create Perl5Compiler and Perl5Matcher instances.
+    PatternCompiler compiler = new Perl5Compiler();
+    PatternMatcher matcher  = new Perl5Matcher();
+
+    // Attempt to compile the pattern.  If the pattern is not valid,
+    // report the error and exit.
+    try {
+      // for relative links with full pathname
+      p1 = compiler.compile(r1,	Perl5Compiler.CASE_INSENSITIVE_MASK
+	   | Perl5Compiler.SINGLELINE_MASK);
+      // for truly relative links
+      p2 = compiler.compile(r2, Perl5Compiler.CASE_INSENSITIVE_MASK
+	   | Perl5Compiler.SINGLELINE_MASK);
+    } catch(MalformedPatternException e){
+      Logger.log(Logger.ERROR, e);
+      return sHTML;
     }
-    return null;
+    String o1 = Util.substitute(matcher, p1, t1, sHTML,
+		    Util.SUBSTITUTE_ALL, Util.INTERPOLATE_ALL);
+    String o2 = Util.substitute(matcher, p2, t2, o1,
+		    Util.SUBSTITUTE_ALL, Util.INTERPOLATE_ALL);
+    return o2;
+  }
+
+  // get rid of the contents of <script> tags
+  protected String eliminateScript(String sHTML) {
+    // uses the ORO regular-expression engine (Perl5-style regexes)
+    String r1 = "<script>.*?</script>";
+    String t1 = "<script><!-- Eliminated by UPortal --></script>";
+
+    Pattern p1 = null;
+
+    // Create Perl5Compiler and Perl5Matcher instances.
+    PatternCompiler compiler = new Perl5Compiler();
+    PatternMatcher matcher  = new Perl5Matcher();
+
+    // Attempt to compile the pattern.  If the pattern is not valid,
+    // report the error and exit.
+    try {
+      // for relative links with full pathname
+      p1 = compiler.compile(r1,	Perl5Compiler.CASE_INSENSITIVE_MASK
+	   | Perl5Compiler.SINGLELINE_MASK);
+    } catch(MalformedPatternException e){
+      Logger.log(Logger.ERROR, e);
+      return sHTML;
+    }
+    String o1 = Util.substitute(matcher, p1, t1, sHTML,
+		    Util.SUBSTITUTE_ALL, Util.INTERPOLATE_ALL);
+    return o1;
+  }
+
+  // get rid of everything that's not inside <portal> tags
+  protected String eliminateIrrelevant(String sHTML) {
+    // uses the ORO regular-expression engine (Perl5-style regexes)
+    String r1 = ".*<portal>";
+    String t1 = "";
+
+    String r2 = "</portal>.*";
+    String t2 = "";
+
+    Pattern p1 = null;
+    Pattern p2 = null;
+
+    // Create Perl5Compiler and Perl5Matcher instances.
+    PatternCompiler compiler = new Perl5Compiler();
+    PatternMatcher matcher  = new Perl5Matcher();
+
+    // Attempt to compile the pattern.  If the pattern is not valid,
+    // report the error and exit.
+    try {
+      // for relative links with full pathname
+      p1 = compiler.compile(r1,	Perl5Compiler.CASE_INSENSITIVE_MASK
+	  | Perl5Compiler.SINGLELINE_MASK);
+      p2 = compiler.compile(r2,	Perl5Compiler.CASE_INSENSITIVE_MASK
+	   | Perl5Compiler.SINGLELINE_MASK);
+    } catch(MalformedPatternException e){
+      Logger.log(Logger.ERROR, e);
+      return sHTML;
+    }
+    String o1 = Util.substitute(matcher, p1, t1, sHTML,
+		    Util.SUBSTITUTE_ALL, Util.INTERPOLATE_ALL);
+    String o2 = Util.substitute(matcher, p2, t2, o1,
+		    Util.SUBSTITUTE_ALL, Util.INTERPOLATE_ALL);
+    return o2;
   }
 }
