10975b3d2ee8e46c66c76a19c7b97970148ffeaf
Ken Weiner <kweiner@gmail.com>
2000-08-23 18:25:57 +0000

    Fixed Bug #9: problem with tabs of the same name becoming active.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-1-0-patches@3420 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/LayoutBean.java
+++ source/org/jasig/portal/LayoutBean.java
@@ -1,38 +1,3 @@
-/**
- * Copyright (c) 2000 The JA-SIG Collaborative.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. Redistributions of any form whatsoever must retain the following
- *    acknowledgment:
- *    "This product includes software developed by the JA-SIG Collaborative
- *    (http://www.jasig.org/)."
- *
- * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
 package org.jasig.portal;
 
 import javax.servlet.*;
@@ -445,24 +410,21 @@ public class LayoutBean extends GenericPortalBean
       for (int i = 0; i < tabs.length; i++)
       {
         sTabName = tabs[i].getAttribute ("name");
-        sBgcolor = sTabName.equals (sActiveTab) ? sActiveTabColor : sTabColor;
-
-        if (sTabName.equals (sActiveTab))
-          activeTab = tabs[i];
+        sBgcolor = i == iTab ? sActiveTabColor : sTabColor;
 
         out.println ("<td bgcolor=" + sBgcolor + " align=center width=20%>");
         out.println ("  <table bgcolor=" + sBgcolor + " border=0 width=100% cellspacing=0 cellpadding=2>");
         out.println ("    <tr align=center>");
 
-        if (sTabName.equals (sActiveTab))
+        if (i == iTab)
           out.println ("      <td><span class=\"PortalTitleText\">&nbsp;" + sTabName + "</span>&nbsp;</td>");
-                  else
+        else
           out.println ("      <td><a href=\"layout.jsp?tab=" + i + "\"><SPAN CLASS=\"PortalTitleText\">" + sTabName + "</SPAN></a>&nbsp;</td>");
 
         out.println ("    </tr>");
         out.println ("  </table>");
         out.println ("</td>");
-        out.println ("<td width=1%>&nbsp;</td>");
+        out.println ("<td width=\"1%\">&nbsp;</td>");
       }
 
       // Area to the right of the tabs
