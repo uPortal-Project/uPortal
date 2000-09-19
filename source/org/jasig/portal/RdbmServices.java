173784fc51b2b91d5a5bf4010ecc4a179ef4a57d
stoth@committers.jasig.org <stoth@committers.jasig.org>
2000-09-19 22:26:06 +0000

    Added static init function. Will try to get a connection multiple times in order to overcome a bug in Oracle drivers that sometimes will reject a connection.
    git-svn-id: https://source.jasig.org/uPortal/trunk@3427 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/RdbmServices.java
+++ source/org/jasig/portal/RdbmServices.java
@@ -20,70 +20,85 @@ public class RdbmServices extends GenericPortalBean
   private static String sJdbcUrl = null;
   private static String sJdbcUser = null;
   private static String sJdbcPassword = null;
-      
+  public static int RETRY_COUNT = 5;
+
+  static
+  {
+    loadProps ();
+  }
   /**
    * Constructor which loades JDBC parameters from property file
    * upon first invocation
    */
   public RdbmServices ()
-  {    
-    try 
-    {    
+  {
+  }
+
+  protected static void loadProps ()
+  {
+    try
+    {
       if (!bPropsLoaded)
       {
         File jdbcPropsFile = new File (getPortalBaseDir () + "properties" + File.separator + "rdbm.properties");
         Properties jdbcProps = new Properties ();
         jdbcProps.load (new FileInputStream (jdbcPropsFile));
-        
+
         sJdbcDriver = jdbcProps.getProperty ("jdbcDriver");
         sJdbcUrl = jdbcProps.getProperty ("jdbcUrl");
         sJdbcUser = jdbcProps.getProperty ("jdbcUser");
         sJdbcPassword = jdbcProps.getProperty ("jdbcPassword");
-        
+
         bPropsLoaded = true;
       }
     }
     catch (Exception e)
     {
       Logger.log (Logger.ERROR, e);
-    }    
-  }    
-  
+    }
+  }
   /**
    * Gets a database connection
    * @return a database Connection object
    */
-	public static Connection getConnection()
-	{
-		Connection conn = null;
+  public static Connection getConnection ()
+  {
+    Connection conn = null;
+    for ( int i = 0 ; i < RETRY_COUNT && conn == null ; ++i )
+    {
+      try
+      {
+        Class.forName (sJdbcDriver);
+        conn = DriverManager.getConnection (sJdbcUrl, sJdbcUser, sJdbcPassword);
+      }
+      catch (ClassNotFoundException cnfe)
+      {
+        Logger.log (Logger.ERROR, "The driver " + sJdbcDriver + " was not found, please check the logs/rdbm.properties file and your classpath.");
+        return null;
+      }
+      catch ( SQLException SQLe )
+      {
+        Logger.log (Logger.WARN, "Driver "+ sJdbcDriver + "produced error " + SQLe.getMessage () + " tring to get connection again.");
+        Logger.log (Logger.INFO, SQLe);
+      }
+    }
+    return conn;
+  }
 
-		try 
-		{
-			Class.forName (sJdbcDriver);
-			conn = DriverManager.getConnection (sJdbcUrl, sJdbcUser, sJdbcPassword);
-		}
-		catch ( Exception e )
-		{
-      Logger.log (Logger.ERROR, e);
-		}
-
-		return conn;
-	}  
-	
   /**
    * Releases database connection
    * @param a database Connection object
    */
-	public static void releaseConnection (Connection con)
-	{
-		try
-		{
+  public static void releaseConnection (Connection con)
+  {
+    try
+    {
       if (con != null)
-		    con.close();
-		}
-		catch ( Exception e )
-		{
+      con.close ();
+    }
+    catch ( Exception e )
+    {
       Logger.log (Logger.ERROR, e);
-		}
-	}
+    }
+  }
 }
