b0aac39be3f2984338396a9d1584f043e5d9654a
Susan Bramhall <susan.bramhall@yale.edu>
2000-10-10 21:20:35 +0000

    Get the name of the channel from the rss title.
    Use relative link in ssl document.
    
    Change references to old name used in external documents.
    
    
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-1-0-patches@3435 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/xmlchannels/CRSSChannel.java
+++ source/org/jasig/portal/xmlchannels/CRSSChannel.java
@@ -43,9 +43,15 @@ import javax.servlet.jsp.*;
 import javax.servlet.http.*;
 import org.jasig.portal.*;
 import org.apache.xalan.xslt.*;
+import org.w3c.dom.*;
+import org.apache.xerces.dom.*;
+
 
 /**
  * A simple example of IXMLChannel that views an RSS Document.
+ * currently this channel will cache the xml document in a DOM
+ * which will live as long as the channel instance (ie user's session).
+ * There is currently no provision to refresh the contents after an interval.
  * @author Peter Kharchenko
  * @version $Revision$
  */
@@ -56,6 +62,13 @@ public class CRSSChannel extends GenericPortalBean implements IXMLChannel
     String uri;
     StylesheetSet set;
     ChannelRuntimeData runtimeData;
+    ChannelSubscriptionProperties csb;
+
+    // a DOM for rss information
+    // Since we get the channel name from the title element, we parse
+    // the whole rss document into a DOM to be used for
+    // both the name and the rendered contents.
+    protected Document RssDom;
 
 
     String fs=System.getProperty("file.separator");
@@ -63,20 +76,52 @@ public class CRSSChannel extends GenericPortalBean implements IXMLChannel
 
     public CRSSChannel() {
 	// initialize a stylesheet set from a file
-	set=new StylesheetSet(stylesheetDir+"RSSDocumentChannel"+fs+"RSSDocumentChannel.ssl");
+	set=new StylesheetSet(stylesheetDir+"CRSSChannel"+fs+"CRSSChannel.ssl");
 	set.setMediaProps(getPortalBaseDir()+"properties"+fs+"media.properties");
     }
 
+	/* Save the ChannelStaticData object and also
+    get the title of the rss document for the channel name
+    save it in the static data.    */
     public void setStaticData(ChannelStaticData sd) {
-	// all we need from the static data is the location of the document
 	this.uri= sd.getParameter("uri");
+	Logger.log(Logger.DEBUG,"CRSSChannel::setStaticData uri=\""+uri+"\"");
+  sd.setParameter("Name",uri);
+    try {
+    int i=0;
+    StringBuffer titleString = new StringBuffer("");
+	  Document root=getDoc();
+    if (root==null) return;
+    NodeList elements=root.getElementsByTagName("title");
+    if (elements==null) return;
+    while (i<elements.getLength()) {
+      Node title=elements.item(i);
+      if (title==null) {i++; continue;}
+      if (title.getParentNode().getNodeName().equals("channel")) {
+        int j=0;
+        NodeList titleNodes = title.getChildNodes();
+        while (j<titleNodes.getLength()) {
+          Node child = titleNodes.item(j);
+          if (child.getNodeType()== child.TEXT_NODE) {
+            titleString=titleString.append(child.getNodeValue());
+            }
+          j++;
+        }
+      }
+      i++;
+      };
+      csb.setName(titleString.toString());
+    }
+    catch (Exception e) {
+      Logger.log(Logger.ERROR,"RSSChannel::setStaticData : "+e);
+    }
     };
 
     public void setRuntimeData(ChannelRuntimeData rd) {
 	// need to save runtimedata so we can determine media from the
 	// request object contained in it
 	runtimeData=rd;
-    }
+  }
 
     public void receiveEvent(LayoutEvent ev) {
 	// we have no events to process here
@@ -84,9 +129,9 @@ public class CRSSChannel extends GenericPortalBean implements IXMLChannel
 
     // report static channel properties to the portal
     public ChannelSubscriptionProperties getSubscriptionProperties() {
-	ChannelSubscriptionProperties csb=new ChannelSubscriptionProperties();
-	// leave most properties at their default values, except a couple.
-	csb.setName("RSSDocumentChannel");
+	  csb=new ChannelSubscriptionProperties();
+	  // leave most properties at their default values, except a couple.
+  	csb.setName("CRSSChannel");
 	return csb;
     }
 
@@ -100,12 +145,28 @@ public class CRSSChannel extends GenericPortalBean implements IXMLChannel
     {
 	try {
 	    if (set!=null) {
-		XSLTInputSource stylesheet=set.getStylesheet(runtimeData.getHttpRequest());
+        XSLTInputSource stylesheet=set.getStylesheet(runtimeData.getHttpRequest());
+        // This allows relative paths in the stylesheet set
+        String sPortalBaseDir = GenericPortalBean.getPortalBaseDir ();
+        stylesheet.setSystemId (sPortalBaseDir + stylesheet.getSystemId ());
 		if(stylesheet!=null) {
 		    XSLTProcessor processor = XSLTProcessorFactory.getProcessor();
-		    processor.process(new XSLTInputSource(uri),stylesheet,new XSLTResultTarget(out));
+		    processor.process(new XSLTInputSource(RssDom),stylesheet,new XSLTResultTarget(out));
 		}
 	    }
 	} catch (Exception e) {};
     }
+    /* parse the rss document into a DOM
+    which will be around for getting the name and for eventually rendering. */
+    private Document getDoc() {
+  	try{
+	    if(RssDom==null) {
+  		org.apache.xerces.parsers.DOMParser parser=new org.apache.xerces.parsers.DOMParser();
+  		parser.parse(new org.xml.sax.InputSource(uri));
+  		RssDom=parser.getDocument();
+  	    }
+  	} catch (Exception e) { Logger.log(Logger.ERROR,"RSSChannel::getDoc() : unable to get input from \""+uri+"\"\n"+e); }
+	return RssDom;
+    }
+
 }
