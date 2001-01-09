b8b2982fd16f3df06940dc7fb2c4b05d4c213425
peterk@committers.jasig.org <peterk@committers.jasig.org>
2001-01-09 19:47:20 +0000

    Added two methods: copyDocument() for generating a clone of a DocumentImpl object that has functional lookup tables, and a small utility function that returns an element child of a particular node.
    git-svn-id: https://source.jasig.org/uPortal/trunk@3577 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/UtilitiesBean.java
+++ source/org/jasig/portal/UtilitiesBean.java
@@ -37,6 +37,9 @@ package org.jasig.portal;
 
 import javax.servlet.http.*;
 import java.text.*;
+import org.apache.xerces.dom.*;
+import org.w3c.dom.*;
+import java.util.*;
 
 /**
  * Provides methods useful for the portal.  Later on, it may be necessary
@@ -124,4 +127,173 @@ public class UtilitiesBean extends GenericPortalBean
 
     return str;
   }  
+
+
+    /*
+     * This allows to create a deep copy of a DocumentImpl that preserves
+     * ID tables, so that getElementById() would work on the returned clone
+     */
+    public static DocumentImpl cloneDocument(DocumentImpl olddoc) {
+	DocumentImpl newdoc = new DocumentImpl();
+	
+	// construct the idTable which is a reverse lookup table of 
+	// identifier table in NodeImpl
+	Hashtable idTable=new Hashtable();
+	for(Enumeration e=olddoc.getIdentifiers(); e.hasMoreElements();) {
+	    String id=(String) e.nextElement();
+	    idTable.put(olddoc.getIdentifier(id),id);
+	}
+
+	for(NodeImpl n = (NodeImpl) olddoc.getFirstChild(); n != null; n = (NodeImpl) n.getNextSibling()) {
+	    newdoc.appendChild(importNodeWithId(newdoc,idTable,n));
+	}
+
+	return newdoc;
+
+    }
+
+    /*
+     * This is similar to the improtNode() method of DocumentImpl, except that the current method
+     * preserves the ID table of the document.
+     */
+    protected static NodeImpl importNodeWithId(DocumentImpl doc,Hashtable idTable, Node source) {
+	NodeImpl newnode=null;
+	
+        int type = source.getNodeType();
+    	switch (type) {
+	    
+	case DocumentImpl.ELEMENT_NODE: {
+	    Element newelement = doc.createElement(source.getNodeName());
+	    // copy the identifier
+	    String id=(String)idTable.get((Element) source);
+	    if(id!=null) 
+		doc.putIdentifier(id,newelement);
+	    
+	    NamedNodeMap srcattr = source.getAttributes();
+	    if (srcattr != null) {
+		for(int i = 0; i < srcattr.getLength(); i++) {
+		    newelement.setAttributeNode((AttrImpl)importNodeWithId(doc,idTable,srcattr.item(i)));
+		}
+	    }
+	    newnode = (NodeImpl)newelement;
+	    break;
+	}
+
+	case DocumentImpl.ATTRIBUTE_NODE: {
+	    newnode = (NodeImpl)doc.createAttribute(source.getNodeName());
+	    // Kids carry value
+	    break;
+	}
+	
+	case DocumentImpl.TEXT_NODE: {
+	    newnode = (NodeImpl)doc.createTextNode(source.getNodeValue());
+	    break;
+	}
+	
+	case DocumentImpl.CDATA_SECTION_NODE: {
+	    newnode = (NodeImpl)doc.createCDATASection(source.getNodeValue());
+	    break;
+	}
+	
+	case DocumentImpl.ENTITY_REFERENCE_NODE: {
+	    newnode = (NodeImpl) doc.createEntityReference(source.getNodeName());
+	    break;
+	}
+
+	case DocumentImpl.ENTITY_NODE: {
+	    Entity srcentity = (Entity)source;
+	    EntityImpl newentity = (EntityImpl)doc.createEntity(source.getNodeName());
+	    newentity.setPublicId(srcentity.getPublicId());
+	    newentity.setSystemId(srcentity.getSystemId());
+	    newentity.setNotationName(srcentity.getNotationName());
+	    // Kids carry additional value
+	    newnode = newentity;
+	    break;
+	}
+	
+	case DocumentImpl.PROCESSING_INSTRUCTION_NODE: {
+	    newnode = (ProcessingInstructionImpl) doc.createProcessingInstruction(source.getNodeName(), source.getNodeValue());
+	    break;
+	}
+
+	case DocumentImpl.COMMENT_NODE: {
+	    newnode = (NodeImpl)doc.createComment(source.getNodeValue());
+	    break;
+	}
+	
+	case DocumentImpl.DOCUMENT_TYPE_NODE: {
+	    DocumentTypeImpl doctype = (DocumentTypeImpl)source;
+	    DocumentTypeImpl newdoctype =(DocumentTypeImpl)doc.createDocumentType(doctype.getNodeName(),doctype.getPublicId(),doctype.getSystemId());
+	    // Values are on NamedNodeMaps
+	    NamedNodeMap smap = ((DocumentType)source).getEntities();
+	    NamedNodeMap tmap = newdoctype.getEntities();
+	    if(smap != null) {
+		for(int i = 0; i < smap.getLength(); i++) {
+		    tmap.setNamedItem((EntityImpl)importNodeWithId(doc,idTable,smap.item(i)));
+		}
+	    }
+	    smap = ((DocumentType)source).getNotations();
+	    tmap = newdoctype.getNotations();
+	    if (smap != null) {
+		for(int i = 0; i < smap.getLength(); i++) {
+		    tmap.setNamedItem((NotationImpl)importNodeWithId(doc,idTable,smap.item(i)));
+		}
+	    }
+	    // NOTE: At this time, the DOM definition of DocumentType
+	    // doesn't cover Elements and their Attributes. domimpl's
+	    // extentions in that area will not be preserved, even if
+	    // copying from domimpl to domimpl. We could special-case
+	    // that here. Arguably we should. Consider. ?????
+	    newnode = newdoctype;
+	    break;
+	}
+
+	case DocumentImpl.DOCUMENT_FRAGMENT_NODE: {
+	    newnode = (NodeImpl)doc.createDocumentFragment();
+	    // No name, kids carry value
+	    break;
+	}
+
+	case DocumentImpl.NOTATION_NODE: {
+	    Notation srcnotation = (Notation)source;
+	    NotationImpl newnotation = (NotationImpl)doc.createNotation(source.getNodeName());
+	    newnotation.setPublicId(srcnotation.getPublicId());
+	    newnotation.setSystemId(srcnotation.getSystemId());
+	    // Kids carry additional value
+	    newnode = newnotation;
+	    // No name, no value
+	    break;
+	}
+
+	case DocumentImpl.DOCUMENT_NODE : // Document can't be child of Document
+	default: {			 // Unknown node type
+	    throw new DOMExceptionImpl(DOMException.HIERARCHY_REQUEST_ERR,
+				       "DOM006 Hierarchy request error");
+	}
+        }
+	
+    	// If deep, replicate and attach the kids.
+	for (Node srckid = source.getFirstChild();
+	     srckid != null;
+	     srckid = srckid.getNextSibling()) {
+	    newnode.appendChild(importNodeWithId(doc,idTable,srckid));
+	}
+
+    	return newnode;
+    }
+
+
+    public static Element getChildElement(Element el,String elementName) {
+        Element theElement=null;
+        NodeList nl=el.getChildNodes();
+        int nnodes=nl.getLength();
+        for(int i=0;i<nnodes;i++) {
+            Node currentNode=nl.item(i);
+            if((currentNode.getNodeType() == Node.ELEMENT_NODE) && (currentNode.getNodeName().equals(elementName))) {
+                theElement=(Element) currentNode;
+                break;
+            }
+        }
+        return theElement;
+    }
 }
