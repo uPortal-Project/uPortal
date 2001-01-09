115f3a6e7b01dcb9c85d715227f446cc814e671c
infrastructure@lists.ja-sig.org <infrastructure@lists.ja-sig.org>
2001-01-09 16:33:56 +0000

    This commit was manufactured by cvs2svn to create branch 'rel-1-0-patches'.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-1-0-patches@3568 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/security/IPerson.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.jasig.portal.security;
+
+import java.util.Enumeration;
+
+/**
+ * @author Bernie Durfee
+ */
+public interface IPerson extends AdditionalDescriptor
+{
+  // A must have an ID that makes them a unique entity in within the portal
+  public void setPortalUserID(int sPortalUserID);
+  public int  getPortalUserID();
+
+  // This is the string that the user has logged in with
+  public void   setPortalLoginName(String sPortalLoginName);
+  public String getPortalLoginName();
+
+  // This is the string that the portal will use as the 'user friendly' name of the user
+  public void   setPreferredName(String sfirstName);
+  public String getPreferredName();
+
+  // This will retrieve an attribute associated with the user
+  public Object getAttribute(String key);
+  public void   setAttribute(String key, Object value);
+
+  // This will return an enumeration of all of the attribute keys stored for the user
+  public Enumeration getAttributeKeys();
+}
