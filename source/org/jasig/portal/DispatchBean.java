8c63a92e70323b1b3936226d4d34aa6854d8d90c
Bernie Durfee <bernard.durfee@suny.edu>
2001-01-25 03:34:56 +0000

    Dispatch bean will now render a channel via its globalChannelID, so that channels can be rendered without them being required to be in a user's layout.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-1-0-patches@3600 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/DispatchBean.java
+++ source/org/jasig/portal/DispatchBean.java
@@ -40,9 +40,19 @@ import javax.servlet.jsp.*;
 import javax.servlet.http.*;
 
 import java.io.*;
+
 import java.util.*;
+
 import java.net.URLEncoder;
-import org.jasig.portal.layout.*;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import org.jasig.portal.layout.IChannel;
+
+import com.objectspace.xml.IXml;
+import com.objectspace.xml.Xml;
 
 /**
  * Methods to accompany dispatch.jsp
@@ -56,25 +66,45 @@ public class DispatchBean extends GenericPortalBean
    * @param the servlet request object
    * @return the channel to call
    */
-  public IChannel getChannel (HttpServletRequest req)
+  public org.jasig.portal.IChannel getChannel(HttpServletRequest req)
   {
     org.jasig.portal.IChannel ch = null;
 
     try
     {
-      HttpSession session = req.getSession (false);
-      ILayoutBean layoutBean = (ILayoutBean) session.getAttribute ("layoutBean");
-      String sChannelID = req.getParameter ("channelID");
+      HttpSession session = req.getSession(false);
+      ILayoutBean layoutBean = (ILayoutBean)session.getAttribute("layoutBean");
 
-      if (sChannelID != null)
-        ch = layoutBean.getChannelInstance (sChannelID);
+      String sChannelID       = req.getParameter("channelID");
+      String sGlobalChannelID = req.getParameter("globalChannelID");
+
+      if(sChannelID == null && sGlobalChannelID == null)
+      {
+        Logger.log(Logger.ERROR, "To dispatch properly, a channel ID must be included in the query string in the form: \"...&channelID=...\"");
+      }
+
+      if(sChannelID != null && sGlobalChannelID != null)
+      {
+        Logger.log(Logger.ERROR, "To dispatch properly, you cannot specify both channelID and globalChannelID in the request");
+      }
+
+      if(sChannelID != null)
+      {
+        // A channel from the user's layout is being requested
+        ch = layoutBean.getChannelInstance(sChannelID);
+      }
       else
-        Logger.log (Logger.ERROR, "To dispatch properly, a channel ID must be included in the query string in the form: \"...&channelID=...\"");
+      if(sGlobalChannelID != null)
+      {
+        // A channel not in user's layout is being requested
+        ch = getChannelInstance(sGlobalChannelID, req);
+      }
     }
     catch (Exception e)
     {
       Logger.log (Logger.ERROR, e);
     }
+
     return ch;
   }
 
@@ -83,15 +113,15 @@ public class DispatchBean extends GenericPortalBean
    * to return to layout.jsp
    * @param the servlet request object
    */
-  public static void finish (HttpServletRequest req, HttpServletResponse res)
+  public static void finish(HttpServletRequest req, HttpServletResponse res)
   {
     try
     {
-      res.sendRedirect ("layout.jsp");
+      res.sendRedirect("layout.jsp");
     }
     catch (Exception e)
     {
-      Logger.log (Logger.ERROR, e);
+      Logger.log(Logger.ERROR, e);
     }
   }
 
@@ -106,11 +136,11 @@ public class DispatchBean extends GenericPortalBean
   {
     try
     {
-      return buildURL (sMethodName, chConfig.getChannelID ());
+      return buildURL(sMethodName, chConfig.getChannelID ());
     }
     catch (Exception e)
     {
-      Logger.log (Logger.ERROR, e);
+      Logger.log(Logger.ERROR, e);
     }
     return null;
   }
@@ -140,4 +170,55 @@ public class DispatchBean extends GenericPortalBean
     return null;
   }
 
+  private org.jasig.portal.IChannel getChannelInstance(String sGlobalChannelID, HttpServletRequest request)
+  {
+    IXml channelXml = null;
+    Connection con = null;
+    RdbmServices rdbmService = new RdbmServices ();
+    HttpSession session = request.getSession(false);
+
+    try
+    {
+      con = rdbmService.getConnection();
+      Statement stmt = con.createStatement();
+
+      String sQuery = "SELECT CHANNEL_XML FROM PORTAL_CHANNELS WHERE CHAN_ID=" + sGlobalChannelID;
+
+      Logger.log(Logger.DEBUG, sQuery);
+      debug(sQuery);
+
+      ResultSet rs = stmt.executeQuery(sQuery);
+
+      if (rs.next ())
+      {
+        String sChannelXml = rs.getString("CHANNEL_XML");
+
+        String xmlFilePackage = "org.jasig.portal.layout";
+        channelXml = Xml.openDocument(xmlFilePackage, new StringReader(sChannelXml));
+      }
+
+      stmt.close();
+
+      // Get the DXML version of the channel
+      org.jasig.portal.layout.IChannel channelDxml = (org.jasig.portal.layout.IChannel)channelXml.getRoot();
+
+      // The channel instance is temporary
+      channelDxml.setInstanceIDAttribute(sGlobalChannelID);
+
+      // Get the layout bean
+      ILayoutBean layoutBean = (ILayoutBean)session.getAttribute("layoutBean");
+
+      return(layoutBean.getChannelInstance(channelDxml));
+    }
+    catch(Exception e)
+    {
+      Logger.log(Logger.ERROR, e);
+    }
+    finally
+    {
+      rdbmService.releaseConnection(con);
+    }
+
+    return(null);
+  }
 }
