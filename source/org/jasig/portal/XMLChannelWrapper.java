363a7f847d640e203c44e5bd01718805d3d3483f
Bernie Durfee <bernard.durfee@suny.edu>
2001-01-25 03:29:57 +0000

    baseActionURL always uses method=render when in dispatch mode. This will force channel authors to write channels that will be compatable with uPortal 2.0.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-1-0-patches@3595 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/XMLChannelWrapper.java
+++ source/org/jasig/portal/XMLChannelWrapper.java
@@ -36,12 +36,15 @@
 package org.jasig.portal;
 
 import java.util.*;
+
 import javax.servlet.jsp.*;
 import javax.servlet.http.*;
 
 import org.apache.xalan.xpath.*;
 import org.apache.xalan.xslt.*;
 import org.apache.xml.serialize.*;
+
+import org.jasig.portal.security.IPerson;
 
 /**
  * Wraps an IXMLChannel into an IChannel interface,
@@ -51,127 +54,220 @@ import org.apache.xml.serialize.*;
  */
 public class XMLChannelWrapper implements IChannel
 {
-    private IXMLChannel ch;
-    private String chanID;
+  private IXMLChannel ch;
+  private String chanID;
 
-    public XMLChannelWrapper(IXMLChannel xmlChannel)
+  public XMLChannelWrapper(IXMLChannel xmlChannel)
+  {
+    ch = xmlChannel;
+  }
+
+  public IXMLChannel getXMLChannel()
+  {
+    return ch;
+  }
+
+  public void init (ChannelConfig chConfig)
+  {
+    ChannelStaticData sd = new ChannelStaticData();
+    sd.setChannelID(chConfig.getChannelID());
+    sd.setParameters(chConfig);
+    ch.setStaticData(sd);
+
+    chanID = chConfig.getChannelID();
+  }
+
+  public Vector getParameters()
+  {
+    ChannelSubscriptionProperties csp;
+
+    if((csp = ch.getSubscriptionProperties())!=null)
     {
-      ch=xmlChannel;
+      return csp.getParameterFields();
     }
-
-    public IXMLChannel getXMLChannel()
+    else
     {
-      return ch;
+      return null;
     }
+  }
 
-    public void init (ChannelConfig chConfig) {
-	ChannelStaticData sd=new ChannelStaticData();
-	sd.setChannelID(chConfig.getChannelID());
-	sd.setParameters(chConfig);
-	ch.setStaticData(sd);
+  public String getName ()
+  {
+    return ch.getSubscriptionProperties().getName();
+  }
 
-	chanID=chConfig.getChannelID();
-    }
+  public boolean isMinimizable ()
+  {
+    return ch.getSubscriptionProperties().isMinimizable();
+  }
 
-    public Vector getParameters()
+  public boolean isDetachable ()
+  {
+    return ch.getSubscriptionProperties().isDetachable();
+  }
+
+  public boolean isRemovable ()
+  {
+    return ch.getSubscriptionProperties().isRemovable();
+  }
+
+  public boolean isEditable ()
+  {
+    return ch.getSubscriptionProperties().isEditable();
+  }
+
+  public boolean hasHelp ()
+  {
+    return ch.getSubscriptionProperties().hasHelp();
+  }
+
+  public int getDefaultDetachWidth ()
+  {
+    return Integer.parseInt(ch.getSubscriptionProperties().getDefaultDetachWidth());
+  }
+
+  public int getDefaultDetachHeight ()
+  {
+    return Integer.parseInt(ch.getSubscriptionProperties().getDefaultDetachHeight());
+  }
+
+  public void render(HttpServletRequest req, HttpServletResponse res, JspWriter out)
+  {
+    ChannelRuntimeData rd = new ChannelRuntimeData();
+    rd.setHttpRequest(req);
+
+    // Since only the render method will be called we need to check for
+    //  method=(edit, help, ...) in the parameter string
+    //processMethod(req, res, out);
+
+    // Determine BaseActionURL
+    // Here it is of the form "file.jsp?channelTarget=channelID&..."
+    // so we need to determine what jsp file is being used
+    // the same jsp is used for forwarding when LayoutEvents occurs
+    rd.setBaseActionURL(getJSP(req) + "&" + "channelTarget=" + chanID + "&");
+
+    // Put the person object into the runtime data for the channel to use
+    HttpSession session = req.getSession();
+    rd.setPerson((IPerson)session.getAttribute("Person"));
+
+    // get the action parameters passed to the channel
+    String channelTarget = null;
+
+    String parameterName = null;
+    String parameterValue = null;
+
+    // Only send request parameters to the proper channel
+    if((channelTarget = req.getParameter("channelTarget")) != null && (channelTarget.equals(chanID)))
     {
-      ChannelSubscriptionProperties csp;
-      if((csp=ch.getSubscriptionProperties())!=null)
-	  return csp.getParameterFields();
-      else return null;
+      Enumeration e = req.getParameterNames();
+
+      while(e.hasMoreElements() && e != null)
+      {
+        parameterName = (String)e.nextElement();
+
+        if(!parameterName.equals("channelTarget"))
+        {
+          parameterValue = req.getParameter(parameterName);
+
+          rd.setParameter(parameterName, parameterValue);
+        }
+      }
     }
 
-    public String getName () {
-	return ch.getSubscriptionProperties().getName();
-    }
-    public boolean isMinimizable () {
-	return ch.getSubscriptionProperties().isMinimizable();
-    }
-    public boolean isDetachable () {
-	return ch.getSubscriptionProperties().isDetachable();
-    }
-    public boolean isRemovable () {
-	return ch.getSubscriptionProperties().isRemovable();
-    }
-    public boolean isEditable () {
-	return ch.getSubscriptionProperties().isEditable();
-    }
-    public boolean hasHelp () {
-	return ch.getSubscriptionProperties().hasHelp();
-    }
+    ch.setRuntimeData(rd);
 
-    public int getDefaultDetachWidth () {
-	return Integer.parseInt(ch.getSubscriptionProperties().getDefaultDetachWidth());
-    }
-    public int getDefaultDetachHeight () {
-	return Integer.parseInt(ch.getSubscriptionProperties().getDefaultDetachHeight());
-    }
+    HTMLSerializer htmlSerializer= new HTMLSerializer(out,new OutputFormat("HTML","UTF-8",true));
+    ch.renderXML(htmlSerializer);
+  }
 
-    public void render (HttpServletRequest req, HttpServletResponse res, JspWriter out) {
-	ChannelRuntimeData rd=new ChannelRuntimeData();
-	rd.setHttpRequest(req);
+  /*
+  private void processMethod(HttpServletRequest req, HttpServletResponse res, JspWriter out)
+  {
+    String method = null;
 
-	// determine BaseActionURL
-	// here it is of the form "file.jsp?channelTarget=channelID&..."
-	// so we need to determine what jsp file is being used
-	// the same jsp is used for forwarding when LayoutEvents occurs
-
-	rd.setBaseActionURL(getJSP(req)+"channelTarget="+chanID+"&");
-	// get the action parameters passed to the channel
-	String channelTarget=null;
-	if((channelTarget=req.getParameter("channelTarget"))!=null && (channelTarget.equals(chanID))) {
-	    Enumeration e=req.getParameterNames();
-	    if(e!=null) {
-		while(e.hasMoreElements()) {
-		    String pName=(String) e.nextElement();
-		    if(!pName.equals("channelTarget")) {
-			rd.setParameter(pName,req.getParameter(pName));
-		    }
-		}
-	    }
-	}
-	ch.setRuntimeData(rd);
-
-	HTMLSerializer htmlSerializer= new HTMLSerializer(out,new OutputFormat("HTML","UTF-8",true));
-	ch.renderXML(htmlSerializer);
-    }
-
-
-    public void edit (HttpServletRequest req, HttpServletResponse res, JspWriter out) {
-	ch.receiveEvent(new LayoutEvent(LayoutEvent.EDIT_BUTTON_EVENT));
-	render(req,res,out);
-    }
-    public void help (HttpServletRequest req, HttpServletResponse res, JspWriter out) {
-	ch.receiveEvent(new LayoutEvent(LayoutEvent.HELP_BUTTON_EVENT));
-	render(req,res,out);
-    }
-
-
-    private String getJSP(HttpServletRequest req)
+    if((method = req.getParameter("method")) != null)
     {
-	String reqURL=req.getRequestURI();
-	String jspfile=reqURL.substring(reqURL.lastIndexOf('/')+1,reqURL.length());
-	// it used to be that XML channels were always rendered through main index.jsp,
-	// now they are allowed to render under other .jsp, such as dispatch.jsp.
-	//	if(jspfile.equals("") || jspfile.equals("dispatch.jsp")) jspfile="index.jsp";
-	if(jspfile.equals("")) jspfile="index.jsp";
-	else if(jspfile.equals("detach.jsp")) {
-	    //reconstruct URL parameters
-	    jspfile=req.getRequestURI()+"?";
-	    for (Enumeration e = req.getParameterNames() ; e.hasMoreElements() ;) {
-		String pName=(String) e.nextElement();
-		String pValue= req.getParameter(pName);
-		jspfile+=pName+"="+pValue+"&";
-	    }
-	}
-	else if(jspfile.equals("dispatch.jsp")) {
-	    //dispatch jsp needs to carry two parameters : method and channelID
-	    jspfile+="?method="+req.getParameter("method")+"&channelID="+req.getParameter("channelID")+"&";
-	}
-	else jspfile+='?';
-	Logger.log(Logger.DEBUG,"XMLChannelWrapper::getJSP() : jspfile=\""+jspfile+"\"");
-	return jspfile;
+      if(method.equals("edit"))
+      {
+        edit(req, res, out);
+        return;
+      }
+      else
+      if(method.equals("help"))
+      {
+        help(req, res, out);
+        return;
+      }
     }
+
+    return;
+  }
+  */
+
+  public void edit(HttpServletRequest req, HttpServletResponse res, JspWriter out)
+  {
+    ch.receiveEvent(new LayoutEvent(LayoutEvent.EDIT_BUTTON_EVENT));
+    render(req, res, out);
+  }
+
+  public void help(HttpServletRequest req, HttpServletResponse res, JspWriter out)
+  {
+    ch.receiveEvent(new LayoutEvent(LayoutEvent.HELP_BUTTON_EVENT));
+    render(req, res, out);
+  }
+
+  private String getJSP(HttpServletRequest req)
+  {
+    String reqURL  = req.getRequestURI();
+    String jspfile = reqURL.substring(reqURL.lastIndexOf('/') + 1, reqURL.length());
+
+    // It used to be that XML channels were always rendered through main index.jsp,
+    // now they are allowed to render under other .jsp, such as dispatch.jsp.
+    if(jspfile.equals(""))
+    {
+      jspfile="index.jsp";
+    }
+    else
+    if(jspfile.equals("detach.jsp"))
+    {
+      // Reconstruct URL parameters
+      jspfile = req.getRequestURI() + "?";
+
+      for(Enumeration e = req.getParameterNames(); e.hasMoreElements();)
+      {
+        String pName  =(String)e.nextElement();
+        String pValue = req.getParameter(pName);
+        jspfile += pName + "=" + pValue + "&";
+      }
+    }
+    else
+    if(jspfile.equals("dispatch.jsp"))
+    {
+      // dispatch.jsp needs to carry two parameters : method and channelID
+      //jspfile += "?method=" + req.getParameter("method") + "&channelID=" + req.getParameter("channelID") + "&";
+      String sMethod = req.getParameter("method");
+      if(sMethod == null)
+      {
+        sMethod = "render";
+      }
+
+      if(req.getParameter("channelID") != null)
+      {
+        jspfile += "?channelID=" + req.getParameter("channelID") + "&method=render"; // + sMethod;
+      }
+      else
+      if(req.getParameter("globalChannelID") != null)
+      {
+        jspfile += "?globalChannelID=" + req.getParameter("globalChannelID") + "&method=render"; // + sMethod;
+      }
+    }
+    else
+    {
+      jspfile += '?';
+    }
+
+    Logger.log(Logger.DEBUG,"XMLChannelWrapper::getJSP() : jspfile=\"" + jspfile + "\"");
+
+    return(jspfile);
+  }
 }
-
-
