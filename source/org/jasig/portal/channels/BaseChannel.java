745cee37e475e404f8b36c8427264b335ba03d92
peterk@committers.jasig.org <peterk@committers.jasig.org>
2001-02-16 23:42:03 +0000

    By popular demand, here is a base class that one can easily extend to obtain default behavior typical to IChannels. -peter.
    git-svn-id: https://source.jasig.org/uPortal/trunk@3630 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/channels/BaseChannel.java
@@ -0,0 +1,61 @@
+package org.jasig.portal.channels;
+
+import java.util.*;
+import javax.servlet.jsp.*;
+import javax.servlet.http.*; 
+import org.xml.sax.DocumentHandler;
+import org.jasig.portal.*;
+
+/**
+ * A base class from which channels implementing IChannel interface can be derived.
+ * Use this only if you are familiar with IChannel interface.
+ * @author Peter Kharchenko
+ * @version $Revision$
+ */
+
+
+
+public class BaseChannel implements IChannel 
+{
+    protected ChannelStaticData staticData;
+    protected ChannelRuntimeData runtimeData;
+    protected String channelName;
+
+
+    public BaseChannel() {
+	channelName="BaseChannel";
+    }
+
+    public ChannelSubscriptionProperties getSubscriptionProperties () {
+	    ChannelSubscriptionProperties csb = new ChannelSubscriptionProperties ();
+	    csb.setName (this.channelName);
+	    return csb;
+    }
+
+    public ChannelRuntimeProperties getRuntimeProperties ()
+    {
+	return new ChannelRuntimeProperties ();
+    }
+
+    public void receiveEvent (LayoutEvent ev)
+    {
+	
+    }
+    
+    public void setStaticData (ChannelStaticData sd)
+    {
+	this.staticData=sd;
+    }
+
+
+    public void setRuntimeData (ChannelRuntimeData rd)
+    {
+	this.runtimeData=rd;
+    }
+
+    public void renderXML (DocumentHandler out)
+    {
+	
+    }
+
+}
