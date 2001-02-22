de33910078e043dcfe6d7f972ef1d581721efe4f
Ken Weiner <kweiner@gmail.com>
2001-02-22 16:56:36 +0000

    New login channel
    git-svn-id: https://source.jasig.org/uPortal/trunk@3676 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/channels/CLogin.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2000 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.jasig.portal.channels;
+
+import org.jasig.portal.*;
+import org.xml.sax.DocumentHandler;
+import java.io.File;
+
+///Transform specific...
+// Won't be needed if we move the Xalan code outside the channel
+import org.apache.xalan.xslt.*;
+import java.io.StringReader;
+
+/** <p>Allows a user to logon to the portal.  Logon info is posted to
+ *  <code>authenticate.jsp</code></p>
+ * @author Ken Weiner, kweiner@interactivebusiness.com
+ * @version $Revision$
+ */
+public class CLogin implements IChannel
+{
+  private ChannelStaticData staticData;
+  private ChannelRuntimeData runtimeData;
+  private String channelName = "Log in...";
+
+  public CLogin()
+  {
+  }
+
+  public ChannelSubscriptionProperties getSubscriptionProperties()
+  {
+    ChannelSubscriptionProperties csb = new ChannelSubscriptionProperties();
+    csb.setName(this.channelName);
+    return csb;
+  }
+
+  public ChannelRuntimeProperties getRuntimeProperties()
+  {
+    return new ChannelRuntimeProperties();
+  }
+
+  public void receiveEvent(LayoutEvent ev)
+  {
+  }
+
+  public void setStaticData (ChannelStaticData sd)
+  {
+    this.staticData = sd;
+  }
+
+  public void setRuntimeData (ChannelRuntimeData rd)
+  {
+    this.runtimeData = rd;
+  }
+
+  public void renderXML (DocumentHandler out) throws Exception
+  {
+    String fs = File.separator;
+    String sslLocation = GenericPortalBean.getPortalBaseDir() + "webpages" + fs + "stylesheets" + fs + "org" + fs + "jasig" + fs + "portal" + fs + "channels" + fs + "CLogin" + fs + "CLogin.ssl";
+
+    StringBuffer sb = new StringBuffer ();
+    sb.append ("<?xml version='1.0'?>\n");
+    sb.append ("<xml/>\n");
+
+    transform(out, sb.toString(), sslLocation, "login");
+  }
+
+  // This will get moved out to a util or service class
+  private void transform (DocumentHandler out, String xml, String sslUrl, String stylesheetKey) throws org.xml.sax.SAXException
+  {
+    StylesheetSet set = new StylesheetSet(sslUrl);
+    set.setMediaProps (GenericPortalBean.getPortalBaseDir() + "properties" + File.separator + "media.properties");
+
+    XSLTInputSource xmlSource = new XSLTInputSource(new StringReader(xml));
+    XSLTInputSource xslSource = set.getStylesheet(stylesheetKey, runtimeData.getHttpRequest());
+    XSLTResultTarget xmlResult = new XSLTResultTarget(out);
+
+    XSLTProcessor processor = XSLTProcessorFactory.getProcessor();
+    processor.setStylesheetParam("baseActionURL", processor.createXString (runtimeData.getBaseActionURL()));
+    processor.process (xmlSource, xslSource, xmlResult);
+  }
+}
