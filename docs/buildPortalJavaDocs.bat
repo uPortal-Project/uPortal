30b34176ae9a674d0833c652e8f5228968492484
Ken Weiner <kweiner@gmail.com>
2001-04-10 19:13:27 +0000

    Updated this batch file for uPortal 2.0
    git-svn-id: https://source.jasig.org/uPortal/trunk@3903 f5dbab47-78f9-eb45-b975-e544023573eb

--- docs/buildPortalJavaDocs.bat
+++ docs/buildPortalJavaDocs.bat
@@ -1,15 +1,16 @@
-set JAVADOC_PATH=D:\jdk1.2.2\bin
-set JAVADOC_DOCSPATH=D:\Projects\JA-SIG\Portal\docs
-set JAVADOC_SOURCEPATH=D:\Projects\JA-SIG\Portal\source
+set JAVADOC_PATH=D:\jdk1.3.0\bin
+set JAVADOC_DOCSPATH=D:\Projects\JA-SIG\uPortal2\docs\JavaDocs
+set JAVADOC_SOURCEPATH=D:\Projects\JA-SIG\uPortal2\source
 
-set JAVADOC_CLASSPATH=%JAVADOC_CLASSPATH%;D:\JavaClasses\DXML1_2\lib\dxml.jar
-set JAVADOC_CLASSPATH=%JAVADOC_CLASSPATH%;D:\JavaClasses\DXML1_2\lib\xml4j.jar
-set JAVADOC_CLASSPATH=%JAVADOC_CLASSPATH%;D:\Projects\JA-SIG\Portal\classes
-set JAVADOC_CLASSPATH=%JAVADOC_CLASSPATH%;"D:\Program Files\Tomcat\lib\servlet.jar"
+set JAVADOC_CLASSPATH=%JAVADOC_CLASSPATH%;D:\Projects\JA-SIG\uPortal2\classes
+set JAVADOC_CLASSPATH=%JAVADOC_CLASSPATH%;D:\JavaClasses\xalan_1_2\xalan.jar
+set JAVADOC_CLASSPATH=%JAVADOC_CLASSPATH%;D:\JavaClasses\xalan_1_2\xerces.jar
+set JAVADOC_CLASSPATH=%JAVADOC_CLASSPATH%;D:\JavaClasses\log4j-1.0.4\log4j.jar
+set JAVADOC_CLASSPATH=%JAVADOC_CLASSPATH%;D:\JavaClasses\Tomcat3_2_1\lib\servlet.jar
 
-set JAVADOC_WINDOWTITLE="JA-SIG Portal"
-set JAVADOC_DOCTITLE="JA-SIG Portal"
-set JAVADOC_PACKAGES=org.jasig.portal org.jasig.portal.layout org.jasig.portal.channels org.jasig.portal.channels.rss org.jasig.portal.channels.bookmarks
+set JAVADOC_WINDOWTITLE="uPortal 2.0"
+set JAVADOC_DOCTITLE="uPortal 2.0"
+set JAVADOC_PACKAGES=org.jasig.portal org.jasig.portal.channels org.jasig.portal.channels.UserPreferences org.jasig.portal.security org.jasig.portal.security.provider org.jasig.portal.utils org.jasig.portal.services org.jasig.portal.tools
 
 %JAVADOC_PATH%\javadoc -d %JAVADOC_DOCSPATH% -sourcepath %JAVADOC_SOURCEPATH% -classpath %JAVADOC_CLASSPATH% -splitindex -version -author -windowtitle %JAVADOC_WINDOWTITLE% -doctitle %JAVADOC_DOCTITLE% %JAVADOC_PACKAGES%
 
