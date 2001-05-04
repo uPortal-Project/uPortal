27f579bb238b2890b9bfe85ef3ab7d090f2e1c76
peterk@committers.jasig.org <peterk@committers.jasig.org>
2001-05-04 05:56:38 +0000

    added javadoc comments
    git-svn-id: https://source.jasig.org/uPortal/trunk@4102 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/ICoreStylesheetDescriptionDB.java
+++ source/org/jasig/portal/ICoreStylesheetDescriptionDB.java
@@ -1,30 +1,111 @@
+/**
+ * Copyright (c) 2000 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 package org.jasig.portal;
 
 import java.util.Hashtable;
 
-/**
+/** 
  * Interface by which portal talks to the stylesheet description database
+ * 
  * @author Peter Kharchenko
  * @version $Revision$
  */
-
 public interface ICoreStylesheetDescriptionDB {
 
     // functions that allow one to browse available core stylesheets in various ways
-    // Hashtables map stylesheet/mime type names to their descriptions
+    /** Obtain a listing of structure stylesheets from the database
+     * 
+     * @param mimeType 
+     * @return Returns a hashtable mapping structure stylesheet names to a 
+     *     word-description (a simple String) of that stylesheet
+     */
     public Hashtable getStructureStylesheetList(String mimeType);
+    /** Obtains a list of theme stylesheets available for a particular structure stylesheet
+     * 
+     * @param structureStylesheetName name of the structure stylehsset
+     * @return Returns a hashtable mapping theme stylesheet names to a word 
+     *     description of the stylesheet ( a simple String)
+     */
     public Hashtable getThemeStylesheetList(String structureStylesheetName);
+    /** Obtains a list of mime types available on the installation
+     * 
+     * @return Returns a hasbtale mapping mime type strings to their word 
+     *     descriptions (simple String)
+     */
     public Hashtable getMimeTypeList();
 
     // functions that allow access to the entire CoreStylesheetDescription object.
     // These functions are used when working with the stylesheet, and not for browsing purposes.
+    /** Obtains a complete description of the structure stylesheet
+     * 
+     * @param stylesheetName name of the structure stylesheet
+     * @return a description of the structure stylesheet
+     */
     public StructureStylesheetDescription getStructureStylesheetDescription(String stylesheetName);
+    /** Obtains a complete description of a theme stylesheet
+     * 
+     * @param stylesheetName name of a theme stylesheet
+     * @return a description of a theme stylesheet
+     */
     public ThemeStylesheetDescription getThemeStylesheetDescription(String stylesheetName);
 
     // functions that allow to manage core stylesheet description collection
+    /** removes stylesheet description
+     * 
+     * @param stylesheetName name of the stylesheet
+     */
     public void removeStructureStylesheetDescription(String stylesheetName);
+    /** Removes theme stylesheet
+     * 
+     * @param stylesheetName name of the stylesheet
+     */
     public void removeThemeStylesheetDescription(String stylesheetName);
+    /** Registers new structure stylesheet with the portal database
+     * 
+     * @param stylesheetDescriptionURI Location of the stylesheet description 
+     *     XML file
+     * @param stylesheetURI Location of the actual stylesshet XML file
+     */
     public void addStructureStylesheetDescription(String stylesheetDescriptionURI,String stylesheetURI);
+    /** Registers a new theme stylesheet with the portal databases
+     * 
+     * @param stylesheetDescriptionURI Location of the stylesheet description 
+     *     XML file
+     * @param stylesheetURI Location of the actual stylesheet XML file
+     */
     public void addThemeStylesheetDescription(String stylesheetDescriptionURI,String stylesheetURI);
 
 }
--- source/org/jasig/portal/IUserPreferencesDB.java
+++ source/org/jasig/portal/IUserPreferencesDB.java
@@ -1,54 +1,169 @@
 package org.jasig.portal;
+import java.util.Hashtable;
 
-/**
+
+/** 
  * Interface through which portal talks to the user preferences database
+ * 
  * @author Peter Kharchenko
  * @version $Revision$
  */
-
-import java.util.Hashtable;
-
 public interface IUserPreferencesDB {
 
 
     // user profiles
+    /** Obtain user profile
+     * 
+     * @param userId user id
+     * @param userAgent User-Agent header string
+     * @return user profile
+     */
     public UserProfile getUserProfile(int userId, String userAgent);
+    /** update user profile
+     * 
+     * @param userId user id
+     * @param profile profile update
+     */
     public void updateUserProfile(int userId,UserProfile profile);
+    /** remove user profile from the database
+     * 
+     * @param userId user id
+     * @param profileId profile id
+     */
     public void deleteUserProfile(int userId,int profileId);
-    /*
+    /** 
      * Creates a new user profile in the database.
      * In the process, new profileId is assigned to the profile
-     * The UserProfile object returned has the profileId set.
+     * 
+     * @param userId user id
+     * @param profile profile object (profile id in this object will be 
+     *     overwritten)
+     * @return profile object with the profile id set to the newly generated 
+     *     id
      */
     public UserProfile addUserProfile(int userId,UserProfile profile);
+    /**      * 
+     * @param userId 
+     * @param profileId 
+     */
     public UserProfile getUserProfileById(int userId,int profileId);
+    /** retreive a list of user profiles
+     * 
+     * @param userId user id
+     * @return hashtable mapping user profile names to the profile objects
+     */
     public Hashtable getUserProfileList(int userId);
 
     // syste profiles
+    /** retreive a system profile
+     * 
+     * @param userAgent User-Agent header string
+     * @return profile object
+     */
     public UserProfile getSystemProfile(String userAgent);
+    /** update system profile
+     * 
+     * @param profile profile object
+     */
     public void updateSystemProfile(UserProfile profile);
+    /** remove system profile from the database
+     * 
+     * @param profileId profile id
+     */
     public void deleteSystemProfile(int profileId);
+    /** add a new system profile to the database. During this process, a new profile id will be assigned to the profile.
+     * 
+     * @param profile profile object (profile id within will be overwritten)
+     * @return profile with an newly assigned id
+     */
     public UserProfile addSystemProfile(UserProfile profile);
+    /**      * 
+     * @param profileId 
+     */
     public UserProfile getSystemProfileById(int profileId);
+    /** obtain a list of system profiles
+     * 
+     * @return a hasbtable mapping system profile names to the profile objects
+     */
     public Hashtable getSystemProfileList();
 
+    /** establish a browser - user profile mapping
+     * 
+     * @param userId user id
+     * @param userAgent User-Agent header string
+     * @param profileId profile id to which given user agent will be mapped
+     */
     public void setUserBrowserMapping(int userId,String userAgent,int profileId);
+    /** establish system profile browser mapping
+     * 
+     * @param userAgent User-Agent header string
+     * @param systemProfileId profile id of a profile to which given 
+     *     user-agent will be mapped
+     */
     public void setSystemBrowserMapping(String userAgent,int systemProfileId);
 
     // returns profileId
+    /** Determine which profile a given browser mapped to
+     * 
+     * @param userId user id
+     * @param userAgent User-Agent header string
+     * @return profile id
+     */
     public Integer getUserBrowserMapping(int userId,String userAgent);
+    /** Determine which system profile given browser is mapped to
+     * 
+     * @param userAgent User-Agent header string
+     * @return system profile id
+     */
     public Integer getSystemBrowserMapping(String userAgent);
 
 
+    /** Retreive the entire UserPreferences object
+     * 
+     * @param userId user id
+     * @param profile profile
+     * @return user preferences
+     */
     public UserPreferences getUserPreferences(int userId, UserProfile profile);
 
+    /** save user preferences 
+     * 
+     * @param userId user id
+     * @param up user preferences object
+     */
     public void putUserPreferences(int userId, UserPreferences up);
 
+    /** Obtain structure stylesheet user preferences
+     * 
+     * @param userId user id
+     * @param profileId profile id
+     * @param stylesheetName structure stylesheet name
+     * @return structure stylesheet user pferences
+     */
     public StructureStylesheetUserPreferences getStructureStylesheetUserPreferences(int userId,int profileId,String stylesheetName);
+    /** Obtain theme styelsheet user preferences
+     * 
+     * @param userId user id
+     * @param profileId profile id
+     * @param stylesheetName theme stylesheet name
+     * @return theme stylesheet user preferences
+     */
     public ThemeStylesheetUserPreferences getThemeStylesheetUserPreferences(int userId,int profileId,String stylesheetName);
 
 
+    /** Save structure stylesheet user pferences
+     * 
+     * @param userId user id
+     * @param profileId profile id
+     * @param fsup structure stylesheet user preferences
+     */
     public void setStructureStylesheetUserPreferences(int userId,int profileId, StructureStylesheetUserPreferences fsup);
+    /** Save theme stylesheet user preferences
+     * 
+     * @param userId user id
+     * @param profileId profile id
+     * @param ssup structure stylesheet user preferneces
+     */
     public void setThemeStylesheetUserPreferences(int userId,int profileId, ThemeStylesheetUserPreferences ssup);
 
 
