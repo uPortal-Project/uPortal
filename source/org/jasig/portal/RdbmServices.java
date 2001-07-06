26a3afe9999a39557237846a9a46561cb29a2bec
Bernie Durfee <bernard.durfee@suny.edu>
2001-07-06 17:16:02 +0000

    Removed getDbImpl() call. The implementation for each interface should be obtained from JNDI or a factory. RdbmServices should be obsoleted by placing the uPortal datasource into JNDI.
    git-svn-id: https://source.jasig.org/uPortal/trunk@4494 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/RdbmServices.java
+++ source/org/jasig/portal/RdbmServices.java
@@ -31,118 +31,94 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ *
+ * formatted with JxBeauty (c) johann.langhofer@nextra.at
  */
 
-package org.jasig.portal;
 
-import javax.servlet.*;
-import javax.servlet.jsp.*;
-import javax.servlet.http.*;
+package  org.jasig.portal;
 
-import java.io.*;
-import java.util.*;
-import java.sql.*;
+import  javax.servlet.*;
+import  javax.servlet.jsp.*;
+import  javax.servlet.http.*;
+import  java.io.*;
+import  java.util.*;
+import  java.sql.*;
+
 
 /**
  * Provides database access
  * @author Ken Weiner, kweiner@interactivebusiness.com
  * @version $Revision$
  */
-public class RdbmServices extends GenericPortalBean
-{
+public class RdbmServices extends GenericPortalBean {
   private static boolean bPropsLoaded = false;
   private static String sJdbcDriver = null;
   private static String sJdbcUrl = null;
   private static String sJdbcUser = null;
   private static String sJdbcPassword = null;
-  private static String sDbImplClass = null;
   public static int RETRY_COUNT = 5;
-  private static String prevErrorMsg = "";  // reduce noise in log file
-
-  static
-  {
-    loadProps ();
+  private static String prevErrorMsg = "";      // reduce noise in log file
+  static {
+    loadProps();
   }
 
   /**
-   * Constructor which loades JDBC parameters from property file
-   * upon first invocation
+   * put your documentation comment here
    */
-  public RdbmServices ()
-  {
-  }
-
-  protected static void loadProps ()
-  {
-    try
-    {
-      if (!bPropsLoaded)
-      {
-        File jdbcPropsFile = new File (getPortalBaseDir () + "properties" + File.separator + "rdbm.properties");
-        Properties jdbcProps = new Properties ();
-        jdbcProps.load (new FileInputStream (jdbcPropsFile));
-
-        sJdbcDriver = jdbcProps.getProperty ("jdbcDriver");
-        sJdbcUrl = jdbcProps.getProperty ("jdbcUrl");
-        sJdbcUser = jdbcProps.getProperty ("jdbcUser");
-        sJdbcPassword = jdbcProps.getProperty ("jdbcPassword");
-        sDbImplClass = jdbcProps.getProperty ("dbImplClass");
+  protected static void loadProps () {
+    try {
+      if (!bPropsLoaded) {
+        File jdbcPropsFile = new File(getPortalBaseDir() + "properties" + File.separator + "rdbm.properties");
+        Properties jdbcProps = new Properties();
+        jdbcProps.load(new FileInputStream(jdbcPropsFile));
+        sJdbcDriver = jdbcProps.getProperty("jdbcDriver");
+        sJdbcUrl = jdbcProps.getProperty("jdbcUrl");
+        sJdbcUser = jdbcProps.getProperty("jdbcUser");
+        sJdbcPassword = jdbcProps.getProperty("jdbcPassword");
         bPropsLoaded = true;
       }
-    }
-    catch (Exception e)
-    {
-      Logger.log (Logger.ERROR, e);
+    } catch (Exception e) {
+      Logger.log(Logger.ERROR, e);
     }
   }
+
   /**
    * Gets a database connection
    * @return a database Connection object
    */
-  public static Connection getConnection ()
-  {
+  public static Connection getConnection () {
     Connection conn = null;
-
-    for ( int i = 0 ; i < RETRY_COUNT && conn == null ; ++i )
-    {
-      try
-      {
-        Class.forName (sJdbcDriver);
-        conn = DriverManager.getConnection (sJdbcUrl, sJdbcUser, sJdbcPassword);
+    for (int i = 0; i < RETRY_COUNT && conn == null; ++i) {
+      try {
+        Class.forName(sJdbcDriver);
+        conn = DriverManager.getConnection(sJdbcUrl, sJdbcUser, sJdbcPassword);
         prevErrorMsg = "";
-      }
-      catch (ClassNotFoundException cnfe)
-      {
-        Logger.log (Logger.ERROR, "The driver " + sJdbcDriver + " was not found, please check the logs/rdbm.properties file and your classpath.");
-        return null;
-      }
-      catch ( SQLException SQLe )
-      {
+      } catch (ClassNotFoundException cnfe) {
+        Logger.log(Logger.ERROR, "The driver " + sJdbcDriver + " was not found, please check the logs/rdbm.properties file and your classpath.");
+        return  null;
+      } catch (SQLException SQLe) {
         String errMsg = SQLe.getMessage();
-        if (!errMsg.equals(prevErrorMsg)) { // Only need to see one instance of this error
-          Logger.log (Logger.WARN, "Driver "+ sJdbcDriver + "produced error " + SQLe.getMessage () + " tring to get connection again.");
-          Logger.log (Logger.INFO, SQLe);
+        if (!errMsg.equals(prevErrorMsg)) {                     // Only need to see one instance of this error
+          Logger.log(Logger.WARN, "Driver " + sJdbcDriver + "produced error " + SQLe.getMessage() + " tring to get connection again.");
+          Logger.log(Logger.INFO, SQLe);
           prevErrorMsg = errMsg;
         }
       }
     }
-    return conn;
+    return  conn;
   }
 
   /**
    * Releases database connection
    * @param a database Connection object
    */
-  public static void releaseConnection (Connection con)
-  {
-    try
-    {
+  public static void releaseConnection (Connection con) {
+    try {
       if (con != null)
-        con.close ();
-    }
-    catch ( Exception e )
-    {
-      Logger.log (Logger.ERROR, e);
+        con.close();
+    } catch (Exception e) {
+      Logger.log(Logger.ERROR, e);
     }
   }
 
@@ -150,35 +126,26 @@ public class RdbmServices extends GenericPortalBean
    * Get the JDBC driver
    * @return the JDBC driver
    */
-  public static String getJdbcDriver ()
-  {
-    return sJdbcDriver;
+  public static String getJdbcDriver () {
+    return  sJdbcDriver;
   }
 
   /**
    * Get the JDBC connection URL
    * @return the JDBC connection URL
    */
-  public static String getJdbcUrl ()
-  {
-    return sJdbcUrl;
+  public static String getJdbcUrl () {
+    return  sJdbcUrl;
   }
 
   /**
    * Get the JDBC user
    * @return the JDBC connection URL
    */
-  public static String getJdbcUser ()
-  {
-    return sJdbcUser;
-  }
-
-  /**
-   * Get the dbImpl class we should use
-   * @return dbImpl object
-   */
-  public static IDBImpl getDbImpl() throws ClassNotFoundException, IllegalAccessException, InstantiationException
-  {
-    return (org.jasig.portal.DBImpl) Class.forName (sDbImplClass).newInstance ();
+  public static String getJdbcUser () {
+    return  sJdbcUser;
   }
 }
+
+
+
