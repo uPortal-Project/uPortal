ee635081c42a19adfc141f337efb2a5bffb75839
Bernie Durfee <bernard.durfee@suny.edu>
2001-07-31 22:19:37 +0000

    Added the dom2PrettyString() method.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-1-0-patches@4712 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/UtilitiesBean.java
+++ source/org/jasig/portal/UtilitiesBean.java
@@ -31,12 +31,19 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ *
+ * formatted with JxBeauty (c) johann.langhofer@nextra.at
  */
 
-package org.jasig.portal;
 
-import javax.servlet.http.*;
-import java.text.*;
+package  org.jasig.portal;
+
+import  javax.servlet.http.*;
+import  java.text.*;
+
+import org.w3c.dom.Document;
+import java.io.IOException;
+
 
 /**
  * Provides methods useful for the portal.  Later on, it may be necessary
@@ -45,27 +52,22 @@ import java.text.*;
  * @author Ken Weiner
  * @version $Revision$
  */
-public class UtilitiesBean extends GenericPortalBean
-{
-    /** The default allowed character for removeSpecialChars */
-    public static final String DEFAULT_ALLOWED = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_ ";
-    
+public class UtilitiesBean extends GenericPortalBean {
+
+  /** The default allowed character for removeSpecialChars */
+  public static final String DEFAULT_ALLOWED = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_ ";
 
   /**
    * Prevents an html page from being cached by the browser
    * @param the servlet response object
    */
-  public static void preventPageCaching (HttpServletResponse res)
-  {
-    try
-    {
+  public static void preventPageCaching (HttpServletResponse res) {
+    try {
       res.setHeader("pragma", "no-cache");
-      res.setHeader( "Cache-Control","no-cache, max-age=0, must-revalidate" ); // Need this for I.E. 5.0
-      res.setDateHeader( "Expires", 0 );
-    }
-    catch (Exception e)
-    {
-      Logger.log (Logger.ERROR, e);
+      res.setHeader("Cache-Control", "no-cache, max-age=0, must-revalidate");                   // Need this for I.E. 5.0
+      res.setDateHeader("Expires", 0);
+    } catch (Exception e) {
+      Logger.log(Logger.ERROR, e);
     }
   }
 
@@ -73,21 +75,16 @@ public class UtilitiesBean extends GenericPortalBean
    * Gets the current date/time
    * @return a formatted date and time string
    */
-  public static String getDate ()
-  {
-    try
-    {
+  public static String getDate () {
+    try {
       // Format the current time.
-      SimpleDateFormat formatter = new SimpleDateFormat ("EEEE, MMM d, yyyy 'at' hh:mm a");
+      SimpleDateFormat formatter = new SimpleDateFormat("EEEE, MMM d, yyyy 'at' hh:mm a");
       java.util.Date currentTime = new java.util.Date();
-      return formatter.format(currentTime);
+      return  formatter.format(currentTime);
+    } catch (Exception e) {
+      Logger.log(Logger.ERROR, e);
     }
-    catch (Exception e)
-    {
-      Logger.log (Logger.ERROR, e);
-    }
-
-    return "&nbsp;";
+    return  "&nbsp;";
   }
 
   /**
@@ -100,98 +97,92 @@ public class UtilitiesBean extends GenericPortalBean
    * 3) A path relative to the portal base dir as determined from
    *    GenericPortalBean.getPortalBaseDir()
    */
-  public static String fixURI (String str)
-  {
-    boolean bWindows = (System.getProperty ("os.name").indexOf ("Windows") != -1) ? true : false;
-    char ch0 = str.charAt (0);
-    char ch1 = str.charAt (1);
-
-    if (str.indexOf ("://") == -1 && ch1 != ':')
-    {
+  public static String fixURI (String str) {
+    boolean bWindows = (System.getProperty("os.name").indexOf("Windows") != -1) ? true : false;
+    char ch0 = str.charAt(0);
+    char ch1 = str.charAt(1);
+    if (str.indexOf("://") == -1 && ch1 != ':') {
       // Relative path was specified, so prepend portal base dir
-      str = (bWindows ? "file:/" : "file://") + GenericPortalBean.getPortalBaseDir () + str;
-    }
-    else if (bWindows && str.startsWith ("file://"))
-    {
+      str = (bWindows ? "file:/" : "file://") + GenericPortalBean.getPortalBaseDir() + str;
+    } 
+    else if (bWindows && str.startsWith("file://")) {
       // Replace "file://" with "file:/" on Windows machines
-      str = "file:/" + str.substring (7);
-    }
-    else if (ch0 == java.io.File.separatorChar || ch1 == ':')
-    {
+      str = "file:/" + str.substring(7);
+    } 
+    else if (ch0 == java.io.File.separatorChar || ch1 == ':') {
       // It's a full path without "file://"
       str = (bWindows ? "file:/" : "file://") + str;
     }
-
     // Handle platform-dependent strings
-    str = str.replace (java.io.File.separatorChar, '/');
-
-    return str;
+    str = str.replace(java.io.File.separatorChar, '/');
+    return  str;
   }
 
-  public static String escapeString(String source)
-  {
-    StringBuffer sb = new StringBuffer ();
-
-    for (int i = 0 ; i < source.length() ; i++)
-    {
-      sb.append(escapeChar(source.charAt (i)));
+  /**
+   * put your documentation comment here
+   * @param source
+   * @return 
+   */
+  public static String escapeString (String source) {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < source.length(); i++) {
+      sb.append(escapeChar(source.charAt(i)));
     }
-
-    return sb.toString ();
+    return  sb.toString();
   }
 
-  private static String escapeChar(char ch)
-  {
-    StringBuffer sb = new StringBuffer ();
+  /**
+   * put your documentation comment here
+   * @param ch
+   * @return 
+   */
+  private static String escapeChar (char ch) {
+    StringBuffer sb = new StringBuffer();
     String charRef;
-
     // If there is a suitable entity reference for this character, print it.
-    charRef = getEntityRef (ch);
-
-    if ( charRef != null )
-    {
-      sb.append ('&');
-      sb.append (charRef);
-      sb.append (';');
-    }
-    else
-    {
-      if(( ch >= ' ' && ch <= 0x7E && ch != 0xF7 ) || ch == '\n' || ch == '\r' || ch == '\t' )
-      {
+    charRef = getEntityRef(ch);
+    if (charRef != null) {
+      sb.append('&');
+      sb.append(charRef);
+      sb.append(';');
+    } 
+    else {
+      if ((ch >= ' ' && ch <= 0x7E && ch != 0xF7) || ch == '\n' || ch == '\r' || ch == '\t') {
         // If the character is not printable, print as character reference.
         // Non printables are below ASCII space but not tab or line
         // terminator, ASCII delete, or above a certain Unicode threshold.
-        sb.append (ch);
-      }
-      else
-      {
-        sb.append ("&#");
-        sb.append (Integer.toString (ch));
-        sb.append (';');
+        sb.append(ch);
+      } 
+      else {
+        sb.append("&#");
+        sb.append(Integer.toString(ch));
+        sb.append(';');
       }
     }
-
-    return sb.toString ();
+    return  sb.toString();
   }
 
-  private static String getEntityRef (char ch)
-  {
+  /**
+   * put your documentation comment here
+   * @param ch
+   * @return 
+   */
+  private static String getEntityRef (char ch) {
     // Encode special XML characters into the equivalent character references.
     // These five are defined by default for all XML documents.
-    switch( ch )
-    {
+    switch (ch) {
       case '<':
-        return "lt";
+        return  "lt";
       case '>':
-        return "gt";
+        return  "gt";
       case '"':
-        return "quot";
+        return  "quot";
       case '\'':
-        return "apos";
+        return  "apos";
       case '&':
-        return "amp";
+        return  "amp";
     }
-    return null;
+    return  null;
   }
 
   /**
@@ -205,11 +196,9 @@ public class UtilitiesBean extends GenericPortalBean
    * may change in the future, but the set of characters that are "desirable" is smaller and easier to maintain
    * over time.
    */
-  public static String removeSpecialChars(String source)
-  {
-      return removeSpecialChars(source, DEFAULT_ALLOWED);
+  public static String removeSpecialChars (String source) {
+    return  removeSpecialChars(source, DEFAULT_ALLOWED);
   }
-
 
   /**
    * Removes any characters that are not in the allowed set. 
@@ -224,27 +213,40 @@ public class UtilitiesBean extends GenericPortalBean
    * @param source -- the string to clean.
    * @param allowed -- a string with all the allowed chars in it
    */
-  public static String removeSpecialChars(String source, String allowed)
-  {
-      
-    StringBuffer sb = new StringBuffer (source);
+  public static String removeSpecialChars (String source, String allowed) {
+    StringBuffer sb = new StringBuffer(source);
     char ch;
-
     int i = 0;
-    while(i < sb.length())
-    {
-	ch = sb.charAt(i);
-	if(allowed.indexOf(ch) == -1) {
-	    // looks like we have a rogue character (a -1 means it isn't in allowed)
-	    sb.deleteCharAt(i);
-	} else {
-	    // only increment if this character is good
-	    i++;
-	}
+    while (i < sb.length()) {
+      ch = sb.charAt(i);
+      if (allowed.indexOf(ch) == -1) {
+        // looks like we have a rogue character (a -1 means it isn't in allowed)
+        sb.deleteCharAt(i);
+      } 
+      else {
+        // only increment if this character is good
+        i++;
+      }
     }
-
-    return sb.toString();
+    return  sb.toString();
   }
 
-
+  /**
+   * put your documentation comment here
+   * @param doc
+   * @return 
+   * @exception IOException
+   */
+  public static String dom2PrettyString (Document doc) throws IOException {
+    java.io.StringWriter outString = new java.io.StringWriter();
+    org.apache.xml.serialize.OutputFormat format = new org.apache.xml.serialize.OutputFormat();
+    format.setOmitXMLDeclaration(true);
+    format.setIndenting(true);
+    org.apache.xml.serialize.XMLSerializer xsl = new org.apache.xml.serialize.XMLSerializer(outString, format);
+    xsl.serialize(doc);
+    return  outString.toString();
+  }
 }
+
+
+
