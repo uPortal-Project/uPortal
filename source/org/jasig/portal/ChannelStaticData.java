b52bde632a85c1e54ba746d95682724e1a5fe592
Bernie Durfee <bernard.durfee@suny.edu>
2001-08-02 22:21:07 +0000

    Copied all methods from uPortal 2.x
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-1-0-patches@4726 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/ChannelStaticData.java
+++ source/org/jasig/portal/ChannelStaticData.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000 The JA-SIG Collaborative.  All rights reserved.
+ * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,11 +31,21 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ *
+ * formatted with JxBeauty (c) johann.langhofer@nextra.at
  */
 
-package org.jasig.portal;
 
-import java.util.Map;
+package  org.jasig.portal;
+
+import  java.util.Map;
+import org.jasig.portal.security.PortalSecurityException;
+import  org.jasig.portal.security.IPerson;
+import  org.jasig.portal.security.SecurityContext;
+import  javax.naming.InitialContext;
+import  javax.naming.Context;
+import  java.util.Hashtable;
+
 
 /**
  * Used to store channel configuration items and parameters.
@@ -43,58 +53,163 @@ import java.util.Map;
  * @version $Revision$
  * @author Peter Kharchenko
  */
+public class ChannelStaticData extends java.util.Hashtable {
+  private InitialContext m_portalContext = null;
+  private String m_channelGlobalID = null;
+  private String m_channelInstanceID = null;
+  private long m_timeout = java.lang.Long.MAX_VALUE;
+  private IPerson m_person = null;
+  private SecurityContext m_securityContext = null;
 
-public class ChannelStaticData extends java.util.Hashtable
-{
-    private String sChannelID = null;
+  /**
+   * Set information contained in a channel <param> element
+   * Parameters are strings!
+   * @param key param name
+   * @param value param value
+   */
+  public synchronized String setParameter (String key, String value) {
+    return  (String)super.put(key, value);
+  }
 
-    // Parameters are strings !
-    /**
-     * Set information contained in a channel <param> element
-     * @param key param name
-     * @param value param value
-     */
-    public synchronized String setParameter (String key, String value) {return (String) super.put (key, value);}
-    /**
-     * Get information contained in a particular <param> element
-     * @param key param name
-     * @return param value
-     */
-    public synchronized String getParameter (String key) {return (String) super.get (key);}
+  /**
+   * Get information contained in a particular <param> element
+   * @param key param name
+   * @return param value
+   */
+  public synchronized String getParameter (String key) {
+    return  (String)super.get(key);
+  }
 
+  /**
+   * If you need to pass Objects, use this
+   * Similar to the {@link #setParameter(String,String)}, but can be used to pass things other then strings.
+   * @param key
+   * @param value
+   * @return
+   */
+  public synchronized Object put (Object key, Object value) {
+    return  super.put(key, value);
+  }
 
-    // if you need to pass Objects, use this
-    /**
-     * Similar to the {@link #setParameter(String,String)}, but can be used to pass things other then strings.
-     */
-    public synchronized Object put (Object key, Object value) {return super.put (key, value);}
-    /**
-     * Similar to the {@link #getParameter(String)}, but can be used to pass things other then strings.
-     */
-    public synchronized Object get (Object key) {return super.get (key);}
+  /**
+   * Similar to the {@link #getParameter(String)}, but can be used to pass things other then strings.
+   * @param key
+   * @return
+   */
+  public synchronized Object get (Object key) {
+    return  super.get(key);
+  }
 
+  /**
+   * Copy parameter list from a Map
+   * @param params a map of params
+   */
+  public void setParameters (Map params) {
+    // copy a Map
+    this.putAll(params);
+  }
 
-    /**
-     * Copy parameter list from a Map
-     * @param params a map of params
-     */
-    public void setParameters(Map params) {
-	// copy a Map
-	this.putAll(params);
-    };
+  /**
+   * Sets the channel instance ID
+   * @param sChannelID the unique channelID
+   */
+  public void setChannelID (String sChID) {
+    m_channelInstanceID = sChID;
+  }
 
+  /**
+   * Gets the channel ID
+   * @return the channel's ID
+   */
+  public String getChannelID () {
+    return  (m_channelInstanceID);
+  }
 
-    /**
-     * Sets the channel ID
-     * @param sChannelID the unique channelID
-     */
-    public void setChannelID (String sChID) {this.sChannelID = sChID;}
+  /**
+   * put your documentation comment here
+   * @param   String channelGlobalID
+   */
+  public void setChannelGlobalID (String channelGlobalID) {
+    m_channelGlobalID = channelGlobalID;
+  }
 
-    /**
-     * Gets the channel ID
-     * @return the channel's ID
-     */
-    public String getChannelID () {return sChannelID;}
+  /**
+   * put your documentation comment here
+   */
+  public String getChannelGlobalID () {
+    return  (m_channelGlobalID);
+  }
+
+  /**
+   * put your documentation comment here
+   * @param value
+   */
+  public void setTimeout (long value) {
+    m_timeout = value;
+  }
+
+  /**
+   * put your documentation comment here
+   * @return
+   */
+  public long getTimeout () {
+    return  (m_timeout);
+  }
+
+  /**
+   * put your documentation comment here
+   * @return
+   */
+  public IPerson getPerson () {
+    return  (m_person);
+  }
+
+  /**
+   * put your documentation comment here
+   * @param per
+   */
+  public void setPerson (IPerson person) {
+    m_person = person;
+  }
+
+  /**
+   * put your documentation comment here
+   * @return
+   */
+  public SecurityContext getSecurityContext () {
+    return  (m_securityContext);
+  }
+
+  /**
+   * put your documentation comment here
+   * @param sc
+   */
+  public void setSecurityContext (SecurityContext securitContext) {
+    m_securityContext = securitContext;
+  }
+
+  /**
+   * This method will fail in uPortal 1.x until someone implements the JNDI context!
+   * @return
+   */
+  public InitialContext getPortalContext () {
+    if (m_portalContext != null) {
+      return  (m_portalContext);
+    }
+    else {
+      Hashtable environment = new Hashtable(1);
+      // Set up the path
+      environment.put(Context.INITIAL_CONTEXT_FACTORY, "org.jasig.portal.jndi.PortalInitialContextFactory");
+      try {
+        InitialContext ctx = new InitialContext(environment);
+        return  (ctx);
+      } catch (Exception e) {
+        e.printStackTrace(System.err);
+        return  (null);
+      }
+    }
+  }
 }
+
 
 
