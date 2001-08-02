57fe561b6ffc7d943cc122134ce4d07f85d8b033
Bernie Durfee <bernard.durfee@suny.edu>
2001-08-02 22:23:06 +0000

    Added more error checking to avoid null pointer exceptions
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-1-0-patches@4727 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/AuthenticationBean.java
+++ source/org/jasig/portal/AuthenticationBean.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000 The JA-SIG Collaborative.  All rights reserved.
+ * Copyright (c) 2001 The JA-SIG Collaborative.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,33 +31,33 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ *
+ * formatted with JxBeauty (c) johann.langhofer@nextra.at
  */
 
-package org.jasig.portal;
 
-import javax.servlet.*;
-import javax.servlet.jsp.*;
-import javax.servlet.http.*;
+package  org.jasig.portal;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.sql.*;
-import com.objectspace.xml.*;
-import org.jasig.portal.layout.*;
-import org.jasig.portal.security.*;
+import  javax.servlet.*;
+import  javax.servlet.jsp.*;
+import  javax.servlet.http.*;
+import  java.io.*;
+import  java.util.*;
+import  java.text.*;
+import  java.sql.*;
+import  com.objectspace.xml.*;
+import  org.jasig.portal.layout.*;
+import  org.jasig.portal.security.*;
+
 
 /**
  * @author Ken Weiner
  * @modified by Vikrant Joshi: June 26  2001.
  */
-
-
-public class AuthenticationBean extends GenericPortalBean implements IAuthenticationBean
-{
+public class AuthenticationBean extends GenericPortalBean
+    implements IAuthenticationBean {
   protected org.jasig.portal.security.IPerson m_Person = null;
-  protected String authenticationFailure = "";
-
+  protected String authenticationFailure = null;
 
   /**
    * Authenticate a user.
@@ -65,9 +65,8 @@ public class AuthenticationBean extends GenericPortalBean implements IAuthentica
    * @param sPassword User password
    * @return true if successful, otherwise false.
    */
-  public boolean authenticate (String sUserName, String sPassword)
-  {
-      return authenticate (sUserName, sPassword, "root");
+  public boolean authenticate (String sUserName, String sPassword) {
+    return  authenticate(sUserName, sPassword, "root");
   }
 
   /**
@@ -77,42 +76,76 @@ public class AuthenticationBean extends GenericPortalBean implements IAuthentica
    * @param contextType  String to be passed to the InitialSecurityContext when initialising it 
    * @return true if successful, otherwise false.
    */
-  public boolean authenticate (String sUserName, String sPassword, String contextType)
-  {
-      // don't bother if SessionManager says there's not enough resources
-        if(!SessionManager.allowLogins()) {
-            return false;
-        }
-
-	SecurityContext ic =  new InitialSecurityContext(contextType);
-	Principal me;
-	OpaqueCredentials op;
-
-	me = ic.getPrincipalInstance();
-	op = ic.getOpaqueCredentialsInstance();
-
-	me.setUID(sUserName);
-	op.setCredentials(sPassword);
-	ic.authenticate();
-	boolean bAuthenticated = ic.isAuthenticated ();
-
-	if(bAuthenticated) {
-	    AdditionalDescriptor addInfo = ic.getAdditionalDescriptor();
-
-	    if (addInfo == null || !(addInfo instanceof PersonImpl)) {
-		m_Person = new PersonImpl ();
-		m_Person.setID(sUserName);
-		m_Person.setFullName(me.getFullName());
-		m_Person.setAttribute("globalUID", me.getGlobalUID());
-	    } else {
-		m_Person = (IPerson)addInfo;
-	    } // END IF (addInfo == null || ........ )
-
-	} else {
-	    authenticationFailure = ic.getAuthenticationFailure();
-	} // ENF IF (bAuthenticated)
-
-	return (bAuthenticated);
+  public boolean authenticate (String sUserName, String sPassword, String contextType) {
+    // Don't bother if SessionManager says there's not enough resources
+    if (!SessionManager.allowLogins()) {
+      return  (false);
+    }
+    // Instantiate the InitialSecurityContext
+    SecurityContext ic = null;
+    try {
+      ic = new InitialSecurityContext(contextType);
+    } catch (Exception e) {
+      Logger.log(Logger.ERROR, "AuthenticationBean.authenticate(): Could not create InitialSecurityContext of type " + contextType
+          + " " + e.getMessage());
+      authenticationFailure = "Could not create InitialSecurityContext of type " + contextType;
+      return  (false);
+    }
+    // Make sure the InitialSecurityContext was initialized
+    if (ic == null) {
+      Logger.log(Logger.ERROR, "AuthenticationBean.authenticate(): Could not create InitialSecurityContext of type " + contextType);
+      authenticationFailure = "Could not create InitialSecurityContext of type " + contextType;
+      return  (false);
+    }
+    Principal me;
+    OpaqueCredentials op;
+    // Get an instance of the principal from the security context
+    me = ic.getPrincipalInstance();
+    if (me == null) {
+      Logger.log(Logger.ERROR, "AuthenticationBean.authenticate(): Could not retrieve Principal instance from security context");
+      authenticationFailure = "Could not retrieve Principal instance from security context";
+      return  (false);
+    }
+    // Get an instance of the credentials from the security context
+    op = ic.getOpaqueCredentialsInstance();
+    if (op == null) {
+      Logger.log(Logger.ERROR, "AuthenticationBean.authenticate(): Could not retrieve OpaqueCredentials instance from security context");
+      authenticationFailure = "Could not retrieve OpaqueCredentials instance from security context";
+      return  (false);
+    }
+    // Set the user name in the principal
+    me.setUID(sUserName);
+    // Set the password in the credentials
+    op.setCredentials(sPassword);
+    try {
+      // Attempt to authenticate the user
+      ic.authenticate();
+    } catch (Exception e) {
+      Logger.log(Logger.ERROR, "AuthenticationBean.authenticate(): Security context thre exception while attempting to authenticate "
+          + e.getMessage());
+      authenticationFailure = "Security context thre exception while attempting to authenticate " + e.getMessage();
+      return  (false);
+    }
+    // Check to see if the user passed authentication
+    boolean bAuthenticated = ic.isAuthenticated();
+    if (bAuthenticated) {
+      // Get the additional descriptor from the security context
+      AdditionalDescriptor addInfo = ic.getAdditionalDescriptor();
+      // Create an IPerson object if one was not passed from the security context
+      if (addInfo == null || !(addInfo instanceof PersonImpl)) {
+        m_Person = new PersonImpl();
+        m_Person.setID(sUserName);
+        m_Person.setFullName(me.getFullName());
+        m_Person.setAttribute("globalUID", me.getGlobalUID());
+      } 
+      else {
+        m_Person = (IPerson)addInfo;
+      }
+    } 
+    else {
+      authenticationFailure = ic.getAuthenticationFailure();
+    }
+    return  (bAuthenticated);
   }
 
   /**
@@ -122,25 +155,18 @@ public class AuthenticationBean extends GenericPortalBean implements IAuthentica
    * @return An object that implements the
    * <code>org.jasig.portal.security.Person</code> interface.
    */
-  public IPerson getPerson ()
-  {
-    return m_Person;
+  public IPerson getPerson () {
+    return  m_Person;
   }
 
-    /**
-     * Returns the reason for AuthenticationFailure (such as "Incorrect UserName/Password"
-     * or "Authentication Service Unavailable").
-     */
-  public String getAuthenticationFailure() {
-	return authenticationFailure;
-  }  
+  /**
+   * Returns the reason for AuthenticationFailure (such as "Incorrect UserName/Password"
+   * or "Authentication Service Unavailable").
+   */
+  public String getAuthenticationFailure () {
+    return  (authenticationFailure);
+  }
 }
-
-
-
-
-
-
 
 
 
