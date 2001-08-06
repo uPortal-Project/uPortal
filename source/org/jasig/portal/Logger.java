a22ab7fa831ee0dbcb2f7378b96de44f69e2fbd0
Bernie Durfee <bernard.durfee@suny.edu>
2001-08-06 21:15:52 +0000

    This object is deprecated, use the org.jasig.portal.services.LogService object instead.
    git-svn-id: https://source.jasig.org/uPortal/trunk@4730 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/Logger.java
+++ source/org/jasig/portal/Logger.java
@@ -31,22 +31,15 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ *
+ * formatted with JxBeauty (c) johann.langhofer@nextra.at
  */
-package org.jasig.portal;
 
-import java.io.File;
-import java.io.StringWriter;
-import java.io.PrintWriter;
-import java.io.FileInputStream;
 
-import java.util.Properties;
+package  org.jasig.portal;
 
-import org.apache.log4j.Category;
-import org.apache.log4j.FileAppender;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.Priority;
+import  org.jasig.portal.services.LogService;
 
-import org.jasig.portal.services.PortalFileAppender;
 
 /**
  * The Logger class is used to output messages to a log file.
@@ -63,10 +56,9 @@ import org.jasig.portal.services.PortalFileAppender;
  * @author Ken Weiner (IBS)
  * @author Bernie Durfee (IBS)
  * @version $Revision$
+ * @deprecated Use org.jasig.portal.LogService instead
  */
-
-public class Logger extends GenericPortalBean
-{
+public class Logger extends GenericPortalBean {
   // Log levels
   public static final int NONE = 0;
   public static final int SEVERE = 1;
@@ -74,26 +66,6 @@ public class Logger extends GenericPortalBean
   public static final int WARN = 3;
   public static final int INFO = 4;
   public static final int DEBUG = 5;
-
-  private static int iLogLevelSetting = DEBUG;
-
-  private static String sLogLevelSetting = "DEBUG";
-
-  private static final String sIllArgExMessage = "Log level must be NONE, SEVERE, ERROR, WARN, INFO, or DEBUG";
-
-  private static final String fs = File.separator;
-
-  private static String sLogRelativePath = "logs";
-
-  private static boolean bInitialized = false;
-
-  private static Category m_catFramework = null;
-
-  private static PortalFileAppender m_logFile = null;
-
-  private static int m_maxBackupIndex = 10; // Maximum number of log files (Default is 10)
-
-  private static int m_maxFileSize = 500000; // Maximum size of each log file (Default is 500k)
 
   /**
    * Sets the current log level.  Use one of the static integer members
@@ -103,255 +75,52 @@ public class Logger extends GenericPortalBean
    *
    * @param a log level
    * @throws IllegalArgumentException if the log level is not one of the acceptable log levels
+   * @deprecated Use org.jasig.portal.LogService instead
    */
-  public static void setLogLevel(int iLogLevel)
-  {
-    if(!bInitialized)
-    {
-      initialize();
-    }
-    if(iLogLevel <= DEBUG)
-    {
-      iLogLevelSetting = iLogLevel;
-    }
-    else
-    {
-      throw new IllegalArgumentException(sIllArgExMessage);
-    }
+  public static void setLogLevel (int iLogLevel) {
+    LogService.instance().setLogLevel(iLogLevel);
   }
 
   /**
    * Gets the current log level setting
    * @return the current log level setting
+   * @deprecated Use org.jasig.portal.LogService instead
    */
-  public static int getLogLevel()
-  {
-    return iLogLevelSetting;
+  public static int getLogLevel () {
+    return  (LogService.instance().getLogLevel());
   }
 
   /**
-   * Increments the old logs and creates a new log with the
-   * current time and log level written at the top
+   * put your documentation comment here
+   * @param iLogLevel
+   * @param sMessage
+   * @param ex
+   * @deprecated Use org.jasig.portal.LogService instead
    */
-  public static void initialize()
-  {
-    String sPortalBaseDir = getPortalBaseDir();
-    File propertiesFile = null;
-
-    try
-    {
-      // Load the portal properties file
-      propertiesFile = new File(sPortalBaseDir + "/properties/portal.properties");
-
-      if(propertiesFile != null && propertiesFile.exists())
-      {
-        Properties portalProperties = new Properties();
-        portalProperties.load(new FileInputStream(propertiesFile));
-
-        if(portalProperties.getProperty("logger.level") != null)
-        {
-          sLogLevelSetting = portalProperties.getProperty("logger.level");
-        }
-        else
-        {
-          System.out.println("Defaulting to " + sLogLevelSetting + " for portal log level.");
-        }
-        if(portalProperties.getProperty("logger.maxLogFiles") != null)
-        {
-          m_maxBackupIndex = Integer.parseInt(portalProperties.getProperty("logger.maxLogFiles"));
-        }
-        else
-        {
-          System.out.println("Defaulting to " + m_maxBackupIndex + " for maximum number of portal log files.");
-        }
-        if(portalProperties.getProperty("logger.maxFileSize") != null)
-        {
-          m_maxFileSize = Integer.parseInt(portalProperties.getProperty("logger.maxFileSize"));
-        }
-        else
-        {
-          System.out.println("Defaulting to " + m_maxFileSize + " for maximum log file size.");
-        }
-      }
-      else
-      {
-        System.out.println("The file portal.properties could not be loaded, using default properties.");
-      }
-    }
-    catch(Exception e)
-    {
-      e.printStackTrace();
-    }
-    try
-    {
-      if(sPortalBaseDir != null && new File(sPortalBaseDir).exists())
-      {
-        PatternLayout patternLayout = new PatternLayout("%-5p %-23d{ISO8601} %m%n");
-
-        // Make sure the relative path ends with a seperator
-        if(!sLogRelativePath.endsWith(File.separator))
-        {
-          sLogRelativePath = sLogRelativePath + File.separator;
-        }
-
-        // Make sure the portal base directory path ends with a seperator
-        if(!sPortalBaseDir.endsWith(File.separator))
-        {
-          sPortalBaseDir = sPortalBaseDir + File.separator;
-        }
-
-        // Create the log file directory path
-        String logFileDirectoryPath = sPortalBaseDir + sLogRelativePath;
-
-        File logFileDirectory = new File(logFileDirectoryPath);
-
-        // Make sure the log file directory exists
-        if(!logFileDirectory.exists())
-        {
-          if(!logFileDirectory.mkdir() || !logFileDirectory.exists() || !logFileDirectory.isDirectory())
-          {
-            System.out.println("Could not create log file directory!");
-          }
-        }
-
-        // Create the file appender
-        m_logFile = new PortalFileAppender(patternLayout, logFileDirectoryPath + "portal.log");
-
-        // Make sure to roll the logs to start fresh
-        m_logFile.rollOver();
-        m_logFile.setMaxBackupIndex(m_maxBackupIndex);
-        m_logFile.setMaxFileSize(m_maxFileSize);
-        m_catFramework = Category.getRoot();
-        m_catFramework.addAppender(m_logFile);
-        m_catFramework.setPriority(Priority.toPriority(sLogLevelSetting));
-
-        // Insures that initialization is only done once
-        bInitialized = true;
-      }
-      else
-      {
-        System.out.println("Logger.initialize(): PortalBaseDir is not set or does not exist!");
-      }
-    }
-    catch(Exception e)
-    {
-      System.err.println("Problem writing to log.");
-      e.printStackTrace();
-    }
-    catch(Error er)
-    {
-      System.err.println("Problem writing to log.");
-      er.printStackTrace();
-    }
-  }
-
-  public static void log(int iLogLevel, String sMessage, Throwable ex)
-  {
-    log(iLogLevel, sMessage);
-    log(iLogLevel, ex);
-  }
-
-  public static void log(int iLogLevel, String sMessage)
-  {
-    try
-    {
-      if(!bInitialized)
-      {
-        initialize();
-      }
-
-      switch(iLogLevel)
-      {
-        case NONE:
-          return ;
-
-        case SEVERE:
-          m_catFramework.fatal(sMessage);
-          return ;
-
-        case ERROR:
-          m_catFramework.error(sMessage);
-          return ;
-
-        case WARN:
-          m_catFramework.warn(sMessage);
-          return ;
-
-        case INFO:
-          m_catFramework.info(sMessage);
-          return ;
-
-        case DEBUG:
-          m_catFramework.debug(sMessage);
-          return ;
-
-        default:
-          throw new IllegalArgumentException();
-      }
-    }
-    catch(Exception e)
-    {
-      System.err.println("Problem writing to log.");
-      e.printStackTrace();
-    }
-    catch(Error er)
-    {
-      System.err.println("Problem writing to log.");
-      er.printStackTrace();
-    }
-  }
-
-  public static void log(int iLogLevel, Throwable ex)
-  {
-    if(!bInitialized)
-    {
-      initialize();
-    }
-
-    StringWriter stackTrace = new StringWriter();
-    ex.printStackTrace(new PrintWriter(stackTrace));
-    log(iLogLevel, stackTrace.toString());
+  public static void log (int iLogLevel, String sMessage, Throwable ex) {
+    LogService.instance().log(iLogLevel, sMessage, ex);
   }
 
   /**
-   * Translates integer version of log level into
-   * string version for printing to the log
-   * @param the log level
-   * @return a string representation of the log level
-   * @throws IllegalArgumentException if the log level is not one of the acceptable log levels
+   * put your documentation comment here
+   * @param iLogLevel
+   * @param sMessage
+   * @deprecated Use org.jasig.portal.LogService instead
    */
-  private static String getLogLevel(int iLogLevel)
-  {
-    String sLogLevel = null;
-    switch(iLogLevel)
-    {
-      case NONE:
-        sLogLevel = "NONE  ";
-        break;
+  public static void log (int iLogLevel, String sMessage) {
+    LogService.instance().log(iLogLevel, sMessage);
+  }
 
-      case SEVERE:
-        sLogLevel = "SEVERE";
-        break;
-
-      case ERROR:
-        sLogLevel = "ERROR ";
-        break;
-
-      case WARN:
-        sLogLevel = "WARN  ";
-        break;
-
-      case INFO:
-        sLogLevel = "INFO  ";
-        break;
-
-      case DEBUG:
-        sLogLevel = "DEBUG ";
-        break;
-
-      default:
-        throw new IllegalArgumentException();
-    }
-    return sLogLevel;
+  /**
+   * put your documentation comment here
+   * @param iLogLevel
+   * @param ex
+   * @deprecated Use org.jasig.portal.LogService instead
+   */
+  public static void log (int iLogLevel, Throwable ex) {
+    LogService.instance().log(iLogLevel, ex);
   }
 }
+
+
+
