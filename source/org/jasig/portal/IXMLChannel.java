177b61160ef28a7244557d46fc2047468d548803
Bernie Durfee <bernard.durfee@suny.edu>
2001-08-06 21:40:49 +0000

    Deprecated getSubscriptionProperties ()
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-1-0-patches@4733 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/IXMLChannel.java
+++ source/org/jasig/portal/IXMLChannel.java
@@ -31,14 +31,17 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ *
+ * formatted with JxBeauty (c) johann.langhofer@nextra.at
  */
 
-package org.jasig.portal;
 
-import java.util.*;
-import javax.servlet.jsp.*;
-import javax.servlet.http.*;
-import org.xml.sax.DocumentHandler;
+package  org.jasig.portal;
+
+import  java.util.*;
+import  javax.servlet.jsp.*;
+import  javax.servlet.http.*;
+import  org.xml.sax.DocumentHandler;
 
 
 /**
@@ -46,64 +49,64 @@ import org.xml.sax.DocumentHandler;
  * @author Peter Kharchenko
  * @version $Revision$
  */
+public interface IXMLChannel {
 
-public interface IXMLChannel
-{
-
-
-    // following section allows portal to pass parameter/settings information to the channel
-
-    /**
-     * Passes ChannelStaticData to the channel.
-     * This is done during channel instantiation time.
-     * see org.jasig.portal.StaticData
-     * @param sd channel static data
-     * @see ChannelStaticData
-     */
-    public void setStaticData (ChannelStaticData sd);
-
-    /**
-     * Passes ChannelRuntimeData to the channel.
-     * This function is called prior to the renderXML() call.
-     * @param rd channel runtime data
-     * @see ChannelRuntimeData
-     */
-    public void setRuntimeData(ChannelRuntimeData rd);
-
-    /**
-     * Passes an outside event to a channel.
-     * Events should normally come from the LayoutBean.
-     * @param ev LayoutEvent object
-     * @see LayoutEvent
-     */
-    public void receiveEvent(LayoutEvent ev);
-
-
-    // following section allows channel to pass parameter/settings information to the portal
-
-    /**
-     * Acquires ChannelSubscriptionProperties from the channel.
-     * This function should be called at the Publishing/Subscription time.
-     * @see ChannelSubscriptionProperties
-     * @deprecated These properties are retrieved from an XML file in uPortal 2.x
-     */
-    public ChannelSubscriptionProperties getSubscriptionProperties();
-
-    /**
-     * Acquires ChannelRuntimeProperites from the channel.
-     * This function may be called by the portal framework throughout the session.
-     * @see ChannelRuntimeProperties
-     */
-    public ChannelRuntimeProperties getRuntimeProperties();
+  /**
+   * Passes ChannelStaticData to the channel.
+   * This is done during channel instantiation time.
+   * see org.jasig.portal.StaticData
+   * @param sd channel static data
+   * @see ChannelStaticData
+   */
+  public void setStaticData (ChannelStaticData sd);
 
 
 
-    // rendering Layer
+  /**
+   * Passes ChannelRuntimeData to the channel.
+   * This function is called prior to the renderXML() call.
+   * @param rd channel runtime data
+   * @see ChannelRuntimeData
+   */
+  public void setRuntimeData (ChannelRuntimeData rd);
 
-    /**
-     * Ask channel to render its content.
-     * @param out the SAX DocumentHandler to output content to
-     */
-    public void renderXML (DocumentHandler out);
 
+
+  /**
+   * Passes an outside event to a channel.
+   * Events should normally come from the LayoutBean.
+   * @param ev LayoutEvent object
+   * @see LayoutEvent
+   */
+  public void receiveEvent (LayoutEvent ev);
+
+
+
+  /**
+   * Acquires ChannelSubscriptionProperties from the channel.
+   * This function should be called at the Publishing/Subscription time.
+   * @see ChannelSubscriptionProperties
+   * @deprecated These properties are retrieved from an XML file in uPortal 2.x
+   */
+  public ChannelSubscriptionProperties getSubscriptionProperties ();
+
+
+
+  /**
+   * Acquires ChannelRuntimeProperites from the channel.
+   * This function may be called by the portal framework throughout the session.
+   * @see ChannelRuntimeProperties
+   */
+  public ChannelRuntimeProperties getRuntimeProperties ();
+
+
+
+  /**
+   * Ask channel to render its content.
+   * @param out the SAX DocumentHandler to output content to
+   */
+  public void renderXML (DocumentHandler out) throws PortalException;
 }
+
+
+
