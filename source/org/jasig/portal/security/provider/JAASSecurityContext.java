3ae3f59295deb24356f1f6873982e390b4935357
Susan Bramhall <susan.bramhall@yale.edu>
2001-08-10 21:06:38 +0000

    Nathan Jacobs' JAAS Security Provider added
    git-svn-id: https://source.jasig.org/uPortal/trunk@4749 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/security/provider/JAASSecurityContext.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2001 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.jasig.portal.security.provider;
+
+import org.jasig.portal.security.*;
+import org.jasig.portal.Logger;
+import org.jasig.portal.RdbmServices;
+import java.util.*;
+import java.sql.*;
+
+import javax.security.auth.*;
+import javax.security.auth.callback.*;
+import javax.security.auth.login.*;
+import org.jasig.portal.security.provider.JAASInlineCallbackHandler;
+
+/**
+ * <p>This is an implementation of a SecurityContext that checks a user's
+ * credentials using JAAS.
+ *
+ * @author Nathan Jacobs
+ * @version $Revision$
+ *
+ */
+
+class JAASSecurityContext extends ChainingSecurityContext implements ISecurityContext {
+
+  private final int JAASSECURITYAUTHTYPE = 0xFF05;
+
+  JAASSecurityContext() {
+    super();
+  }
+
+  public int getAuthType() {
+    return this.JAASSECURITYAUTHTYPE;
+  }
+
+  public synchronized void authenticate() throws PortalSecurityException {
+    this.isauth = false;
+    RdbmServices rdbmservices = new RdbmServices();
+
+    if (this.myPrincipal.UID != null && this.myOpaqueCredentials.credentialstring != null) {
+
+      Connection conn = null;
+      PreparedStatement stmt = null;
+      ResultSet rset = null;
+      String first_name = null, last_name = null;
+      int globalUID;
+
+      try {
+        String query = "SELECT ID, FIRST_NAME, LAST_NAME FROM PORTAL_USERS WHERE " +
+                       "PORTAL_USERS.USER_NAME = ?";
+
+        conn = rdbmservices.getConnection();
+        stmt = conn.prepareStatement(query);
+        stmt.setString(1, this.myPrincipal.UID);
+        rset = stmt.executeQuery();
+
+        if (rset.next()) {
+
+          globalUID  = rset.getInt("ID");
+          first_name = rset.getString("FIRST_NAME");
+          last_name  = rset.getString("LAST_NAME");
+
+          boolean isAuthenticated = false;
+
+          // JAAS Stuff
+
+          LoginContext lc = null;
+
+          lc = new LoginContext("uPortal",
+                   new JAASInlineCallbackHandler(
+                               this.myPrincipal.UID,
+                               (new String(this.myOpaqueCredentials.credentialstring)).toCharArray())); // could not come up w/ a better way to do this
+
+          lc.login();
+
+          // the above will throw an exception if authentication does not succeed
+
+          this.myPrincipal.globalUID = globalUID;
+          this.myPrincipal.FullName = first_name + " " + last_name;
+          Logger.log(Logger.INFO, "User " + this.myPrincipal.UID + " is authenticated");
+          this.isauth = true;
+
+        } else {
+          Logger.log(Logger.INFO, "No such user: " + this.myPrincipal.UID);
+        }
+      } catch (SQLException e) {
+        Logger.log(Logger.ERROR, new PortalSecurityException ("error"));
+      } catch (LoginException e) {
+        Logger.log(Logger.INFO, "User " + this.myPrincipal.UID + ": invalid password");
+        Logger.log(Logger.DEBUG,"LoginException: " + e.getMessage());
+      } finally {
+        try { rset.close(); } catch (Exception e) { }
+        try { stmt.close(); } catch (Exception e) { }
+        rdbmservices.releaseConnection(conn);
+      }
+    } else {
+      Logger.log (Logger.ERROR, "Principal or OpaqueCredentials not initialized prior to authenticate");
+    }
+
+    // authenticate all subcontexts.
+    super.authenticate();
+
+    return;
+  }
+}
