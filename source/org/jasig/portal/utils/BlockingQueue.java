166775444b61c1db731b4f5a5f545df92a886e58
peterk@committers.jasig.org <peterk@committers.jasig.org>
2001-08-13 06:39:35 +0000

    thread-pool related stuff.
    git-svn-id: https://source.jasig.org/uPortal/trunk@4750 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/utils/BlockingQueue.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or withoutu
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+package  org.jasig.portal.utils;
+
+import java.util.LinkedList;
+
+/**
+ * A simple FIFO queue that has MIN/MAX capacity and
+ * that blocks if either enqueue/dequeue would result
+ * in violation of these limits.
+ * Default values for min/max are 0/infinite
+ * @author Peter Kharchenko
+ */
+
+public class BlockingQueue
+{
+    int maxSize;
+    int minSize;
+    
+    volatile LinkedList queue = null;
+
+    BlockingQueue()
+    {
+	maxSize=-1;
+	minSize=0;
+        queue = new LinkedList();
+    }
+
+    /**
+     * Construct a new blocking queue with predefined max/min limits
+     */
+    BlockingQueue(int min, int max)
+    {
+	this();
+	maxSize=max;
+	minSize=min;
+    }
+
+    BlockingQueue(int max) {
+	this(0,max);
+    }
+
+    /**
+     * Add new object to the end of the queue
+     * @param o object to be placed on the queue
+     */
+    public synchronized void enqueue( Object o ) throws InterruptedException
+    {
+	//    	while((queue.size()>=maxSize) || maxSize!=-1) {
+    	while(queue.size()>=maxSize && maxSize!=-1) {
+	    wait();
+	}
+	queue.addLast(o);
+	notifyAll();
+    }
+
+    /**
+     * Remove object from the beginning of the queue
+     * @throws <code>InterruptedException</code> if the wait was interrupted
+     */
+    public synchronized Object dequeue() throws InterruptedException
+    {
+    	while(queue.size()<=minSize) {
+	    wait();
+	}
+	notifyAll();
+        return queue.removeFirst();
+    }
+
+    /**
+     * Set the queue limits. 
+     * To specify a queue without an upper bound (that is max=inifinity) use max value of -1
+     */
+    public synchronized void setLimits(int max, int min) {
+	maxSize=max; minSize=min;
+	notifyAll();
+    }
+    
+    public int getMaxSize() { return maxSize; }
+    public int getMinSize() { return minSize; }
+    public synchronized void setMaxSize(int max) { maxSize=max; notifyAll();}
+    public synchronized void setMinSize(int min) { minSize=min; notifyAll(); }
+    public synchronized int size() { return queue.size(); }
+}
---
+++ source/org/jasig/portal/utils/ResourceLimits.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or withoutu
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+package  org.jasig.portal.utils;
+
+/**
+ * Basic resource limits
+ * Limits include : hard upper/lower limits, desired size,
+ * activeLimit, update time and prune factor.
+ * @author Peter Kharchenko <a href="mailto:">pkharchenko@interactivebusiness.com</a>
+ */
+public class ResourceLimits 
+{
+    public int maxSize=-1;
+    public int minSize=0;
+    public int optimalSize= 1;
+
+    //    public int maxActiveSize = -1;
+    
+    public int updateTime = 10000;
+    public float pruneFactor= 0.1F;
+
+    public ResourceLimits(){}
+}
