ebb4f1b85257266179983b90c6287e55d4908874
Ken Weiner <kweiner@gmail.com>
2001-08-28 19:03:21 +0000

    New general portal properties file manager
    git-svn-id: https://source.jasig.org/uPortal/trunk@4902 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/PropertiesManager.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package  org.jasig.portal;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import org.jasig.portal.services.LogService;
+import org.jasig.portal.UtilitiesBean;
+
+/**
+ * Provides access to properties
+ * @author Ken Weiner, kweiner@interactivebusiness.com
+ * @version $Revision$
+ */
+public class PropertiesManager {
+
+  private static final String PORTAL_PROPERTIES_FILE_NAME = UtilitiesBean.getPortalBaseDir() + "properties" + File.separator + "portal.properties";
+  private static final Properties props = new Properties();
+  
+  static {
+    loadProps();
+  }
+
+  /**
+   * Load up the portal properties.  Right now the portal properties is a simple
+   * .properties file with name value pairs.  It may evolve to become an XML file
+   * later on.
+   */
+  protected static void loadProps () {
+    try {
+      File propsFile = new File(PORTAL_PROPERTIES_FILE_NAME);
+      props.load(new FileInputStream(propsFile));
+    } catch (FileNotFoundException fnfe) {
+      LogService.instance().log(LogService.ERROR, "Unable to open portal.properties file.");
+      LogService.instance().log(LogService.ERROR, fnfe);
+    } catch (IOException ioe) {
+      LogService.instance().log(LogService.ERROR, "Unable to read portal.properties file.");
+      LogService.instance().log(LogService.ERROR, ioe);
+    }    
+  }
+  
+  /**
+   * Returns the value of a property for a given name.
+   * @param name the name of the requested property
+   * @return value the value of the property matching the requested name, <code>null</code> if proeprty doesn't exist
+   */
+  public static String getProperty(String name) {
+    return props.getProperty(name);
+  }
+  
+  /**
+   * Returns the value of a property for a given name.
+   * This method can be used if the property is boolean in
+   * nature and you want to make sure that <code>true</code> is
+   * returned if the property is set to "true", "yes", "y", or "on"
+   * (regardless of case),
+   * and <code>false</code> is returned in all other cases.
+   * @param name the name of the requested property
+   * @return value <code>true</code> if property is set to "true", "yes", "y", or "on" regardless of case, otherwise <code>false</code>
+   */
+  public static boolean getBooleanProperty(String name) {
+    boolean retValue = false;
+    String value = props.getProperty(name);
+    if (value != null) {
+      if (value.equalsIgnoreCase("true") ||
+          value.equalsIgnoreCase("yes") ||
+          value.equalsIgnoreCase("y") ||
+          value.equalsIgnoreCase("on"))
+        retValue = true;
+    }
+    return retValue;
+  }  
+}
+
+
+
