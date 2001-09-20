938eaf7c7a35b3c32e109ad4a29119fcf8525841
Ken Weiner <kweiner@gmail.com>
2001-09-20 06:34:07 +0000

    New class to manage the channel registry.
    git-svn-id: https://source.jasig.org/uPortal/trunk@4989 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/ChannelRegistryManager.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package  org.jasig.portal;
+
+import org.jasig.portal.services.LogService;
+import org.jasig.portal.utils.SmartCache;
+import org.w3c.dom.Document;
+import java.sql.SQLException;
+
+/**
+ * Manages the channel registry which is a listing of published channels
+ * that one can subscribe to (add to their layout).
+ * @author Ken Weiner, kweiner@interactivebusiness.com
+ * @version $Revision$
+ */
+public class ChannelRegistryManager {
+  protected static final IChannelRegistryStore chanRegStore = RdbmServices.getChannelRegistryStoreImpl();
+  protected static final int registryCacheTimeout = PropertiesManager.getPropertyAsInt("org.jasig.portal.ChannelRegistryManager.channel_registry_cache_timeout");
+  protected static final SmartCache channelRegistryCache = new SmartCache(registryCacheTimeout);
+  
+  /**
+   * Returns the channel registry as a Document.  This list is not filtered by roles.
+   * @return the channel registry as a Document
+   */
+  public static Document getChannelRegistry() throws PortalException {
+    Document channelRegistry = (Document)channelRegistryCache.get("channelRegistry");
+    if (channelRegistry == null)
+    {
+      // Channel registry has expired, so get it and cache it
+      try {
+        channelRegistry = chanRegStore.getChannelRegistryXML();
+      } catch (SQLException sqle) {
+        throw new GeneralRenderingException(sqle.getMessage());
+      }
+
+      if (channelRegistry != null)
+      {
+        channelRegistryCache.put("channelRegistry", channelRegistry);
+        LogService.instance().log(LogService.INFO, "Caching channel registry.");
+      }
+    }
+    return channelRegistry;    
+  }         
+}
+
+
+
+
+
+
