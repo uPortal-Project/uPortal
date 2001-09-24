f76b9266d4d5eba66002a4d3507d288bf52debf7
Ken Weiner <kweiner@gmail.com>
2001-09-24 19:06:24 +0000

    Enabled a few more screens.
    git-svn-id: https://source.jasig.org/uPortal/trunk@5008 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/channels/CChannelManager.java
+++ source/org/jasig/portal/channels/CChannelManager.java
@@ -4,13 +4,16 @@ import org.jasig.portal.ChannelRuntimeData;
 import org.jasig.portal.UtilitiesBean;
 import org.jasig.portal.PortalException;
 import org.jasig.portal.GeneralRenderingException;
+import org.jasig.portal.ResourceMissingException;
 import org.jasig.portal.ChannelRegistryManager;
 import org.jasig.portal.channels.BaseChannel;
 import org.jasig.portal.utils.XSLT;
 import org.jasig.portal.factories.DocumentFactory;
 import org.xml.sax.DocumentHandler;
+import org.w3c.dom.Node;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Text;
 import java.sql.SQLException;
 
 
@@ -19,7 +22,11 @@ public class CChannelManager extends BaseChannel {
   protected static final Document emptyDoc = DocumentFactory.getNewDocument();
   protected short state;
   protected static final short DEFAULT_STATE = 0;
-  protected static final short MODIFY_CHANNEL_STATE = 1;
+  protected static final short CHANNEL_TYPE_STATE = 1;
+  protected static final short GENERAL_SETTINGS_STATE = 2;
+  protected static final short CHANNEL_DEF_STATE = 3;
+  protected static final short CHANNEL_CONTROLS_STATE = 4;  
+  protected static final short MODIFY_CHANNEL_STATE = 5;
   protected Document channelManagerDoc;
   protected UserSettings userSettings = new UserSettings();
   
@@ -27,7 +34,19 @@ public class CChannelManager extends BaseChannel {
     runtimeData = rd;
     String action = runtimeData.getParameter("action");
     if (action != null) {
-      if (action.equals("selectModifyChannel")) {
+      if (action.equals("selectChannelType")) {
+        state = CHANNEL_TYPE_STATE;
+        channelManagerDoc = getChannelManagerDoc();
+      } else if (action.equals("selectGeneralSettings")) {
+        state = GENERAL_SETTINGS_STATE;
+        channelManagerDoc = getChannelManagerDoc();              
+      } else if (action.equals("channelDef")) {
+        state = CHANNEL_DEF_STATE;
+        channelManagerDoc = getChannelManagerDoc();              
+      } else if (action.equals("selectControls")) {
+        state = CHANNEL_CONTROLS_STATE;
+        channelManagerDoc = getChannelManagerDoc();              
+      } else if (action.equals("selectModifyChannel")) {
         state = MODIFY_CHANNEL_STATE;
         channelManagerDoc = getChannelManagerDoc(userSettings);              
       } else if (action.equals("changePage")) {
@@ -64,18 +83,32 @@ public class CChannelManager extends BaseChannel {
     xslt.setTarget(out);
     xslt.setStylesheetParameter("baseActionURL", runtimeData.getBaseActionURL());
     
+    String action = null;
     switch (state) {
       case DEFAULT_STATE:
-        xslt.setStylesheetParameter("action", "none");
+        action = "none";
         break;
+      case CHANNEL_TYPE_STATE:
+        action = "selectChannelType";
+        break;
+      case GENERAL_SETTINGS_STATE:
+        action = "selectGeneralSettings";
+        break;        
+      case CHANNEL_DEF_STATE:
+        action = "channelDef";
+        break;        
+      case CHANNEL_CONTROLS_STATE:
+        action = "selectControls";
+        break;        
       case MODIFY_CHANNEL_STATE:
-        xslt.setStylesheetParameter("action", "selectModifyChannel");
+        action = "selectModifyChannel";
         break;
       default:
-        xslt.setStylesheetParameter("action", "none");
+        action = "none";
         break;
     }
     
+    xslt.setStylesheetParameter("action", action);
     xslt.transform();
     
     // Remove this!!!
@@ -86,8 +119,72 @@ public class CChannelManager extends BaseChannel {
     }
   }
   
+  
+  protected Document getChannelManagerDoc() throws PortalException {
+    Document channelManagerDoc = DocumentFactory.getNewDocument();
+    
+    // Add the top level <manageChannels> to the document
+    Element channelManagerE = channelManagerDoc.createElement("manageChannels");
+    channelManagerDoc.appendChild(channelManagerE);
+    
+    // Add <selectChannelType> workflow step
+    Document channelTypesDoc = ChannelRegistryManager.getChannelTypes();
+    Element channelTypes = (Element)channelManagerDoc.importNode(channelTypesDoc.getDocumentElement(), true);
+    appendWorkflowStep(channelManagerE, "selectChannelType", "Channel Types", channelTypes);
+      
+    // Add <selectGeneralSettings> workflow step
+    appendWorkflowStep(channelManagerE, "selectGeneralSettings", "General Settings", null);
+    
+    // Add the channel publishing document (.cpd file)
+    Element cpdE = (Element)channelManagerDoc.importNode(getCPDDoc("1").getDocumentElement(), true);
+    channelManagerE.appendChild(cpdE);
+ 
+    // Add <selectControls> workflow step
+    appendWorkflowStep(channelManagerE, "selectControls", "Channel Controls", null);
+     
+    // Add <selectCategory> workflow step
+    Document channelRegistryDoc = ChannelRegistryManager.getChannelRegistry();
+    Element channelRegistry = (Element)channelManagerDoc.importNode(channelRegistryDoc.getDocumentElement(), true);
+    appendWorkflowStep(channelManagerE, "selectCategory", "Categories", channelRegistry);
+
+    // Add <selectRoles> workflow step
+    appendWorkflowStep(channelManagerE, "selectRoles", "Roles", null);
+
+    // Add <reviewChannel> workflow step
+    appendWorkflowStep(channelManagerE, "reviewChannel", "Review", null);
+
+    return channelManagerDoc;
+  }
+  
+  protected void appendWorkflowStep(Element channelManager, String workflowElement, String stepName, Element insideElement) {
+    Document doc = channelManager.getOwnerDocument();
+    
+    // Add this step's workflow element
+    Element worflowElementE = doc.createElement(workflowElement);
+    channelManager.appendChild(worflowElementE);
+    
+    // Add the <params>, <step>, <ID>, and <name> elements
+    Element paramsE = doc.createElement("params");
+    worflowElementE.appendChild(paramsE);
+    Element stepE = doc.createElement("step");
+    paramsE.appendChild(stepE);
+    Element IDE = doc.createElement("ID");
+    IDE.appendChild(doc.createTextNode("1"));
+    stepE.appendChild(IDE);
+    Element nameE = doc.createElement("name");
+    nameE.appendChild(doc.createTextNode(stepName));
+    stepE.appendChild(nameE);
+    
+    if (insideElement != null)
+      stepE.appendChild(insideElement);
+  }
+  
   protected Document getChannelManagerDoc(UserSettings userSettings) throws PortalException {
     Document channelManagerDoc = DocumentFactory.getNewDocument();
+
+    // Add the top level <manageChannels> to the document
+    Element channelManager = channelManagerDoc.createElement("manageChannels");
+    channelManagerDoc.appendChild(channelManager);
 
     // Get the channel registry
     Document channelRegistryDoc = ChannelRegistryManager.getChannelRegistry();
@@ -96,15 +193,11 @@ public class CChannelManager extends BaseChannel {
     Element registry = channelRegistryDoc.getDocumentElement();
     registry.setAttribute("ID", "-1");
 
-    // Add the top level <channelManager> to the document
-    Element channelManager = channelManagerDoc.createElement("channelManager");
-    channelManagerDoc.appendChild(channelManager);
-
-    // Add the <registry> to the <channelManager>
+    // Add the <registry> to <manageChannels>
     Element channelRegistry = (Element)channelManagerDoc.importNode(channelRegistryDoc.getDocumentElement(), true);
     channelManager.appendChild(channelRegistry);
 
-    // Add a <userSettings> fragment to the <channelManager>
+    // Add a <userSettings> fragment to <manageChannels>
     appendUserSettings(channelManager, userSettings);   
     
     return channelManagerDoc;
@@ -127,7 +220,46 @@ public class CChannelManager extends BaseChannel {
     channelManager.appendChild(userSettingsE);
   }
   
-  
+  protected static Document getCPDDoc(String chanTypeID) throws PortalException {
+    Element channelTypes = ChannelRegistryManager.getChannelTypes().getDocumentElement();
+    
+    // Look for channel type element matching the channel type ID
+    Element chanType = null;
+    for (Node n = channelTypes.getFirstChild(); n != null; n = n.getNextSibling()) {
+      if (n.getNodeType() == Node.ELEMENT_NODE && n.getNodeName().equals("channelType")) {
+        chanType = (Element)n;
+        if (chanTypeID.equals(chanType.getAttribute("ID")))
+          break;
+      }
+    }
+    
+    // Find the cpd-uri within this element
+    String cpdUri = null;
+    for (Node n = chanType.getLastChild(); n != null; n = n.getPreviousSibling()) { 
+      if (n.getNodeType() == Node.ELEMENT_NODE && n.getNodeName().equals("cpd-uri")) {
+        // Found the <cpd-uri> element, now get its value
+        for (Node m = n.getFirstChild(); m != null; m = m.getNextSibling()) {
+          if (m instanceof Text)
+            cpdUri = m.getNodeValue();
+        }
+        break;
+      }
+    }
+    
+    Document cpdDoc = null;
+    if (cpdUri != null) {
+      try {
+        org.apache.xerces.parsers.DOMParser parser = new org.apache.xerces.parsers.DOMParser();
+        parser.parse(UtilitiesBean.fixURI(cpdUri));
+        cpdDoc = parser.getDocument();
+      } catch (java.io.IOException ioe) {
+        throw new ResourceMissingException(cpdUri, "Channel publishing document", ioe.getMessage());
+      } catch (org.xml.sax.SAXException se) {
+        throw new GeneralRenderingException("Unable to parse CPD file: " + se.getMessage());
+      }
+    }
+    return cpdDoc;
+  }
   
   
   
@@ -149,6 +281,11 @@ public class CChannelManager extends BaseChannel {
     // Getting the channel registry...
     Document channelRegistry = chanReg.getChannelRegistryXML();
     //System.out.println(UtilitiesBean.dom2PrettyString(channelRegistry));
+    
+    // Getting the CPDDoc...
+    Document cpdDoc = getCPDDoc("3");
+    System.out.println(UtilitiesBean.dom2PrettyString(cpdDoc));
+    
   }
   
   /**
@@ -174,6 +311,36 @@ public class CChannelManager extends BaseChannel {
     protected void setCurrentPage(String currentPage) { this.currentPage = currentPage; }
     protected void setFilterByID(String filterByID) { this.filterByID = filterByID; }    
   }
+  
+  /*
+   * Channel Types
+   *   Channel types
+   * General settings
+   *   CPD
+   *   Channel XML
+   * CPD steps
+   *   CPD
+   *   Channel XML
+   * Channel controls
+   *   CPD
+   *   Channel XML
+   * Categories
+   *   CPD
+   *   Channel Registry
+   *   Browsed category/selected category
+   * Roles
+   *   CPD
+   *   Roles
+   * Review
+   *   CPD
+   *   Channel XML
+   *   Selected categories
+   *   Channel Registry
+   *   Selected Roles
+   *   Channel types
+   *   
+   * 
+   */
 }
 
 
