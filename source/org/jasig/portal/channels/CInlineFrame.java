353dbd824c91553e57bc9d9383547cf0cb24d48b
Ken Weiner <kweiner@gmail.com>
2001-10-31 22:20:03 +0000

    Changed CIFrame to be multithreaded and cacheable. Changed XML from a String to a Document.
    git-svn-id: https://source.jasig.org/uPortal/trunk@5263 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/channels/CInlineFrame.java
+++ source/org/jasig/portal/channels/CInlineFrame.java
@@ -31,22 +31,26 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *
- * formatted with JxBeauty (c) johann.langhofer@nextra.at
  */
 
 
-package  org.jasig.portal.channels;
+package org.jasig.portal.channels;
 
-import  org.jasig.portal.*;
-import  java.io.IOException;
-import  java.util.Hashtable;
-import  java.net.URL;
-import  java.net.MalformedURLException;
-import  org.xml.sax.ContentHandler;
-import  org.xml.sax.SAXException;
-import  org.jasig.portal.utils.XSLT;
-import  org.jasig.portal.services.LogService;
+import org.jasig.portal.PortalException;
+import org.jasig.portal.GeneralRenderingException;
+import org.jasig.portal.ChannelCacheKey;
+import org.jasig.portal.ChannelStaticData;
+import org.jasig.portal.ChannelRuntimeData;
+import org.jasig.portal.IMultithreadedCacheable;
+import org.jasig.portal.UtilitiesBean;
+import org.jasig.portal.utils.XSLT;
+import org.jasig.portal.services.LogService;
+import org.xml.sax.ContentHandler;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 
 /**
@@ -60,34 +64,49 @@ import  org.jasig.portal.services.LogService;
  * @author Susan Bramhall
  * @version $Revision$
  */
-public class CInlineFrame extends BaseChannel {
-  protected String srcUrl;      // the url for the IFrame content
-  protected String frameHeight; // the height of the IFrame in pixels
-
+public class CInlineFrame extends BaseMultithreadedChannel implements IMultithreadedCacheable {
   private static final String sslLocation = UtilitiesBean.fixURI("webpages/stylesheets/org/jasig/portal/channels/CInlineFrame/CInlineFrame.ssl");
 
   /**
-   * Get channel parameters: url, height and name
-   */
-  public void setStaticData (ChannelStaticData sd) {
-    this.srcUrl = sd.getParameter("url");
-    this.frameHeight = sd.getParameter("height");
-  }
-
-  /**
-   * Build an XML string and transform for display using org.jasig.portal.util.XSLT
+   * Build an XML document and transform for display using org.jasig.portal.util.XSLT
    * Creates IFrame or link depending on browser capability.
+   * The XML will look something like this:
+   *
+   * <iframe>
+   *   <url>http://blah.blah.blah</url>
+   *    <height>600</height>
+   * </iframe>
    */
-  public void renderXML (ContentHandler out) throws PortalException {
-    StringBuffer sbXML = new StringBuffer("<?xml version=\"1.0\"?>");
-    sbXML.append("<iframe>");
-    sbXML.append("  <url>").append(srcUrl).append("</url>");
-    sbXML.append("  <height>").append(frameHeight).append("</height>");
-    sbXML.append("</iframe>");
+  public void renderXML (ContentHandler out, String uid) throws PortalException {
+    ChannelState channelState = (ChannelState)channelStateMap.get(uid);
+    ChannelStaticData staticData = channelState.getStaticData();
+    ChannelRuntimeData runtimeData = channelState.getRuntimeData();
+
+    // Obtain url and height, both static parameters
+    String srcUrl = staticData.getParameter("url"); // the url for the IFrame content
+    String frameHeight = staticData.getParameter("height"); // the height of the IFrame in pixels
+
+    Document doc = null;
+    try {
+      doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+    } catch (ParserConfigurationException pce) {
+      LogService.log(LogService.ERROR, pce);
+      throw new GeneralRenderingException(pce.getMessage());
+    }
+
+    // Create XML doc
+    Element iframeE = doc.createElement("iframe");
+    Element urlE = doc.createElement("url");
+    urlE.appendChild(doc.createTextNode(srcUrl));
+    iframeE.appendChild(urlE);
+    Element heightE = doc.createElement("height");
+    heightE.appendChild(doc.createTextNode(frameHeight));
+    iframeE.appendChild(heightE);
+    doc.appendChild(iframeE);
 
     XSLT xslt = new XSLT();
-    xslt.setXML(sbXML.toString());
-    xslt.setXSL(sslLocation, getStylesheetTitle(), runtimeData.getBrowserInfo());
+    xslt.setXML(doc);
+    xslt.setXSL(sslLocation, getStylesheetTitle(runtimeData.getBrowserInfo().getUserAgent()), runtimeData.getBrowserInfo());
     xslt.setTarget(out);
     xslt.transform();
   }
@@ -95,17 +114,31 @@ public class CInlineFrame extends BaseChannel {
   /**
    * Uses the user agent string to determine which stylesheet title to use.
    * We wouldn't need this method if stylesheet sets could distinguish between browser versions
+   * @param userAgent the user agent string
    * @return ssTitle the stylesheet title
    */
-  private String getStylesheetTitle () {
+  private String getStylesheetTitle (String userAgent) {
     String ssTitle = "noIFrameSupport";
-    String userAgent = runtimeData.getBrowserInfo().getUserAgent();
     if ((userAgent.indexOf("MSIE 3") >= 0) || (userAgent.indexOf("MSIE 4") >= 0) ||
         (userAgent.indexOf("MSIE 5") >= 0) || (userAgent.indexOf("MSIE 6") >= 0) ||
         (userAgent.indexOf("Mozilla/5") >= 0)) {
       ssTitle = "IFrameSupport";
     }
     return  ssTitle;
+  }
+
+  // IMultithreadedCachable methods...
+
+  public ChannelCacheKey generateKey(String uid) {
+    ChannelCacheKey key = new ChannelCacheKey();
+    key.setKey("org.jasig.portal.channels.CInlineFrame");
+    key.setKeyScope(ChannelCacheKey.SYSTEM_KEY_SCOPE);
+    key.setKeyValidity(null);
+    return key;
+  }
+
+  public boolean isCacheValid(Object validity, String uid) {
+    return true;
   }
 }
 
