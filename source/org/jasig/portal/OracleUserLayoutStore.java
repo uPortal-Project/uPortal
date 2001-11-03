fec119671dff4070f4c12fc3de3918af743fbe62
George.Lindholm@committers.jasig.org <George.Lindholm@committers.jasig.org>
2001-11-03 00:11:59 +0000

    Support for outer joins now in the base class
    git-svn-id: https://source.jasig.org/uPortal/trunk@5307 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/OracleUserLayoutStore.java
+++ source/org/jasig/portal/OracleUserLayoutStore.java
@@ -54,92 +54,9 @@ import  org.apache.xerces.dom.DocumentImpl;
  * have been created by hand before uPortal is started. See UP_SEQUENCE in properties/data.xml
  * for the tables that expect sequence counters, and the expected starting value.
  */
-public class OracleUserLayoutStore extends RDBMUserLayoutStore
+public final class OracleUserLayoutStore extends RDBMUserLayoutStore
     implements IUserLayoutStore {
 
-  /**
-   * put your documentation comment here
-   * @param con
-   * @param doc
-   * @param chanId
-   * @param idTag
-   * @return
-   * @exception java.sql.SQLException
-   */
-  protected Element createChannelNode (Connection con, DocumentImpl doc, int chanId, String idTag) throws java.sql.SQLException {
-    Element channel = null;
-    String sQuery = "SELECT UC.*, CHAN_PARM_NM, CHAN_PARM_VAL,CHAN_PARM_OVRD,CHAN_PARM_DESC FROM UP_CHANNEL UC, UP_CHAN_PARAM UCP WHERE UC.CHAN_ID="
-        + chanId + " AND UC.CHAN_ID = UCP.CHAN_ID(+)";
-    Logger.log(Logger.DEBUG, "OracleUserLayoutStore::createChannelNode(): " + sQuery);
-    Statement stmt = con.createStatement();
-    try {
-      ResultSet rs = stmt.executeQuery(sQuery);
-      try {
-        if (rs.next()) {
-          channel = doc.createElement("channel");
-          createChannelNodeHeaders(doc, chanId, idTag, rs, channel);
-          do {
-            createChannelNodeParameters(doc, rs, channel);
-          } while (rs.next());
-          rs.close();
-        }
-      } finally {
-        rs.close();
-      }
-    } finally {
-      stmt.close();
-    }
-    return  channel;
-  }
-
-  /**
-   * put your documentation comment here
-   * @param con
-   * @param doc
-   * @param stmt
-   * @param root
-   * @param userId
-   * @param profileId
-   * @param layoutId
-   * @param structId
-   * @exception java.sql.SQLException
-   */
-  protected void createLayout (Connection con, DocumentImpl doc, Statement stmt, Element root, int userId, int profileId,
-      int layoutId, int structId) throws java.sql.SQLException {
-    if (structId == 0) {        // End of line
-      return;
-    }
-    int nextStructId;
-    int chldStructId;
-    int chanId;
-    Element system = null;
-    Element parameter = null;
-    Element structure;
-    String sQuery = "SELECT ULS.*,STRUCT_PARM_NM,STRUCT_PARM_VAL FROM UP_LAYOUT_STRUCT ULS, UP_STRUCT_PARAM USP WHERE ULS.USER_ID="
-        + userId + " AND ULS.LAYOUT_ID = " + layoutId + " AND ULS.STRUCT_ID=" + structId + " AND ULS.STRUCT_ID = USP.STRUCT_ID(+)";
-    Logger.log(Logger.DEBUG, "OracleUserLayoutStore::createLayout(): " + sQuery);
-    ResultSet rs = stmt.executeQuery(sQuery);
-    try {
-      rs.next();
-      nextStructId = rs.getInt("NEXT_STRUCT_ID");
-      chldStructId = rs.getInt("CHLD_STRUCT_ID");
-      chanId = rs.getInt("CHAN_ID");
-      structure = createLayoutStructure(rs, chanId, userId, con, doc);
-      if (chanId != 0) {        // Channel
-        parameter = (Element)structure.getElementsByTagName("parameter").item(0);
-      }
-      do {
-        createLayoutStructureParameter(chanId, rs, structure, parameter);
-      } while (rs.next());
-    } finally {
-      rs.close();
-    }
-    root.appendChild(structure);
-    if (chanId == 0) {          // Folder
-      createLayout(con, doc, stmt, structure, userId, profileId, layoutId, chldStructId);
-    }
-    createLayout(con, doc, stmt, root, userId, profileId, layoutId, nextStructId);
-  }
 
   /* DBCounter */
   /*
