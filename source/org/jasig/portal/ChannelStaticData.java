1e3b04f14685bf20527cfc9dddecf0efa6428ca3
Dan Ellentuck <de3@columbia.edu>
2002-01-07 20:51:38 +0000

    use groups/permissions types
    git-svn-id: https://source.jasig.org/uPortal/trunk@5652 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/ChannelStaticData.java
+++ source/org/jasig/portal/ChannelStaticData.java
@@ -1,47 +1,11 @@
-/**
- * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. Redistributions of any form whatsoever must retain the following
- *    acknowledgment:
- *    "This product includes software developed by the JA-SIG Collaborative
- *    (http://www.jasig.org/)."
- *
- * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *
- * formatted with JxBeauty (c) johann.langhofer@nextra.at
- */
-
-
-package  org.jasig.portal;
+package org.jasig.portal;
 
 import  java.util.Map;
 import  org.jasig.portal.security.IPerson;
 import  org.jasig.portal.security.ISecurityContext;
-import  org.jasig.portal.security.PermissionManager;
+import  org.jasig.portal.security.IAuthorizationPrincipal;
+import  org.jasig.portal.services.AuthorizationService;
+// import  org.jasig.portal.security.PermissionManager;
 import  org.jasig.portal.services.LogService;
 import  javax.naming.Context;
 import  java.util.Hashtable;
@@ -66,18 +30,54 @@ public class ChannelStaticData extends Hashtable {
   // Cache the security context
   private ISecurityContext m_securityContext = null;
   // Cache the PermissionManager for this channel
-  private PermissionManager m_permissionManager = null;
+  //  private PermissionManager m_permissionManager = null;
 
   /**
-   * Set information contained in a channel <param> element
-   * Parameters are strings!
-   * @param key param name
-   * @param value param value
+   * Similar to the {@link #getParameter(String)}, but can be used to pass things other then strings.
+   * @param key
+   * @return
    */
-  public synchronized String setParameter (String key, String value) {
-    return  (String)super.put(key, value);
+  public synchronized Object get (Object key) {
+    return  super.get(key);
   }
-
+  /**
+   * Returns an instance of the IAuthorizationPrincipal for the IPerson
+   * @return
+   */
+  public IAuthorizationPrincipal getAuthorizationPrincipal () {
+    String key = "" + getPerson().getID();
+    Class type = org.jasig.portal.security.IPerson.class;
+    IAuthorizationPrincipal ap = null;
+    try
+    {
+      ap = AuthorizationService.instance().newPrincipal(key, type);
+    }
+    catch (AuthorizationException ae)
+    {
+        LogService.instance().log(LogService.ERROR, "Could not get authorization service: " + ae);
+    }
+    return ap;
+  }
+  /**
+   * put your documentation comment here
+   */
+  public String getChannelGlobalID () {
+    return  (m_channelGlobalID);
+  }
+  /**
+   * Gets the channel ID
+   * @return the channel's ID
+   */
+  public String getChannelID () {
+    return  (m_channelInstanceID);
+  }
+  /**
+   * Obtain a channel JNDI context
+   * @return JNDI context
+   */
+    public Context getJNDIContext() {
+        return m_portalContext;
+    }
   /**
    * Get information contained in a particular <param> element
    * @param key param name
@@ -86,7 +86,20 @@ public class ChannelStaticData extends Hashtable {
   public synchronized String getParameter (String key) {
     return  (String)super.get(key);
   }
-
+  /**
+   * put your documentation comment here
+   * @return
+   */
+  public IPerson getPerson () {
+    return  (m_person);
+  }
+  /**
+   * put your documentation comment here
+   * @return
+   */
+  public long getTimeout () {
+    return  (m_timeout);
+  }
   /**
    * If you need to pass Objects, use this
    * Similar to the {@link #setParameter(String,String)}, but can be used to pass things other then strings.
@@ -97,41 +110,6 @@ public class ChannelStaticData extends Hashtable {
   public synchronized Object put (Object key, Object value) {
     return  super.put(key, value);
   }
-
-  /**
-   * Similar to the {@link #getParameter(String)}, but can be used to pass things other then strings.
-   * @param key
-   * @return
-   */
-  public synchronized Object get (Object key) {
-    return  super.get(key);
-  }
-
-  /**
-   * Copy parameter list from a Map
-   * @param params a map of params
-   */
-  public void setParameters (Map params) {
-    // Copy the map
-    putAll(params);
-  }
-
-  /**
-   * Sets the channel instance ID
-   * @param sChannelID the unique channelID
-   */
-  public void setChannelID (String sChID) {
-    m_channelInstanceID = sChID;
-  }
-
-  /**
-   * Gets the channel ID
-   * @return the channel's ID
-   */
-  public String getChannelID () {
-    return  (m_channelInstanceID);
-  }
-
   /**
    * put your documentation comment here
    * @param   String channelGlobalID
@@ -139,54 +117,13 @@ public class ChannelStaticData extends Hashtable {
   public void setChannelGlobalID (String channelGlobalID) {
     m_channelGlobalID = channelGlobalID;
   }
-
   /**
-   * put your documentation comment here
+   * Sets the channel instance ID
+   * @param sChannelID the unique channelID
    */
-  public String getChannelGlobalID () {
-    return  (m_channelGlobalID);
+  public void setChannelID (String sChID) {
+    m_channelInstanceID = sChID;
   }
-
-  /**
-   * put your documentation comment here
-   * @param value
-   */
-  public void setTimeout (long value) {
-    m_timeout = value;
-  }
-
-  /**
-   * put your documentation comment here
-   * @return
-   */
-  public long getTimeout () {
-    return  (m_timeout);
-  }
-
-  /**
-   * put your documentation comment here
-   * @return
-   */
-  public IPerson getPerson () {
-    return  (m_person);
-  }
-
-  /**
-   * put your documentation comment here
-   * @param per
-   */
-  public void setPerson (IPerson person) {
-    m_person = person;
-  }
-
-  /**
-   * Obtain a channel JNDI context
-   * @return JNDI context
-   */
-    public Context getJNDIContext() {
-        return m_portalContext;
-    }
-
     /**
      * Set channel JNDI context.
      *
@@ -195,23 +132,35 @@ public class ChannelStaticData extends Hashtable {
     public void setJNDIContext(Context c) {
         m_portalContext=c;
     }
-
   /**
-   * Returns an instance of the PermissionManager for this channel
-   * @return
+   * Set information contained in a channel <param> element
+   * Parameters are strings!
+   * @param key param name
+   * @param value param value
    */
-  public PermissionManager getPermissionManager () {
-    return  (m_permissionManager);
+  public synchronized String setParameter (String key, String value) {
+    return  (String)super.put(key, value);
   }
-
   /**
-   * Sets the PermissionManager that belongs to this channel
-   * @param permissionManager
+   * Copy parameter list from a Map
+   * @param params a map of params
    */
-  public void setPermissionManager (PermissionManager permissionManager) {
-    m_permissionManager = permissionManager;
+  public void setParameters (Map params) {
+    // Copy the map
+    putAll(params);
+  }
+  /**
+   * put your documentation comment here
+   * @param per
+   */
+  public void setPerson (IPerson person) {
+    m_person = person;
+  }
+  /**
+   * put your documentation comment here
+   * @param value
+   */
+  public void setTimeout (long value) {
+    m_timeout = value;
   }
 }
-
-
-
