e2b8dc31b244d5d6307180ef7673ddde4e15ae62
peterk@committers.jasig.org <peterk@committers.jasig.org>
2002-02-08 15:47:36 +0000

    cleaned up javadocs
    git-svn-id: https://source.jasig.org/uPortal/trunk@5873 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/ChannelRuntimeProperties.java
+++ source/org/jasig/portal/ChannelRuntimeProperties.java
@@ -42,27 +42,38 @@ package org.jasig.portal;
  *  <li> willRender  - a boolean flag signaling if the channel will render at all in the current state. This flag will be checked after the ChannelRuntimeData has been passed to a channel, but prior to the renderXML() call.</li>
  * </ul>
  * @version $Revision$
- * @author Peter Kharchenko
+ * @author <a href="mailto:pkharchenko@interactivebusiness.com">Peter Kharchenko</a>
  */
 public class ChannelRuntimeProperties
 {
   private boolean bool_willRender;
 
-  public ChannelRuntimeProperties ()
-  {
-          // set the default values here
-    bool_willRender = true;
-  }
+    /**
+     * Default constructor.
+     * willRender property is initialized to <code>true</code>.
+     *
+     */
+    public ChannelRuntimeProperties() {
+      // set the default values here
+      bool_willRender = true;
+    }
 
-  // the set functions ...
-  public void setWillRender (boolean value)
-  {
-    bool_willRender = value;
-  }
+    /**
+     * Setter method for willRender property.
+     *
+     * @param value a <code>boolean</code> value
+     */
+    public void setWillRender (boolean value) {
+        bool_willRender = value;
+    }
 
-  // the get functions ...
-  public boolean willRender()
-  {
-    return bool_willRender;
-  }
+
+    /**
+     * Getter method for willRender property.
+     *
+     * @return a <code>boolean</code> value
+     */
+    public boolean willRender() {
+        return bool_willRender;
+    }
 }
