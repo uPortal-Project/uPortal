2be69a22b9f98fea21edccdc66fbbc743efaa8b8
peterk@committers.jasig.org <peterk@committers.jasig.org>
2002-02-08 15:34:35 +0000

    Moved counter functionality into a separate interface, with its own implementation and factory method. Implementation is specified by "org.jasig.portal.utils.CounterStoreFactory.implementation" parameter in the portal.properties file.
    git-svn-id: https://source.jasig.org/uPortal/trunk@5863 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/utils/RDBMCounterStore.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+package org.jasig.portal.utils;
+
+import org.jasig.portal.RDBMServices;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import org.jasig.portal.services.LogService;
+
+/**
+ * A reference implementation for the counter store
+ *
+ * @author <a href="mailto:pkharchenko@interactivebusiness.com">Peter Kharchenko</a>
+ * @version $Revision$
+ */
+public class RDBMCounterStore implements ICounterStore {
+
+
+    /**
+     * put your documentation comment here
+     * @param counterName
+     * @exception Exception
+     */
+    public synchronized void createCounter (String counterName) throws Exception {
+        Connection con = RDBMServices.getConnection();
+        try {
+            Statement stmt = con.createStatement();
+            try {
+                String sInsert = "INSERT INTO UP_SEQUENCE (SEQUENCE_NAME,SEQUENCE_VALUE/*/) VALUES ('" + counterName + "',0)";
+                LogService.instance().log(LogService.DEBUG, "RDBMUserLayoutStore::createCounter(): " + sInsert);
+                stmt.executeUpdate(sInsert);
+            } finally {
+                stmt.close();
+            }
+        } finally {
+            RDBMServices.releaseConnection(con);
+        }
+    }
+
+
+    /**
+     * put your documentation comment here
+     * @param counterName
+     * @param value
+     * @exception Exception
+     */
+    public synchronized void setCounter (String counterName, int value) throws Exception {
+        Connection con = RDBMServices.getConnection();
+        try {
+            Statement stmt = con.createStatement();
+            try {
+                String sUpdate = "UPDATE UP_SEQUENCE SET SEQUENCE_VALUE=" + value + "WHERE SEQUENCE_NAME='" + counterName + "'";
+                LogService.instance().log(LogService.DEBUG, "RDBMUserLayoutStore::setCounter(): " + sUpdate);
+                stmt.executeUpdate(sUpdate);
+            } finally {
+                stmt.close();
+            }
+        } finally {
+            RDBMServices.releaseConnection(con);
+        }
+    }
+
+    /*
+     * get&increment method.
+     */
+    public synchronized int getIncrementIntegerId (String counterName) throws Exception {
+        Connection con = RDBMServices.getConnection();
+        try {
+            Statement stmt = con.createStatement();
+            try {
+                String sQuery = "SELECT SEQUENCE_VALUE FROM UP_SEQUENCE WHERE SEQUENCE_NAME='" + counterName + "'";
+                for (int i = 0; i < 25; i++) {
+                    try {
+                        LogService.instance().log(LogService.DEBUG, "RDBMUserLayoutStore::getIncrementInteger(): " + sQuery);
+                        ResultSet rs = stmt.executeQuery(sQuery);
+                        int origId;
+                        int nextId;
+                        try {
+                            rs.next();
+                            origId = rs.getInt(1);
+                            nextId = origId + 1;
+                        } catch (SQLException e) { // If the query fails, there is no hope in h**k
+                            throw new Exception("RDBMUserLayoutStore::getIncrementInteger():" + e);
+                        } finally {
+                            rs.close();
+                        }
+                        String sInsert = "UPDATE UP_SEQUENCE SET SEQUENCE_VALUE=" + nextId + " WHERE SEQUENCE_NAME='" + counterName + "'" +
+                            " AND SEQUENCE_VALUE=" + origId;
+                        LogService.instance().log(LogService.DEBUG, "RDBMUserLayoutStore::getIncrementInteger(): " + sInsert);
+                        stmt.executeUpdate(sInsert);
+                        return  nextId;
+                    } catch (SQLException e) {
+                        /**
+                         * Assume a concurrent update. Try again after some random amount of milliseconds.
+                         */
+                        Thread.sleep(java.lang.Math.round(java.lang.Math.random()* 3 * 1000)); // Retry in up to 3 seconds
+                    }
+                }
+            } finally {
+                stmt.close();
+            }
+        } catch (Exception e) {
+            LogService.instance().log(LogService.ERROR, e);
+        } finally {
+            RDBMServices.releaseConnection(con);
+        }
+        throw new Exception("Unable to increment counter for " + counterName);
+    }
+}
