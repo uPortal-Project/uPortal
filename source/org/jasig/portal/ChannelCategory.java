eb2885e032f3135490deaee5dc5cdb2ba6ff2f25
Ken Weiner <kweiner@gmail.com>
2002-02-13 04:27:41 +0000

    Initial checkin
    git-svn-id: https://source.jasig.org/uPortal/trunk@5926 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/ChannelCategory.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright © 2002 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.jasig.portal;
+
+import org.jasig.portal.services.GroupService;
+import org.jasig.portal.groups.IEntityGroup;
+import org.jasig.portal.groups.IEntity;
+
+/**
+ * A channel category.
+ * @author Ken Weiner, kweiner@interactivebusiness.com
+ * @version $Revision$
+ */
+public class ChannelCategory {
+
+  private IEntityGroup categoryGroup;
+
+  /**
+   * Constructs a ChannelCategory based on an existing category
+   */
+  public ChannelCategory(int id) throws PortalException {
+    categoryGroup = GroupService.findGroup(String.valueOf(id));
+  }
+
+  /**
+   * Constructs a brand new ChannelCategory
+   */
+  public ChannelCategory(String name, String descr) throws PortalException {
+    categoryGroup = GroupService.newGroup(ChannelDefinition.class);
+    categoryGroup.setName(name);
+    categoryGroup.setDescription(descr);
+  }
+
+  // Getter methods
+  public String getId() { return categoryGroup.getKey(); }
+  public String getName() { return categoryGroup.getName(); }
+  public String getDescription() { return categoryGroup.getDescription(); }
+
+  public void addChannelDefinition(ChannelDefinition channelDef) throws PortalException {
+    String key = String.valueOf(channelDef.getPublishId());
+    IEntity channelDefEntity = GroupService.getEntity(key, ChannelDefinition.class);
+    categoryGroup.addMember(channelDefEntity);
+    categoryGroup.updateMembers();
+  }
+
+  public void removeChannelDefinition(ChannelDefinition channelDef) throws PortalException {
+    String key = String.valueOf(channelDef.getPublishId());
+    IEntity channelDefEntity = GroupService.getEntity(key, ChannelDefinition.class);
+    categoryGroup.removeMember(channelDefEntity);
+    categoryGroup.updateMembers();
+  }
+
+}
