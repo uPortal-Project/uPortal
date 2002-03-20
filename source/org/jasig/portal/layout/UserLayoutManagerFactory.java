db13b9a19cd0c847ff3d379cbd3ed3d3c2b1787e
peterk@committers.jasig.org <peterk@committers.jasig.org>
2002-03-20 06:36:43 +0000

    a factory class for user layout manager
    git-svn-id: https://source.jasig.org/uPortal/trunk@6054 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/layout/UserLayoutManagerFactory.java
@@ -0,0 +1,50 @@
+package org.jasig.portal.layout;
+
+import java.lang.reflect.Constructor;
+import org.jasig.portal.PropertiesManager;
+import org.jasig.portal.IUserLayoutStore;
+import org.jasig.portal.UserProfile;
+import org.jasig.portal.PortalException;
+import org.jasig.portal.UserLayoutStoreFactory;
+import org.jasig.portal.services.LogService;
+import org.jasig.portal.security.IPerson;
+
+
+/**
+ * A factory class for obtaining {@link IUserLayoutManager} implementations.
+ *
+ * @author <a href="mailto:pkharchenko@interactivebusiness.com">Peter Kharchenko</a>
+ * @version 1.0
+ */
+public class UserLayoutManagerFactory {
+    static Class coreUserLayoutManagerImpl=SimpleUserLayoutManager.class;
+
+    static {
+        // Retrieve the class name of the core IUserLayoutManager implementation
+        String className = PropertiesManager.getProperty("org.jasig.portal.layout.UserLayoutManagerFactory.coreImplementation");
+        if (className == null)
+            LogService.instance().log(LogService.ERROR, "UserLayoutManagerFactory: org.jasig.portal.layout.UserLayoutManagerFactory.coreImplementation must be specified in portal.properties");
+        try {
+            Class newClass = Class.forName(className);
+            coreUserLayoutManagerImpl=newClass;
+        } catch (Exception e) {
+            LogService.instance().log(LogService.ERROR, "UserLayoutManagerFactory: Could not instantiate " + className, e);
+        }
+    }
+
+    /**
+     * Obtain a regular user layout manager implementation
+     *
+     * @return an <code>IUserLayoutManager</code> value
+     */
+    public static IUserLayoutManager getUserLayoutManager(IPerson person, UserProfile profile) throws PortalException {
+        try {
+            Class[] cArgsClasses={IPerson.class,UserProfile.class,IUserLayoutStore.class};
+            Constructor c=coreUserLayoutManagerImpl.getConstructor(cArgsClasses);
+            Object[] cArgs={person,profile,UserLayoutStoreFactory.getUserLayoutStoreImpl()};
+            return (IUserLayoutManager)c.newInstance(cArgs);
+        } catch (Exception e) {
+            throw new PortalException("Unable to instantiate a \""+coreUserLayoutManagerImpl.getName()+"\"",e);
+        }
+    }
+}
