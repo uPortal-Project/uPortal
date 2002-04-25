1882c46dcc93f1fccb559c5deb729f296b14be6a
Ken Weiner <kweiner@gmail.com>
2002-04-25 17:20:50 +0000

    Added code to ensure that autocommit is set to true before handing out a connection.  Thanks Alex!
    git-svn-id: https://source.jasig.org/uPortal/trunk@6193 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/RDBMServices.java
+++ source/org/jasig/portal/RDBMServices.java
@@ -235,6 +235,11 @@ public class RDBMServices {
       DataSource ds = (DataSource)envCtx.lookup("jdbc/" + dbName);
       if (ds != null) {
         conn = ds.getConnection();
+        // Make sure autocommit is set to true
+        if (conn != null && !conn.getAutoCommit()) {
+          conn.rollback();
+          conn.setAutoCommit(true);
+        }         
       } else {
         LogService.instance().log(LogService.ERROR, "The database '" + dbName + "' could not be found.");
       }
@@ -270,6 +275,11 @@ public class RDBMServices {
       try {
         Class.forName(sJdbcDriver).newInstance();
         conn = DriverManager.getConnection(sJdbcUrl, sJdbcUser, sJdbcPassword);
+        // Make sure autocommit is set to true
+        if (conn != null && !conn.getAutoCommit()) {
+          conn.rollback();
+          conn.setAutoCommit(true);
+        } 
         prevErrorMsg = "";
       } catch (ClassNotFoundException cnfe) {
         LogService.instance().log(LogService.ERROR, "The driver " + sJdbcDriver + " was not found, please check the rdbm.properties file and your classpath.");
