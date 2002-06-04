c38a6ca57403d29af337fe66a9da2c5fbe2cb1b7
av317@committers.jasig.org <av317@committers.jasig.org>
2002-06-04 20:30:39 +0000

    A channel adapter for multithreaded cacheable channels that implement IMultithreadedMimeResponse (esp. CWebProxy)
    git-svn-id: https://source.jasig.org/uPortal/trunk@6323 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/MultithreadedCacheableMimeResponseChannelAdapter.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+package  org.jasig.portal;
+
+import  java.io.InputStream;
+import  java.io.OutputStream;
+import  java.util.Map;
+import  java.io.IOException;
+
+/**
+ * Internal adapter for a multithreaded channel that is also cacheable and 
+ * implements IMimeResponse (capable of using DonwloadWorker)
+ * @author Alex Vigdor
+ * @version $Revision$
+ * @see MultithreadedCacheableChannelAdapter
+ */
+
+public class MultithreadedCacheableMimeResponseChannelAdapter extends MultithreadedCacheableChannelAdapter
+        implements IMimeResponse {
+
+    public MultithreadedCacheableMimeResponseChannelAdapter (IMultithreadedChannel channel, 
+            String uid) throws PortalException
+    {
+        super(channel, uid);
+        if (!(channel instanceof IMultithreadedMimeResponse)) {
+            throw  (new PortalException("MultithreadedCacheableMimeResponseChannelAdapter: Cannot adapt "
+                    + channel.getClass().getName()));
+        }
+    }
+
+    public String getContentType () {
+        return  ((IMultithreadedMimeResponse)channel).getContentType(uid);
+    }
+
+    public InputStream getInputStream () throws IOException {
+        return  ((IMultithreadedMimeResponse)channel).getInputStream(uid);
+    }
+
+    public void downloadData (OutputStream out) throws IOException {
+        ((IMultithreadedMimeResponse)channel).downloadData(out, uid);
+    }
+
+    public String getName () {
+        return  ((IMultithreadedMimeResponse)channel).getName(uid);
+    }
+
+    public Map getHeaders () {
+        return  ((IMultithreadedMimeResponse)channel).getHeaders(uid);
+    }
+}
+
+
+
