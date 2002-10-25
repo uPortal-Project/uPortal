8ba080134f1992fd62bfdaa91df2e2db7266fae7
Ken Weiner <kweiner@gmail.com>
2002-10-25 23:43:09 +0000

    Added constant PERSON_SESSION_KEY which is used to store and retrieve a person instance from the session. This will help ensure that the same String is used thoughout the code to store or lookup the person
    git-svn-id: https://source.jasig.org/uPortal/trunk@6667 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/security/IPersonManager.java
+++ source/org/jasig/portal/security/IPersonManager.java
@@ -31,26 +31,27 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * formatted with JxBeauty (c) johann.langhofer@nextra.at
  */
 
+package org.jasig.portal.security;
 
-package  org.jasig.portal.security;
-
-import  javax.servlet.http.HttpServletRequest;
-
+import javax.servlet.http.HttpServletRequest;
 
 /**
- * @author Bernie Durfee (bdurfee@interactivebusiness.com)
+ * Manages the storage of an IPerson object in a user's session.
+ * @author Bernie Durfee, bdurfee@interactivebusiness.com
  */
 public interface IPersonManager {
+
+  // Can be used to store an instance of IPerson in the user's session
+  public static final String PERSON_SESSION_KEY = "org.jasig.portal.security.IPerson";
 
   /**
    * Returns an IPerson associated with the incoming request
    * @param request
    * @return IPerson associated with the incoming request
    */
-  public IPerson getPerson (HttpServletRequest request) throws PortalSecurityException;
+  public IPerson getPerson(HttpServletRequest request) throws PortalSecurityException;
 }
 
 
