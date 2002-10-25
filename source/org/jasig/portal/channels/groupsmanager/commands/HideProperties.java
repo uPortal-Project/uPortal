c68d5c7cc506efece37548e8feb597b97d67cbc8
av317@committers.jasig.org <av317@committers.jasig.org>
2002-10-25 23:48:02 +0000

    new Groups Manager command
    git-svn-id: https://source.jasig.org/uPortal/trunk@6670 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/channels/groupsmanager/commands/HideProperties.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.jasig.portal.channels.groupsmanager.commands;
+
+import org.jasig.portal.channels.groupsmanager.*;
+import org.jasig.portal.*;
+import org.w3c.dom.*;
+import org.jasig.portal.services.*;
+/**
+ * A Groups Manager command to hide properties for any entity or group
+ *
+ * @author Alex Vigdor
+ * @version $Revision$
+ */
+
+public class HideProperties extends GroupsManagerCommand {
+
+  public HideProperties() {
+  }
+  
+  public void execute (CGroupsManagerSessionData sessionData) {
+    String id = this.getCommandArg(sessionData.runtimeData);
+    Element e = GroupsManagerXML.getElementById(sessionData.model,id);
+    if (e != null){
+      NodeList nl = e.getChildNodes();
+      for(int i=(nl.getLength()-1); i>=0;i--){
+        if (nl.item(i).getNodeName().equals("properties")){
+          e.removeChild(nl.item(i));
+        } 
+      }
+    }
+  }
+}
