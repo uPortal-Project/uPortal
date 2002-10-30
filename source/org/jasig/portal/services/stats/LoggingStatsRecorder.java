56f2736ef9402d6e60e74083373e0ce7b726d527
Ken Weiner <kweiner@gmail.com>
2002-10-30 04:03:54 +0000

    Added new methods: recordChannelAddedToLayout() and recordChannelRemovedFromLayout() Added ability to specify logging priority.
    git-svn-id: https://source.jasig.org/uPortal/trunk@6707 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/services/stats/LoggingStatsRecorder.java
+++ source/org/jasig/portal/services/stats/LoggingStatsRecorder.java
@@ -36,50 +36,85 @@
 package org.jasig.portal.services.stats;
 
 import org.jasig.portal.services.LogService;
+import org.jasig.portal.layout.UserLayoutChannelDescription;
 import org.jasig.portal.security.IPerson;
+import org.jasig.portal.UserProfile;
 import org.jasig.portal.ChannelDefinition;
+import org.apache.log4j.Priority;
 
 /**
- * Logs portal statistics to the portal's log.
+ * Logs portal statistics to the portal's log.  Contains
+ * set and get methods to control the log priority.
  * @author Ken Weiner, kweiner@interactivebusiness.com
  * @version $Revision$
  */
 public class LoggingStatsRecorder extends MessageStatsRecorder {
 
+  // Unfortunately this is tied to Apache's Log4J.
+  // It would be nice if the LogService was 
+  // logger implementation agnostic!
+  private Priority priority;
+  
+  public LoggingStatsRecorder() {
+    this.priority = LogService.INFO;
+  }
+  
+  public LoggingStatsRecorder(Priority priority) {
+    this.priority = priority;
+  }  
+  
+  public void setPriority(Priority priority) {
+    this.priority = priority;
+  }
+  
+  public Priority getPriority() {
+    return this.priority;
+  }
+  
   public void recordLogin(IPerson person) {
     String msg = super.getMessageForLogin(person);
-    LogService.instance().log(LogService.INFO, msg);
+    LogService.instance().log(priority, msg);
   }
   
   public void recordLogout(IPerson person) {
     String msg = super.getMessageForLogout(person);
-    LogService.instance().log(LogService.INFO, msg);
+    LogService.instance().log(priority, msg);
   }  
   
   public void recordSessionCreated(IPerson person) {
     String msg = super.getMessageForSessionCreated(person);
-    LogService.instance().log(LogService.INFO, msg);
+    LogService.instance().log(priority, msg);
   }
   
   public void recordSessionDestroyed(IPerson person) {
     String msg = super.getMessageForSessionDestroyed(person);
-    LogService.instance().log(LogService.INFO, msg);
+    LogService.instance().log(priority, msg);
   }
   
   public void recordChannelDefinitionPublished(IPerson person, ChannelDefinition channelDef) {
     String msg = super.getMessageForChannelDefinitionPublished(person, channelDef);
-    LogService.instance().log(LogService.INFO, msg);
+    LogService.instance().log(priority, msg);
   }
 
   public void recordChannelDefinitionModified(IPerson person, ChannelDefinition channelDef) {
     String msg = super.getMessageForChannelDefinitionModified(person, channelDef);
-    LogService.instance().log(LogService.INFO, msg);
+    LogService.instance().log(priority, msg);
   }
 
   public void recordChannelDefinitionRemoved(IPerson person, ChannelDefinition channelDef) {
     String msg = super.getMessageForChannelDefinitionRemoved(person, channelDef);
-    LogService.instance().log(LogService.INFO, msg);
+    LogService.instance().log(priority, msg);
   }  
+
+  public void recordChannelAddedToLayout(IPerson person, UserProfile profile, UserLayoutChannelDescription channelDesc) {
+    String msg = super.getMessageForChannelAddedToLayout(person, profile, channelDesc);
+   LogService.instance().log(priority, msg);
+  }    
+  
+  public void recordChannelRemovedFromLayout(IPerson person, UserProfile profile, UserLayoutChannelDescription channelDesc) {
+    String msg = super.getMessageForChannelRemovedFromLayout(person, profile, channelDesc);
+    LogService.instance().log(priority, msg);
+  }    
 }
 
 
