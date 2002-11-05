4fd5b920517c0bef7b959a43c185a4759683e5ea
av317@committers.jasig.org <av317@committers.jasig.org>
2002-11-05 01:09:29 +0000

    moved string literal "username" to new IPerson constant USERNAME, set username to guest when creating guest user
    git-svn-id: https://source.jasig.org/uPortal/trunk@6751 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/webservices/RemoteChannel.java
+++ source/org/jasig/portal/webservices/RemoteChannel.java
@@ -158,7 +158,7 @@ public class RemoteChannel implements IRemoteChannel {
     int channelPublishId = channelDef.getId();
     boolean authorized = ap.canSubscribe(channelPublishId);
     if (!authorized)
-      throw new AuthorizationException("User '" + person.getAttribute("username") + "' is not authorized to access channel with functional name '" + fname + "'");
+      throw new AuthorizationException("User '" + person.getAttribute(person.USERNAME) + "' is not authorized to access channel with functional name '" + fname + "'");
       
     // Instantiate channel
     String javaClass = channelDef.getJavaClass();
@@ -359,6 +359,7 @@ public class RemoteChannel implements IRemoteChannel {
     IPerson person = new PersonImpl();
     person.setSecurityContext(InitialSecurityContextFactory.getInitialContext("root"));
     person.setID(1); // Guest users have a UID of 1
+    person.setAttribute(person.USERNAME,"guest");
     return person;
   }
   
