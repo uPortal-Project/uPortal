b623833f536e34852db6a6f0582852bd3438eee5
Sarah Arnott <sarnott@mun.ca>
2003-02-03 15:38:03 +0000

    Fixed a bug where the <base> element was being ignored when determining relative URLs
    git-svn-id: https://source.jasig.org/uPortal/trunk@7125 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/utils/XHTMLURLFilter.java
+++ source/org/jasig/portal/utils/XHTMLURLFilter.java
@@ -61,7 +61,12 @@ public class XHTMLURLFilter extends AbsoluteURLFilter {
   public void startElement (String uri, String localName, String qName,  Attributes atts) throws SAXException {
     AttributesImpl attsImpl = new AttributesImpl(atts);
 
-    if (atts.getValue("src") != null)
+    if (qName.equals("base"))
+    {
+      super.setBaseUrl(atts.getValue("href"));
+      return;  // do not include base element in channel output
+    }
+    else if (atts.getValue("src") != null)
     {
       fixURL("img", "src", qName, atts, attsImpl);
       fixURL("input", "src", qName, atts, attsImpl);
@@ -83,7 +88,10 @@ public class XHTMLURLFilter extends AbsoluteURLFilter {
     super.startElement(uri, localName, qName, attsImpl);   
   }
 
-  //public void endElement (String uri, String localName, String qName) throws SAXException {
-  //  super.endElement(uri, localName, qName);
-  //}  
+  public void endElement (String uri, String localName, String qName) throws SAXException {
+    if (qName.equals("base"))
+      return; // do not include base element in channel output
+    else
+      super.endElement(uri, localName, qName);
+  }  
 }
