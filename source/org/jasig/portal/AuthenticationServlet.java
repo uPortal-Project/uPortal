2b1a04c2b8fd779ceb2acbff8d657b3b3a13a782
Susan Bramhall <susan.bramhall@yale.edu>
2003-02-05 22:26:52 +0000

    resolve Bugzilla Bug 1224   Whitespace in attempted username
    git-svn-id: https://source.jasig.org/uPortal/trunk@7138 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/AuthenticationServlet.java
+++ source/org/jasig/portal/AuthenticationServlet.java
@@ -190,7 +190,7 @@ public class AuthenticationServlet extends HttpServlet {
       String parmValue = request.getParameter(parmName);
       // null value causes exception in context.authentication
       // alternately we could just not set parm if value is null
-      parmValue = (parmValue == null ? "" : parmValue);
+      parmValue = (parmValue == null ? "" : parmValue).trim();
       // The relationship between the way the properties are stored and the way
       // the subcontexts are named has to be closely looked at to make this work.
       // The keys are either "root" or the subcontext name that follows "root.". As
