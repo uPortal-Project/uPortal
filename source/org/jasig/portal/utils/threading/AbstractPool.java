748b4c8582bd128ff8cf27e2d16641979054ebe7
William G. Thompson, Jr <wgthom@gmail.com>
2003-04-06 14:47:08 +0000

    Fixed copyright symbol to be ISO-8859-1 character #A9. JASIG source code is encode in ISO-8859-1 (Latin1). Make sure your source code editor is configure the same.
    Eclipse users go to:
    Window > Preferences > Workbench > Editors
    Then set: Text file encoding to ISO-8859-1
    
    
    git-svn-id: https://source.jasig.org/uPortal/trunk@7272 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/utils/threading/AbstractPool.java
+++ source/org/jasig/portal/utils/threading/AbstractPool.java
@@ -1,5 +1,5 @@
 /**
- * Copyright ï¿½ 2001 The JA-SIG Collaborative.  All rights reserved.
+ * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or withoutu
  * modification, are permitted provided that the following conditions
