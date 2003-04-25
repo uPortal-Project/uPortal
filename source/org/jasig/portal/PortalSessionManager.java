f118262edba1d6efb7155921423c3e2215d34095
Ken Weiner <kweiner@gmail.com>
2003-04-25 12:47:57 +0000

    Changed uPortal version to 2.1.2 for release.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-1-patches@7365 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/PortalSessionManager.java
+++ source/org/jasig/portal/PortalSessionManager.java
@@ -179,7 +179,7 @@ public class PortalSessionManager extends HttpServlet {
      */
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         // Send the uPortal version in a header
-        res.setHeader("uPortal-version", "uPortal_rel-2-1-1+");
+        res.setHeader("uPortal-version", "uPortal_rel-2-1-2");
 
         HttpSession session = req.getSession();
 
