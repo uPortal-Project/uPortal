2c204b98ccb94a67f34b28506de73d1b1f409dc8
Sarah Arnott <sarnott@mun.ca>
2003-09-16 12:20:08 +0000

    bug fix #1388 - mailto and other non-http URLs are not rewritten
    git-svn-id: https://source.jasig.org/uPortal/trunk@7613 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/channels/webproxy/CWebProxyURLFilter.java
+++ source/org/jasig/portal/channels/webproxy/CWebProxyURLFilter.java
@@ -115,7 +115,7 @@ public abstract class CWebProxyURLFilter extends SAX2FilterImpl
                                   || passThrough.equals("marked")) )
       {
         String attValue = atts.getValue(attName);
-        if (attValue != null)
+        if (attValue != null && attValue.startsWith("http://"))
         {
           String query = getQueryString(attValue); 
           String base = getBase(attValue);
