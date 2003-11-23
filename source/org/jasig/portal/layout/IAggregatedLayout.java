a5e2c4dac4036f87e35056a3422941b6ce252143
Michael Ivanov <mvi@immagic.com>
2003-11-23 07:25:38 +0000

    Added basic interfaces for user layout
    git-svn-id: https://source.jasig.org/uPortal/trunk@7864 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/layout/IAggregatedLayout.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright © 2002 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+
+package org.jasig.portal.layout;
+
+import java.util.Enumeration;
+import org.jasig.portal.PortalException;
+
+/**
+ * An aggregated-layout specific extension of the user layout interface
+ *
+ * @author <a href="mailto:mvi@immagic.com">Michael Ivanov</a>
+ * @version 1.1
+ */
+public interface IAggregatedLayout extends IUserLayout {
+
+    // the tag names constants
+    public static final String LAYOUT = "layout";
+    public static final String FRAGMENT = "fragment";
+    public static final String FOLDER = "folder";
+    public static final String CHANNEL = "channel";
+    public static final String PARAMETER = "parameter";
+    public static final String RESTRICTION = "restriction";
+    // The names for marking nodes
+    public static final String ADD_TARGET = "add_target";
+    public static final String MOVE_TARGET = "move_target";
+
+
+    /**
+     * Returns a list of fragment Ids existing in the layout.
+     *
+     * @return a <code>List</code> of <code>String</code> fragment Ids.
+     * @exception PortalException if an error occurs
+     */
+    public Enumeration getFragmentIds() throws PortalException;
+
+    /**
+     * Returns an fragment Id for a given node.
+     * Returns null if the node is not part of any fragments.
+     *
+     * @param nodeId a <code>String</code> value
+     * @return a <code>String</code> fragment Id
+     * @exception PortalException if an error occurs
+     */
+    public String getFragmentId(String nodeId) throws PortalException;
+
+    /**
+     * Returns an fragment root Id for a given fragment.
+     *
+     * @param fragmentId a <code>String</code> value
+     * @return a <code>String</code> fragment root Id
+     * @exception PortalException if an error occurs
+     */
+    public String getFragmentRootId(String fragmentId) throws PortalException;
+
+}
---
+++ source/org/jasig/portal/layout/IUserLayout.java
@@ -0,0 +1,196 @@
+/**
+ * Copyright © 2002 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+
+package org.jasig.portal.layout;
+
+import java.util.Enumeration;
+import org.jasig.portal.PortalException;
+import org.w3c.dom.Document;
+import org.xml.sax.ContentHandler;
+
+/**
+ * An interface representing the user layout.
+ *
+ * @author <a href="mailto:mvi@immagic.com">Michael Ivanov</a>
+ * @version 1.1
+ */
+public interface IUserLayout {
+
+    /**
+     * Writes user layout content (with appropriate markings) into
+     * a <code>ContentHandler</code>
+     *
+     * @param ch a <code>ContentHandler</code> value
+     * @exception PortalException if an error occurs
+     */
+    public void write(ContentHandler ch) throws PortalException;
+
+    /**
+     * Writes subtree of a user layout (with appropriate markings) defined by a particular node into
+     * a <code>ContentHandler</code>
+     *
+     * @param nodeId a <code>String</code> a node determining a user layout subtree.
+     * @param ch a <code>ContentHandler</code> value
+     * @exception PortalException if an error occurs
+     */
+    public void write(String nodeId, ContentHandler ch) throws PortalException;
+
+    /**
+     * Writes user layout content (with appropriate markings) into
+     * a <code>Document</code> object
+     *
+     * @param document a <code>Document</code> value
+     * @exception PortalException if an error occurs
+     */
+    public void write(Document document) throws PortalException;
+
+    /**
+     * Writes subtree of a user layout (with appropriate markings) defined by a particular node into
+     * a <code>Document</code>
+     *
+     * @param nodeId a <code>String</code> a node determining a user layout subtree.
+     * @param document a <code>Document</code> object
+     * @exception PortalException if an error occurs
+     */
+    public void write(String nodeId, Document document) throws PortalException;
+
+    /**
+     * Obtain a description of a node (channel or a folder) in a given user layout.
+     *
+     * @param nodeId a <code>String</code> channel subscribe id or folder id.
+     * @return an <code>UserLayoutNodeDescription</code> value
+     * @exception PortalException if an error occurs
+     */
+    public IUserLayoutNodeDescription getNodeDescription(String nodeId) throws PortalException;
+
+    /**
+     * Returns an Id of a parent user layout node.
+     * The user layout root node always has ID="root"
+     *
+     * @param nodeId a <code>String</code> value
+     * @return a <code>String</code> value
+     * @exception PortalException if an error occurs
+     */
+    public String getParentId(String nodeId) throws PortalException;
+
+    /**
+     * Returns a list of child node Ids for a given node.
+     *
+     * @param nodeId a <code>String</code> value
+     * @return a <code>Enumeration</code> of <code>String</code> child node Ids.
+     * @exception PortalException if an error occurs
+     */
+    public Enumeration getChildIds(String nodeId) throws PortalException;
+
+    /**
+     * Determine an Id of a next sibling node.
+     *
+     * @param nodeId a <code>String</code> value
+     * @return a <code>String</code> Id value of a next sibling node, or <code>null</code> if this is the last sibling.
+     * @exception PortalException if an error occurs
+     */
+    public String getNextSiblingId(String nodeId) throws PortalException;
+
+
+    /**
+     * Determine an Id of a previous sibling node.
+     *
+     * @param nodeId a <code>String</code> value
+     * @return a <code>String</code> Id value of a previous sibling node, or <code>null</code> if this is the first sibling.
+     * @exception PortalException if an error occurs
+     */
+    public String getPreviousSiblingId(String nodeId) throws PortalException;
+
+    /**
+     * Return a cache key, uniqly corresponding to the composition and the structure of the user layout.
+     *
+     * @return a <code>String</code> value
+     * @exception PortalException if an error occurs
+     */
+    public String getCacheKey() throws PortalException;
+
+
+    /**
+     * Register a layout event listener
+     *
+     * @param l a <code>LayoutEventListener</code> object
+     * @return a <code>boolean</code> success status
+     */
+    public boolean addLayoutEventListener(LayoutEventListener l);
+
+
+    /**
+     * Remove a registered layout event listener.
+     *
+     * @param l a <code>LayoutEventListener</code> object
+     * @return a <code>boolean</code> success status
+     */
+    public boolean removeLayoutEventListener(LayoutEventListener l);
+
+    /**
+     * Returns a layout Id associated with this manager/
+     *
+     * @return an <code>String</code> layout Id value;
+     */
+    public String getId();
+
+    /**
+     * Returns a node id associated with the supplied functional name.
+     *
+     * @param fname  the functional name to lookup
+     * @return a <code>String</code> subscription id
+     * @exception PortalException if an error occurs
+     */
+    public String getNodeId(String fname) throws PortalException;
+
+     /**
+     * Returns a list of node Ids in the layout.
+     *
+     * @return a <code>Enumeration</code> of node Ids
+     * @exception PortalException if an error occurs
+     */
+    public Enumeration getNodeIds() throws PortalException;
+
+    /**
+     * Returns an id of the root node.
+     *
+     * @return a <code>String</code> value
+     */
+    public String getRootId();
+
+
+}
