ec608f468c51342633807d395cb53e50acafca4d
Dan Ellentuck <de3@columbia.edu>
2003-11-26 19:14:41 +0000

    Make AuthorizationImpl a true singleton.  Bugzilla #1443.
    git-svn-id: https://source.jasig.org/uPortal/trunk@7889 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/security/provider/AuthorizationImpl.java
+++ source/org/jasig/portal/security/provider/AuthorizationImpl.java
@@ -80,7 +80,7 @@ public class AuthorizationImpl implements IAuthorizationService {
   /**
    *
    */
-public AuthorizationImpl () throws AuthorizationException
+private AuthorizationImpl () throws AuthorizationException
 {
     super();
     initialize();
