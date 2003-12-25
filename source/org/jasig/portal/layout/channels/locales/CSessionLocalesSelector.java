bc0095ffa5107eabdb6faa9b3c55f580be62ba89
Ken Weiner <kweiner@gmail.com>
2003-12-25 00:31:33 +0000

    Changing CLocaleChooser to CSessionLocalesSelector
    git-svn-id: https://source.jasig.org/uPortal/trunk@8004 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/layout/channels/locales/CSessionLocalesSelector.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright © 2003 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.jasig.portal.layout.channels.locales;
+
+import java.util.Locale;
+
+import org.jasig.portal.Constants;
+import org.jasig.portal.IPrivileged;
+import org.jasig.portal.PortalControlStructures;
+import org.jasig.portal.PortalException;
+import org.jasig.portal.channels.BaseChannel;
+import org.jasig.portal.i18n.LocaleManager;
+import org.jasig.portal.utils.XSLT;
+import org.w3c.dom.Document;
+import org.xml.sax.ContentHandler;
+
+/**
+ * Changes the locale preferences for the current session.
+ * Any changes made via this channel will not be persisted
+ * between sessions.
+ * @author Shoji Kajita <a href="mailto:">kajita@itc.nagoya-u.ac.jp</a>
+ * @author Ken Weiner, kweiner@unicon.net
+ * @version $Revision$
+ */
+public class CSessionLocalesSelector extends BaseChannel implements IPrivileged {
+    
+    protected final String sslUri = "sessionLocales.ssl";
+    private LocaleManager lm = null;
+
+    public void setPortalControlStructures(PortalControlStructures pcs)
+        throws PortalException {
+        lm = pcs.getUserPreferencesManager().getUserPreferences().getProfile().getLocaleManager();
+        Locale[] userLocales = lm.getUserLocales();
+    }
+
+    public void renderXML(ContentHandler out) throws PortalException {
+        if (lm.isLocaleAware()) {
+            Locale[] locales = runtimeData.getLocales();
+            Document doc = LocaleManager.xmlValueOf(locales, locales[0]);
+            XSLT xslt = XSLT.getTransformer(this, runtimeData.getLocales());
+            xslt.setXML(doc);
+            xslt.setXSL(sslUri, runtimeData.getBrowserInfo());
+            xslt.setTarget(out);
+            xslt.setStylesheetParameter("baseActionURL", runtimeData.getBaseActionURL());
+            xslt.setStylesheetParameter("localesParam", Constants.LOCALES_PARAM);
+            xslt.transform();
+        }
+    }
+
+}
