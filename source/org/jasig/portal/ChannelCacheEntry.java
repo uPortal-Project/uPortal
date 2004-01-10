e15786ec35783f6366db04c85acd2d526b15f60c
Ken Weiner <kweiner@gmail.com>
2004-01-10 01:20:36 +0000

    Made inner class into an outer class so that references to outer classes are not held longer than they should be - bug 1444.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-1-patches@8050 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/ChannelCacheEntry.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright © 2004 The JA-SIG Collaborative.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the JA-SIG Collaborative
+ *    (http://www.jasig.org/)."
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.jasig.portal;
+
+/**
+ * This class takes care of initiating channel rendering thread, 
+ * monitoring it for timeouts, retreiving cache, and returning 
+ * rendering results and status.  It is used by ChannelRenderer.
+ * @author <a href="mailto:pkharchenko@unicon.net">Peter Kharchenko</a>
+ * @version $Revision$
+ */
+public class ChannelCacheEntry {
+    protected Object buffer;
+    protected final Object validity;
+    public ChannelCacheEntry() {
+        buffer = null;
+        validity = null;
+    }
+    public ChannelCacheEntry(Object buffer, Object validity) {
+        this.buffer = buffer;
+        this.validity = validity;
+    }
+}
+
--- source/org/jasig/portal/UserInstance.java
+++ source/org/jasig/portal/UserInstance.java
@@ -42,7 +42,6 @@ import java.io.StringWriter;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Properties;
-import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -679,15 +678,6 @@ public class UserInstance implements HttpSessionBindingListener {
             for (int i = 0; i < values.length; i++) {
                 ulm.deleteNode(values[i]);
             }
-        }
-    }
-
-    private class CharacterCacheEntry {
-        Vector systemBuffers;
-        Vector channelIds;
-        public CharacterCacheEntry() {
-            systemBuffers=null;
-            channelIds=null;
         }
     }
 
