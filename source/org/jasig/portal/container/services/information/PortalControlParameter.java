dc05525598102c492a1f4299eb93afcd730d413b
Ken Weiner <kweiner@gmail.com>
2004-02-05 14:34:52 +0000

    Reformatted code to match the style of the rest of the uPortal Pluto implementation code.
    git-svn-id: https://source.jasig.org/uPortal/trunk@8159 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/container/services/information/PortalControlParameter.java
+++ source/org/jasig/portal/container/services/information/PortalControlParameter.java
@@ -33,7 +33,6 @@
  *
  */
 
-
 package org.jasig.portal.container.services.information;
 
 import java.util.ArrayList;
@@ -44,6 +43,7 @@ import java.util.StringTokenizer;
 
 import javax.portlet.PortletMode;
 import javax.portlet.WindowState;
+
 import org.apache.pluto.om.window.PortletWindow;
 import org.apache.pluto.util.StringUtils;
 
@@ -52,7 +52,6 @@ import org.apache.pluto.util.StringUtils;
  * @author Michael Ivanov, mvi@immagic.com
  * @version $Revision$
  */
-
 public class PortalControlParameter {
 
     static public final String ACTION = "ac";
@@ -63,70 +62,66 @@ public class PortalControlParameter {
     static public final String PREV_STATE = "ps";
     static public final String RENDER_PARAM = "rp";
     static public final String STATE = "st";
-    
-	private Map requestParameters = new HashMap();
-	private Map statefulControlParameters = null;
-	private Map statelessControlParameters = null;
-	private String portalURL = null;
 
+    private Map requestParameters = new HashMap();
+    private Map statefulControlParameters = null;
+    private Map statelessControlParameters = null;
+    private String portalURL = null;
 
-
-    public static String decodeParameterName(String paramName)
-    {
+    public static String decodeParameterName(String paramName) {
         return paramName.substring(PREFIX.length());
     }
 
-    public static String decodeParameterValue(String paramName, String paramValue)
-    {
+    public static String decodeParameterValue(String paramName, String paramValue) {
         return paramValue;
     }
 
-    private static String decodeRenderParamName(String encodedParamName)
-    {
+    private static String decodeRenderParamName(String encodedParamName) {
         StringTokenizer tokenizer = new StringTokenizer(encodedParamName, "_");
-        if (!tokenizer.hasMoreTokens()) return null;
+        if (!tokenizer.hasMoreTokens())
+            return null;
         String constant = tokenizer.nextToken();
-        if (!tokenizer.hasMoreTokens()) return null;
+        if (!tokenizer.hasMoreTokens())
+            return null;
         String objectId = tokenizer.nextToken();
-        if (!tokenizer.hasMoreTokens()) return null;
+        if (!tokenizer.hasMoreTokens())
+            return null;
         String name = tokenizer.nextToken();
         return name;
     }
 
-    private static String[] decodeRenderParamValues(String encodedParamValues)
-    {
+    private static String[] decodeRenderParamValues(String encodedParamValues) {
         StringTokenizer tokenizer = new StringTokenizer(encodedParamValues, "_");
-        if (!tokenizer.hasMoreTokens()) return null;
+        if (!tokenizer.hasMoreTokens())
+            return null;
         String _count = tokenizer.nextToken();
         int count = Integer.valueOf(_count).intValue();
         String[] values = new String[count];
-        for (int i=0; i<count; i++) {
-            if (!tokenizer.hasMoreTokens()) return null;
+        for (int i = 0; i < count; i++) {
+            if (!tokenizer.hasMoreTokens())
+                return null;
             values[i] = decodeValue(tokenizer.nextToken());
         }
         return values;
     }
 
-    private static String decodeValue(String value)
-    {
-        value = StringUtils.replace(value, "0x1", "_" );
-        value = StringUtils.replace(value, "0x2", "." );
-        value = StringUtils.replace(value, "0x3", "/" );
-        value = StringUtils.replace(value, "0x4", "\r" );
-        value = StringUtils.replace(value, "0x5", "\n" );
-        value = StringUtils.replace(value, "0x6", "<" );
-        value = StringUtils.replace(value, "0x7", ">" );
-        value = StringUtils.replace(value, "0x8", " " );
+    private static String decodeValue(String value) {
+        value = StringUtils.replace(value, "0x1", "_");
+        value = StringUtils.replace(value, "0x2", ".");
+        value = StringUtils.replace(value, "0x3", "/");
+        value = StringUtils.replace(value, "0x4", "\r");
+        value = StringUtils.replace(value, "0x5", "\n");
+        value = StringUtils.replace(value, "0x6", "<");
+        value = StringUtils.replace(value, "0x7", ">");
+        value = StringUtils.replace(value, "0x8", " ");
         return value;
     }
 
-    public static String encodeParameter(String param)
-    {
-        return PREFIX+param;
+    public static String encodeParameter(String param) {
+        return PREFIX + param;
     }
 
-    public static String encodeRenderParamName(PortletWindow window, String paramName)
-    {
+    public static String encodeRenderParamName(PortletWindow window, String paramName) {
         StringBuffer returnvalue = new StringBuffer(50);
         returnvalue.append(RENDER_PARAM);
         returnvalue.append("_");
@@ -136,65 +131,55 @@ public class PortalControlParameter {
         return returnvalue.toString();
     }
 
-    public static String encodeRenderParamValues(String[] paramValues)
-    {
+    public static String encodeRenderParamValues(String[] paramValues) {
         StringBuffer returnvalue = new StringBuffer(100);
         returnvalue.append(paramValues.length);
-        for (int i=0; i<paramValues.length; i++) {
+        for (int i = 0; i < paramValues.length; i++) {
             returnvalue.append("_");
             returnvalue.append(encodeValue(paramValues[i]));
         }
         return returnvalue.toString();
     }
 
-
-    private static String encodeValue(String value)
-    {
-        value = StringUtils.replace(value, "_", "0x1" );
-        value = StringUtils.replace(value, ".", "0x2" );
-        value = StringUtils.replace(value, "/", "0x3" );
-        value = StringUtils.replace(value, "\r", "0x4" );
-        value = StringUtils.replace(value, "\n", "0x5" );
-        value = StringUtils.replace(value, "<", "0x6" );
-        value = StringUtils.replace(value, ">", "0x7" );
-        value = StringUtils.replace(value, " ", "0x8" );
+    private static String encodeValue(String value) {
+        value = StringUtils.replace(value, "_", "0x1");
+        value = StringUtils.replace(value, ".", "0x2");
+        value = StringUtils.replace(value, "/", "0x3");
+        value = StringUtils.replace(value, "\r", "0x4");
+        value = StringUtils.replace(value, "\n", "0x5");
+        value = StringUtils.replace(value, "<", "0x6");
+        value = StringUtils.replace(value, ">", "0x7");
+        value = StringUtils.replace(value, " ", "0x8");
         return value;
     }
 
-    public static String getRenderParamKey(PortletWindow window)
-    {
-        return RENDER_PARAM+"_"+window.getId().toString();
+    public static String getRenderParamKey(PortletWindow window) {
+        return RENDER_PARAM + "_" + window.getId().toString();
     }
 
-    public static boolean isControlParameter(String param)
-    {
+    public static boolean isControlParameter(String param) {
         return param.startsWith(PREFIX);
     }
 
-    public static boolean isstatefulParameter(String param)
-    {
+    public static boolean isstatefulParameter(String param) {
         if (isControlParameter(param)) {
-            if ((param.startsWith(PREFIX+MODE)) ||
-                (param.startsWith(PREFIX+PREV_MODE)) ||
-                (param.startsWith(PREFIX+STATE)) ||
-                (param.startsWith(PREFIX+PREV_STATE)) ||
-                (param.startsWith(PREFIX+RENDER_PARAM))) {
+            if ((param.startsWith(PREFIX + MODE))
+                || (param.startsWith(PREFIX + PREV_MODE))
+                || (param.startsWith(PREFIX + STATE))
+                || (param.startsWith(PREFIX + PREV_STATE))
+                || (param.startsWith(PREFIX + RENDER_PARAM))) {
                 return true;
             }
         }
         return false;
     }
-   
 
-
-    public PortalControlParameter( String url )
-    {
+    public PortalControlParameter(String url) {
         this.portalURL = url;
         // GET statefulControlParameters and statelessControlParameters from portalURL!!!
     }
 
-    public void clearRenderParameters(PortletWindow portletWindow)
-    {
+    public void clearRenderParameters(PortletWindow portletWindow) {
         String prefix = getRenderParamKey(portletWindow);
         Iterator keyIterator = statefulControlParameters.keySet().iterator();
 
@@ -206,54 +191,43 @@ public class PortalControlParameter {
         }
     }
 
-    private String getActionKey(PortletWindow window)
-    {
-        return ACTION+"_"+window.getId().toString();
+    private String getActionKey(PortletWindow window) {
+        return ACTION + "_" + window.getId().toString();
     }
 
-    public String[] getActionParameter(PortletWindow window, String paramName)
-    {
+    public String[] getActionParameter(PortletWindow window, String paramName) {
         String encodedValues = (String)statefulControlParameters.get(encodeRenderParamName(window, paramName));
         String[] values = decodeRenderParamValues(encodedValues);
         return values;
     }
 
-
-    public PortletMode getMode(PortletWindow window)
-    {
+    public PortletMode getMode(PortletWindow window) {
         String mode = (String)statefulControlParameters.get(getModeKey(window));
-        if (mode!=null)
+        if (mode != null)
             return new PortletMode(mode);
         else
             return PortletMode.VIEW;
     }
 
-
-
-
-    private String getModeKey(PortletWindow window)
-    {
-        return MODE+"_"+window.getId().toString();
+    private String getModeKey(PortletWindow window) {
+        return MODE + "_" + window.getId().toString();
     }
 
-    public String getPIDValue()
-    {
+    public String getPIDValue() {
         String value = (String)statelessControlParameters.get(getPortletIdKey());
-        return value==null?"":value;
+        return value == null ? "" : value;
     }
 
-    private String getPortletIdKey()
-    {
+    private String getPortletIdKey() {
         return PORTLET_ID;
     }
 
-    public PortletWindow getPortletWindowOfAction()
-    {
+    public PortletWindow getPortletWindowOfAction() {
         Iterator iterator = getStatelessControlParameters().keySet().iterator();
         while (iterator.hasNext()) {
             String name = (String)iterator.next();
             if (name.startsWith(ACTION)) {
-                String id = name.substring(ACTION.length()+1);
+                String id = name.substring(ACTION.length() + 1);
                 /*Fragment fragment = org.apache.pluto.portalImpl.services.pageregistry.PageRegistry.getFragment(id);
                 if (fragment instanceof PortletFragment) {
                     return((PortletFragment)fragment).getPortletWindow();
@@ -264,34 +238,29 @@ public class PortalControlParameter {
         return null;
     }
 
-    public PortletMode getPrevMode(PortletWindow window)
-    {
+    public PortletMode getPrevMode(PortletWindow window) {
         String mode = (String)statefulControlParameters.get(getPrevModeKey(window));
-        if (mode!=null)
+        if (mode != null)
             return new PortletMode(mode);
         else
             return null;
     }
-    private String getPrevModeKey(PortletWindow window)
-    {
-        return PREV_MODE+"_"+window.getId().toString();
+    private String getPrevModeKey(PortletWindow window) {
+        return PREV_MODE + "_" + window.getId().toString();
     }
 
-    public WindowState getPrevState(PortletWindow window)
-    {
+    public WindowState getPrevState(PortletWindow window) {
         String state = (String)statefulControlParameters.get(getPrevStateKey(window));
-        if (state!=null)
+        if (state != null)
             return new WindowState(state);
         else
             return null;
     }
-    private String getPrevStateKey(PortletWindow window)
-    {
-        return PREV_STATE+"_"+window.getId().toString();
+    private String getPrevStateKey(PortletWindow window) {
+        return PREV_STATE + "_" + window.getId().toString();
     }
 
-    public Iterator getRenderParamNames(PortletWindow window)
-    {
+    public Iterator getRenderParamNames(PortletWindow window) {
         ArrayList returnvalue = new ArrayList();
         String prefix = getRenderParamKey(window);
         Iterator keyIterator = statefulControlParameters.keySet().iterator();
@@ -299,51 +268,44 @@ public class PortalControlParameter {
         while (keyIterator.hasNext()) {
             String name = (String)keyIterator.next();
             if (name.startsWith(prefix)) {
-                returnvalue.add(name.substring(prefix.length()+1));
+                returnvalue.add(name.substring(prefix.length() + 1));
             }
         }
 
         return returnvalue.iterator();
     }
 
-    public String[] getRenderParamValues(PortletWindow window, String paramName)
-    {
+    public String[] getRenderParamValues(PortletWindow window, String paramName) {
         String encodedValues = (String)statefulControlParameters.get(encodeRenderParamName(window, paramName));
         String[] values = decodeRenderParamValues(encodedValues);
         return values;
     }
 
-    public Map getRequestParameters()
-    {
+    public Map getRequestParameters() {
         return requestParameters;
     }
 
-    public WindowState getState(PortletWindow window)
-    {
+    public WindowState getState(PortletWindow window) {
         String state = (String)statefulControlParameters.get(getStateKey(window));
-        if (state!=null)
+        if (state != null)
             return new WindowState(state);
         else
             return WindowState.NORMAL;
     }
 
-    public Map getstatefulControlParameters()
-    {
+    public Map getstatefulControlParameters() {
         return statefulControlParameters;
     }
 
-    private String getStateKey(PortletWindow window)
-    {
-        return STATE+"_"+window.getId().toString();
+    private String getStateKey(PortletWindow window) {
+        return STATE + "_" + window.getId().toString();
     }
 
-    public Map getStatelessControlParameters()
-    {
+    public Map getStatelessControlParameters() {
         return statelessControlParameters;
     }
 
-    public boolean isOnePortletWindowMaximized()
-    {
+    public boolean isOnePortletWindowMaximized() {
         Iterator iterator = statefulControlParameters.keySet().iterator();
         while (iterator.hasNext()) {
             String name = (String)iterator.next();
@@ -356,42 +318,34 @@ public class PortalControlParameter {
         return false;
     }
 
-    public void setAction(PortletWindow window)
-    {
-        getstatefulControlParameters().put(getActionKey(window),ACTION.toUpperCase());
+    public void setAction(PortletWindow window) {
+        getstatefulControlParameters().put(getActionKey(window), ACTION.toUpperCase());
     }
 
-    public void setMode(PortletWindow window, PortletMode mode)
-    {
+    public void setMode(PortletWindow window, PortletMode mode) {
         Object prevMode = statefulControlParameters.get(getModeKey(window));
-        if (prevMode!=null)
+        if (prevMode != null)
             statefulControlParameters.put(getPrevModeKey(window), prevMode);
         // set current mode
         statefulControlParameters.put(getModeKey(window), mode.toString());
     }
 
-    public void setPortletId(PortletWindow window)
-    {
-        getstatefulControlParameters().put(getPortletIdKey(),window.getId().toString());
+    public void setPortletId(PortletWindow window) {
+        getstatefulControlParameters().put(getPortletIdKey(), window.getId().toString());
         //getStateLessControlParameter().put(getPortletIdKey(),window.getId().toString());
     }
 
-    public void setRenderParam(PortletWindow window, String name, String[] values)
-    {
-        statefulControlParameters.put(encodeRenderParamName(window, name), 
-                                      encodeRenderParamValues(values) );
+    public void setRenderParam(PortletWindow window, String name, String[] values) {
+        statefulControlParameters.put(encodeRenderParamName(window, name), encodeRenderParamValues(values));
     }
 
-    public void setRequestParam(String name, String[] values )
-    {
-        requestParameters.put(name, values );
+    public void setRequestParam(String name, String[] values) {
+        requestParameters.put(name, values);
     }
 
-
-    public void setState(PortletWindow window, WindowState state)
-    {
+    public void setState(PortletWindow window, WindowState state) {
         Object prevState = statefulControlParameters.get(getStateKey(window));
-        if (prevState!=null)
+        if (prevState != null)
             statefulControlParameters.put(getPrevStateKey(window), prevState);
         statefulControlParameters.put(getStateKey(window), state.toString());
     }
