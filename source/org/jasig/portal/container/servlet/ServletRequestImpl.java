2fcfb89e3696cb5419e7fe0e57a7a807cd2d36f3
Ken Weiner <kweiner@gmail.com>
2004-03-01 17:00:33 +0000

    Followed Michael's suggestion to utilize HttpServletRequestWrapper instead of implemeting all the methods in HttpServletRequest.
    git-svn-id: https://source.jasig.org/uPortal/trunk@8338 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/container/servlet/ServletRequestImpl.java
+++ source/org/jasig/portal/container/servlet/ServletRequestImpl.java
@@ -35,179 +35,28 @@
 
 package org.jasig.portal.container.servlet;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.Principal;
 import java.util.Enumeration;
 import java.util.Hashtable;
-import java.util.Locale;
 import java.util.Map;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequestWrapper;
 
 /**
- * A wrapper of the real HttpServletRequest that lets us
- * modify the request parameters.
+ * A wrapper of the real HttpServletRequest that allows
+ * modification of the request parameters.
  * @author Ken Weiner, kweiner@unicon.net
  * @version $Revision$
  */
-public class ServletRequestImpl implements HttpServletRequest {
+public class ServletRequestImpl extends HttpServletRequestWrapper {
     
-    protected HttpServletRequest request;
     protected Hashtable parameters;
     
     public ServletRequestImpl(HttpServletRequest request) {
-        this.request = request;
+        super(request);
         this.parameters = new Hashtable(request.getParameterMap());
     }
     
-    /**
-     * Replaces the existing request parameters with a new set
-     * of parameters.
-     * @param parameters the new parameters
-     */
-    public void setParameters(Map parameters) {
-        this.parameters.clear();
-        this.parameters.putAll(parameters);
-    }
-
-    public String getAuthType() {
-        return request.getAuthType();
-    }
-
-    public String getContextPath() {
-        return request.getContextPath();
-    }
-
-    public Cookie[] getCookies() {
-        return request.getCookies();
-    }
-
-    public long getDateHeader(String arg0) {
-        return request.getDateHeader(arg0);
-    }
-
-    public String getHeader(String arg0) {
-        return request.getHeader(arg0);
-    }
-
-    public Enumeration getHeaderNames() {
-        return request.getHeaderNames();
-    }
-
-    public Enumeration getHeaders(String arg0) {
-        return request.getHeaders(arg0);
-    }
-
-    public int getIntHeader(String arg0) {
-        return request.getIntHeader(arg0);
-    }
-
-    public String getMethod() {
-        return request.getMethod();
-    }
-
-    public String getPathInfo() {
-        return request.getPathInfo();
-    }
-
-    public String getPathTranslated() {
-        return request.getPathTranslated();
-    }
-
-    public String getQueryString() {
-        return request.getQueryString();
-    }
-
-    public String getRemoteUser() {
-        return getRemoteUser();
-    }
-
-    public String getRequestedSessionId() {
-        return request.getRequestedSessionId();
-    }
-
-    public String getRequestURI() {
-        return request.getRequestURI();
-    }
-
-    public StringBuffer getRequestURL() {
-        return request.getRequestURL();
-    }
-
-    public String getServletPath() {
-        return request.getServletPath();
-    }
-
-    public HttpSession getSession() {
-        return request.getSession();
-    }
-
-    public HttpSession getSession(boolean arg0) {
-       return request.getSession(arg0);
-    }
-
-    public Principal getUserPrincipal() {
-        return request.getUserPrincipal();
-    }
-
-    public boolean isRequestedSessionIdFromCookie() {
-        return request.isRequestedSessionIdFromCookie();
-    }
-
-    public boolean isRequestedSessionIdFromUrl() {
-        return request.isRequestedSessionIdFromURL();
-    }
-
-    public boolean isRequestedSessionIdFromURL() {
-        return isRequestedSessionIdFromURL();
-    }
-
-    public boolean isRequestedSessionIdValid() {
-        return isRequestedSessionIdValid();
-    }
-
-    public boolean isUserInRole(String arg0) {
-        return request.isUserInRole(arg0);
-    }
-
-    public Object getAttribute(String arg0) {
-        return request.getAttribute(arg0);
-    }
-
-    public Enumeration getAttributeNames() {
-        return request.getAttributeNames();
-    }
-
-    public String getCharacterEncoding() {
-        return request.getCharacterEncoding();
-    }
-
-    public int getContentLength() {
-        return request.getContentLength();
-    }
-
-    public String getContentType() {
-        return request.getContentType();
-    }
-
-    public ServletInputStream getInputStream() throws IOException {
-        return request.getInputStream();
-    }
-
-    public Locale getLocale() {
-        return request.getLocale();
-    }
-
-    public Enumeration getLocales() {
-        return request.getLocales();
-    }
-
     public String getParameter(String name) {
         return (String)parameters.get(name);
     }
@@ -224,56 +73,14 @@ public class ServletRequestImpl implements HttpServletRequest {
         return (String[])parameters.values().toArray(new String[0]);
     }
 
-    public String getProtocol() {
-        return request.getProtocol();
+    /**
+     * Replaces the existing request parameters with a new set
+     * of parameters.
+     * @param parameters the new parameters
+     */
+    public void setParameters(Map parameters) {
+        this.parameters.clear();
+        this.parameters.putAll(parameters);
     }
-
-    public BufferedReader getReader() throws IOException {
-        return request.getReader();
-    }
-
-    public String getRealPath(String arg0) {
-        return request.getRealPath(arg0);
-    }
-
-    public String getRemoteAddr() {
-        return request.getRemoteAddr();
-    }
-
-    public String getRemoteHost() {
-        return request.getRemoteHost();
-    }
-
-    public RequestDispatcher getRequestDispatcher(String arg0) {
-        return request.getRequestDispatcher(arg0);
-    }
-
-    public String getScheme() {
-        return request.getScheme();
-    }
-
-    public String getServerName() {
-        return request.getServerName();
-    }
-
-    public int getServerPort() {
-        return request.getServerPort();
-    }
-
-    public boolean isSecure() {
-        return request.isSecure();
-    }
-
-    public void removeAttribute(String arg0) {
-        request.removeAttribute(arg0);
-    }
-
-    public void setAttribute(String arg0, Object arg1) {
-        request.setAttribute(arg0, arg1);
-    }
-
-    public void setCharacterEncoding(String arg0) throws UnsupportedEncodingException {
-        request.setCharacterEncoding(arg0);
-    } 
 
 }
