aec7d2df089cdc7f721d3ec1930103d33d3445c6
Michael Ivanov <mvi@immagic.com>
2004-05-07 22:21:04 +0000

    Changed the copyright symbol
    git-svn-id: https://source.jasig.org/uPortal/trunk@8740 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/layout/AggregatedLayoutManager.java
+++ source/org/jasig/portal/layout/AggregatedLayoutManager.java
@@ -1,5 +1,5 @@
 /**
- * Copyright � 2002 The JA-SIG Collaborative.  All rights reserved.
+ * Copyright (c) 2002 The JA-SIG Collaborative.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
