6c64da956ce0c8e10b24da69adb04107e16d7339
Michael Ivanov <mvi@immagic.com>
2004-05-07 22:02:23 +0000

    Removed the duplicate call of setLayoutData()
    git-svn-id: https://source.jasig.org/uPortal/trunk@8738 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/layout/AggregatedLayoutManager.java
+++ source/org/jasig/portal/layout/AggregatedLayoutManager.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2002 The JA-SIG Collaborative.  All rights reserved.
+ * Copyright ï¿½ 2002 The JA-SIG Collaborative.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -1061,11 +1061,10 @@ public class AggregatedLayoutManager implements IAggregatedUserLayoutManager {
       rootFolder.setFirstChildNodeId(((Element)rootNode.getFirstChild()).getAttribute("ID"));
       layoutData.put(rootId,rootFolder);
       NodeList childNodes = rootNode.getChildNodes();
-      // initialize layout
-	  layout.setLayoutData(layoutData);
+    
       for ( int i = 0; i < childNodes.getLength(); i++ )
        setUserLayoutDOM ( childNodes.item(i), rootId, layoutData );
-      // update layout for cheldren
+      
       layout.setLayoutData(layoutData);
       updateCacheKey();
     }
