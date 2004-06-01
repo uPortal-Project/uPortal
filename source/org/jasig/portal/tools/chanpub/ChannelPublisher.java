7dda5756ab0729ca204f9dea188043e9e458f2b6
Faizan Ahmed <faizan@rutgers.edu>
2004-06-01 19:13:01 +0000

    Added check for null in the finally block.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-3-patches@8805 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/tools/chanpub/ChannelPublisher.java
+++ source/org/jasig/portal/tools/chanpub/ChannelPublisher.java
@@ -402,7 +402,8 @@ public class ChannelPublisher {
           LogService.log(LogService.ERROR, e);
           throw e;
         } finally {
-			is.close();
+			if(is != null)
+        	is.close();
 		}
         return ci;
   }  
