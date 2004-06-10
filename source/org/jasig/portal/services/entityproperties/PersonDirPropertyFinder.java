11d53ea7116d700839c7eb2b6f6ac28490b42e86
Ken Weiner <kweiner@gmail.com>
2004-06-10 15:45:54 +0000

    Bug 1575, Added multi-valued attribute support.
    git-svn-id: https://source.jasig.org/uPortal/trunk@8857 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/services/entityproperties/PersonDirPropertyFinder.java
+++ source/org/jasig/portal/services/entityproperties/PersonDirPropertyFinder.java
@@ -76,11 +76,21 @@ public class PersonDirPropertyFinder
             Object o = getPropertiesHash(entityID).get(name);
             if (o instanceof String) {
                 r = (String)o;
+            } else if (o instanceof List) {
+                StringBuffer sb = new StringBuffer();
+                List values = (List)o;
+                for (Iterator iter = values.iterator(); iter.hasNext();) {
+                    Object value = (Object)iter.next();
+                    sb.append(value.toString());
+                    if (iter.hasNext()) {
+                        sb.append(", ");
+                    }
+                }
+                r = sb.toString();
             }
         }
         return  r;
     }
-
     protected Hashtable getPropertiesHash(EntityIdentifier entityID) {
         Hashtable ht;
         if ((ht = (Hashtable)cache.get(entityID.getKey())) == null) {
