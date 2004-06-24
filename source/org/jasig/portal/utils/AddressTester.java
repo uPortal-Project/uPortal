e79f87e81026e3daea955f55e94de0c798d6aa76
George.Lindholm@committers.jasig.org <George.Lindholm@committers.jasig.org>
2004-06-24 22:38:47 +0000

    Bug 1597: Better thread usage. Allow ProxyWriter to get connection
    git-svn-id: https://source.jasig.org/uPortal/trunk@8878 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/utils/AddressTester.java
+++ source/org/jasig/portal/utils/AddressTester.java
@@ -35,11 +35,15 @@
 
 package org.jasig.portal.utils;
 
+import java.io.InterruptedIOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.net.URLConnection;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+import org.jasig.portal.services.LogService;
 
 /**
  * This class checks a URL or a webserver hosting a URL. It only allows a specific time allocated for
@@ -54,144 +58,227 @@ import java.net.URL;
 
 public class AddressTester {
     /**The timer object that takes a timerTask as a parameter when constructed*/
-    Timer timer;
-    
+    private static final Timer timer = new Timer();
+
     /**The connection thread inwhich the connection attempt is made*/
     Thread connectionThread;
-    
+
+    /* urlConnectio to be used */
+    private HttpURLConnection urlConnect = null;
+
     /**The connectioncode returned from connetion attempt*/
     private int connectionCode = 0;
-    
+
     /**This the url to try. the value is set through the class constructor*/
     private String urlToTry = "";
-    
-    /**The amount of time connection attempt can take, the deffault is 100 ms*/
+
+    /**The amount of time connection attempt can take, the default is 100 ms*/
     int timeToWait = 100;
+    static final int defaultTimeToWait = 100;
 
-    /** The code 200 that corresponds to HttpURlConnection.HTTP_OK*/
-    private int HTTP_OK = HttpURLConnection.HTTP_OK; 
- 
+    /** Get header data only **/
+    private boolean headOnly = false;
 
-	/**
-	 * Constructor
-	 * @param milliSeconds the number of milliseconds to let the connectioon attempt run 
-	 * @param urlString the String representing a URL
-	 */
-    public AddressTester(int milliSeconds, String urlString) throws Exception{
-		urlToTry = urlString;
-		timeToWait = milliSeconds;		
-		checkURL();
+
+    /** Debug the code */
+    private static boolean DEBUG = false;
+
+    static {
+      Runtime.getRuntime().addShutdownHook(new Thread(
+      "AddressTester JVM shutdown hook") {
+        public void run() {
+          timer.cancel();
+        }
+      });
     }
-    
-        /**
-         * Constructor
-         * @param urlString the String representing a URL
-         */
-    public AddressTester(String urlString) throws Exception{
-                urlToTry = urlString;
-                checkURL();
+
+
+    /**
+     * Constructor
+     * @param milliSeconds the number of milliseconds to let the connectioon attempt run
+     * @param urlString the String representing a URL
+     * @param getHead use setRequestMathod("HEAD")
+     */
+    public AddressTester(final String urlString, final int milliSeconds, final boolean getHead) throws Exception{
+      urlToTry = urlString;
+      timeToWait = milliSeconds;
+      connectionThread = new Thread() {
+        public void run() {
+          try {
+            URL url = new URL(urlToTry);
+            if (DEBUG) {
+              System.out.println("URL to try is " + urlToTry);
+            }
+
+            RemindTask rt = new RemindTask();
+            try {
+              timer.schedule(rt, timeToWait);
+              urlConnect = (HttpURLConnection) url.openConnection();
+              if (headOnly) {
+                urlConnect.setRequestMethod("HEAD");
+              }
+              urlConnect.setInstanceFollowRedirects(false);
+              connectionCode = urlConnect.getResponseCode();
+            } catch (InterruptedIOException iie) { /* Thread Interrupt */
+              if (DEBUG) {
+                System.out.println("timed out on " + urlToTry);
+              }
+              LogService.instance().log(LogService.INFO,
+                                        "AddressTest::checkURL(): timed out on " +
+                                        urlToTry);
+
+            } catch (Exception e) { /* Something went wrong */
+              if (DEBUG) {
+                System.out.println(urlToTry + " generated exception: " +
+                                   e.getMessage());
+              }
+              LogService.instance().log(LogService.INFO,
+                                        "AddressTest::checkURL(): "
+                                        + urlToTry + " generated exception: " +
+                                        e.getMessage());
+            } finally {
+              rt.cancel();
+            }
+
+          } catch (MalformedURLException mue) { /* Garbage In */
+            if (DEBUG) {
+              System.out.println("Bad URL: " + urlToTry);
+            }
+            LogService.instance().log(LogService.ERROR,
+                                      "AddressTest::checkURL(): Bad URL: " +
+                                      urlToTry);
+          }
+        }
+      };
+
+      connectionThread.start();
     }
 
     /**
-     * This method uses an annymous inner thread class and attepmts 
-     * receving some response codes from the specified URLString 
-     * @throws Exception
+     * Constructor
+     * @param urlString
+     * @param getHead
+     * @throws java.lang.Exception
      */
-    public void checkURL() throws Exception
-    {
-		connectionThread = (new Thread()
-		{
-				public void run() {
-				  try {
-					URL url = new URL(urlToTry);
-					//System.out.println("URL totry is " + urlToTry);
-					URLConnection urlConnect = url.openConnection();
-					HttpURLConnection httpUrlConnect = (HttpURLConnection) urlConnect;
-					connectionCode = httpUrlConnect.getResponseCode();								
-				  }
-				  catch(Exception e) {
-				     //System.out.println(e.getMessage());
-                                    connectionCode = 0; 
-                                  }
-				}
-		 });
-		 
-		connectionThread.start();        
-		timer = new Timer();
-		timer.schedule(new RemindTask(), timeToWait);    	
+    public AddressTester(String urlString, boolean getHead) throws Exception {
+      this(urlString, defaultTimeToWait, getHead);  }
+
+    /**
+     * Constructor
+     * @param milliSeconds - the number of milliseconds to let the connectioon attempt run
+     * @param urlString - the String representing a URL
+     */
+    public AddressTester(int milliSeconds, String urlString) throws Exception {
+      this(urlString, milliSeconds, false);
     }
-    
+
+    /**
+     * Constructor
+     * @param urlString the String representing a URL
+     */
+    public AddressTester(String urlString) throws Exception{
+      this(urlString, defaultTimeToWait, false);
+    }
+
     /**
      * This method returns the response code that was set in checkURL ()
      * @return the response code
      */
     public int getResponseCode()
     {
-       //make sure we wait for connection thread to finish                                                                                    
-       if(connectionThread.isAlive()){                                                                                                        
-           try{                                                                                                                               
-               connectionThread.join();                                                                                                       
-           }catch(InterruptedException e){                                                                                                    
-               return 0;                                                                                                                      
-           }                                                                                                                                  
-       } 
-    	return this.connectionCode;
+       //make sure we wait for connection thread to finish
+       if(connectionThread.isAlive()){
+           try{
+               connectionThread.join();
+           }catch(InterruptedException e){
+               return 0;
+           }
+       }
+        return this.connectionCode;
     }
-    
+
+
     /**
-     * 
+     * Get the (valid) URL connection
+     * @return URL connection
+     */
+    public URLConnection getConnection() {
+      //make sure we wait for connection thread to finish
+      if (connectionThread.isAlive()) {
+        try {
+          connectionThread.join();
+        } catch (InterruptedException e) {
+          return null;
+        }
+      }
+      return urlConnect;
+    }
+
+    /**
+     * Shut down the connection
+     */
+    public void disconnect() {
+      if (urlConnect != null) {
+        urlConnect.disconnect();
+      }
+    }
+
+    /**
+     *
      * @return <code>false</code> if the address is not available. <code>True</code> otherwise
      */
     public boolean URLAvailable()
     {
        //make sure we wait for connection thread to finish
        if(connectionThread.isAlive()){
-           try{    			
+           try{
                connectionThread.join();
-	   }catch(InterruptedException e){
-	       return false;
-	   }
+           }catch(InterruptedException e){
+               return false;
+           }
        }
-       if(this.connectionCode == HTTP_OK)
-           return true;	
-       else
-    	   return false;
-    }
-    
+       return connectionCode == HttpURLConnection.HTTP_OK;
+     }
+
     /**
      * Class RemidTask
      * @author knaderi
      *
      * This is a TimerTask class that interuupts the connectionThread
-     * After the timer scheduled time.  
-     * 
+     * After the timer scheduled time.
+     *
      */
-	class RemindTask extends TimerTask {
-		public void run() {
-           	
-			//System.out.println("Canceled");          
-			timer.cancel(); //Terminate the timer thread		
-			connectionThread.interrupt();            
-		}
-	}
-	    
+    class RemindTask extends TimerTask {
+      public void run() {
+        if (connectionThread.isAlive()) {
+          if (DEBUG) {
+            System.out.println("Canceled");
+          }
+          connectionThread.interrupt();
+        }
+      }
+    }
+
     /**
      * This is the main method and is left as a usage sample
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("About to schedule task.");       
-        try{
-        
-        	AddressTester myReminder = new AddressTester("http://data.my.ubc.ca/verf/cbc.html");         
-        
-        System.out.println("Task scheduled.");
-		System.out.println("Running the test URL");
-		//myReminder.checkURL();
-		System.out.println("is URL available " + myReminder.getResponseCode());
-        System.out.println("is URL available " + myReminder.URLAvailable());
-		}catch(Exception e){}
+      String[] testUrl = new String[] {
+        "http://www.cbcsrc.ca/cbcsrc/1010991/11566",
+        "http://www.theweathernetwork.com/weatherbutton/test.js",
+        "http://localhost:666/a.nothere",
+        "http://www.linuxmandrake.com",
+        "https://localhost:443/a.nothere"};
+      for (int i = 0; i < testUrl.length; i++) {
+        try {
+          System.out.println("About to schedule task: " + testUrl[i]);
+          AddressTester myReminder = new AddressTester(testUrl[i], false);
+          System.out.println("  Running the test URL");
+          System.out.println("available: " + myReminder.URLAvailable() + ", HTTP code: " + myReminder.getResponseCode());
+        } catch (Exception e) {}
+      }
     }
-    
+
 }
 
