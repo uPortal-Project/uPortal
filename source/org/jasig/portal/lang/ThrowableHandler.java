f5ad9bab90f60e43516991bab8fda94e585c533f
Andrew Petro <apetro@unicon.net>
2004-10-17 19:11:06 +0000

    changed to short copyright header, retaining existing copyright year where present. removed extra whitespace from JavaDoc comment reformated method declarations.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9288 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/lang/ThrowableHandler.java
+++ source/org/jasig/portal/lang/ThrowableHandler.java
@@ -1,41 +1,8 @@
-/**
- * Copyright © 2004 The JA-SIG Collaborative.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. Redistributions of any form whatsoever must retain the following
- *    acknowledgment:
- *    "This product includes software developed by the JA-SIG Collaborative
- *    (http://www.jasig.org/)."
- *
- * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-
+/* Copyright 2004 The JA-SIG Collaborative.  All rights reserved.
+*  See license distributed with this file and
+*  available online at http://www.uportal.org/license.html
+*/
 package org.jasig.portal.lang;
-
 /**
  * The <code>ThrowableHandler</code> interface defines the error
  * management mechanism for handler implementations. Error management
@@ -56,8 +23,8 @@ package org.jasig.portal.lang;
  *
  * @version "$Revision$"
  **/
-public interface ThrowableHandler
-{
+public interface ThrowableHandler {
+    
     /**
      * Handles the error condition specified in the parameters. The
      * handler can use the client class to resolve the error message
@@ -67,22 +34,13 @@ public interface ThrowableHandler
      * the client or error message property name is <code>null</code>.
      *
      * @param client client calling the handle method
-     *
      * @param property property name associated with error message
-     * 
      * @param objects objects associated with the error message, or
      * <code>null</code>
-     *
      * @param cause throwable condition which caused the error, or
      * <code>null</code>
-     *
      * @throws NullPointerException if client or property is
      * <code>null</code>
      **/
-    void handle(
-        Class client,
-        String property,
-        String[] objects,
-        Throwable cause
-        );
+    void handle(Class client, String property, String[] objects, Throwable cause);
 }
