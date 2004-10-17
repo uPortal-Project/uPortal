93f5ab8966010e5ed0c9600d01ee750f356e6169
Andrew Petro <apetro@unicon.net>
2004-10-17 19:53:23 +0000

    improved formatting, JavaDoc comment.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9300 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/security/provider/AuthorizationServiceFactoryImpl.java
+++ source/org/jasig/portal/security/provider/AuthorizationServiceFactoryImpl.java
@@ -6,7 +6,8 @@
 package org.jasig.portal.security.provider;
 
 /**
- * <p>The factory class for the uPortal reference authorization class.</p>
+ * <p>The factory class for the uPortal reference 
+ * IAuthorizationService implementation.</p>
  *
  * @author Dan Ellentuck
  * @version $Revision$
@@ -15,10 +16,11 @@ import org.jasig.portal.AuthorizationException;
 import org.jasig.portal.security.IAuthorizationService;
 import org.jasig.portal.security.IAuthorizationServiceFactory;
 
-public class AuthorizationServiceFactoryImpl implements IAuthorizationServiceFactory
-{
-  public IAuthorizationService getAuthorization() throws AuthorizationException
-  {
+public class AuthorizationServiceFactoryImpl 
+    implements IAuthorizationServiceFactory {
+    
+  public IAuthorizationService getAuthorization() 
+      throws AuthorizationException {
     return AuthorizationImpl.singleton();
   }
 }
