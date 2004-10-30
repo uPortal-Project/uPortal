b8a19497cd238ba00d096fae4ea7278ca55c7cfa
Andrew Petro <apetro@unicon.net>
2004-10-30 23:16:32 +0000

    enhanced to support JDK 1.4 chaining throwables through constructor argument. added JavaDoc comments.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9465 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/security/PortalSecurityException.java
+++ source/org/jasig/portal/security/PortalSecurityException.java
@@ -1,27 +1,43 @@
-/* Copyright 2001 The JA-SIG Collaborative.  All rights reserved.
+/* Copyright 2001, 2004 The JA-SIG Collaborative.  All rights reserved.
 *  See license distributed with this file and
 *  available online at http://www.uportal.org/license.html
 */
 
 package org.jasig.portal.security;
 
-/**
- * <p>An exception handler that will provide portal security-specific
- * information during an exception condition.</p>
- *
- * @author Andrew Newman, newman@yale.edu
- * @version $Revision$
- */
-
 import org.jasig.portal.PortalException;
 
+/**
+ * <p>An exception representing a Portal security problem.</p>
+ *
+ * @author Andrew Newman, newman-andy@yale.edu
+ * @version $Revision$ $Date$
+ */
 public class PortalSecurityException extends PortalException {
 
-  public PortalSecurityException() {
-    super();
-  }
+    /**
+     * Instantiate a bare PortalSecurityException.
+     * @deprecated instead use a more informative constructor
+     */
+    public PortalSecurityException() {
+        super();
+    }
 
-  public PortalSecurityException(String s) {
-    super(s);
-  }
+    /**
+     * Instantiate a PortalSecurityException with the given message.
+     * @param message information about the error condition
+     */
+    public PortalSecurityException(String message) {
+        super(message);
+    }
+
+    /**
+     * Instantiate a PortalSecurityException with the given message and 
+     * underlying cause
+     * @param message information about the error condition
+     * @param cause underlying cause of error condition
+     */
+    public PortalSecurityException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
