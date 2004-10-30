2936717ae38ca5093b264a9174bf051b56e6b4dc
Andrew Petro <apetro@unicon.net>
2004-10-30 19:57:06 +0000

    refactored InternalTimeoutException to support JDK 1.4 chaining throwables. Adding JavaDoc comment and refactored name of private field to be more Java-like.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9399 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/InternalTimeoutException.java
+++ source/org/jasig/portal/InternalTimeoutException.java
@@ -1,4 +1,4 @@
-/* Copyright 2001 The JA-SIG Collaborative.  All rights reserved.
+/* Copyright 2001, 2004 The JA-SIG Collaborative.  All rights reserved.
 *  See license distributed with this file and
 *  available online at http://www.uportal.org/license.html
 */
@@ -12,34 +12,89 @@ package org.jasig.portal;
  * @author Peter Kharchenko
  * @version $Revision$
  */
-
 public class InternalTimeoutException extends PortalException {
     
-    private Long l_timeoutValue=null;
+    /**
+     * Timeout value, in milliseconds, that was exceeded.
+     */
+    private Long timeoutValue = null;
 
+    /**
+     * Instantiate a bare InternalTimeoutException.
+     * Deprecated because it would be so much more helpful if you
+     * instead use a constructor that takes a message, etc.
+     */
     public InternalTimeoutException() {
+        super();
     }
 
+    /**
+     * Instantiate an InternalTimeoutException, conveying the given message.
+     * @param msg message explaining the nature of the timeout
+     */
     public InternalTimeoutException(String msg) {
         super(msg);
     }
 
+    /**
+     * Instantiate an InternalTimeoutException conveying a message and
+     * specifying the timeout that was exceeded.
+     * @param msg describes nature of timeout
+     * @param timeoutValue the timeout value in milliseconds that was exceeded
+     */
     public InternalTimeoutException(String msg,long timeoutValue) {
         super(msg);
-        l_timeoutValue=new Long(timeoutValue);
+        this.timeoutValue = new Long(timeoutValue);
     }
 
-    public InternalTimeoutException(String msg,long timeoutValue,boolean refresh,boolean reinstantiate) {
-        super(msg,refresh, reinstantiate);
-        l_timeoutValue=new Long(timeoutValue);
+    /**
+     * Instantiate an InternalTimeoutException conveying a message and
+     * specifying the timeout value that was exceeded as well as whether
+     * refresh and reinstantiation are appropriate responses to this problem.
+     * @param msg describes nature of timeout
+     * @param timeoutValue timeout value in milliseconds that was exceeded
+     * @param refresh true if refresh is an appropriate response
+     * @param reinstantiate true if reinstantiation is an appropriate response
+     */
+    public InternalTimeoutException(String msg, long timeoutValue,
+            boolean refresh, boolean reinstantiate) {
+        super(msg, refresh, reinstantiate);
+        this.timeoutValue = new Long(timeoutValue);
+    }
+    
+    /**
+     * Instantiate an InternalTimeoutException conveying a message and underlying 
+     * cause and specifying the timeout value that was exceeded as well as whether
+     * refresh and reinstantiation are appropriate responses to this problem.
+     * @param msg describes nature of timeout
+     * @param cause underlying cause
+     * @param timeoutValue timeout value in milliseconds that was exceeded
+     * @param refresh true if refresh is an appropriate response
+     * @param reinstantiate true if reinstantiation is an appropriate response
+     */
+    public InternalTimeoutException(String msg, Throwable cause, long timeoutValue,
+            boolean refresh, boolean reinstantiate) {
+        super(msg, cause, refresh, reinstantiate);
+        this.timeoutValue = new Long(timeoutValue);
     }
 
+    /**
+     * Instantiate an InternalTimeoutException conveying a message and specifying
+     * whether refresh and reinstantiation are appropriate responses.
+     * @param msg describes nature of timeout problem
+     * @param refresh true if refresh is an appropriate response
+     * @param reinstantiate true if reinstantiation is an appropriate response
+     */
     public InternalTimeoutException(String msg,boolean refresh, boolean reinstantiate) {
         super(msg,refresh,reinstantiate);
     }
 
+    /**
+     * Get the timeout value, in milliseconds, that was exceeded.
+     * @return the timeout value, or null if none was recorded.
+     */
     public Long getTimeoutValue() {
-        return l_timeoutValue;
+        return this.timeoutValue;
     }
 
 }
