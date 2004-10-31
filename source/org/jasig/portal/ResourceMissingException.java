96e866f2052e83243b28b4aecf4fce2fccbacd49
Andrew Petro <apetro@unicon.net>
2004-10-31 04:46:29 +0000

    added constructor to support JDK 1.4 chaining throwables via constructor. added JavaDoc comments modernized private instance variable names.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9499 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/ResourceMissingException.java
+++ source/org/jasig/portal/ResourceMissingException.java
@@ -10,37 +10,88 @@ package org.jasig.portal;
  * resource required to complete channel operation is
  * missing.
  * @author Peter Kharchenko
- * @version $Revision$
+ * @version $Revision$ $Date$
  */
-
 public class ResourceMissingException extends PortalException {
 
-    // URI of the missing resource
-    private String str_resourceURI=null;
-    private String str_description=null;
+    /**
+     * URI of the missing resource
+     */ 
+    private String resourceURI = null;
+    
+    /**
+     * Description of the missing resource.
+     */
+    private String description = null;
 
+    /**
+     * Instantiate a ResourceMissingException providing the URI of the missing resource,
+     * a description of the missing resource, and a message.
+     * @param resourceURI URI of the missing resource
+     * @param resourceDescription description of the missing resource
+     * @param msg message about the error condition
+     */
     public ResourceMissingException(String resourceURI, String resourceDescription, String msg) {
         super(msg);
-        str_resourceURI=resourceURI;
-        str_description=resourceDescription;
+        this.resourceURI = resourceURI;
+        this.description = resourceDescription;
     }
 
+    /**
+     * Instantiate a ResourceMissingException providing a URI of the missing resource,
+     * a description of the missing resource, a message, and indicating whether
+     * channel refresh and channel reinstantiation are appropriate responses to the
+     * error condition.
+     * @param resourceURI URI of the missing resource
+     * @param resourceDescription description of the missing resource
+     * @param msg message about the error condition
+     * @param refresh true if refreshing is an appropriate response
+     * @param reinstantiate true if reinstantiation is an appropriate response
+     */
     public ResourceMissingException(String resourceURI, String resourceDescription, String msg, boolean refresh, boolean reinstantiate) {
         super(msg,refresh,reinstantiate);
-        str_resourceURI=resourceURI;
-        str_description=resourceDescription;
+        this.resourceURI=resourceURI;
+        this.description=resourceDescription;
     }
 
+    /**
+     * Instantiate a bare MissingResourceException.
+     * @deprecated use a more informative constructor
+     */
     public ResourceMissingException() {
         super();
     }
-
-    public String getResourceURI() {
-        return str_resourceURI;
+    
+    /**
+     * Instantiate a ResourceMissingException providing the URI of the missing
+     * resource, a description of the missing resource, a message, and an
+     * underlying cause.
+     * @param resourceUri URI of the missing resource
+     * @param description description of the missing resource
+     * @param message message about the error condition
+     * @param cause underlying cause of this problem
+     */
+    public ResourceMissingException(String resourceUri, String description, 
+            String message, Throwable cause) {
+        super(message, cause);
+        this.resourceURI = resourceUri;
+        this.description = description;
     }
 
+    /**
+     * Get the URI of the missing resource.
+     * @return the URI of the missing resource, or null if not specified.
+     */
+    public String getResourceURI() {
+        return this.resourceURI;
+    }
+
+    /**
+     * Get a description of the missing resource.
+     * @return a description of the missing resource, or null if not specified
+     */
     public String getResourceDescription() {
-        return str_description;
+        return this.description;
     }
 
 }
