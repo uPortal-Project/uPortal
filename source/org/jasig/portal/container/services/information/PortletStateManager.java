a303f7c1556f4f423fcc6e3729327f07d135a547
Eric Dalquist <eric.dalquist@gmail.com>
2004-11-15 17:50:10 +0000

    Fix for bug 1791 	EXCLUSIVE window state urls now work.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-4-patches@9585 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/container/services/information/PortletStateManager.java
+++ source/org/jasig/portal/container/services/information/PortletStateManager.java
@@ -549,26 +549,36 @@ public class PortletStateManager {
 		return strURL;
 	} 
 	
-	public String getActionURL() {
-		String baseActionURL;
-		if ( nextState != null && nextState.equals(PortalContextProviderImpl.EXCLUSIVE) ) {
-		    baseActionURL = runtimeData.getBaseWorkerURL(UPFileSpec.FILE_DOWNLOAD_WORKER);
-		}        
-		else {
-		    baseActionURL = runtimeData.getBaseActionURL();
-		}
+    public String getActionURL() {
+        final StringBuffer actionUrl = new StringBuffer();
         
-		String encodedURLParams = encodeURLParameters(this.toString());
-        StringBuffer url = new StringBuffer((encodedURLParams.length()>0)?
-				 (UPFileSpec.PORTLET_PARAMS_DELIM_BEG+
-                  java.net.URLEncoder.encode(encodedURLParams)+
-				  UPFileSpec.PORTLET_PARAMS_DELIM_END+
-				  UPFileSpec.PORTAL_URL_SEPARATOR+baseActionURL):baseActionURL);
+        final String encodedUrlParams = encodeURLParameters(this.toString());
         
-        if (ChannelManager.isUseAnchors()) {
-            url.append("#").append(windowOfAction.getId());
+        if (encodedUrlParams.length() > 0) {
+            actionUrl.append(UPFileSpec.PORTLET_PARAMS_DELIM_BEG);
+            
+            final String urlEncodedUrlParams = URLEncoder.encode(encodedUrlParams);
+            actionUrl.append(urlEncodedUrlParams);
+            
+            actionUrl.append(UPFileSpec.PORTLET_PARAMS_DELIM_END);
         }
         
-		return url.toString();
-	}
+        if (PortalContextProviderImpl.EXCLUSIVE.equals(this.nextState)) {
+            final String urlBase = runtimeData.getBaseWorkerURL(UPFileSpec.FILE_DOWNLOAD_WORKER);
+            actionUrl.insert(0, UPFileSpec.PORTAL_URL_SEPARATOR);
+            actionUrl.insert(0, urlBase);
+        }
+        else {
+            final String urlBase = runtimeData.getBaseActionURL();
+            actionUrl.append(UPFileSpec.PORTAL_URL_SEPARATOR);
+            actionUrl.append(urlBase);
+        }
+        
+        if (ChannelManager.isUseAnchors()) {
+            actionUrl.append("#");
+            actionUrl.append(windowOfAction.getId());
+        }
+        
+        return actionUrl.toString();
+    }
 }
