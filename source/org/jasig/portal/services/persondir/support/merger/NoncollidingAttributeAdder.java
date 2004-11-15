6a2e28f3684dbfa2434ce43c26a323b3610ea11a
Andrew Petro <apetro@unicon.net>
2004-11-15 00:21:47 +0000

    Adding basic implementations of the merge strategy interface.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9558 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/services/persondir/support/merger/NoncollidingAttributeAdder.java
@@ -0,0 +1,43 @@
+/* Copyright 2004 The JA-SIG Collaborative.  All rights reserved.
+*  See license distributed with this file and
+*  available online at http://www.uportal.org/license.html
+*/
+
+package org.jasig.portal.services.persondir.support.merger;
+
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Merger which implements accumulation of Map entries such that entries once
+ * established are individually immutable.
+ * @author andrew.petro@yale.edu
+ * @version $Revision$ $Date$
+ */
+public class NoncollidingAttributeAdder implements AttributeMerger {
+
+    /**
+     * For entries in toConsider the keys of which are not keys in toModify, 
+     * adds the entry to toModify and returns toModify.
+     * @param toModify - base Map the entries of which are considered here
+     * to be immutable.
+     * @param toConsider - Map to merge into toModify for all noncolliding keys.
+     * @return toModify with entries the intersection of our method arguments,
+     * with preference given to toModify's existing entries for all key collisions.
+     */
+    public Map mergeAttributes(Map toModify, Map toConsider) {
+        if (toModify == null)
+            throw new IllegalArgumentException("toModify argument illegally null.");
+        if (toConsider == null)
+            throw new IllegalArgumentException("toConsider argument illegally null.");
+        
+        for (Iterator iter = toConsider.keySet().iterator(); iter.hasNext();){
+            String key = (String) iter.next();
+            if (! toModify.containsKey(key))
+                toModify.put(key, toConsider.get(key));
+        }
+    
+        return toModify;
+    }
+
+}
---
+++ source/org/jasig/portal/services/persondir/support/merger/ReplacingAttributeAdder.java
@@ -0,0 +1,36 @@
+/* Copyright 2004 The JA-SIG Collaborative.  All rights reserved.
+*  See license distributed with this file and
+*  available online at http://www.uportal.org/license.html
+*/
+
+package org.jasig.portal.services.persondir.support.merger;
+
+import java.util.Map;
+
+/**
+ * Attribute merge strategy whereby considered attributes over-write
+ * previously set values for attributes with colliding names.
+ * @author andrew.petro@yale.edu
+ * @version $Revision$ $Date$
+ */
+public class ReplacingAttributeAdder 
+    implements AttributeMerger {
+
+    /**
+     * Simply puts all the values in toConsider into toModify and returns toModify.
+     * This means that for keys in both toConsider and toModify, the
+     * value in toConsider will be controlling.
+     * @param toModify - the Map we are to modify
+     * @param toConsider - the Map we are to consider in modifying toModify
+     * @return the result of toModify.putAll(toConsider)
+     */
+    public Map mergeAttributes(Map toModify, Map toConsider) {
+        if (toModify == null)
+            throw new IllegalArgumentException("toModify illegally null");
+        if (toConsider == null)
+            throw new IllegalArgumentException("toConsider illegally null");
+        
+        toModify.putAll(toConsider);
+        return toModify;
+    }
+}
