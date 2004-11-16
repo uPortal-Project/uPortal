ffb83384dad9f031a48f3902b3bf1d68a76f6fe9
Andrew Petro <apetro@unicon.net>
2004-11-16 16:46:21 +0000

    updated so that getEntityIdentifier() reads through to underlying IPerson
    git-svn-id: https://source.jasig.org/uPortal/trunk@9588 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/security/provider/RestrictedPerson.java
+++ source/org/jasig/portal/security/provider/RestrictedPerson.java
@@ -101,7 +101,7 @@ public class RestrictedPerson implements IPerson {
      * @return null
      */
     public EntityIdentifier getEntityIdentifier() {
-        return null;
+        return this.person.getEntityIdentifier();
     }
 }
 
