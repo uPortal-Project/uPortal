e6f777f19f720d8469c2e5758fe4921b47639e4f
Andrew Petro <apetro@unicon.net>
2004-12-01 19:30:38 +0000

    added testing of attribute name reporting.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9630 f5dbab47-78f9-eb45-b975-e544023573eb

--- tests/org/jasig/portal/services/persondir/support/JdbcPersonAttributeDaoImplTest.java
+++ tests/org/jasig/portal/services/persondir/support/JdbcPersonAttributeDaoImplTest.java
@@ -25,10 +25,47 @@ import junit.framework.TestCase;
  */
 public class JdbcPersonAttributeDaoImplTest extends TestCase {
 
-   public void testDao() throws SQLException {
-       DataSource testDataSource = new TransientDatasource();
+    /**
+     * The JDBC person attribute dao implementation we are testing.
+     */
+    private JdbcPersonAttributeDaoImpl dao;
+    
+    private DataSource ds;
+    
+    protected void setUp() throws SQLException {
+        this.ds = new TransientDatasource();
+        
+        
+        
+        JdbcPersonAttributeDaoImpl impl = 
+            new JdbcPersonAttributeDaoImpl(this.ds, 
+                    "SELECT name, email, shirt_color FROM user_table WHERE netid = ?");
+        
+        Map columnsToAttributes = new HashMap();
+        columnsToAttributes.put("name", "firstName");
+        
+        Set emailAttributeNames = new HashSet();
+        emailAttributeNames.add("email");
+        emailAttributeNames.add("emailAddress");
+        columnsToAttributes.put("email", emailAttributeNames);
+        columnsToAttributes.put("shirt_color", "dressShirtColor");
+        impl.setColumnsToAttributes(columnsToAttributes);
+        
+        this.dao = impl;
+    }
+    
+    protected void tearDown() throws SQLException {
        
-       Connection con = testDataSource.getConnection();
+    }
+    
+    /**
+     * Test that the implementation properly looks up attributes for a user.
+     * @throws SQLException
+     */
+   public void testAttributesForUser() throws SQLException {
+       
+       //  set up the datasource
+       Connection con = this.ds.getConnection();
        
        con.prepareStatement("CREATE TABLE user_table " +
             "(netid VARCHAR, " +
@@ -42,27 +79,38 @@ public class JdbcPersonAttributeDaoImplTest extends TestCase {
        
        con.close();
        
-       JdbcPersonAttributeDaoImpl impl = 
-           new JdbcPersonAttributeDaoImpl(testDataSource, 
-                   "SELECT name, email, shirt_color FROM user_table WHERE netid = ?");
+       try {
+           Map attribs = this.dao.attributesForUser("awp9");
+           assertEquals("andrew.petro@yale.edu", attribs.get("email"));
+           assertEquals("andrew.petro@yale.edu", attribs.get("emailAddress"));
+           assertEquals("blue", attribs.get("dressShirtColor"));
+           assertNull(attribs.get("shirt_color"));
+           assertEquals("Andrew", attribs.get("firstName"));
+       } finally {
+           // reset the database
+           con = this.ds.getConnection();
+            
+            con.prepareStatement("DROP TABLE user_table").execute();
+            
+            
+            con.close();
+       }
+
+   }
+   
+   /**
+    * Test that the implementation properly reports the attribute names it
+    * expects to map.
+    */
+   public void testAttributeNames() {
+       Set expectedAttributeNames = new HashSet();
+       expectedAttributeNames.add("firstName");
+       expectedAttributeNames.add("email");
+       expectedAttributeNames.add("emailAddress");
+       expectedAttributeNames.add("dressShirtColor");
        
-       Map columnsToAttributes = new HashMap();
-       columnsToAttributes.put("name", "firstName");
-       
-       Set emailAttributeNames = new HashSet();
-       emailAttributeNames.add("email");
-       emailAttributeNames.add("emailAddress");
-       columnsToAttributes.put("email", emailAttributeNames);
-       columnsToAttributes.put("shirt_color", "dressShirtColor");
-       impl.setColumnsToAttributes(columnsToAttributes);
-       
-       Map attribs = impl.attributesForUser("awp9");
-       assertEquals("andrew.petro@yale.edu", attribs.get("email"));
-       assertEquals("andrew.petro@yale.edu", attribs.get("emailAddress"));
-       assertEquals("blue", attribs.get("dressShirtColor"));
-       assertNull(attribs.get("shirt_color"));
-       assertEquals("Andrew", attribs.get("firstName"));
-       
+       Set attributeNames = this.dao.getAttributeNames();
+       assertEquals(attributeNames, expectedAttributeNames);
    }
 
 }
