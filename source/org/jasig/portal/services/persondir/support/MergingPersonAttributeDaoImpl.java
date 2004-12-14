390fc9b1de1e944971fdde7cffdd11d385e67358
Andrew Petro <apetro@unicon.net>
2004-12-14 03:16:34 +0000

    removed imports made unnecessary by Log being available in superclass.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9661 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/services/persondir/support/MergingPersonAttributeDaoImpl.java
+++ source/org/jasig/portal/services/persondir/support/MergingPersonAttributeDaoImpl.java
@@ -13,8 +13,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.jasig.portal.services.persondir.support.merger.IAttributeMerger;
 import org.jasig.portal.services.persondir.support.merger.MultivaluedAttributeMerger;
 
