77e000efc50516eb4a270650412220946e076acb
Michael Ivanov <mvi@immagic.com>
2004-12-18 20:09:40 +0000

    Added ILayoutManager interface
    git-svn-id: https://source.jasig.org/uPortal/trunk@9697 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/layout/umf/ILayoutManager.java
+++ source/org/jasig/portal/layout/umf/ILayoutManager.java
@@ -7,18 +7,19 @@ package org.jasig.portal.layout.umf;
 
 import org.jasig.portal.layout.node.INodeId;
 import org.jasig.portal.layout.node.ILayoutNode;
+import org.jasig.portal.layout.node.IUserLayoutNodeDescription;
 
 /**
  * User Modifiable Fragment Layout Manager interface.
  * @author Michael Ivanov <a href="mailto:">mvi@immagic.com</a>
  * @version $Revision$
  */
-public interface IUmfLayoutManager extends ILayoutManager {
+public interface ILayoutManager {
 	
-	public ILayoutNode addNode ( ILayoutNodeId nodeId, IFragmentNodeId parentId, INodeId nextId );
-	public ILayoutNode addNode ( IFragmentNodeId nodeId, ILayoutNodeId parentId, INodeId nextId );
-	public ILayoutNode addNode ( IFragmentNodeId nodeId, IFragmentNodeId parentId, INodeId nextId );
+	public ILayoutNode addNode ( ILayoutNodeId nodeId, ILayoutNodeId parentId, INodeId nextId );
 	
-	public boolean deleteNode ( IFragmentNodeId nodeId );
+	public boolean deleteNode ( ILayoutNodeId nodeId );
 	
+	public IUserLayoutNodeDescription updateNode ( IUserLayoutNodeDescription nodeDesc );
+
 }
