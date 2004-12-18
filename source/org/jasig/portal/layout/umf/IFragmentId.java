f4eee427a08a05e61a7f3b41f9f0699c5d35e8bb
Michael Ivanov <mvi@immagic.com>
2004-12-18 21:41:23 +0000

    Added the fragment id interface
    git-svn-id: https://source.jasig.org/uPortal/trunk@9703 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ source/org/jasig/portal/layout/umf/IFragmentId.java
@@ -0,0 +1,18 @@
+/* Copyright 2004 The JA-SIG Collaborative.  All rights reserved.
+*  See license distributed with this file and
+*  available online at http://www.uportal.org/license.html
+*/
+
+package org.jasig.portal.layout.umf;
+
+import org.jasig.portal.core.IObjectId;
+
+
+/**
+ * The fragment D interface.
+ * @author Michael Ivanov <a href="mailto:">mvi@immagic.com</a>
+ * @version $Revision$
+ */
+public interface IFragmentId extends IObjectId {
+
+}
