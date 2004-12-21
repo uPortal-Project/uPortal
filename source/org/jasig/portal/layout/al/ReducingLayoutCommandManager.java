4e23ae576ffa46a1f9386126d2f3ed761c087088
peterk@committers.jasig.org <peterk@committers.jasig.org>
2004-12-21 17:47:28 +0000

    moved away from ILayoutCommand arguments, implemeted new ILayoutManagerCommands interface
    git-svn-id: https://source.jasig.org/uPortal/trunk@9730 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/layout/al/ReducingLayoutCommandManager.java
+++ source/org/jasig/portal/layout/al/ReducingLayoutCommandManager.java
@@ -8,6 +8,11 @@ package org.jasig.portal.layout.al;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import org.jasig.portal.PortalException;
+import org.jasig.portal.layout.al.common.ILayoutManager;
+import org.jasig.portal.layout.al.common.node.INode;
+import org.jasig.portal.layout.al.common.node.INodeDescription;
+import org.jasig.portal.layout.al.common.node.INodeId;
 
 /**
  * A layout command manager that optimizies command recording
@@ -15,18 +20,22 @@ import java.util.List;
  * removing commands that can no longer be performed.
  * 
  * @author Peter Kharchenko: pkharchenko at unicon.net
- * @author Michael Ivanov: mvi at immagic.com
  */
-public class ReducingLayoutCommandManager extends AbstractLayoutCommandManager {
+public class ReducingLayoutCommandManager implements ILayoutCommandManager {
+    List commands;
+    ILayoutManager layoutManager;
     
-    public ReducingLayoutCommandManager() {
-       super();
+    
+    /* (non-Javadoc)
+     * @see org.jasig.portal.layout.al.ILayoutCommandManager#clearCommands()
+     */
+    public void clearCommands() {
+        commands.clear();
     }
-  
     /* (non-Javadoc)
      * @see org.jasig.portal.layout.al.ILayoutCommandManager#executeLayoutCommands(org.jasig.portal.layout.al.ILayoutManager)
      */
-    public void executeLayoutCommands() {
+    public void executeLayoutCommands() throws PortalException {
         List successfulCommands=new ArrayList();
         for (Iterator iter = this.commands.iterator(); iter.hasNext();) {
             ILayoutCommand command = (ILayoutCommand) iter.next();
@@ -39,10 +48,49 @@ public class ReducingLayoutCommandManager extends AbstractLayoutCommandManager {
     }
     
     /* (non-Javadoc)
-     * @see org.jasig.portal.layout.al.ILayoutCommandManager#recordAttachCommand(org.jasig.portal.layout.node.INodeId, org.jasig.portal.layout.node.INodeId, org.jasig.portal.layout.node.INodeId)
+     * @see org.jasig.portal.layout.al.ILayoutCommandManager#recordAddCommand(org.jasig.portal.layout.node.IUserLayoutNodeDescription, org.jasig.portal.layout.node.INodeId, org.jasig.portal.layout.node.INodeId)
      */
-    public void recordLayoutCommand(ILayoutCommand layoutCommand) {
-        // TODO
+    public INodeDescription addNode(INode nodeId, INodeId parentId, INodeId nextId) {
+        // reduction rules: add operation is always done on a new node, so there
+        // are should not be any commands to reduce
+        commands.add(new AddCommand(nodeId,parentId,nextId));
+        return null;
     }
-   
+    /* (non-Javadoc)
+     * @see org.jasig.portal.layout.al.ILayoutCommandManager#recordMoveCommand(org.jasig.portal.layout.node.INodeId, org.jasig.portal.layout.node.INodeId, org.jasig.portal.layout.node.INodeId)
+     */
+    public boolean moveNode(INodeId nodeId, INodeId parentId, INodeId nextId) {
+        commands.add(new MoveCommand(nodeId,parentId,nextId));
+        return true;
+    }
+    /* (non-Javadoc)
+     * @see org.jasig.portal.layout.al.common.ILayoutManagerCommands#updateNode(org.jasig.portal.layout.al.common.node.INodeId, org.jasig.portal.layout.al.common.node.INodeDescription)
+     */
+    public boolean updateNode(INodeId nodeId, INodeDescription nodeDescription) throws PortalException {
+        commands.add(new UpdateCommand(nodeId,nodeDescription));
+        return true;
+    }
+    /* (non-Javadoc)
+     * @see org.jasig.portal.layout.al.ILayoutCommandManager#recordDeleteCommand(org.jasig.portal.layout.node.INodeId)
+     */
+    public boolean deleteNode(INodeId nodeId) {
+        // reduction rules: reduce all commands involving that node id, unless
+        //   nodeId is a fragment root and there are commands involving
+        //   that fragment (not node!) in history besides the attach command.        
+        commands.add(new DeleteCommand(nodeId));
+        return true;
+    }
+    
+    /**
+     * @return Returns the layoutManager.
+     */
+    public ILayoutManager getLayoutManager() {
+        return layoutManager;
+    }
+    /**
+     * @param layoutManager The layoutManager to set.
+     */
+    public void setLayoutManager(ILayoutManager layoutManager) {
+        this.layoutManager = layoutManager;
+    }
 }
--- source/org/jasig/portal/layout/al/SimpleLayoutCommandManager.java
+++ source/org/jasig/portal/layout/al/SimpleLayoutCommandManager.java
@@ -6,24 +6,27 @@
 package org.jasig.portal.layout.al;
 
 import java.util.Iterator;
+import java.util.List;
+import org.jasig.portal.PortalException;
+import org.jasig.portal.layout.al.common.ILayoutManager;
+import org.jasig.portal.layout.al.common.node.INode;
+import org.jasig.portal.layout.al.common.node.INodeDescription;
+import org.jasig.portal.layout.al.common.node.INodeId;
 
 /**
  * A naive implementation of the command manager that
  * appends commands to a list without any reduction procedures.
  * 
  * @author Peter Kharchenko: pkharchenko at unicon.net
- * @author Michael Ivanov: mvi at immagic.com
  */
-public class SimpleLayoutCommandManager extends AbstractLayoutCommandManager {
-    
-    public SimpleLayoutCommandManager() {
-    	super();
-    }
+public class SimpleLayoutCommandManager implements ILayoutCommandManager {
+    List commands;
+    ILayoutManager layoutManager;
     
     /* (non-Javadoc)
      * @see org.jasig.portal.layout.al.ILayoutCommandManager#executeLayoutCommands(org.jasig.portal.layout.al.ILayoutManager)
      */
-    public void executeLayoutCommands() {
+    public void executeLayoutCommands() throws PortalException {
         for (Iterator iter = commands.iterator(); iter.hasNext();) {
             ILayoutCommand command = (ILayoutCommand) iter.next();
             command.execute(this.layoutManager);
@@ -32,8 +35,47 @@ public class SimpleLayoutCommandManager extends AbstractLayoutCommandManager {
     /* (non-Javadoc)
      * @see org.jasig.portal.layout.al.ILayoutCommandManager#recordAddCommand(org.jasig.portal.layout.node.IUserLayoutNodeDescription, org.jasig.portal.layout.node.INodeId, org.jasig.portal.layout.node.INodeId)
      */
-    public void recordLayoutCommand(ILayoutCommand layoutCommand) {
-        commands.add(layoutCommand);
+    public INodeDescription addNode(INode nodeId, INodeId parentId, INodeId nextId) {
+        commands.add(new AddCommand(nodeId,parentId,nextId));
+        return null;
     }
-    
+    /* (non-Javadoc)
+     * @see org.jasig.portal.layout.al.ILayoutCommandManager#recordMoveCommand(org.jasig.portal.layout.node.INodeId, org.jasig.portal.layout.node.INodeId, org.jasig.portal.layout.node.INodeId)
+     */
+    public boolean moveNode(INodeId nodeId, INodeId parentId, INodeId nextId) {
+        commands.add(new MoveCommand(nodeId,parentId,nextId));
+        return true;
+    }
+    /* (non-Javadoc)
+     * @see org.jasig.portal.layout.al.common.ILayoutManagerCommands#updateNode(org.jasig.portal.layout.al.common.node.INodeId, org.jasig.portal.layout.al.common.node.INodeDescription)
+     */
+    public boolean updateNode(INodeId nodeId, INodeDescription nodeDescription) throws PortalException {
+        commands.add(new UpdateCommand(nodeId,nodeDescription));
+        return true;
+    }
+    /* (non-Javadoc)
+     * @see org.jasig.portal.layout.al.ILayoutCommandManager#recordDeleteCommand(org.jasig.portal.layout.node.INodeId)
+     */
+    public boolean deleteNode(INodeId nodeId) {
+        commands.add(new DeleteCommand(nodeId));
+        return true;
+    }
+    /* (non-Javadoc)
+     * @see org.jasig.portal.layout.al.ILayoutCommandManager#clearCommands()
+     */
+    public void clearCommands() {
+        commands.clear();
+    }
+    /**
+     * @return Returns the layoutManager.
+     */
+    public ILayoutManager getLayoutManager() {
+        return layoutManager;
+    }
+    /**
+     * @param layoutManager The layoutManager to set.
+     */
+    public void setLayoutManager(ILayoutManager layoutManager) {
+        this.layoutManager = layoutManager;
+    }
 }
