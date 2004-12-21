66410e037b900011ca386dd25d15d3e6d09ed130
peterk@committers.jasig.org <peterk@committers.jasig.org>
2004-12-21 17:20:48 +0000

    rollback
    git-svn-id: https://source.jasig.org/uPortal/trunk@9724 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/layout/al/common/node/NodeType.java
+++ source/org/jasig/portal/layout/al/common/node/NodeType.java
@@ -9,32 +9,20 @@ package org.jasig.portal.layout.al.common.node;
  * Node type constant class
  * 
  * @author Peter Kharchenko: pkharchenko at unicon.net
- * @author Michael Ivanov: mvi at immagic.com
  */
 public class NodeType {
-	
     private final String name;
     
     public final static NodeType CHANNEL=new NodeType("channel");
     public final static NodeType FOLDER=new NodeType("folder");
     
-    protected NodeType(String name) {
+    public NodeType(String name) {
         this.name=name;
     }
-    
     /**
      * @return Returns the name.
      */
     public String getName() {
         return name;
     }
-    
-    public String toString() {
-      	return name;
-    }
-      
-    public boolean equals ( Object obj ) {
-      	return ( (obj instanceof NodeType) && obj.toString().equals(name) );
-    }
-    
 }
