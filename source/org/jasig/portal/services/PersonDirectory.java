965cce2f07e310b1accbf7a62ba38f5f98c9660a
Andrew Petro <apetro@unicon.net>
2004-12-24 19:38:47 +0000

    javadoc improvements.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9749 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/services/PersonDirectory.java
+++ source/org/jasig/portal/services/PersonDirectory.java
@@ -22,18 +22,24 @@ import org.jasig.portal.spring.PortalApplicationContextFacade;
 import org.springframework.beans.factory.BeanFactory;
 
 /**
- * PersonDirectory is a source for user attributes.  It is configurable via a
+ * PersonDirectory is a static lookup mechanism for a singleton instance of 
+ * IPersonAttributeDao.  It is configurable via a
  * Spring beans.dtd compliant configuration file in the properties directory
- * called personDirectory.xml. 
+ * called personDirectory.xml (as referenced by applicationContext.xml -
+ * that is, you could choose to declare the underlying IPersonAttributesDao
+ * backing your PersonDirectory directly in applicationContext.xml, 
+ * or elsewhere. PersonDirectory looks for an IPersonAttributeDao instance 
+ * named 'personAttributeDao'.
  * 
- * This class serves as the lookup mechanism and the interface by which client
- * code accesses user attributes.  Implementation of actually getting attributes
- * is delegated to an IPersonDirectory implementation which is declared as the
- * bean named "personAttributeDao" in the personDirectory.xml Spring configuration
- * file.
+ * This class serves as the lookup mechanism for clients to obtain a reference
+ * to the singleton IPersonAttributeDao instance.  Via legacy methods, 
+ * PersonDirectory also serves as the interface by which client
+ * code accesses user attributes.  These deprecated legacy methods are a facade
+ * to the PersonAttributeDao.
  * 
  * The default configuration of that file implements the legacy behavior of using
- * the PersonDirs.xml file for configuration.
+ * the PersonDirs.xml file for configuration.  It is expected that PersonDirs.xml
+ * offers the flexibility necessary to support most uPortal installations.
  * 
  * @author Howard Gilbert
  * @author andrew.petro@yale.edu
@@ -42,7 +48,8 @@ import org.springframework.beans.factory.BeanFactory;
  */
 public class PersonDirectory {
 
-    private static final Log log = LogFactory.getLog(PersonDirectory.class);
+    private static final String PADAO_BEAN_NAME = "personAttributeDao";
+	private static final Log log = LogFactory.getLog(PersonDirectory.class);
 
     /**
      * This instance variable used to contain the set of attributes mapped in
@@ -70,23 +77,24 @@ public class PersonDirectory {
     }
     
     /**
-     * Static lookup for a the Spring configured {@link IPersonAttributeDao}
-     * implementation.
+     * Static lookup for a the configured {@link IPersonAttributeDao}
+     * implementation available from PortalApplicationContextFacade.
      * 
-     * @return The Spring configured {@link IPersonAttributeDao} implementation.
+     * @return The PortalApplicationContextFacade configured {@link IPersonAttributeDao} implementation.
+     * @throws IllegalStateException - if PortalApplicationContextFacade does not
+     * supply the IPersonAttributeDao instance.
      */
     public static IPersonAttributeDao getPersonAttributeDao() {
         final BeanFactory factory = PortalApplicationContextFacade.getPortalApplicationContext();
         
-        final Object objectDelegate = factory.getBean("personAttributeDao");
+        final IPersonAttributeDao delegate = (IPersonAttributeDao) 
+			factory.getBean(PADAO_BEAN_NAME, IPersonAttributeDao.class);
         
-        if (objectDelegate == null)
-            throw new NullPointerException("Spring config file did not declare a bean named 'personDirectory'.");
-        
-        if (!(objectDelegate instanceof IPersonAttributeDao))
-            throw new ClassCastException("Spring config file declared a 'personDirectory' bean which was not an instance of IPersonAttributeDao.");
-        
-        return (IPersonAttributeDao)objectDelegate;
+        if (delegate == null)
+            throw new IllegalStateException("PortalAppicationContextFacade " +
+            		"config did not declare a bean named '" + PADAO_BEAN_NAME + "'.");
+                
+        return delegate;
     }
 
     /**
@@ -109,17 +117,18 @@ public class PersonDirectory {
     }
 
     /**
-     * This method used to return an iterator over the names of attributes. The
+     * This method returns an iterator over the names of attributes. The
      * method behavior is not well defined because attribute sources may choose
      * to return different attributes depending upon about whom they are asked.
      * Therefore, you can only know the attributes for particular users, not the
      * namespace of all possible attributes.
      * 
-     * @return an iterator for an empty list.
+     * @return an iterator over the attribute names declared by the underlying
+     * IPersonAttributeDao instance, if any.
      * @deprecated Use {@link IPersonAttributeDao#getPossibleUserAttributeNames()}
      */
     public static Iterator getPropertyNamesIterator() {
-        final Set attrNames = instance().impl.getPossibleUserAttributeNames();
+        final Set attrNames = getPersonAttributeDao().getPossibleUserAttributeNames();
         
         if (attrNames != null)
             return attrNames.iterator();
