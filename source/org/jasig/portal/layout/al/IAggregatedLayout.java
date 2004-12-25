c3b351b72f1ce81c2057c4f64a6687b5d92dce9c
peterk@committers.jasig.org <peterk@committers.jasig.org>
2004-12-25 05:05:11 +0000

    merged aggregated layout and agg. layout manager.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9757 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/layout/al/IAggregatedLayout.java
+++ source/org/jasig/portal/layout/al/IAggregatedLayout.java
@@ -7,8 +7,7 @@ package org.jasig.portal.layout.al;
 
 import java.util.Set;
 import org.jasig.portal.PortalException;
-import org.jasig.portal.layout.al.common.IUserLayout;
-import org.jasig.portal.layout.al.common.node.INodeId;
+import org.jasig.portal.layout.al.common.ILayout;
 
 /**
  * An aggregated-layout specific extension of the user layout interface
@@ -16,7 +15,7 @@ import org.jasig.portal.layout.al.common.node.INodeId;
  * @author <a href="mailto:mvi@immagic.com">Michael Ivanov</a>
  * @version 1.1
  */
-public interface IAggregatedLayout extends IUserLayout {
+public interface IAggregatedLayout extends ILayout {
 
     // the tag names constants
     public static final String LAYOUT = "layout";
@@ -37,48 +36,5 @@ public interface IAggregatedLayout extends IUserLayout {
      * @exception PortalException if an error occurs
      */
     public Set getFragmentIds() throws PortalException;
-
-    /**
-     * Returns an fragment Id for a given node.
-     * Returns null if the node is not part of any fragments.
-     *
-     * @param nodeId a node id
-     * @return a fragment id
-     * @exception PortalException if an error occurs
-     */
-    public IFragmentId getFragmentId(INodeId nodeId) throws PortalException;
-
-    /**
-     * Returns an fragment root Id for a given fragment.
-     *
-     * @param fragmentId a fragment id
-     * @return a fragment root node id
-     * @exception PortalException if an error occurs
-     */
-    public IFragmentLocalNodeId getFragmentRootId(IFragmentId fragmentId) throws PortalException;
     
-    /**
-     * Returns the node by a given node id.
-     *
-     * @param nodeId a <code>String</code> value
-     * @return a <code>ALNode</code> instance
-     */
-    public IALNode getLayoutNode(INodeId nodeId);
-    
-    /**
-     * Returns the folder by a given folder ID.
-     *
-     * @param folderId a folder node id
-     * @return a <code>ALFolder</code> instance
-     */
-    public IALFolder getLayoutFolder(INodeId folderId);
-    
-    /**
-     * Gets the tree depth for a given node
-     * @param nodeId a node id
-     * @return a depth value
-     * @exception PortalException if an error occurs
-     */
-    public int getDepth(INodeId nodeId) throws PortalException;
-   
 }
