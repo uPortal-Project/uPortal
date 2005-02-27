dadcfa7c7be987fe958ff16ec9515d4dd7687b80
Andrew Petro <apetro@unicon.net>
2005-02-27 21:34:48 +0000

    UP-757 fixed license to be JA-SIG license.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9917 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/tools/checks/ICheck.java
+++ source/org/jasig/portal/tools/checks/ICheck.java
@@ -1,10 +1,7 @@
-/*
- * Created on Feb 25, 2005
- *
- * Copyright(c) Yale University, Feb 25, 2005.  All rights reserved.
- * (See licensing and redistribution disclosures at end of this file.)
- * 
- */
+/* Copyright 2001, 2004 The JA-SIG Collaborative.  All rights reserved.
+*  See license distributed with this file and
+*  available online at http://www.uportal.org/license.html
+*/
 package org.jasig.portal.tools.checks;
 
 /**
@@ -44,37 +41,3 @@ public interface ICheck {
     public String getDescription();
     
 }
-
-
-/* Check.java
- * 
- * Copyright (c) Feb 25, 2005 Yale University.  All rights reserved.
- *
- * THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ARE EXPRESSLY
- * DISCLAIMED. IN NO EVENT SHALL YALE UNIVERSITY OR ITS EMPLOYEES BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED, THE COSTS OF
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED IN ADVANCE OF THE POSSIBILITY OF SUCH
- * DAMAGE.
- * 
- * Redistribution and use of this software in source or binary forms,
- * with or without modification, are permitted, provided that the
- * following conditions are met.
- * 
- * 1. Any redistribution must include the above copyright notice and
- * disclaimer and this list of conditions in any related documentation
- * and, if feasible, in the redistributed software.
- * 
- * 2. Any redistribution must include the acknowledgment, "This product
- * includes software developed by Yale University," in any related
- * documentation and, if feasible, in the redistributed software.
- * 
- * 3. The names "Yale" and "Yale University" must not be used to endorse
- * or promote products derived from this software.
- */
