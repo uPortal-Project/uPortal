636f8d43c0328661e3adaa78d960013bc4085c3b
Andrew Petro <apetro@unicon.net>
2005-03-29 18:24:45 +0000

    UP-896 added testcase demonstrating ChannelDefinition's having failed to set ID attribute.
    git-svn-id: https://source.jasig.org/uPortal/trunk@9990 f5dbab47-78f9-eb45-b975-e544023573eb

--- tests/org/jasig/portal/ChannelDefinitionTest.java
+++ tests/org/jasig/portal/ChannelDefinitionTest.java
@@ -1,19 +1,28 @@
-/* Copyright 2004 The JA-SIG Collaborative.  All rights reserved.
+/* Copyright 2004 - 2005 The JA-SIG Collaborative.  All rights reserved.
 *  See license distributed with this file and
 *  available online at http://www.uportal.org/license.html
 */
 
 package org.jasig.portal;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import junit.framework.TestCase;
 
 /**
  * Testcase for ChannelDefinition class.
- * @author andrew.petro@yale.edu
  * @version $Revision$ $Date$
  */
 public class ChannelDefinitionTest extends TestCase {
 
+    /**
+     * Test that ChannelDefinition identifies CPortletAdaptor as a portlet but
+     * does not identify CGenericXSLT as a portlet.
+     */
     public void testIsPortlet() {
         ChannelDefinition cd = new ChannelDefinition(72);
         
@@ -22,6 +31,53 @@ public class ChannelDefinitionTest extends TestCase {
         
         cd.setJavaClass("org.jasig.portal.channels.portlet.CPortletAdapter");
         assertTrue(cd.isPortlet());
+        
+    }
+    
+    /**
+     * Test Element representation of channel to see that its node name is 
+     * 'channel' and that it has the proper owning document and that the channel
+     * element has the correct identifer such that it can be gotten by ID.
+     * @throws ParserConfigurationException
+     */
+    public void testGetDocument() throws ParserConfigurationException {
+        ChannelDefinition cd = new ChannelDefinition(73);
+        
+        cd.setDescription("A test channel description.");
+        cd.setEditable(false);
+        cd.setFName("test_fname");
+        cd.setHasAbout(true);
+        cd.setHasHelp(false);
+        cd.setIsSecure(false);
+        cd.setJavaClass("org.jasig.portal.channels.CGenericXSLT");
+        cd.setName("testName");
+        cd.setTimeout(500);
+        cd.setTitle("testTitle");
+        cd.setTypeId(12);
+        
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+        
+        Element channelElement = cd.getDocument(doc, "testId");
+        
+        assertEquals("channel", channelElement.getNodeName());
+        assertSame(doc, channelElement.getOwnerDocument());
+        
+        doc.appendChild(channelElement);
+        
+        assertSame(channelElement, doc.getElementById("testId"));
+        
+        assertEquals("A test channel description.", channelElement.getAttribute("description"));
+        assertEquals("false", channelElement.getAttribute("editable"));
+        assertEquals("test_fname", channelElement.getAttribute("fname"));
+        assertEquals("true", channelElement.getAttribute("hasAbout"));
+        assertEquals("false", channelElement.getAttribute("hasHelp"));
+        assertEquals("false", channelElement.getAttribute("secure"));
+        assertEquals("org.jasig.portal.channels.CGenericXSLT", channelElement.getAttribute("class"));
+        assertEquals("testName", channelElement.getAttribute("name"));
+        assertEquals("500", channelElement.getAttribute("timeout"));
+        assertEquals("testTitle", channelElement.getAttribute("title"));
+        assertEquals("12", channelElement.getAttribute("typeID"));
+
         
     }
 
