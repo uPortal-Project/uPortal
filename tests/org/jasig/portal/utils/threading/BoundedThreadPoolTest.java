b040247d3764938366dd8238d58364dfebccfbce
Andrew Petro <apetro@unicon.net>
2005-04-28 18:25:39 +0000

    Adding (non-passing!) JUnit testcase to document UP-1016
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-4-patches@10211 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ tests/org/jasig/portal/utils/threading/BoundedThreadPoolTest.java
@@ -0,0 +1,41 @@
+/* Copyright 2005 The JA-SIG Collaborative.  All rights reserved.
+*  See license distributed with this file and
+*  available online at http://www.uportal.org/license.html
+*/
+
+package org.jasig.portal.utils.threading;
+
+import junit.framework.TestCase;
+
+/**
+ * Testcase for BoundedThreadPool.
+ * @version $Revision$ $Date$
+ */
+public class BoundedThreadPoolTest extends TestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * This testcase exercises BoundedThreadPool to ensure that thread destroying
+     * at least doesn't throw anything.
+     * @throws Exception as a failure modality
+     */
+    public void testDestroyThread() throws Exception {
+        BoundedThreadPool btp = new BoundedThreadPool(1, 1, 5);
+        
+        Thread thread = btp.createNewThread();
+        
+        // In BoundedThreadPool version 1.8, the following call resulted in
+        // StackOverflowError
+        
+        btp.destroyThread(thread);
+
+    }
+
+}
