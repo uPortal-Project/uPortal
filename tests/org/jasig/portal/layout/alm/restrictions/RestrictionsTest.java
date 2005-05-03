0850d252bf9868639aaa5b80d6006af31ee8629c
Andrew Petro <apetro@unicon.net>
2005-05-03 20:54:45 +0000

    UP-1027 Repackaged from org.jasig.portal.layout.restrictions.
    git-svn-id: https://source.jasig.org/uPortal/trunk@10267 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ tests/org/jasig/portal/layout/alm/restrictions/RestrictionsTest.java
@@ -0,0 +1,31 @@
+/* Copyright 2002 The JA-SIG Collaborative.  All rights reserved.
+*  See license distributed with this file and
+*  available online at http://www.uportal.org/license.html
+*/
+
+package org.jasig.portal.layout.alm.restrictions;
+
+import org.jasig.portal.layout.restrictions.alm.RestrictionTypes;
+
+import junit.framework.*;
+
+
+public class RestrictionsTest extends TestCase {
+  
+
+    protected void setUp() throws Exception {
+       
+    }
+
+    public void testRestrictionTypes() throws Exception {
+    	
+    	System.out.println ( RestrictionTypes.DEPTH_RESTRICTION.hashCode() );
+    	System.out.println ( RestrictionTypes.PRIORITY_RESTRICTION.hashCode() );
+    	System.out.println ( RestrictionTypes.HIDDEN_RESTRICTION.hashCode() );
+    	System.out.println ( RestrictionTypes.UNREMOVABLE_RESTRICTION.hashCode() );
+    	System.out.println ( RestrictionTypes.IMMUTABLE_RESTRICTION.hashCode() );
+    	System.out.println ( RestrictionTypes.GROUP_RESTRICTION.hashCode() );
+    	
+    }
+    
+}
