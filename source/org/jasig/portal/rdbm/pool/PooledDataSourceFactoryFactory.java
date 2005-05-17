1cef65bd5f0271148b62fb08a43d0d12aee9e1e0
Andrew Petro <apetro@unicon.net>
2005-05-17 23:10:15 +0000

    Re-implementation to resolve bugs and improve logging, add code comments. UP-1082 fixed default we use when our property is not set UP-1081 resolved double checked locking by blanket synchronization
    git-svn-id: https://source.jasig.org/uPortal/trunk@10340 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/rdbm/pool/PooledDataSourceFactoryFactory.java
+++ source/org/jasig/portal/rdbm/pool/PooledDataSourceFactoryFactory.java
@@ -1,4 +1,4 @@
-/* Copyright 2004 The JA-SIG Collaborative.  All rights reserved.
+/* Copyright 2004-2005 The JA-SIG Collaborative.  All rights reserved.
 *  See license distributed with this file and
 *  available online at http://www.uportal.org/license.html
 */
@@ -7,69 +7,122 @@ package org.jasig.portal.rdbm.pool;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.jasig.portal.PortalException;
-import org.jasig.portal.layout.UserLayoutStoreFactory;
+import org.jasig.portal.properties.MissingPropertyException;
 import org.jasig.portal.properties.PropertiesManager;
 
 /**
  * @author Eric Dalquist <a href="mailto:edalquist@unicon.net">edalquist@unicon.net</a>
  * @version $Revision$ $Date$
+ * @since uPortal 2.5
  */
 public final class PooledDataSourceFactoryFactory {
 
-    private static final Log log = LogFactory.getLog(PooledDataSourceFactoryFactory.class);
+    /**
+     * The name of the portal.properties property the value of which will be the name of the
+     * IPooledDataSourceFactory implementation that we will use.
+     */
+    public static final String POOLED_DATA_SOURCE_FACTORY_PROPERTY = "org.jasig.portal.PooledDataSourceFactory.implementation";
+    
+    /**
+     * A Commons Logging log instance.
+     */
+    private static final Log LOG = LogFactory.getLog(PooledDataSourceFactoryFactory.class);
 
-    private static final String DEFAULT_CLASS_NAME = "org.jasig.portal.RDBMPortletPreferencesStore";
+    /**
+     * Our default IPooledDataSourceFactory implementation upon which we will fall back if
+     * our property is not set or we cannot instantiate the implementation specified by
+     * our property.
+     * 
+     * Default scoped to be accessible to our testcase.
+     */
+    static final Class DEFAULT_POOLED_DATASOURCE_FACTORY = DBCPDataSourceFactory.class;
 
+    /**
+     * Our static singleton instance that we're managing.
+     */
     private static IPooledDataSourceFactory pooledDataSourceFactoryImpl = null;
-    private static String className = null;
 
-    static {
-        try {
-            // Retrieve the class name of the concrete IPortletPreferencesStore implementation
-            className = PropertiesManager.getProperty("org.jasig.portal.PooledDataSourceFactory.implementation");
-        }
-        catch (Exception e) { }
-
-        if (className == null || className.length() == 0)
-            log.error("org.jasig.portal.PooledDataSourceFactory.implementation must be specified in portal.properties");
-    }
-
-    public static final IPooledDataSourceFactory getPooledDataSourceFactory() {
-        try {
-            return getPooledDataSourceFactory(className);
-        }
-        catch (PortalException pe) {
-            log.error(" Could not load " + className, pe);
-
-            try {
-                return getPooledDataSourceFactory(DEFAULT_CLASS_NAME);
-            }
-            catch (PortalException pe1) {
-                log.error("Could not load " + DEFAULT_CLASS_NAME, pe1);
-                return null;
-            }
-        }
-    }
-
-    protected static IPooledDataSourceFactory getPooledDataSourceFactory(String className)
-            throws PortalException {
-        try {
-            if (pooledDataSourceFactoryImpl == null) {
-                synchronized (UserLayoutStoreFactory.class) {
-                    if (pooledDataSourceFactoryImpl == null) {
-                        pooledDataSourceFactoryImpl = (IPooledDataSourceFactory)Class.forName(className).newInstance();
-                    }
-                }
-            }
-            
+    /**
+     * Get a reference to our static singleton instance of IPooledDataSourceFactory
+     * as specified in our portal.properties property, or our default implementation, or null.
+     * 
+     * That is, this method returns our static singleton instance of IPooledDataSourceFactory.
+     * That instance will be an instance of the implementation named in our portal.properties property
+     * if we are able to instantiate that, or an instance of our default implementation if we were
+     * unable to instantiate the configured implementation, or null if we can instantiate neither.
+     * 
+     * This method is synchronized to avoid using the much-feared Double Checked Locking
+     * idiom.  By synchronizing we force the change we make when we first (lazily) initialize 
+     * our pooledDataSourceFactoryImpl to be written back to main memory and thereby
+     * be available to other threads when they succeed in obtaining the lock and entering
+     * this method.
+     * 
+     * @return the configured or default IPooledDataSourceFactory, or null if neither can be instantiated.
+     */
+    public static synchronized IPooledDataSourceFactory getPooledDataSourceFactory() {
+        
+        // if we already have instantiated our static singleton instance, return it
+        if (pooledDataSourceFactoryImpl != null) {
             return pooledDataSourceFactoryImpl;
         }
-        catch (Exception e) {
-            log.error("Could not instantiate " + className, e);
-            throw new PortalException(e);
+        
+        // initialize the class name so we can try to use it in our error logging
+        // on failure
+        String className = "unknown";
+        try {
+            className = PropertiesManager.getProperty(POOLED_DATA_SOURCE_FACTORY_PROPERTY);
+            pooledDataSourceFactoryImpl = (IPooledDataSourceFactory)Class.forName(className).newInstance();
+            return pooledDataSourceFactoryImpl;
+        } catch (MissingPropertyException mpe) {
+            // we recover from our property not having been set by falling back on our default.
+            // This is not necessarily an error condition.  The deployer may simply have chosen
+            // not to set our property and expects us to fall back on our default.
+            LOG.info("The portal.properties property " + POOLED_DATA_SOURCE_FACTORY_PROPERTY 
+                    + " was not set, so PooledDataSourceFactoryFactory will fall back on its default " 
+                    + "IPooledDataSourceFactory implementation, which is "
+                    + DEFAULT_POOLED_DATASOURCE_FACTORY);
+        } catch (Exception e) {
+            // we also recover from any other problem that happened in instantiating the configured
+            // IPooledDataSourceFactory by falling back on our default.  However, this is an error
+            // condition and logged as such.
+            
+            LOG.error("Unable to instantiate the configured IPooledDataSourceFactory :" 
+                    + className + ", so falling back on default of " 
+                    + DEFAULT_POOLED_DATASOURCE_FACTORY, e);
+            
+            System.out.println("Unable to instantiate " + className);
+            System.out.println(e);
+            e.printStackTrace();
         }
+        
+        try {
+            pooledDataSourceFactoryImpl = (IPooledDataSourceFactory) DEFAULT_POOLED_DATASOURCE_FACTORY.newInstance();
+        }
+        catch (Exception e2) {
+            LOG.error("Could not instantiate our default IPooledDataSourceFactory " + DEFAULT_POOLED_DATASOURCE_FACTORY, e2);
+        }
+        
+        // the field will either be our default if we succeeded in instantiating our default
+        // or will be null if we failed.
+        return pooledDataSourceFactoryImpl;
+        
     }
 
-    private PooledDataSourceFactoryFactory() { }
+    /**
+     * This private constructor prevents instantiation of this static 
+     * factory class.
+     */
+    private PooledDataSourceFactoryFactory() { 
+        // do nothing
+    }
+    
+    /**
+     * Clears this static factory's static singleton instance of 
+     * IPooledDataSourceFactory.  This method exists to support the unit test
+     * for this class and should not be considered part of the API exported
+     * by this class.
+     */
+    static void reset() {
+        pooledDataSourceFactoryImpl = null;
+    }
 }
