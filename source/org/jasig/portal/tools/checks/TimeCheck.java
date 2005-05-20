e60a03fb26ec8cd81b4cf9fd63ee665a968ace1a
Andrew Petro <apetro@unicon.net>
2005-05-20 01:51:25 +0000

    UP-1099 declared @since uPortal 2.5 removed pseudo-Javadoc boilerplate.
    git-svn-id: https://source.jasig.org/uPortal/trunk@10359 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/tools/checks/TimeCheck.java
+++ source/org/jasig/portal/tools/checks/TimeCheck.java
@@ -10,25 +10,22 @@ import java.util.Date;
 
 /**
  * An example ICheck implementation which is just a diagnostic and not an assertion.
- * This check cannot fail, but it still exposes useful information.
- * @author andrew.petro@yale.edu
+ * This check cannot fail, but it still exposes useful information, namely when the check
+ * was run.  When included in a batch of checks, allows you to see when the batch
+ * was run.
+ * 
  * @version $Revision$ $Date$
+ * @since uPortal 2.5
  */
 public class TimeCheck 
     implements ICheck {
 
-    /* (non-Javadoc)
-     * @see org.jasig.portal.tools.checks.ICheck#doCheck()
-     */
     public CheckResult doCheck() {
         
         String dateTime = DateFormat.getDateTimeInstance().format(new Date());
         return CheckResult.createSuccess("Check ran at " + dateTime);
     }
 
-    /* (non-Javadoc)
-     * @see org.jasig.portal.tools.checks.ICheck#getDescription()
-     */
     public String getDescription() {
         return "This check reports the date and time at which it is run.";
     }
