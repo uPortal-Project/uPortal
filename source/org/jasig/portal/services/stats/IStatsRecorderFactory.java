6147cf7f5d50e2339aef42675813ed362aaa92eb
Andrew Petro <apetro@unicon.net>
2005-07-15 21:11:08 +0000

    UP-608 Deprecated factory interface.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-5-patches@10459 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/services/stats/IStatsRecorderFactory.java
+++ source/org/jasig/portal/services/stats/IStatsRecorderFactory.java
@@ -7,8 +7,15 @@ package org.jasig.portal.services.stats;
 
 /**
  * A factory that produces IStatsRecorder implementations.
+ * 
+ * This factory interface is deprecated because the preferred approach is to 
+ * use Spring bean configuration to wire together your IStatsRecorder instances 
+ * rather than relying upon custom factories.  Think of Spring as the ultimate general
+ * factory.
+ * 
  * @author Ken Weiner, kweiner@unicon.net
- * @version $Revision$
+ * @version $Revision$ $Date$
+ * @deprecated as of uP 2.5.1, factory not needed when using Spring to instantiate the IStatsRecorder implementation.
  */
 public interface IStatsRecorderFactory {
   /**
