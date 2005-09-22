37f51fb3935f7f83f84436497ff1102b394d76f0
Dan Mindler <dmindler@rutgers.edu>
2005-09-22 21:25:36 +0000

    UP-1088 Instantiate the singleton in a static block, removing double-checked locking
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-4-patches@10618 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/PortletPreferencesStoreFactory.java
+++ source/org/jasig/portal/PortletPreferencesStoreFactory.java
@@ -1,37 +1,8 @@
-/**
- * Copyright © 2001 The JA-SIG Collaborative.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. Redistributions of any form whatsoever must retain the following
- *    acknowledgment:
- *    "This product includes software developed by the JA-SIG Collaborative
- *    (http://www.jasig.org/)."
- *
- * THIS SOFTWARE IS PROVIDED BY THE JA-SIG COLLABORATIVE "AS IS" AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE JA-SIG COLLABORATIVE OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
+/* Copyright 2001, 2005 The JA-SIG Collaborative.  All rights reserved.
+*  See license distributed with this file and
+*  available online at http://www.uportal.org/license.html
+*/
+
 package org.jasig.portal;
 
 import org.jasig.portal.properties.PropertiesManager;
@@ -53,17 +24,38 @@ public class PortletPreferencesStoreFactory {
     private static IPortletPreferencesStore portletPreferencesStoreImpl = null;
 
     private static final String DEFAULT_CLASS_NAME = "org.jasig.portal.RDBMPortletPreferencesStore";
-    private static String className;
 
     static {
+        // preserve previous implementation behavior
+        String className = null;
+        
         try {
             // Retrieve the class name of the concrete IPortletPreferencesStore implementation
             className = PropertiesManager.getProperty("org.jasig.portal.PortletPreferencesStoreFactory.implementation");
-        } catch (Exception e ) {}
-    
-        if (className == null || className.length() == 0 )
+        } catch (Exception e) {
             log.error( "PortletPreferencesStoreFactory: org.jasig.portal.PortletPreferencesStoreFactory.implementation must be specified in portal.properties");
-    }
+        }
+        
+        // retrieve the configured (or default) instance
+        try {
+        	portletPreferencesStoreImpl = (IPortletPreferencesStore)Class.forName(className).newInstance();
+            
+        } catch (Exception e ) {
+        	log.error( "PortletPreferencesStoreFactory: Could not instantiate " + className);
+            
+            
+            // if unable to load AND it was not the default class name, try the default
+            if (!DEFAULT_CLASS_NAME.equals(className)) {
+                try {
+                	portletPreferencesStoreImpl = (IPortletPreferencesStore)Class.forName(DEFAULT_CLASS_NAME).newInstance();
+                } catch (Exception e2) {
+                    log.error( "PortletPreferencesStoreFactory: Could not instantiate " + DEFAULT_CLASS_NAME);
+                }
+            }
+        }
+        
+        // in the end, portletPreferencesStoreImpl may still be null
+   }
 
     /**
      * Returns an instance of the IPortletPreferencesStore specified in portal.properties
@@ -71,40 +63,7 @@ public class PortletPreferencesStoreFactory {
      * @return an IPortletPreferencesStore implementation
      */
     public static IPortletPreferencesStore getPortletPreferencesStoreImpl() {
-        try {
-            return getPortletPreferencesImpl( className );
-        } catch ( PortalException pe ) {
-            log.error( "PortletPreferencesStoreFactory: Could not load " + className, pe);
-            
-            try {
-                return getPortletPreferencesImpl( DEFAULT_CLASS_NAME );
-            } catch ( PortalException pe1 ) {
-                log.error( "PortletPreferencesStoreFactory: Could not load " + DEFAULT_CLASS_NAME, pe1);
-                return null;
-            }
-        }
-    }
-
-    /**
-     * Returns an instance of the IPortletPreferencesStore
-     * 
-     * @param className <code>String</code> object specifying the class to be loaded
-     * @return an IPortletPreferencesStore implementation
-     */
-    protected static IPortletPreferencesStore getPortletPreferencesImpl( String className ) throws PortalException {
-      try {
-          if (portletPreferencesStoreImpl == null) {
-              synchronized (UserLayoutStoreFactory.class) {
-                  if (portletPreferencesStoreImpl == null) {
-                      portletPreferencesStoreImpl = (IPortletPreferencesStore)Class.forName(className).newInstance();
-                  }
-              }
-          }
-          return portletPreferencesStoreImpl;
-      } catch (Exception e) {
-          log.error( "PortletPreferencesStoreFactory: Could not instantiate " + className, e);
-          throw new PortalException(e.getMessage());  
-      }
+        return (portletPreferencesStoreImpl);
     }
 
 }
