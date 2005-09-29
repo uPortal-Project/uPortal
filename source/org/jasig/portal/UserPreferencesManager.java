9f335fcb901a68d01edc27879bba599bdb61fa8d
Mark R. Boyd <mark.boyd@sungardhe.com>
2005-09-29 22:10:17 +0000

    UP-1252 changes order of uP_save so that preference changes and be persisted at the same time as being changed.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-4-patches@10677 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/UserPreferencesManager.java
+++ source/org/jasig/portal/UserPreferencesManager.java
@@ -277,25 +277,6 @@ public class UserPreferencesManager implements IUserPreferencesManager {
      * @param req current <code>HttpServletRequest</code>
      */
     public void processUserPreferencesParameters(HttpServletRequest req) {
-        // save processing
-        String saveWhat=req.getParameter("uP_save");
-        if(saveWhat!=null) {
-            try {
-                if(saveWhat.equals("preferences")) {
-                    ulsdb.putUserPreferences(m_person, complete_up);
-                } else if(saveWhat.equals("layout")) {
-                    ulm.saveUserLayout();
-                } else if(saveWhat.equals("all")) {
-                    ulsdb.putUserPreferences(m_person, complete_up);
-                    ulm.saveUserLayout();
-                  }
-                log.debug("UserPreferencesManager::processUserPreferencesParameters() : persisted "+saveWhat+" changes.");
-
-            } catch (Exception e) {
-                log.error( "UserPreferencesManager::processUserPreferencesParameters() : unable to persist "+saveWhat+" changes. "+e);
-            }
-        }
-
         // layout root setting
         String root;
         if ((root = req.getParameter("uP_root")) != null) {
@@ -407,6 +388,24 @@ public class UserPreferencesManager implements IUserPreferencesManager {
                         log.debug("UserPreferencesManager::processUserPreferencesParameters() : setting tcattr \"" + aName + "\" of \"" + aNode[j] + "\" to \"" + aValue + "\".");
                     }
                 }
+            }
+        }
+        // save processing
+        String saveWhat=req.getParameter("uP_save");
+        if(saveWhat!=null) {
+            try {
+                if(saveWhat.equals("preferences")) {
+                    ulsdb.putUserPreferences(m_person, complete_up);
+                } else if(saveWhat.equals("layout")) {
+                    ulm.saveUserLayout();
+                } else if(saveWhat.equals("all")) {
+                    ulsdb.putUserPreferences(m_person, complete_up);
+                    ulm.saveUserLayout();
+                  }
+                log.debug("UserPreferencesManager::processUserPreferencesParameters() : persisted "+saveWhat+" changes.");
+
+            } catch (Exception e) {
+                log.error( "UserPreferencesManager::processUserPreferencesParameters() : unable to persist "+saveWhat+" changes. "+e);
             }
         }
     }
