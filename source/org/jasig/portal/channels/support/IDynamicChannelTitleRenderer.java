5e01ebb87e4d57790b9dbadeac9e9dd090c96bb7
Andrew Petro <apetro@unicon.net>
2005-10-27 18:19:52 +0000

    UP-1246 adding interface for dynamic titles supporting channel renderers.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-4-patches@10796 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/channels/support/IDynamicChannelTitleRenderer.java
+++ source/org/jasig/portal/channels/support/IDynamicChannelTitleRenderer.java
@@ -7,14 +7,14 @@ package org.jasig.portal.channels.support;
 
 /**
  * Interface for IChannelRenderers that support dynamic channel titles.
- * Dynamic channel titles were added in uPortal 2.5.1.  In order to maintain
+ * Dynamic channel titles were added in uPortal 2.4.4 and 2.5.1.  In order to maintain
  * backwards compatibility, rather than adding this method to IChannelRenderer
  * itself, it is added to this optional extension interface.  IChannelRenderers
  * implementing this extention interface can take advantage of dynamic channel
  * support in the uPortal framework (specifically, in ChannelManager).
  * IChannelRenderers not changed to implement this interface will continue to
  * behave exactly as before dynamic channel title capabilities were introduced.
- * @since uPortal 2.5.1
+ * @since uPortal 2.4.4, 2.5.1
  */
 public interface IDynamicChannelTitleRenderer {
 
