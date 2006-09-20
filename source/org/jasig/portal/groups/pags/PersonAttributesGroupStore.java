a413c1f0430f28aaac57ac82f18be5cfd098c9a8
Andrew Petro <apetro@unicon.net>
2006-09-20 20:08:36 +0000

    UP-1556 log exception on failure to obtain user attributes.
    git-svn-id: https://source.jasig.org/uPortal/trunk@11237 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/groups/pags/PersonAttributesGroupStore.java
+++ source/org/jasig/portal/groups/pags/PersonAttributesGroupStore.java
@@ -137,8 +137,10 @@ public class PersonAttributesGroupStore implements IEntityGroupStore, IEntitySto
              
              person = rp;
          }
-         catch (Exception ex)
-             { return false; }
+         catch (Exception ex) { 
+             log.error("Exception acquiring attributes for member " + member + " while checking if group " + group + " contains this member.", ex);
+             return false; 
+         }
          return testRecursively(groupDef, person);
       }
    }
