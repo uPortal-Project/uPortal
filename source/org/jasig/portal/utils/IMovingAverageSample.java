586670e3fc281f0545f77c0cb9063db7d18f8e42
George Lindholm <George.Lindholm@ubc.ca>
2006-09-27 18:24:17 +0000

    UP-864 Add copyright notice
    git-svn-id: https://source.jasig.org/uPortal/trunk@11259 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/utils/IMovingAverageSample.java
+++ source/org/jasig/portal/utils/IMovingAverageSample.java
@@ -1,4 +1,15 @@
+/* Copyright 2005 The JA-SIG Collaborative.  All rights reserved.
+*  See license distributed with this file and
+*  available online at http://www.uportal.org/license.html
+*/
+
 package org.jasig.portal.utils;
+
+/**
+ * @author George Lindholm <a href="mailto:George.Lindholm@ubc.ca">George.Lindholm@ubc.ca</a>
+ * @version $Revision$
+ * @since uPortal 2.5
+ */
 
 public interface IMovingAverageSample {
 	public long getAverage();
--- source/org/jasig/portal/utils/MovingAverageSample.java
+++ source/org/jasig/portal/utils/MovingAverageSample.java
@@ -1,6 +1,17 @@
+/* Copyright 2005 The JA-SIG Collaborative.  All rights reserved.
+*  See license distributed with this file and
+*  available online at http://www.uportal.org/license.html
+*/
+
 package org.jasig.portal.utils;
 
 import java.io.Serializable;
+
+/**
+ * @author George Lindholm <a href="mailto:George.Lindholm@ubc.ca">George.Lindholm@ubc.ca</a>
+ * @version $Revision$
+ * @since uPortal 2.5
+ */
 
 public final class MovingAverageSample implements IMovingAverageSample, Serializable {
 	//public static long SerialVersionUID = 2006l;
