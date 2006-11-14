71d188d470be9f3688c55be04a4c13ce9b47d391
Andrew Wills <wills.drew@gmail.com>
2006-11-14 21:46:04 +0000

    Added the concept of a 'default' cache factory impl for when the bean container is not available (e.g. when running Ant tasks).
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-5-patches@11282 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/utils/cache/CacheFactoryLocator.java
+++ source/org/jasig/portal/utils/cache/CacheFactoryLocator.java
@@ -5,33 +5,51 @@
  */
 package org.jasig.portal.utils.cache;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jasig.portal.utils.cache.WhirlyCacheCacheFactory;
 import org.jasig.portal.spring.PortalApplicationContextFacade;
 
 /**
- * 
+ *
  * Convenience locator class for locating the CacheFactory without each class
  * needing the cache factory to do the lookup in the application context.
- * 
+ *
  * @author Scott Battaglia
  * @version $Revision$ $Date$
  * @since 2.5
- * 
+ *
  * @see CacheFactory
  *
  */
 public class CacheFactoryLocator {
-	
-	/** String name of the CacheFactory we wish to retrieve. */
-    private static final String CACHE_FACTORY_BEAN = "cacheFactory";
-	
-	/** Single instance of the cache factory that uPortal will use. */
-	private final static CacheFactory cacheFactory = ((CacheFactory) PortalApplicationContextFacade.getPortalApplicationContext().getBean(CACHE_FACTORY_BEAN));
 
-	/**
-	 * Method to retrieve the cache factory
-	 * @return the cache factory.
-	 */
-	public static final CacheFactory getCacheFactory() {
-		return cacheFactory;
-	}
+    /** Instance of Commons Logging for logging purposes */
+    private static final Log log = LogFactory.getLog(CacheFactoryLocator.class);
+
+    /** String name of the CacheFactory we wish to retrieve. */
+    private static final String CACHE_FACTORY_BEAN = "cacheFactory";
+
+    /** Single instance of the cache factory that uPortal will use. */
+    private static CacheFactory cacheFactory;
+
+    static {
+        // This form of initialization is necessary b/c  we can't bootstrap the entire bean
+        // container to perform tasks based on Ant scripts (e.g. publish channels or portlets).
+        try {
+            cacheFactory = ((CacheFactory) PortalApplicationContextFacade.getPortalApplicationContext().getBean(CACHE_FACTORY_BEAN));
+        }
+        catch (Throwable t) {
+            log.warn("The 'cacheFactory' bean is unavailable");
+            cacheFactory = new WhirlyCacheCacheFactory();   // default...
+        }
+    }
+
+    /**
+     * Method to retrieve the cache factory
+     * @return the cache factory.
+     */
+    public static final CacheFactory getCacheFactory() {
+        return cacheFactory;
+    }
 }
