ba907f3767a48e97bde90e16a7ec39e9d38b2887
Andrew Petro <apetro@unicon.net>
2006-11-16 17:19:33 +0000

    UP-1612 added dottedTriple() method
    git-svn-id: https://source.jasig.org/uPortal/trunk@11284 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/tools/versioning/Version.java
+++ source/org/jasig/portal/tools/versioning/Version.java
@@ -6,13 +6,13 @@
 package org.jasig.portal.tools.versioning;
 
 /**
- * Represents the version of a specific unit of code that is currently installed 
+ * Represents the version of a specific unit of code that is currently installed
  * in the portal. The unit of code is represented by its functional name. For
- * the portal codebase this functional name uses the value of the IPermission 
+ * the portal codebase this functional name uses the value of the IPermission
  * interface, PORTAL_FRAMEWORK variable which currently is "UP_FRAMEWORK".
- * 
+ *
  * The version of a specific piece of code is represented by three integers.
- * In most significant order these are Major, Minor, and Micro. 
+ * In most significant order these are Major, Minor, and Micro.
  *
  * @author Mark Boyd  {@link <a href="mailto:mark.boyd@engineer.com">mark.boyd@engineer.com</a>}
  * @version $Revision$
@@ -24,11 +24,11 @@ public class Version
     private int major = 0;
     private int minor = 0;
     private int micro = 0;
-    
+
     /**
-     * Only the versioning package classes are expected to create these 
+     * Only the versioning package classes are expected to create these
      * objects.
-     * 
+     *
      * @param fname the functional name of the code being represented
      * @param major the major version of this code
      * @param minor the minor version of this code
@@ -42,67 +42,67 @@ public class Version
         this.minor = minor;
         this.micro = micro;
     }
-    
+
     /**
      * Returns the functional name of the unit of code for which this object
-     * represents the version of this code that is currently installed in the 
+     * represents the version of this code that is currently installed in the
      * portal.
-     * 
+     *
      * @return String
      */
     public String getFname()
     {
         return fname;
     }
-    
+
     /**
      * Returns the description of the unit of code for which this object
-     * represents the version of this code that is currently installed in the 
+     * represents the version of this code that is currently installed in the
      * portal.
-     * 
+     *
      * @return String
      */
     public String getDescription()
     {
         return description;
     }
-    
+
     /**
      * The major version of the code represented by this object.
-     * 
+     *
      * @return int
      */
     public int getMajor()
     {
         return major;
     }
-    
+
     /**
      * The minor version of the code represented by this object.
-     * 
+     *
      * @return int
      */
     public int getMinor()
     {
         return minor;
     }
-    
+
     /**
      * The micro version of the code represented by this object.
-     * 
+     *
      * @return int
      */
     public int getMicro()
     {
         return micro;
     }
-    
+
     /**
      * Returns a string representation of the version. The format is:
      * <functionalName>: <major>.<minor>.<micro>
-     * 
+     *
      * @return java.lang.String
-     * 
+     *
      */
     public String toString()
     {
@@ -113,15 +113,15 @@ public class Version
             + this.getMinor()
             + "."
             + this.getMicro()
-            + " [" 
+            + " ["
             + this.getDescription()
             + "]";
     }
     /**
-     * Returns true of the passed in object is a Version object and the 
-     * functional names of the objects are equal and the version numbers 
+     * Returns true of the passed in object is a Version object and the
+     * functional names of the objects are equal and the version numbers
      * within this class are equal to those of the passed in version object.
-     * 
+     *
      * @param obj a Version object to be compared to this one
      * @return boolean true if the above conditions are met
      */
@@ -129,9 +129,9 @@ public class Version
     {
         if (!(obj instanceof Version))
             return false;
-            
+
         Version v = (Version) obj;
-        
+
         return fname.equals(v.getFname()) && equalTo(v);
     }
 
@@ -139,7 +139,7 @@ public class Version
      * Returns true of the version represented by this class is equal to
      * that represented by the passed in version object. The functional name
      * is not queried for this evaluation. Only version numbers are compared.
-     * 
+     *
      * @param v the version to be compared with this version
      * @return boolean true if this version is equal to the passed in version
      */
@@ -149,12 +149,12 @@ public class Version
             && minor == v.getMinor()
             && micro == v.getMicro();
     }
-    
+
     /**
      * Returns true of the version represented by this class is less than
      * that represented by the passed in version object. The functional name
      * is not queried for this evaluation. Only version numbers are compared.
-     * 
+     *
      * @param v the version to be compared with this version
      * @return boolean true if this version is less than to the passed in version
      */
@@ -166,12 +166,12 @@ public class Version
                 && minor == v.getMinor()
                 && micro < v.getMicro());
     }
-    
+
     /**
      * Returns true of the version represented by this class is greater than
      * that represented by the passed in version object. The functional name
      * is not queried for this evaluation. Only version numbers are compared.
-     * 
+     *
      * @param v the version to be compared with this version
      * @return boolean true if this version is greater than to the passed in version
      */
@@ -182,5 +182,20 @@ public class Version
             || (major == v.getMajor()
                 && minor == v.getMinor()
                 && micro > v.getMicro());
+    }
+
+    /**
+     * Returns a String of the form "2.5.4".
+     * @return a String representing the dotted triple version
+     */
+    public String dottedTriple() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(this.major);
+        sb.append(".");
+        sb.append(this.minor);
+        sb.append(".");
+        sb.append(this.micro);
+        return sb.toString();
+
     }
 }
