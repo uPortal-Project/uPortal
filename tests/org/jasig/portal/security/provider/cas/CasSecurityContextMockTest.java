577c70c87806a52c8a64173397723544a1f43cca
Andrew Petro <apetro@unicon.net>
2007-04-12 23:08:58 +0000

    UP-1648 updated exception handling to cope with CasProxyTicketAcquisitionException subclassing RuntimeException.
    git-svn-id: https://source.jasig.org/uPortal/trunk@11355 f5dbab47-78f9-eb45-b975-e544023573eb

--- tests/org/jasig/portal/security/provider/cas/CasSecurityContextMockTest.java
+++ tests/org/jasig/portal/security/provider/cas/CasSecurityContextMockTest.java
@@ -16,39 +16,39 @@ import junit.framework.TestCase;
  * an ICasSecurityContext instance as configured.  This test is intended to
  * increase confidence that the mock object behaves as advertised.
  */
-public class CasSecurityContextMockTest 
+public class CasSecurityContextMockTest
     extends TestCase {
-    
+
     /**
      * Test that the mock object properly logs targets.
      */
     public void testTargetsLog() throws CasProxyTicketAcquisitionException {
-        
+
         CasSecurityContextMock mock = new CasSecurityContextMock();
-        
+
         mock.getCasServiceToken("aTarget");
         mock.getCasServiceToken("anotherTarget");
-        
+
         List expectedTargets = new ArrayList();
         expectedTargets.add("aTarget");
         expectedTargets.add("anotherTarget");
-        
+
         assertEquals(expectedTargets, mock.getServiceTokenTargets());
-        
+
     }
-    
+
     /**
      * Test that the mock object returns and throws as designed.
      */
     public void testProgrammedReturns() {
         CasSecurityContextMock mock = new CasSecurityContextMock();
-        
+
         List dummyProxyTickets = new ArrayList();
         dummyProxyTickets.add("token1");
         dummyProxyTickets.add("token2");
-        
+
         mock.setServiceTokensToVend(dummyProxyTickets);
-        
+
         try {
             assertEquals("token1", mock.getCasServiceToken("aTarget"));
             assertEquals("token2", mock.getCasServiceToken("anotherTarget"));
@@ -56,36 +56,36 @@ public class CasSecurityContextMockTest
             fail("Threw where should have returned the given Strings.");
         }
 
-        
+
         List vendedTokens = new ArrayList();
         vendedTokens.add("token1");
         vendedTokens.add("token2");
-        
+
         assertEquals(vendedTokens, mock.getVendedServiceTokens());
-        
+
         List dummyExceptions = new ArrayList();
         RuntimeException dummyRuntimeException = new RuntimeException();
         dummyExceptions.add(dummyRuntimeException);
-        
-        CasProxyTicketAcquisitionException cptae 
+
+        CasProxyTicketAcquisitionException cptae
             = new CasProxyTicketAcquisitionException("dummyservice", "dummypgtiou");
-        
+
         dummyExceptions.add(cptae);
-        
+
         mock.setServiceTokensToVend(dummyExceptions);
-        
+
         boolean exceptionThrown = false;
         try {
             mock.getCasServiceToken("yetAnotherTarget");
+        }  catch (CasProxyTicketAcquisitionException e) {
+            fail("Expected RuntimeException, not CasProxyTicketAcquisitionException");
         } catch (RuntimeException re) {
             assertEquals(dummyRuntimeException, re);
             exceptionThrown = true;
-        } catch (CasProxyTicketAcquisitionException e) {
-            fail("Expected RuntimeException, not CasProxyTicketAcquisitionException");
         }
-        
+
         assertTrue(exceptionThrown);
-        
+
         exceptionThrown = false;
         try {
             mock.getCasServiceToken("finalTarget");
@@ -93,13 +93,13 @@ public class CasSecurityContextMockTest
             assertEquals(cptae, e);
             exceptionThrown = true;
         }
-        
+
         assertTrue(exceptionThrown);
-        
+
         vendedTokens.addAll(dummyExceptions);
-        
+
         assertEquals(vendedTokens, mock.getVendedServiceTokens());
-        
+
     }
 
 }
