81aaefd199095c6973e66a8f6d01765650019919
Eric Dalquist <eric.dalquist@gmail.com>
2007-09-05 21:13:55 +0000

    UP-1788 DbLoader works from ant Partial progress on ChannelPublisher
    git-svn-id: https://source.jasig.org/uPortal/branches/working-maven2@14424 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-impl/src/main/java/org/jasig/portal/utils/DTDResolver.java
+++ uportal-impl/src/main/java/org/jasig/portal/utils/DTDResolver.java
@@ -5,16 +5,10 @@
 
 package org.jasig.portal.utils;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.jasig.portal.ChannelRegistryManager;
 import org.jasig.portal.PortalSessionManager;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
@@ -26,99 +20,93 @@ import org.xml.sax.InputSource;
  * @author Dave Wallace, dwallace@udel.edu modifications
  * @version $Revision$
  */
-public class DTDResolver implements EntityResolver
-{
+public class DTDResolver implements EntityResolver {
 
-  private static final Log log = LogFactory.getLog(DTDResolver.class);
+    private static final Log log = LogFactory.getLog(DTDResolver.class);
 
-  private static final String dtdPath = "dtd";
+    private static final String dtdPath = "/dtd";
 
-  private static class PublicId {
-      public String publicId;
-      public String dtdFile;
-      public PublicId(final String publicId, final String dtdFile) {
-          this.publicId = publicId;
-          this.dtdFile = dtdPath + "/"  + dtdFile;
-      }
-  }
-  private static final PublicId[] publicIds = new PublicId[]{
-      new PublicId ("-//Netscape Communications//DTD RSS 0.91//EN", "rss-0.91.dtd"),
-      new PublicId ("-//uPortal//Tables/EN", "tables.dtd"),
-      new PublicId ("-//uPortal//PersonDirs/EN", "PersonDirs.dtd"),
-      new PublicId ("-//uPortal//Channel Publishing/EN", "channelPublishingDocument.dtd"),
-      new PublicId ("-//uPortal//Data/EN", "data.dtd"),
-      new PublicId ("-//uPortal//PAGSGroupStore/EN", "PAGSGroupStore.dtd"),
-      new PublicId ("-//uPortal//LDAPGroupStore/EN", "LDAPGroupStore.dtd")};
-  private String dtdName = null;
+    private static class PublicId {
+        public String publicId;
+        public String dtdFile;
 
-  /**
-   * Constructor for DTDResolver
-   */
-  public DTDResolver () {
-  }
-
-  /**
-   * Constructor for DTDResolver
-   * @param dtdName the name of the dtd
-   */
-  public DTDResolver (String dtdName) {
-    this.dtdName = dtdName;
-  }
-
-  /**
-   * Sets up a new input source based on the dtd specified in the xml document
-   * @param publicId the public ID
-   * @param systemId the system ID
-   * @return an input source based on the dtd specified in the xml document
-   *               or null if we don't have a dtd that matches systemId or publicId
-   */
-  public InputSource resolveEntity (String publicId, String systemId) {
-    InputStream inStream = null;
-
-    // Check for a match on the systemId
-    if (systemId != null) {
-      if (dtdName != null && systemId.indexOf(dtdName) != -1) {
-        inStream = getResourceAsStream(dtdPath + "/" + dtdName);
-      } else if (systemId.trim().equalsIgnoreCase("http://my.netscape.com/publish/formats/rss-0.91.dtd")) {
-        inStream = getResourceAsStream(dtdPath + "/rss-0.91.dtd");
-      }
-
-      if ( null != inStream ) {
-          return new InputSource(inStream);
-      }
-    }
-
-    // Check for a match on the public id
-    if ( publicId != null ) {
-        publicId = publicId.trim();
-        for (int i = 0; i < publicIds.length; i++) {
-            if (publicId.equalsIgnoreCase(publicIds[i].publicId)) {
-                inStream = getResourceAsStream(publicIds[i].dtdFile);
-                if ( null != inStream ) {
-                    return new InputSource(inStream);
-                }
-                break;
-            }
+        public PublicId(final String publicId, final String dtdFile) {
+            this.publicId = publicId;
+            this.dtdFile = dtdPath + "/" + dtdFile;
         }
     }
 
-    // Return null to let the parser handle this entity
-    return null;
-  }
+    private static final PublicId[] publicIds = new PublicId[] {
+            new PublicId("-//Netscape Communications//DTD RSS 0.91//EN", "rss-0.91.dtd"),
+            new PublicId("-//uPortal//Tables/EN", "tables.dtd"),
+            new PublicId("-//uPortal//PersonDirs/EN", "PersonDirs.dtd"),
+            new PublicId("-//uPortal//Channel Publishing/EN", "channelPublishingDocument.dtd"),
+            new PublicId("-//uPortal//Data/EN", "data.dtd"),
+            new PublicId("-//uPortal//PAGSGroupStore/EN", "PAGSGroupStore.dtd"),
+            new PublicId("-//uPortal//LDAPGroupStore/EN", "LDAPGroupStore.dtd") };
+    private String dtdName = null;
 
-  public InputStream getResourceAsStream(String resource){
-      if (PortalSessionManager.isServletContext()) {
-          return PortalSessionManager.getResourceAsStream(resource);
-      } else {
-            try {
-				final URL root = DTDResolver.class.getResource("/");
-				return new FileInputStream(root.toURI().getPath() + "../../" + resource);
-			} catch (FileNotFoundException e) {
-				log.error("Unable to find path to dtd " + resource, e);
-			} catch (URISyntaxException e) {
-				log.error("Unable to find path to dtd " + resource, e);
-			}
-			return null;
-      }
-  }
+    /**
+     * Constructor for DTDResolver
+     */
+    public DTDResolver() {
+    }
+
+    /**
+     * Constructor for DTDResolver
+     * @param dtdName the name of the dtd
+     */
+    public DTDResolver(String dtdName) {
+        this.dtdName = dtdName;
+    }
+
+    /**
+     * Sets up a new input source based on the dtd specified in the xml document
+     * @param publicId the public ID
+     * @param systemId the system ID
+     * @return an input source based on the dtd specified in the xml document
+     *               or null if we don't have a dtd that matches systemId or publicId
+     */
+    public InputSource resolveEntity(String publicId, String systemId) {
+        InputStream inStream = null;
+
+        // Check for a match on the systemId
+        if (systemId != null) {
+            if (dtdName != null && systemId.indexOf(dtdName) != -1) {
+                inStream = getResourceAsStream(dtdPath + "/" + dtdName);
+            }
+            else if (systemId.trim().equalsIgnoreCase("http://my.netscape.com/publish/formats/rss-0.91.dtd")) {
+                inStream = getResourceAsStream(dtdPath + "/rss-0.91.dtd");
+            }
+
+            if (null != inStream) {
+                return new InputSource(inStream);
+            }
+        }
+
+        // Check for a match on the public id
+        if (publicId != null) {
+            publicId = publicId.trim();
+            for (int i = 0; i < publicIds.length; i++) {
+                if (publicId.equalsIgnoreCase(publicIds[i].publicId)) {
+                    inStream = getResourceAsStream(publicIds[i].dtdFile);
+                    if (null != inStream) {
+                        return new InputSource(inStream);
+                    }
+                    break;
+                }
+            }
+        }
+
+        // Return null to let the parser handle this entity
+        return null;
+    }
+
+    public InputStream getResourceAsStream(String resource) {
+        if (PortalSessionManager.isServletContext()) {
+            return PortalSessionManager.getResourceAsStream(resource);
+        }
+        
+        return DTDResolver.class.getResourceAsStream(resource);
+    }
 }
