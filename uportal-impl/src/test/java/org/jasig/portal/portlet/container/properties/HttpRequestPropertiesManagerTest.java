b5e1a242802c13ba6ba6763a42ac19d10bad689a
Eric Dalquist <eric.dalquist@gmail.com>
2007-10-17 21:43:32 +0000

    UP-1846 Starting on property manager tests
    git-svn-id: https://source.jasig.org/uPortal/branches/working-pluto@14575 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ uportal-impl/src/test/java/org/jasig/portal/portlet/container/properties/HttpRequestPropertiesManagerTest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2007 The JA-SIG Collaborative.  All rights reserved.
+ * See license distributed with this file and
+ * available online at http://www.uportal.org/license.html
+ */
+package org.jasig.portal.portlet.container.properties;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+import org.easymock.EasyMock;
+import org.jasig.portal.portlet.om.IPortletWindow;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Eric Dalquist
+ * @version $Revision$
+ */
+public class HttpRequestPropertiesManagerTest extends TestCase {
+    private HttpRequestPropertiesManager httpRequestPropertiesManager;
+    
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    @Override
+    protected void setUp() throws Exception {
+        this.httpRequestPropertiesManager = new HttpRequestPropertiesManager();
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    @Override
+    protected void tearDown() throws Exception {
+        this.httpRequestPropertiesManager = null;
+    }
+
+    public void testGetRequestProperties() {
+        final MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setRemoteAddr("1.2.3.4");
+        request.setMethod("POST");
+        
+        final IPortletWindow portletWindow = EasyMock.createMock(IPortletWindow.class);
+        EasyMock.replay(portletWindow);
+        
+        final Map<String, String[]> properties = this.httpRequestPropertiesManager.getRequestProperties(request, portletWindow);
+
+        assertNotNull("properties Map should not be null", properties);
+        assertEquals("properties Map should have 2 values", 2, properties.size());
+        assertEquals(Collections.singletonList("1.2.3.4"), Arrays.asList(properties.get("REMOTE_ADDR")));
+        assertEquals(Collections.singletonList("POST"), Arrays.asList(properties.get("REQUEST_METHOD")));
+    }
+}
