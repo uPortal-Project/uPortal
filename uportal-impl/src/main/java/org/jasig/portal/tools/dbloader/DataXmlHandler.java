32261089cdf0eb9440a9dd12af5a5f9ed823cf62
Eric Dalquist <eric.dalquist@gmail.com>
2008-09-24 17:40:26 +0000

    UP-2170 Try adding SQLType information to fix problem on postgres
    git-svn-id: https://source.jasig.org/uPortal/trunk@16300 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-impl/src/main/java/org/jasig/portal/tools/dbloader/DataXmlHandler.java
+++ uportal-impl/src/main/java/org/jasig/portal/tools/dbloader/DataXmlHandler.java
@@ -5,7 +5,9 @@
  */
 package org.jasig.portal.tools.dbloader;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -13,12 +15,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.collections.map.CaseInsensitiveMap;
+import org.apache.commons.lang.ArrayUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
 
 /**
  * Generates and executes SQL INSERT statements as the data XML document is
@@ -30,6 +39,7 @@ import org.xml.sax.SAXException;
 public class DataXmlHandler extends BaseDbXmlHandler {
     private final JdbcTemplate jdbcTemplate;
     private final TransactionTemplate transactionTemplate;
+    private final Map<String, Map<String, Integer>> tableColumnInfo = new CaseInsensitiveMap();
     private final List<String> script = new LinkedList<String>();
     
     public DataXmlHandler(JdbcTemplate jdbcTemplate, TransactionTemplate transactionTemplate) {
@@ -98,24 +108,35 @@ public class DataXmlHandler extends BaseDbXmlHandler {
             return;
         }
         
+        final Map<String, Integer> columnInfo = this.getTableColumnTypes(this.currentTable);
+        
         final StringBuilder columns = new StringBuilder();
         final StringBuilder parameters = new StringBuilder();
-        final List<Object> values = new ArrayList<Object>(this.rowData.size());
+        final Object[] values = new Object[this.rowData.size()];
+        final int[] types = new int[this.rowData.size()];
         
+        int index = 0;
         for (final Iterator<Entry<String, String>> rowIterator = this.rowData.entrySet().iterator(); rowIterator.hasNext(); ) {
             final Entry<String, String> row = rowIterator.next();
-            columns.append(row.getKey());
+            final String columnName = row.getKey();
+            columns.append(columnName);
             parameters.append("?");
-            values.add(row.getValue());
+            
+            values[index] = row.getValue();
+            types[index] = columnInfo.get(columnName);
             
             if (rowIterator.hasNext()) {
                 columns.append(", ");
                 parameters.append(", ");
             }
+            
+            index++;
         }
         
         final String sql = "INSERT INTO " + this.currentTable + " (" + columns + ") VALUES (" + parameters + ")";
-        this.logger.info(sql + "\t" + values);
+        if (this.logger.isInfoEnabled()) {
+            this.logger.info(sql + "\t" + Arrays.asList(values) + "\t" + Arrays.asList(ArrayUtils.toObject(types)));
+        }
 
         this.transactionTemplate.execute(new TransactionCallback() {
 
@@ -123,8 +144,45 @@ public class DataXmlHandler extends BaseDbXmlHandler {
              * @see org.springframework.transaction.support.TransactionCallback#doInTransaction(org.springframework.transaction.TransactionStatus)
              */
             public Object doInTransaction(TransactionStatus status) {
-                return jdbcTemplate.update(sql, values.toArray());
+                return jdbcTemplate.update(sql, values, types);
             }
         });
+    }
+    
+    protected Map<String, Integer> getTableColumnTypes(String tableName) {
+        final Map<String, Integer> columnInfo;
+        synchronized (this.tableColumnInfo) {
+            if (this.tableColumnInfo.containsKey(tableName)) {
+                columnInfo = this.tableColumnInfo.get(tableName);
+            }
+            else {
+                columnInfo = new CaseInsensitiveMap();
+                
+                final DataSource dataSource = jdbcTemplate.getDataSource();
+                final Connection connection = DataSourceUtils.getConnection(dataSource);
+                try {
+                    final DatabaseMetaData metaData = connection.getMetaData();
+                    final ResultSet columns = metaData.getColumns(null, null, tableName, null);
+                    try {
+                        while (columns.next()) {
+                            final String name = columns.getString("COLUMN_NAME");
+                            final int type = columns.getInt("DATA_TYPE");
+                            columnInfo.put(name, type);
+                        }
+                    }
+                    finally {
+                        columns.close();
+                    }
+                }
+                catch (Exception e) {
+                    throw new RuntimeException("Could not determine database table information for populating tables", e);
+                }
+                finally {
+                    DataSourceUtils.releaseConnection(connection, dataSource);
+                }
+            }
+        }
+        
+        return columnInfo;
     }
 }
