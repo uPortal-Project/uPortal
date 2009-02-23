2cad2f8f9a6b9b340f0f8eb2d5c25d4dbd323374
Eric Dalquist <eric.dalquist@gmail.com>
2009-02-23 02:19:58 +0000

    Merged revisions 44994 via svnmerge from https://www.ja-sig.org/svn/uPortal/trunk
    ........
      r44994 | edalquist | 2009-02-18 22:04:05 -0600 (Wed, 18 Feb 2009) | 1 line
      
      UP-2289 Fix reference to old CError channel
    ........
    
    
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-3-0-patches@17063 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-impl/src/main/java/org/jasig/portal/ChannelDefinition.java
+++ uportal-impl/src/main/java/org/jasig/portal/ChannelDefinition.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.jasig.portal.channels.error.CError;
 import org.jasig.portal.channels.portlet.IPortletAdaptor;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -326,7 +327,7 @@ public class ChannelDefinition implements IBasicEntity {
    * Display a message where this channel should be
    */
   public Element getDocument(Document doc, String idTag, String statusMsg, int errorId) {
-    Element channel = getBase(doc, idTag, "org.jasig.portal.channels.CError", false, false, false);
+    Element channel = getBase(doc, idTag, CError.class.getName(), false, false, false);
     addParameters(doc, channel);
     channel.appendChild(nodeParameter(doc, "CErrorMessage", statusMsg));
     channel.appendChild(nodeParameter(doc, "CErrorChanId", idTag));
