1533c7efbdad512c241e6d1689483ef462b29c7d
Eric Dalquist <eric.dalquist@gmail.com>
2009-02-23 04:38:30 +0000

    UP-2114 fix null entry display
    git-svn-id: https://source.jasig.org/uPortal/trunk@17088 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-impl/src/main/java/org/jasig/portal/portlets/Attribute.java
+++ uportal-impl/src/main/java/org/jasig/portal/portlets/Attribute.java
@@ -16,7 +16,7 @@ import java.io.Serializable;
  */
 public class Attribute implements Serializable {
     private static final long serialVersionUID = 1L;
-    private String value;
+    private String value = "";
     
     public Attribute() {
     }
