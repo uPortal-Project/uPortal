1d581106e23120e809f6acbec887ef438d7b42e9
Eric Dalquist <eric.dalquist@gmail.com>
2009-02-26 19:03:49 +0000

    NOJIRA update clean repo CI build script
    git-svn-id: https://source.jasig.org/uPortal/trunk@17183 f5dbab47-78f9-eb45-b975-e544023573eb

--- src/test/clean-repo-build.sh
+++ src/test/clean-repo-build.sh
@@ -4,38 +4,24 @@
 set -e 
 #set -o pipefail
 
-echo PWD=`pwd`
-env
+PATH=$M2_HOME/bin:$JAVA_HOME/bin:$PATH
+WORKING_DIR=`pwd`
+LOCAL_REPO=$WORKING_DIR/local-repository/
 
-#PATH=$PATH:/opt/java/tools/maven2/current/bin/
+echo "PATH:        $PATH"
+echo "WORKING_DIR: $WORKING_DIR"
+echo "LOCAL_REPO:  $LOCAL_REPO"
 
-#WORKING_DIR=`pwd`
-#LOCAL_REPO=$WORKING_DIR/local-repository/
-#SETTINGS_FILE=$WORKING_DIR/settings.xml
+if [ -d $LOCAL_REPO ]; then
+    # Remove existing local maven repository, if present
+    echo "Removing local repository $LOCAL_REPO"
+    rm -rf $LOCAL_REPO/*
+elif
+    # Create local repository directory since it doesn't exist
+    echo "Creating local repository $LOCAL_REPO"
+    mkdir -p $LOCAL_REPO
+fi
 
-# Remove existing archetype dir, if present
-#rm -rf $WORKING_DIR/test-confluence-plugin
-
-#Remove old settings.xml
-#rm -rf $WORKING_DIR/settings.xml
-
-# Remove existing local maven repository, if present
-#rm -rf $WORKING_DIR/local-repository/*
-
-#download most recent settings.xml from SVN
-#wget http://svn.atlassian.com/svn/public/atlassian/maven-plugins/example-settings-file/settings.xml
-
-# Install Java Activation manually, since we can't host it in Archiva
-#mvn -s $SETTINGS_FILE install:install-file -Dmaven.repo.local=$LOCAL_REPO -DgroupId=javax.activation -DartifactId=activation -Dversion=1.0.2 -Dpackaging=jar -Dfile=$WORKING_DIR/lib/activation.jar
-
-# generate template dir from archetype
-#mvn -s $SETTINGS_FILE archetype:generate -Dmaven.repo.local=$LOCAL_REPO -DinteractiveMode=false -DarchetypeGroupId=com.atlassian.maven.archetypes -DarchetypeArtifactId=confluence-plugin-archetype -DarchetypeVersion=14 -DremoteRepositories=https://maven.atlassian.com/repository/public/ -DgroupId=com.atlassian.confluence.plugins -DartifactId=test-confluence-plugin
-
-# cd to newly created directory
-#cd $WORKING_DIR/test-confluence-plugin
-
-# creating IDEA files, downloading all dependencies
-#mvn -s $SETTINGS_FILE idea:idea -Dmaven.repo.local=$LOCAL_REPO
-
-# package plugin, downloading all dependencies
-#mvn -s $SETTINGS_FILE package -Dmaven.repo.local=$LOCAL_REPO
+# Run Maven install
+echo "Running: 'mvn -Dmaven.repo.local=$LOCAL_REPO install'"
+mvn -Dmaven.repo.local=$LOCAL_REPO install
