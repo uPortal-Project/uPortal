86841ce5cea2c1098ee8d9e50ce1ee3c41336aba
Eric Dalquist <eric.dalquist@gmail.com>
2009-09-24 15:36:37 +0000

    UP-2490 fix regex matching for cleaning filenames to allow international characters
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-2-5-export@18719 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/org/jasig/portal/io/SafeFileNamePhrase.java
+++ source/org/jasig/portal/io/SafeFileNamePhrase.java
@@ -5,6 +5,12 @@
  */
 package org.jasig.portal.io;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.danann.cernunnos.EntityConfig;
 import org.danann.cernunnos.Formula;
 import org.danann.cernunnos.Phrase;
@@ -20,6 +26,26 @@ import org.danann.cernunnos.TaskResponse;
  * @version $Revision$
  */
 public class SafeFileNamePhrase implements Phrase {
+
+    // Reserved names on Windows (see http://en.wikipedia.org/wiki/Filename)
+    private static final Pattern[] WINDOWS_INVALID_PATTERNS = new Pattern[] {
+                            Pattern.compile("AUX"),
+                            Pattern.compile("CLOCK\\$"),
+                            Pattern.compile("COM\\d*"),
+                            Pattern.compile("CON"),
+                            Pattern.compile("LPT\\d*"),
+                            Pattern.compile("NUL"),
+                            Pattern.compile("PRN")
+                        };
+    
+    private static final Map<Pattern, String> REPLACEMENT_PAIRS;
+    
+    static {
+        final Map<Pattern, String> pairs = new LinkedHashMap<Pattern, String>();
+        pairs.put(Pattern.compile("/|\\\\"), ".");
+        pairs.put(Pattern.compile("[~`@\\|\\s#$\\*]"), "_");
+        REPLACEMENT_PAIRS = Collections.unmodifiableMap(pairs);
+    }
 
     public static final Reagent HUMAN_FILE_NAME = new SimpleReagent("HUMAN_FILE_NAME", "descendant-or-self::text()", ReagentType.PHRASE,
             String.class, "Human readable version of the file name to make safe");
@@ -51,12 +77,24 @@ public class SafeFileNamePhrase implements Phrase {
     }
 
     protected String getSafeFileName(String name) {
-        //Replace slashes with .
-        name = name.replaceAll("/|\\\\", ".");
-        
-        //Replace all non-ok char with _
-        name = name.replaceAll("[^a-zA-Z0-9_.-]", "_");
+        //Replace invalid characters
+        for (final Map.Entry<Pattern, String> pair : REPLACEMENT_PAIRS.entrySet()) {
+            final Pattern pattern = pair.getKey();
+            final Matcher matcher = pattern.matcher(name);
+            name = matcher.replaceAll(pair.getValue());
+        }
+
+        // Make sure the name doesn't violate a Windows reserved word...
+        final String upperCaseName = name.toUpperCase();
+        for (Pattern pattern : WINDOWS_INVALID_PATTERNS) {
+            if (pattern.matcher(upperCaseName).matches()) {
+                name = "uP-" + name;
+                break;
+            }
+        }
 
         return name;
+
     }
+
 }
