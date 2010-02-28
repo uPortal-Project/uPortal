3b777c7a13e8188c860f14998328a00b36fdb9ce
Nicholas Blair <nicholas.blair@gmail.com>
2010-02-28 16:41:30 +0000

    UP-2488, switch to relocated equivalents; remove deprecated getUserInfo with single argument
    git-svn-id: https://source.jasig.org/uPortal/branches/working-pluto-2.0@20022 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-impl/src/main/java/org/jasig/portal/portlet/container/services/MergingUserInfoService.java
+++ uportal-impl/src/main/java/org/jasig/portal/portlet/container/services/MergingUserInfoService.java
@@ -26,9 +26,9 @@ import java.util.Set;
 
 import javax.portlet.PortletRequest;
 
-import org.apache.pluto.PortletContainerException;
-import org.apache.pluto.PortletWindow;
-import org.apache.pluto.spi.optional.UserInfoService;
+import org.apache.pluto.container.PortletContainerException;
+import org.apache.pluto.container.PortletWindow;
+import org.apache.pluto.container.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -58,31 +58,6 @@ public class MergingUserInfoService implements UserInfoService {
 	 */
 	public Set<UserInfoService> getUserInfoServices() {
 		return this.userInfoServices;
-	}
-	
-    /* (non-Javadoc)
-     * @see org.apache.pluto.spi.optional.UserInfoService#getUserInfo(javax.portlet.PortletRequest)
-     */
-    @Deprecated
-    public Map<String, String> getUserInfo(PortletRequest request)
-			throws PortletContainerException {
-    	
-    	Map<String, String> mergedInfo = new HashMap<String, String>();
-    	
-    	// iterate over all supplied user info services and add their
-    	// resulting key/value pairs to our merged map
-    	for (final UserInfoService service : this.userInfoServices){
-    		
-    		Map<String, String> userInfo = service.getUserInfo(request);
-    		for (final Map.Entry<String, String> entry : userInfo.entrySet()) {
-    			final String attributeName = entry.getKey();
-    			final String valueObj = entry.getValue();
-    			mergedInfo.put(attributeName, valueObj);
-    		}
-    		
-    	}
-    	
-		return mergedInfo;
 	}
     
     /* (non-Javadoc)
