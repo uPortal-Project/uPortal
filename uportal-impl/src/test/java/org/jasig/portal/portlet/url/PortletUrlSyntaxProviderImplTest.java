414b83c0490d13c5a28b818a46edf3c55c1870db
Nicholas Blair <nicholas.blair@gmail.com>
2010-05-21 21:42:04 +0000

    UP-2045, remove test for removed method
    git-svn-id: https://source.jasig.org/uPortal/branches/working-pluto-2.0@20646 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-impl/src/test/java/org/jasig/portal/portlet/url/PortletUrlSyntaxProviderImplTest.java
+++ uportal-impl/src/test/java/org/jasig/portal/portlet/url/PortletUrlSyntaxProviderImplTest.java
@@ -29,90 +29,69 @@ import org.springframework.mock.web.MockHttpServletRequest;
  * @version $Revision$
  */
 public class PortletUrlSyntaxProviderImplTest {
-    
+
 	@Test
-    public void testEncodeAndAppend() throws Exception {
-        final PortletUrlSyntaxProviderImpl portletUrlSyntaxProvider = new PortletUrlSyntaxProviderImpl();
-        
-        StringBuilder url = new StringBuilder();
-        portletUrlSyntaxProvider.encodeAndAppend(url, "UTF-8", "name");
-        Assert.assertEquals("name=", url.toString());
-        
-        url = new StringBuilder();
-        portletUrlSyntaxProvider.encodeAndAppend(url, "UTF-8", "name", "value1");
-        Assert.assertEquals("name=value1", url.toString());
-        
-        url = new StringBuilder();
-        portletUrlSyntaxProvider.encodeAndAppend(url, "UTF-8", "name", "value1", "value2");
-        Assert.assertEquals("name=value1&name=value2", url.toString());
-        
-        portletUrlSyntaxProvider.encodeAndAppend(url.append("&"), "UTF-8", "name2", "value21", "value22");
-        Assert.assertEquals("name=value1&name=value2&name2=value21&name2=value22", url.toString());
-    }
-    
-    
-    
-    public void testParsePortletParameters() throws Exception {
-        final PortletUrlSyntaxProviderImpl portletUrlSyntaxProvider = new PortletUrlSyntaxProviderImpl();
-        
-        final MockHttpServletRequest request = new MockHttpServletRequest();
-        
-        try {
-            portletUrlSyntaxProvider.parsePortletParameters(null);
-            Assert.fail("generatePortletUrl should have thrown an IllegalArgumentException with a null request");
-        }
-        catch (IllegalArgumentException iae) {
-        }
-        
-        
-        final ITransientPortletWindowRegistry portletWindowRegistry = EasyMock.createMock(ITransientPortletWindowRegistry.class);
-        EasyMock.expect(portletWindowRegistry.getPortletWindowId("windowId1"))
-            .andReturn(new MockPortletWindowId("windowId1"))
-            .anyTimes();
-        
-        
-        EasyMock.replay(portletWindowRegistry);
-        
-        portletUrlSyntaxProvider.setPortletWindowRegistry(portletWindowRegistry);
-        
-        Tuple<IPortletWindowId, PortletUrl> parsedPortletUrl = portletUrlSyntaxProvider.parsePortletParameters(request);
-        Assert.assertNull(parsedPortletUrl);
-        
-        IPortletWindowId portletWindowId = new MockPortletWindowId("windowId1");
-        PortletUrl portletUrl1 = new PortletUrl(portletWindowId);
-        Tuple<IPortletWindowId, PortletUrl> expectedParsedUrl = new Tuple<IPortletWindowId, PortletUrl>(portletWindowId, portletUrl1);
-        
-        request.setParameter("pltc_target", "windowId1");
-        request.setParameter("pltc_type", "RENDER");
-        portletUrl1.setRequestType(TYPE.RENDER);
-        portletUrl1.setParameters(Collections.EMPTY_MAP);
-        portletUrl1.setSecure(false);
-        
-        parsedPortletUrl = portletUrlSyntaxProvider.parsePortletParameters(request);
-        Assert.assertEquals(expectedParsedUrl, parsedPortletUrl);
-        
-        
-        Map<String, List<String>> parameters = new HashMap<String, List<String>>();
-        parameters.put("key1", Arrays.asList(new String[] { "value1.1", "value1.2" }));
-        
-        request.setParameter("pltc_state", "MAXIMIZED");
-        request.setParameter("pltc_mode", "HELP");
-        request.setParameter("pltp_key1", new String[] { "value1.1", "value1.2" });
-        portletUrl1.setWindowState(WindowState.MAXIMIZED);
-        portletUrl1.setPortletMode(PortletMode.HELP);
-        portletUrl1.setParameters(parameters);
-        
-        parsedPortletUrl = portletUrlSyntaxProvider.parsePortletParameters(request);
-        Assert.assertEquals(expectedParsedUrl, parsedPortletUrl);
-        
-        
-        parameters.put("post_parameter", Arrays.asList(new String[] { "post_value" }));
-        
-        request.setMethod("POST");
-        request.setParameter("post_parameter", "post_value");
-        request.setQueryString("pltc_target=windowId1&pltc_type=RENDER&pltc_state=MAXIMIZED&pltc_mode=HELP&pltp_key1=value1.1&pltp_key1=value1.2");
-        
-        parsedPortletUrl = portletUrlSyntaxProvider.parsePortletParameters(request);
-        Assert.assertEquals(expectedParsedUrl, parsedPortletUrl);
-    }
+	public void testParsePortletParameters() throws Exception {
+		final PortletUrlSyntaxProviderImpl portletUrlSyntaxProvider = new PortletUrlSyntaxProviderImpl();
+
+		final MockHttpServletRequest request = new MockHttpServletRequest();
+
+		try {
+			portletUrlSyntaxProvider.parsePortletParameters(null);
+			Assert.fail("generatePortletUrl should have thrown an IllegalArgumentException with a null request");
+		}
+		catch (IllegalArgumentException iae) {
+		}
+
+
+		final ITransientPortletWindowRegistry portletWindowRegistry = EasyMock.createMock(ITransientPortletWindowRegistry.class);
+		EasyMock.expect(portletWindowRegistry.getPortletWindowId("windowId1"))
+		.andReturn(new MockPortletWindowId("windowId1"))
+		.anyTimes();
+
+
+		EasyMock.replay(portletWindowRegistry);
+
+		portletUrlSyntaxProvider.setPortletWindowRegistry(portletWindowRegistry);
+
+		Tuple<IPortletWindowId, PortletUrl> parsedPortletUrl = portletUrlSyntaxProvider.parsePortletParameters(request);
+		Assert.assertNull(parsedPortletUrl);
+
+		IPortletWindowId portletWindowId = new MockPortletWindowId("windowId1");
+		PortletUrl portletUrl1 = new PortletUrl(portletWindowId);
+		Tuple<IPortletWindowId, PortletUrl> expectedParsedUrl = new Tuple<IPortletWindowId, PortletUrl>(portletWindowId, portletUrl1);
+
+		request.setParameter("pltc_target", "windowId1");
+		request.setParameter("pltc_type", "RENDER");
+		portletUrl1.setRequestType(TYPE.RENDER);
+		portletUrl1.setParameters(Collections.EMPTY_MAP);
+		portletUrl1.setSecure(false);
+
+		parsedPortletUrl = portletUrlSyntaxProvider.parsePortletParameters(request);
+		Assert.assertEquals(expectedParsedUrl, parsedPortletUrl);
+
+
+		Map<String, List<String>> parameters = new HashMap<String, List<String>>();
+		parameters.put("key1", Arrays.asList(new String[] { "value1.1", "value1.2" }));
+
+		request.setParameter("pltc_state", "MAXIMIZED");
+		request.setParameter("pltc_mode", "HELP");
+		request.setParameter("pltp_key1", new String[] { "value1.1", "value1.2" });
+		portletUrl1.setWindowState(WindowState.MAXIMIZED);
+		portletUrl1.setPortletMode(PortletMode.HELP);
+		portletUrl1.setParameters(parameters);
+
+		parsedPortletUrl = portletUrlSyntaxProvider.parsePortletParameters(request);
+		Assert.assertEquals(expectedParsedUrl, parsedPortletUrl);
+
+
+		parameters.put("post_parameter", Arrays.asList(new String[] { "post_value" }));
+
+		request.setMethod("POST");
+		request.setParameter("post_parameter", "post_value");
+		request.setQueryString("pltc_target=windowId1&pltc_type=RENDER&pltc_state=MAXIMIZED&pltc_mode=HELP&pltp_key1=value1.1&pltp_key1=value1.2");
+
+		parsedPortletUrl = portletUrlSyntaxProvider.parsePortletParameters(request);
+		Assert.assertEquals(expectedParsedUrl, parsedPortletUrl);
+	}
 }
