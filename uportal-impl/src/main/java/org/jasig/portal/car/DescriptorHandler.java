3dcd8f2bc1c98448aeb17263b68002a480abb39c
Eric Dalquist <eric.dalquist@gmail.com>
2010-07-19 21:26:18 +0000

    r21164 | edalquist | 2010-07-19 16:23:06 -0500 (Mon, 19 Jul 2010) | 1 line
    UP-2384 Fix for potential NPEs
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-3-0-patches@21165 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-impl/src/main/java/org/jasig/portal/car/DescriptorHandler.java
+++ uportal-impl/src/main/java/org/jasig/portal/car/DescriptorHandler.java
@@ -118,6 +118,7 @@ public class DescriptorHandler
         {
             is = jarFile.getInputStream( entry );
             parser = SAXParserFactory.newInstance().newSAXParser();
+            doParse(jarFile, handler, parser, is);
         }
         catch( ZipException ze )
         {
@@ -172,7 +173,11 @@ public class DescriptorHandler
                             " Details: " + sxe );
         }
         
-        try
+    }
+
+	private void doParse(JarFile jarFile, ComponentTagHandler handler,
+			SAXParser parser, InputStream is) {
+		try
         {
             // can throw SAXException, IOException
             parser.parse( is, handler );
@@ -211,7 +216,7 @@ public class DescriptorHandler
                         "Unable to close inputStream", ioe);
             }
         }
-    }
+	}
 
     /////// Methods for use by CarResources to aquire descriptor info.
 
