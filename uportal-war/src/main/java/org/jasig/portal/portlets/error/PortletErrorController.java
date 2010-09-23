baa0b75bbe98b395fd55841f7984722cff0b6bc3
Nicholas Blair <nicholas.blair@gmail.com>
2010-09-23 18:49:54 +0000

    UP-2661 initial import of error portlet
    git-svn-id: https://source.jasig.org/uPortal/trunk@21720 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ uportal-war/src/main/java/org/jasig/portal/portlets/error/PortletErrorController.java
@@ -0,0 +1,103 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jasig.portal.portlets.error;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.jasig.portal.EntityIdentifier;
+import org.jasig.portal.portlet.om.IPortletWindowId;
+import org.jasig.portal.portlet.rendering.PortletExecutionManager;
+import org.jasig.portal.security.IAuthorizationPrincipal;
+import org.jasig.portal.services.AuthorizationService;
+import org.jasig.portal.url.IPortalRequestUtils;
+import org.jasig.portal.user.IUserInstance;
+import org.jasig.portal.user.IUserInstanceManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.portlet.mvc.AbstractController;
+
+/**
+ * Portlet intended to replace the output for a portlet that failed
+ * to render due to an {@link Exception}.
+ * 
+ * @author Nicholas Blair
+ * @version $Id$
+ */
+public class PortletErrorController extends AbstractController {
+
+	protected static final String ERROR_OWNER = "UP_ERROR_CHAN";
+	protected static final String ERROR_ACTIVITY = "VIEW";
+	protected static final String ERROR_TARGET = "DETAILS";
+	private IUserInstanceManager userInstanceManager;
+	private IPortalRequestUtils portalRequestUtils;
+	//private IPortletWindowRegistry portletWindowRegistry;
+	
+	/**
+	 * @param userInstanceManager the userInstanceManager to set
+	 */
+	@Autowired
+	public void setUserInstanceManager(IUserInstanceManager userInstanceManager) {
+		this.userInstanceManager = userInstanceManager;
+	}
+
+	/**
+	 * @param portalRequestUtils the portalRequestUtils to set
+	 */
+	@Autowired
+	public void setPortalRequestUtils(IPortalRequestUtils portalRequestUtils) {
+		this.portalRequestUtils = portalRequestUtils;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.web.portlet.mvc.AbstractController#handleRenderRequestInternal(javax.portlet.RenderRequest, javax.portlet.RenderResponse)
+	 */
+	@Override
+	protected ModelAndView handleRenderRequestInternal(RenderRequest request,
+			RenderResponse response) throws Exception {
+		Map<String, Object> model = new HashMap<String, Object>();
+		HttpServletRequest httpRequest = this.portalRequestUtils.getOriginalPortalRequest(request);
+		IUserInstance userInstance = this.userInstanceManager.getUserInstance(httpRequest);
+		if(canSeeErrorDetails(userInstance)) {
+			// error controller needs to know which portletWindowId is the source
+			// to be able to locate the Exception that was raised to bring it into existence
+			IPortletWindowId currentFailedPortletWindowId = (IPortletWindowId) request.getAttribute(PortletExecutionManager.REQUEST_ATTRIBUTE__CURRENT_FAILED_PORTLET_WINDOW_ID);
+			Exception cause = (Exception) request.getAttribute(PortletExecutionManager.REQUEST_ATTRIBUTE__CURRENT_EXCEPTION_CAUSE);
+			model.put("portletWindowId", currentFailedPortletWindowId);
+			model.put("exception", cause);
+			return new ModelAndView("/jsp/PortletError/detailed", model);
+		} else {
+			return new ModelAndView("/jsp/PortletError/generic", model);
+		}
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	protected boolean canSeeErrorDetails(IUserInstance userInstance) {
+		EntityIdentifier ei = userInstance.getPerson().getEntityIdentifier();
+	    IAuthorizationPrincipal ap = AuthorizationService.instance().newPrincipal(ei.getKey(), ei.getType());
+	    return ap.hasPermission(ERROR_OWNER, ERROR_ACTIVITY, ERROR_TARGET);
+	}
+}
