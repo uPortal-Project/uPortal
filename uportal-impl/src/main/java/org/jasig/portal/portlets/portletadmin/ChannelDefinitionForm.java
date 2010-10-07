255b5217fafb0a23e6345c76c69c673720d2b3df
Eric Dalquist <eric.dalquist@gmail.com>
2010-10-07 02:46:37 +0000

    UP-2838 Increase default channel timeout to 5 seconds
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-3-2-patches@21895 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-impl/src/main/java/org/jasig/portal/portlets/portletadmin/ChannelDefinitionForm.java
+++ uportal-impl/src/main/java/org/jasig/portal/portlets/portletadmin/ChannelDefinitionForm.java
@@ -66,7 +66,7 @@ public class ChannelDefinitionForm implements Serializable {
 	private String description = "";
 	private String title = "";
 	private String javaClass = "";
-	private int timeout = 500;
+	private int timeout = 5000;
 	private int typeId;
 	
 	/**
