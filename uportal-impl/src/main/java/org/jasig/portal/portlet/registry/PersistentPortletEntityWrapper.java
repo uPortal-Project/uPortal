f336be195b64462e63948c900b33016f025b1c49
Eric Dalquist <eric.dalquist@gmail.com>
2010-10-15 16:56:20 +0000

    UP-2854 Enforce consistent entity IDs by mapping the composite ID to the persistent ID in the case of entities with persisted data. Gracefully handle getting entities for a user when not in a request.
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-3-1-patches@22001 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ uportal-impl/src/main/java/org/jasig/portal/portlet/registry/PersistentPortletEntityWrapper.java
@@ -0,0 +1,102 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.jasig.portal.portlet.registry;
+
+import org.jasig.portal.portlet.om.IPortletDefinitionId;
+import org.jasig.portal.portlet.om.IPortletEntity;
+import org.jasig.portal.portlet.om.IPortletEntityId;
+import org.jasig.portal.portlet.om.IPortletPreferences;
+
+/**
+ * Wrapper for portlet entities that are persistent. Overrides the entity ID to be a consistent value 
+ * 
+ * @author Eric Dalquist
+ * @version $Revision$
+ */
+class PersistentPortletEntityWrapper implements IPortletEntity {
+    private final IPortletEntity persistentEntity;
+    private final IPortletEntityId standardEntityId;
+    /**
+     * @param persistentEntity
+     */
+    public PersistentPortletEntityWrapper(IPortletEntity persistentEntity) {
+        this.persistentEntity = persistentEntity;
+        
+        this.standardEntityId = new PortletEntityIdImpl(
+                this.persistentEntity.getPortletDefinitionId(), 
+                this.persistentEntity.getChannelSubscribeId(),
+                this.persistentEntity.getUserId());
+    }
+    
+    /**
+     * @return The wrapped entity
+     */
+    public IPortletEntity getPersistentEntity() {
+        return this.persistentEntity;
+    }
+
+    public IPortletEntityId getPortletEntityId() {
+        return this.standardEntityId;
+    }
+    public IPortletDefinitionId getPortletDefinitionId() {
+        return this.persistentEntity.getPortletDefinitionId();
+    }
+    public String getChannelSubscribeId() {
+        return this.persistentEntity.getChannelSubscribeId();
+    }
+    public int getUserId() {
+        return this.persistentEntity.getUserId();
+    }
+    public IPortletPreferences getPortletPreferences() {
+        return this.persistentEntity.getPortletPreferences();
+    }
+    public void setPortletPreferences(IPortletPreferences portletPreferences) {
+        this.persistentEntity.setPortletPreferences(portletPreferences);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.standardEntityId.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PersistentPortletEntityWrapper other = (PersistentPortletEntityWrapper) obj;
+        if (this.persistentEntity == null) {
+            if (other.persistentEntity != null)
+                return false;
+        }
+        else if (!this.persistentEntity.equals(other.persistentEntity))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "PersistentPortletEntityWrapper [standardEntityId=" + this.standardEntityId + ", persistentEntity="
+                + this.persistentEntity + "]";
+    }
+}
--- uportal-impl/src/main/java/org/jasig/portal/portlet/registry/PortletEntityRegistryImpl.java
+++ uportal-impl/src/main/java/org/jasig/portal/portlet/registry/PortletEntityRegistryImpl.java
@@ -6,9 +6,11 @@
 package org.jasig.portal.portlet.registry;
 
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -19,7 +21,6 @@ import org.apache.commons.lang.Validate;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jasig.portal.portlet.dao.IPortletEntityDao;
-import org.jasig.portal.portlet.om.AbstractObjectId;
 import org.jasig.portal.portlet.om.IPortletDefinition;
 import org.jasig.portal.portlet.om.IPortletDefinitionId;
 import org.jasig.portal.portlet.om.IPortletEntity;
@@ -41,6 +42,7 @@ import org.springframework.web.util.WebUtils;
  */
 public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
     public static final String INTERIM_PORTLET_ENTITY_MAP_ATTRIBUTE = PortletEntityRegistryImpl.class.getName() + ".INTERIM_PORTLET_ENTITY_MAP";
+    public static final String PORTLET_ENTITY_ID_MAP_ATTRIBUTE = PortletEntityRegistryImpl.class.getName() + ".PORTLET_ENTITY_ID_MAP";
     
     protected final Log logger = LogFactory.getLog(this.getClass());
     
@@ -90,6 +92,10 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
         Validate.notNull(channelSubscribeId, "channelSubscribeId can not be null");
         
         final InterimPortletEntityImpl interimPortletEntity = new InterimPortletEntityImpl(portletDefinitionId, channelSubscribeId, userId);
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace("Created InterimPortletEntity " + interimPortletEntity.getPortletEntityId() + " for def=" + portletDefinitionId + ", sub=" + channelSubscribeId + ", usr=" + userId);
+        }
+        
         this.storeInterimPortletEntity(interimPortletEntity);
         return interimPortletEntity;
     }
@@ -99,16 +105,23 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
      */
     public IPortletEntity getPortletEntity(IPortletEntityId portletEntityId) {
         Validate.notNull(portletEntityId, "portletEntityId can not be null");
-        if (InterimPortletEntityImpl.isInterimPortletEntityId(portletEntityId)) {
-            final InterimPortletEntityImpl interimPortletEntity = this.getInterimPortletEntity(portletEntityId);
-            if (interimPortletEntity != null) {
-                return interimPortletEntity;
-            }
-            
-            return new InterimPortletEntityImpl(portletEntityId);
+        final InterimPortletEntityImpl interimPortletEntity = this.getInterimPortletEntity(portletEntityId);
+        if (interimPortletEntity != null) {
+            return interimPortletEntity;
         }
         
-        return this.portletEntityDao.getPortletEntity(portletEntityId);
+        //Need to find a mapped persistent entity ID to do a DB lookup
+        final IPortletEntityId persistentEntityId = this.getPersistentId(portletEntityId);
+        if (persistentEntityId == null) {
+            return null;
+        }
+        
+        final IPortletEntity persistentPortletEntity = this.portletEntityDao.getPortletEntity(persistentEntityId);
+        if (persistentPortletEntity == null) {
+            return null;
+        }
+        
+        return new PersistentPortletEntityWrapper(persistentPortletEntity);
     }
     
     /* (non-Javadoc)
@@ -116,25 +129,7 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
      */
     public IPortletEntity getPortletEntity(String portletEntityIdString) {
         Validate.notNull(portletEntityIdString, "portletEntityId can not be null");
-        if (InterimPortletEntityImpl.isInterimPortletEntityId(portletEntityIdString)) {
-            final IPortletEntityId portletEntityId = new InterimPortletEntityImpl.InterimPortletEntityIdImpl(portletEntityIdString);
-            final InterimPortletEntityImpl interimPortletEntity = this.getInterimPortletEntity(portletEntityId);
-            if (interimPortletEntity != null) {
-                return interimPortletEntity;
-            }
-            
-            return new InterimPortletEntityImpl(portletEntityId);
-        }
-        
-        final long portletEntityIdLong;
-        try {
-            portletEntityIdLong = Long.parseLong(portletEntityIdString);
-        } catch (NumberFormatException nfe) {
-            throw new IllegalArgumentException("PortletEntityId must parsable as a long", nfe);
-        }
-
-        final PortletEntityIdImpl portletEntityId = new PortletEntityIdImpl(portletEntityIdLong);
-        return this.portletEntityDao.getPortletEntity(portletEntityId);
+        return this.getPortletEntity(new PortletEntityIdImpl(portletEntityIdString));
     }
     
     /* (non-Javadoc)
@@ -147,14 +142,36 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
             return interimPortletEntity;
         }
         
-        return this.portletEntityDao.getPortletEntity(channelSubscribeId, userId);
+        final IPortletEntity persistentPortletEntity = this.portletEntityDao.getPortletEntity(channelSubscribeId, userId);
+        if (persistentPortletEntity == null) {
+            return null;
+        }
+        
+        final PersistentPortletEntityWrapper wrappedPortletEntity = new PersistentPortletEntityWrapper(persistentPortletEntity);
+        this.setPersistentIdMapping(wrappedPortletEntity.getPortletEntityId(), persistentPortletEntity.getPortletEntityId());
+        
+        return wrappedPortletEntity;
     }
     
     /* (non-Javadoc)
      * @see org.jasig.portal.portlet.registry.IPortletEntityRegistry#getPortletEntitiesForUser(int)
      */
     public Set<IPortletEntity> getPortletEntitiesForUser(int userId) {
-        return this.portletEntityDao.getPortletEntitiesForUser(userId);
+        final Set<IPortletEntity> persistentPortletEntities = this.portletEntityDao.getPortletEntitiesForUser(userId);
+        
+        final Set<IPortletEntity> wrappedPortletEntities = new LinkedHashSet<IPortletEntity>();
+        for (final IPortletEntity persistentPortletEntity : persistentPortletEntities) {
+            final PersistentPortletEntityWrapper wrappedPortletEntity = new PersistentPortletEntityWrapper(persistentPortletEntity);
+            this.setPersistentIdMapping(wrappedPortletEntity.getPortletEntityId(), persistentPortletEntity.getPortletEntityId());
+            wrappedPortletEntities.add(wrappedPortletEntity);
+        }
+        
+        final Set<InterimPortletEntityImpl> interimPortletEntities = this.getInterimPortletEntities(userId);
+        if (interimPortletEntities != null) {
+            wrappedPortletEntities.addAll(interimPortletEntities);
+        }
+        
+        return wrappedPortletEntities;
     }
     
     /* (non-Javadoc)
@@ -185,7 +202,33 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
         final IPortletPreferences portletPreferences = portletEntity.getPortletPreferences();
         final List<IPortletPreference> preferences = portletPreferences.getPortletPreferences();
         
-        if (InterimPortletEntityImpl.isInterimPortletEntityId(portletEntityId)) {
+        if (portletEntity instanceof PersistentPortletEntityWrapper) {
+            //Unwrap the persistent entity
+            portletEntity = ((PersistentPortletEntityWrapper)portletEntity).getPersistentEntity();
+            
+            //Already persistent entity that still has prefs 
+            if (preferences.size() > 0) {
+                this.portletEntityDao.updatePortletEntity(portletEntity);
+            }
+            //Already persistent entity with no preferences, DELETE!
+            else {
+                this.portletEntityDao.deletePortletEntity(portletEntity);
+                
+                //remove the persistent ID mapping after the delete
+                this.removePersistentId(portletEntityId);
+                
+                //Setup an interim entity in its place
+                final IPortletDefinitionId portletDefinitionId = portletEntity.getPortletDefinitionId();
+                final String channelSubscribeId = portletEntity.getChannelSubscribeId();
+                final int userId = portletEntity.getUserId();
+                final IPortletEntity interimPortletEntity = this.createPortletEntity(portletDefinitionId, channelSubscribeId, userId);
+                
+                if (this.logger.isTraceEnabled()) {
+                    this.logger.trace("Persistent portlet entity " + portletEntityId + " no longer has preferences. Deleted it and created InterimPortletEntity " + interimPortletEntity.getPortletEntityId());
+                }
+            }
+        }
+        else {
             //There are preferences on the interim entity, create an store it
             if (preferences.size() > 0) {
                 final IPortletDefinitionId portletDefinitionId = portletEntity.getPortletDefinitionId();
@@ -199,22 +242,13 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
                 
                 //Remove the in-memory interim entity
                 this.removeInterimPortletEntity(portletEntityId);
-            }
-        }
-        else {
-            //Already persistent entity that still has prefs 
-            if (preferences.size() > 0) {
-                this.portletEntityDao.updatePortletEntity(portletEntity);
-            }
-            //Already persistent entity with no preferences, DELETE!
-            else {
-                this.portletEntityDao.deletePortletEntity(portletEntity);
                 
-                //Setup an interim entity in its place
-                final IPortletDefinitionId portletDefinitionId = portletEntity.getPortletDefinitionId();
-                final String channelSubscribeId = portletEntity.getChannelSubscribeId();
-                final int userId = portletEntity.getUserId();
-                this.createPortletEntity(portletDefinitionId, channelSubscribeId, userId);
+                //Setup the persistent ID mapping
+                this.setPersistentIdMapping(portletEntityId, persistantEntity.getPortletEntityId());
+                
+                if (this.logger.isTraceEnabled()) {
+                    this.logger.trace("InterimPortletEntity " + portletEntityId + " now has preferences. Deleted it and created persistent portlet entity " + persistantEntity.getPortletEntityId());
+                }
             }
         }
     }
@@ -225,12 +259,17 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
     public void deletePortletEntity(IPortletEntity portletEntity) {
         Validate.notNull(portletEntity, "portletEntity can not be null");
         
-        final IPortletEntityId portletEntityId = portletEntity.getPortletEntityId();
-        if (InterimPortletEntityImpl.isInterimPortletEntityId(portletEntityId)) {
-            this.removeInterimPortletEntity(portletEntityId);
+        if (portletEntity instanceof PersistentPortletEntityWrapper) {
+            //Unwrap the persistent entity
+            portletEntity = ((PersistentPortletEntityWrapper)portletEntity).getPersistentEntity();
+            
+            this.portletEntityDao.deletePortletEntity(portletEntity);
+            
+            //remove the persistent ID mapping after the delete
+            this.removePersistentId(portletEntity.getPortletEntityId());
         }
         else {
-            this.portletEntityDao.deletePortletEntity(portletEntity);
+            this.removeInterimPortletEntity(portletEntity.getPortletEntityId());
         }
     }
 
@@ -240,16 +279,67 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
     public IPortletDefinition getParentPortletDefinition(IPortletEntityId portletEntityId) {
         Validate.notNull(portletEntityId, "portletEntityId can not be null");
         
-        final IPortletDefinitionId portletDefinitionId;
-        if (InterimPortletEntityImpl.isInterimPortletEntityId(portletEntityId)) {
-            IPortletEntity portletEntity = new InterimPortletEntityImpl(portletEntityId);
-            portletDefinitionId = portletEntity.getPortletDefinitionId();
-        } else {
-            final IPortletEntity portletEntity = this.getPortletEntity(portletEntityId);
-            portletDefinitionId = portletEntity.getPortletDefinitionId();
-        }
+        final IPortletEntity portletEntity = this.getPortletEntity(portletEntityId);
+        final IPortletDefinitionId portletDefinitionId = portletEntity.getPortletDefinitionId();
 
         return this.portletDefinitionRegistry.getPortletDefinition(portletDefinitionId);
+    }
+    
+    protected void setPersistentIdMapping(IPortletEntityId wrapperId, IPortletEntityId persistentId) {
+        final HttpSession session = this.getSession();
+        if (session == null) {
+            return;
+        }
+        
+        Map<IPortletEntityId, IPortletEntityId> idMapping;
+        //Sync on the session to ensure other threads aren't creating the Map at the same time
+        synchronized (WebUtils.getSessionMutex(session)) {
+            idMapping = (Map<IPortletEntityId, IPortletEntityId>)session.getAttribute(PORTLET_ENTITY_ID_MAP_ATTRIBUTE);
+            if (idMapping == null) {
+                idMapping = new ConcurrentHashMap<IPortletEntityId, IPortletEntityId>();
+                session.setAttribute(PORTLET_ENTITY_ID_MAP_ATTRIBUTE, idMapping);
+            }
+        }
+        
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace("Mapping wraper ID " + wrapperId + " to persistent ID " + persistentId);
+        }
+        
+        idMapping.put(wrapperId, persistentId);
+    }
+    
+    protected IPortletEntityId getPersistentId(IPortletEntityId wrapperId) {
+        final Map<IPortletEntityId, IPortletEntityId> idMapping = this.getPersistentIdMap();
+        if (idMapping == null) {
+            return null;
+        }
+        
+        return idMapping.get(wrapperId);
+    }
+    
+    protected void removePersistentId(IPortletEntityId wrapperId) {
+        final Map<IPortletEntityId, IPortletEntityId> idMapping = this.getPersistentIdMap();
+        if (idMapping == null) {
+            return;
+        }
+        
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace("Removed persistent ID for wraper ID " + wrapperId);
+        }
+        
+        idMapping.remove(wrapperId);
+    }
+
+    protected Map<IPortletEntityId, IPortletEntityId> getPersistentIdMap() {
+        final HttpSession session = this.getSession();
+        if (session == null) {
+            return null;
+        }
+        
+        //Sync on the session to ensure other threads aren't creating the Map at the same time
+        synchronized (WebUtils.getSessionMutex(session)) {
+            return (Map<IPortletEntityId, IPortletEntityId>)session.getAttribute(PORTLET_ENTITY_ID_MAP_ATTRIBUTE);
+        }
     }
     
     protected void removeInterimPortletEntity(IPortletEntityId portletEntityId) {
@@ -279,8 +369,20 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
         return entityCache.getEntity(channelSubscribeId, userId);
     }
     
+    protected Set<InterimPortletEntityImpl> getInterimPortletEntities(int userId) {
+        final InterimPortletEntityCache entityCache = getInterimPortletEntityCache();
+        if (entityCache == null) {
+            return null;
+        }
+        
+        return entityCache.getEntities(userId);
+    }
+    
     protected void storeInterimPortletEntity(InterimPortletEntityImpl interimPortletEntity) {
         final HttpSession session = this.getSession();
+        if (session == null) {
+            return;
+        }
         
         InterimPortletEntityCache entityCache;
         //Sync on the session to ensure other threads aren't creating the Map at the same time
@@ -297,6 +399,9 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
     
     protected InterimPortletEntityCache getInterimPortletEntityCache() {
         final HttpSession session = this.getSession();
+        if (session == null) {
+            return null;
+        }
         
         //Sync on the session to ensure other threads aren't creating the Map at the same time
         synchronized (WebUtils.getSessionMutex(session)) {
@@ -305,26 +410,23 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
     }
 
     /**
-     * Gets the session for the request.
-     * 
-     * @return The session for the current request, will not return null.
+     * @return The session for the current request, will return null if not in request
      */
     protected HttpSession getSession() {
-        final HttpServletRequest request = this.portalRequestUtils.getCurrentPortalRequest();
+        final HttpServletRequest request;
+        try {
+            request = this.portalRequestUtils.getCurrentPortalRequest();
+        }
+        catch (IllegalStateException e) {
+            //No current request, just return null
+            return null;
+        }
+        
         final HttpSession session = request.getSession(false);
         if (session == null) {
             throw new IllegalStateException("A HttpSession must already exist for the PortletEntityRegistryImpl to function");
         }
         return session;
-    }
-    
-
-    private static final class PortletEntityIdImpl extends AbstractObjectId implements IPortletEntityId {
-        private static final long serialVersionUID = 1L;
-    
-        public PortletEntityIdImpl(long portletEntityId) {
-            super(Long.toString(portletEntityId));
-        }
     }
     
     private static final class InterimPortletEntityCache {
@@ -354,6 +456,24 @@ public class PortletEntityRegistryImpl implements IPortletEntityRegistry {
             finally {
                 this.cacheLock.readLock().unlock();
             }
+        }
+        
+        public Set<InterimPortletEntityImpl> getEntities(int userId) {
+            final Set<InterimPortletEntityImpl> entities = new LinkedHashSet<InterimPortletEntityImpl>();
+            
+            this.cacheLock.readLock().lock();
+            try {
+                for (final InterimPortletEntityImpl interimPortletEntity : this.entitiesById.values()) {
+                    if (userId == interimPortletEntity.getUserId()) {
+                        entities.add(interimPortletEntity);
+                    }
+                }
+            }
+            finally {
+                this.cacheLock.readLock().unlock();
+            }
+            
+            return entities;
         }
         
         public InterimPortletEntityImpl getEntity(IPortletEntityId portletEntityId) {
