26f0846822252b05b2711773a6a6ed60407254ed
Andrew Petro <apetro@unicon.net>
2010-10-21 15:21:03 +0000

    UP-2593 Fixed unit test to not use Java lang assertions and to correct test prefix on SHA hashed passwords.
    git-svn-id: https://source.jasig.org/uPortal/trunk@22034 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-war/src/test/java/org/jasig/portal/security/PortalPasswordServiceImplTest.java
+++ uportal-war/src/test/java/org/jasig/portal/security/PortalPasswordServiceImplTest.java
@@ -1,20 +1,33 @@
 package org.jasig.portal.security;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class PortalPasswordServiceImplTest {
 
     IPortalPasswordService passwordService = new PortalPasswordServiceImpl();
     
+    /**
+     * Test that new password encryptions are via SHA256.
+     */
     @Test
     public void testSHAPassword() {
         String encrypted = passwordService.encryptPassword("admin");
-        assert encrypted.startsWith("(SHA256)");
-        assert passwordService.validatePassword("admin", encrypted);
+        
+        assertTrue("Expected encrypted password to start with '(SHA256)' but was [" + encrypted + "]", encrypted.startsWith("(SHA256)"));
+        assertTrue(passwordService.validatePassword("admin", encrypted));
+        
+        assertFalse(passwordService.validatePassword("bob", encrypted));
     }
     
+    /**
+     * Test that validating passwords against the legacy, MD5-encoding of passwords still works.
+     */
     @Test
     public void testLegacyPassword() {
-        assert passwordService.validatePassword("admin", "(MD5)PfgN2CNzDOPAociIqa31KrWXuxHTgLMp");
+        assertTrue(passwordService.validatePassword("admin", "(MD5)PfgN2CNzDOPAociIqa31KrWXuxHTgLMp"));
+        assertFalse(passwordService.validatePassword("admin", "(MD5)PfgN2CNzDOPAociIqa31KrWXuxHTgLMz"));
     }
 }
