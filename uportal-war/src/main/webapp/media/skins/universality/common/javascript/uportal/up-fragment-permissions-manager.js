319b222c473d09b164af85b7941206c6fb5d9ea5
Jen Bourey <jennifer.bourey@gmail.com>
2011-01-11 21:25:13 +0000

    UP-2701 Cleaning up fragment permissions javascript and removing old ajax-preferences-jquery javascript.
    git-svn-id: https://source.jasig.org/uPortal/trunk@22801 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-war/src/main/webapp/media/skins/universality/common/javascript/uportal/up-fragment-permissions-manager.js
+++ uportal-war/src/main/webapp/media/skins/universality/common/javascript/uportal/up-fragment-permissions-manager.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-var uportal = uportal || {};
+var up = up || {};
 
 (function($, fluid){
     
@@ -107,7 +107,7 @@ var uportal = uportal || {};
             data.addChildAllowed = $(form).find("[name=addChildAllowed]").attr("checked");
         }
         
-        $.post(that.options.savePermissionsUrl, data, null, "xml");
+        $.post(that.options.savePermissionsUrl, data, null, "json");
         
         var permissions = {
             deletable: data.deletable,
@@ -158,8 +158,8 @@ var uportal = uportal || {};
         }
     };
     
-    uportal.FragmentPermissionsManager = function(container, options) {
-        var that = fluid.initView("uportal.FragmentPermissionsManager", container, options);
+    up.FragmentPermissionsMenu = function(container, options) {
+        var that = fluid.initView("up.FragmentPermissionsMenu", container, options);
         
         that.state = {};
 
@@ -182,7 +182,7 @@ var uportal = uportal || {};
 
     
     // defaults
-    fluid.defaults("uportal.FragmentPermissionsManager", {
+    fluid.defaults("up.FragmentPermissionsMenu", {
         savePermissionsUrl: "mvc/layout",
         elementExtractor: null,
         titleExtractor: null,
@@ -198,6 +198,96 @@ var uportal = uportal || {};
         },
         listeners: {
             onUpdatePermissions: null
+        }
+    });
+    
+    up.FragmentPermissionsManager = function(container, options) {
+        var that = fluid.initView("up.FragmentPermissionsManager", container, options);
+        
+        that.menus = {};
+
+        // tabs permissions manager
+        that.locate("pageDialog").dialog({ width: 550, modal: true, autoOpen: false });
+        that.menus.pagePermissionsManager = up.FragmentPermissionsMenu(that.locate("pageDialog"), {
+            savePermissionsUrl: that.options.savePermissionsUrl,
+            elementExtractor: function(that, link){
+                return $("#portalNavigationList li.active"); 
+            },
+            titleExtractor: function(element){ return $("#portalNavigationList li.active a.portal-navigation-link span").text(); },
+            selectors: {
+                formTitle: "h2"
+            }
+        });
+        that.locate("pageDialogLink").click(function(){
+            that.menus.pagePermissionsManager.refresh($(this));
+            that.locate("pageDialog").dialog("open");
+        });
+        // TODO: on update, refresh tab name, editing, delete
+        
+        // columns permissions manager
+        that.locate("columnDialog").dialog({ width: 550, modal: true, autoOpen: false });
+        that.menus.columnPermissionsManager = up.FragmentPermissionsMenu(that.locate("columnDialog"), {
+            savePermissionsUrl: that.options.savePermissionsUrl,
+            elementExtractor: function(that, link){
+                return $(link).parents(".portal-page-column"); 
+            },
+            titleExtractor: function(element){ 
+                return "Column " + ($(".portal-page-column").index(element) + 1); 
+            },
+            selectors: {
+                formTitle: "h2"
+            }
+        });
+        that.locate("columnDialogLink").click(function(){
+            that.menus.columnPermissionsManager.refresh($(this));
+            that.locate("columnDialog").dialog("open");
+        });
+        // TODO: on update, refresh layout options
+        
+        // portlet permissions manager
+        that.locate("portletDialog").dialog({ width: 550, modal: true, autoOpen: false });
+        that.menus.portletPermissionsManager = up.FragmentPermissionsMenu(that.locate("portletDialog"), {
+            savePermissionsUrl: that.options.savePermissionsUrl,
+            elementExtractor: function(that, link){
+                return $(link).parents(".up-portlet-wrapper"); 
+            },
+            titleExtractor: function(element){ return element.find(".up-portlet-wrapper-inner h2 a").text(); },
+            selectors: {
+                formTitle: "h2"
+            },
+            listeners: {
+                onUpdatePermissions: function(element, newPermissions) {
+                    if (!newPermissions.movable) {
+                        element.addClass("locked").removeClass("fl-reorderer-movable-default");
+                        element.find("[id*=toolbar_]").removeClass("ui-draggable");
+                    } else {
+                        element.removeClass("locked").addClass("fl-reorderer-movable-default");
+                        element.find("[id*=toolbar_]").addClass("ui-draggable");
+                    }
+                    // TODO: Apply portlet permissions: 
+                    // includes refreshing reorderer and displaying/hiding delete
+                }
+            }
+        });
+        that.locate("portletDialogLink").click(function(){
+            that.menus.portletPermissionsManager.refresh($(this));
+            that.locate("portletDialog").dialog("open");
+        });
+        
+        return that;
+    };
+
+    // defaults
+    fluid.defaults("up.FragmentPermissionsManager", {
+        savePermissionsUrl: "mvc/layout",
+        selectors: {
+            formTitle: null,
+            pageDialog: ".edit-page-permissions-dialog",
+            pageDialogLink: "#editPagePermissionsLink",
+            columnDialog: ".edit-column-permissions-dialog",
+            columnDialogLink: ".portal-column-permissions-link",
+            portletDialog: ".edit-portlet-permissions-dialog",
+            portletDialogLink: ".portlet-permissions-link"
         }
     });
     
