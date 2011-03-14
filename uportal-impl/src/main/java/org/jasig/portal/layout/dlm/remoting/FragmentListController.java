57765d3d5e3af7e0f8bee3794d132d214cc28a6d
Andrew Wills <wills.drew@gmail.com>
2011-03-14 18:36:43 +0000

    UP-2958:  Enhance the DLM fragment audit portlet to show included portlets as well
    git-svn-id: https://source.jasig.org/uPortal/branches/rel-3-2-patches@23161 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-impl/src/main/java/org/jasig/portal/layout/dlm/remoting/FragmentListController.java
+++ uportal-impl/src/main/java/org/jasig/portal/layout/dlm/remoting/FragmentListController.java
@@ -23,19 +23,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jasig.portal.AuthorizationException;
+import org.jasig.portal.IChannelRegistryStore;
+import org.jasig.portal.channel.IChannelDefinition;
+import org.jasig.portal.layout.IUserLayoutStore;
+import org.jasig.portal.layout.UserLayoutStoreFactory;
 import org.jasig.portal.layout.dlm.ConfigurationLoader;
 import org.jasig.portal.layout.dlm.Evaluator;
 import org.jasig.portal.layout.dlm.FragmentDefinition;
+import org.jasig.portal.layout.dlm.RDBMDistributedLayoutStore;
 import org.jasig.portal.security.AdminEvaluator;
 import org.jasig.portal.security.IPerson;
 import org.jasig.portal.security.IPersonManager;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 /**
  * Spring controller that returns a JSON or XML representation of DLM fragments.  For
@@ -54,10 +70,33 @@ import org.springframework.web.servlet.mvc.AbstractController;
  */
 public class FragmentListController extends AbstractController {
     
-    private static final Sort DEFAULT_SORT = Sort.PRECEDENCE; 
+    private static final Sort DEFAULT_SORT = Sort.PRECEDENCE;
+    private static final String CHANNEL_FNAME_XPATH = "//channel/@fname";
     
     private ConfigurationLoader dlmConfig;
     private IPersonManager personManager;
+    private RDBMDistributedLayoutStore dlm;
+    private XPathExpression expr;
+    private IChannelRegistryStore channelRegistryStore;
+    private final Log log = LogFactory.getLog(getClass());
+    
+    public FragmentListController() {
+        IUserLayoutStore uls = UserLayoutStoreFactory.getUserLayoutStoreImpl();
+        if (uls instanceof RDBMDistributedLayoutStore) {
+            // Overkill perhaps, but making this code less likely to throw an 
+            // error if in the future someone, somewhere uses an ILayoutStore 
+            // impl other than DLM.
+            dlm = (RDBMDistributedLayoutStore) uls;
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            try {
+                expr = xpath.compile(CHANNEL_FNAME_XPATH);
+            } catch (XPathExpressionException e) {
+                String msg = "Failed to bootstrap an XPathExpression";
+                log.error(msg, e);
+                throw new RuntimeException(msg, e);
+            }
+        }
+    }
     
     public void setConfigurationLoader(ConfigurationLoader dlmConfig) {
         this.dlmConfig = dlmConfig;
@@ -67,8 +106,12 @@ public class FragmentListController extends AbstractController {
         this.personManager = personManager;
     }
 
+    public void setChannelRegistryStore(IChannelRegistryStore channelRegistryStore) {
+        this.channelRegistryStore = channelRegistryStore;
+    }
+
     @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse res) {
+    protected ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse res) throws ServletException {
 
         // Verify that the user is allowed to use this service
         IPerson user = personManager.getPerson(req);
@@ -76,9 +119,43 @@ public class FragmentListController extends AbstractController {
             throw new AuthorizationException("User " + user.getUserName() + " not an administrator.");
         }
 
+        Map<String,Document> fragmentLayoutMap = null;
+        if (dlm != null) {
+            try {
+                fragmentLayoutMap = dlm.getFragmentLayoutCopies();
+            } catch (Exception e) {
+                String msg = "Failed to access fragment layouts";
+                log.error(msg, e);
+                throw new ServletException(msg, e);
+            }
+        }
+
         List<FragmentBean> fragments = new ArrayList<FragmentBean>(); 
         for (FragmentDefinition frag : dlmConfig.getFragments()) {
-            fragments.add(FragmentBean.fromFragmentDefinition(frag));
+
+            Document layout = fragmentLayoutMap != null 
+                                ? fragmentLayoutMap.get(frag.getOwnerId())
+                                : null;
+
+            List<String> portlets = null;
+            if (layout != null) {
+                portlets = new ArrayList<String>();
+                try {
+                    NodeList channelFNames = (NodeList) expr.evaluate(layout, XPathConstants.NODESET);
+                    for (int i=0; i < channelFNames.getLength(); i++) {
+                        String fname = channelFNames.item(i).getTextContent();
+                        IChannelDefinition chanDef = channelRegistryStore.getChannelDefinition(fname);
+                        portlets.add(chanDef.getTitle());
+                    }
+                } catch (XPathExpressionException e) {
+                    String msg = "Failed to search layout documents for channels";
+                    log.error(msg, e);
+                    throw new ServletException(msg, e);
+                }
+            }
+            
+            fragments.add(FragmentBean.create(frag, portlets));
+
         }
         
         // Determine & follow sorting preference...
@@ -138,17 +215,20 @@ public class FragmentListController extends AbstractController {
         private final String ownerId;
         private final Double precedence;
         private final List<String> audience;
+        private final List<String> portlets;
         
-        public static FragmentBean fromFragmentDefinition(FragmentDefinition frag) {
+        public static FragmentBean create(FragmentDefinition frag, List<String> portlets) {
             
             // Assertions.
             if (frag == null) {
                 String msg = "Argument 'frag' cannot be null";
                 throw new IllegalArgumentException(msg);
             }
-            
+            // NB:  'portlets' may be null
+
             return new FragmentBean(frag.getName(), frag.getOwnerId(), 
-                            frag.getPrecedence(), frag.getAudience());
+                            frag.getPrecedence(), frag.getAudience(),
+                            portlets);
             
         }
         
@@ -168,7 +248,11 @@ public class FragmentListController extends AbstractController {
             return audience;
         }
         
-        private FragmentBean(String name, String ownerId, Double precedence, List<Evaluator> audience) {
+        public List<String> getPortlets() {
+            return portlets;
+        }
+
+        private FragmentBean(String name, String ownerId, Double precedence, List<Evaluator> audience, List<String> portlets) {
 
             this.name = name;
             this.ownerId = ownerId;
@@ -179,6 +263,11 @@ public class FragmentListController extends AbstractController {
                 list.add(ev.getSummary());
             }
             this.audience = Collections.unmodifiableList(list);
+            if (portlets != null) {
+                this.portlets = Collections.unmodifiableList(portlets);
+            } else {
+                this.portlets = Collections.emptyList();
+            }
 
         }
         
