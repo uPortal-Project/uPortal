4f615ae66660b135217bfb9ceccabd0aa760d76d
Eric Dalquist <eric.dalquist@gmail.com>
2011-09-29 20:32:08 +0000

    UP-3184 Add db update tasks
    git-svn-id: https://source.jasig.org/uPortal/trunk@25206 f5dbab47-78f9-eb45-b975-e544023573eb

--- uportal-war/src/main/java/org/jasig/portal/tools/dbloader/ISchemaExport.java
+++ uportal-war/src/main/java/org/jasig/portal/tools/dbloader/ISchemaExport.java
@@ -26,10 +26,25 @@ package org.jasig.portal.tools.dbloader;
 public interface ISchemaExport {
 
     /**
-     * @param export If the database should have the SQL executed agaisnt it
+     * @deprecated use {@link #create(boolean, boolean, boolean, String, boolean)}
+     */
+    @Deprecated
+    public void hbm2ddl(boolean export, boolean create, boolean drop, String outputFile, boolean haltOnError);
+    
+    /**
+     * @param export If the database should have the SQL executed against it
+     * @param create If database objects should be created
      * @param drop If existing database objects should be dropped before creating new objects
      * @param outputFile Optional file to write out the SQL to.
+     * @param haltOnError If an error should cause creation to halt with an exception
      */
-    public void hbm2ddl(boolean export, boolean create, boolean drop, String outputFile, boolean haltOnError);
+    public void create(boolean export, boolean create, boolean drop, String outputFile, boolean haltOnError);
+    
+    /**
+     * @param export If the database should have the SQL executed against it
+     * @param outputFile Optional file to write out the SQL to.
+     * @param haltOnError If an error should cause creation to halt with an exception
+     */
+    public void update(boolean export, String outputFile, boolean haltOnError);
 
 }
