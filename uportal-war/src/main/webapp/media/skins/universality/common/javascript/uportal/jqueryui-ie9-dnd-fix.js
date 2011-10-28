fd5eb57db26a5ff9b3330633ea9703b1ab614172
Bob Finch <bob.finch@wwu.edu>
2011-10-28 14:37:51 -0700

    fix copy and paste of jquery ui plugin names

--- uportal-war/src/main/webapp/media/skins/universality/common/javascript/uportal/jqueryui-ie9-dnd-fix.js
+++ uportal-war/src/main/webapp/media/skins/universality/common/javascript/uportal/jqueryui-ie9-dnd-fix.js
@@ -1,9 +1,9 @@
 // HOTFIX: We can't upgrade to jQuery UI 1.8.6 (yet)
 // This hotfix makes older versions of jQuery UI drag-and-drop work in IE9
 (function($) {
-	$.each([$.ui.draggable.prototype, $.ui.draggable.prototype,
-	        $.ui.draggable.prototype, $.ui.draggable.prototype,
-	        $.ui.draggable.prototype], function (index, value) {
+	$.each([$.ui.draggable.prototype, $.ui.resizable.prototype,
+	        $.ui.selectable.prototype, $.ui.sortable.prototype,
+	        $.ui.slider.prototype], function (index, value) {
 		var inner = value._mouseMove;
 		value._mouseMove = function (event) {
 	        if ($.browser.msie && document.documentMode >= 9) {
