79ee115df52c0c59abf0e82b09f2a391f79b8ee5
Bob Finch <bob.finch@wwu.edu>
2011-10-28 13:13:44 -0700

    UP-3052 Drag & drop doesn't work on IE9

---
+++ uportal-war/src/main/webapp/media/skins/universality/common/javascript/uportal/jqueryui-ie9-dnd-fix.js
@@ -0,0 +1,15 @@
+// HOTFIX: We can't upgrade to jQuery UI 1.8.6 (yet)
+// This hotfix makes older versions of jQuery UI drag-and-drop work in IE9
+(function($) {
+	$.each([$.ui.draggable.prototype, $.ui.draggable.prototype,
+	        $.ui.draggable.prototype, $.ui.draggable.prototype,
+	        $.ui.draggable.prototype], function (index, value) {
+		var inner = value._mouseMove;
+		value._mouseMove = function (event) {
+	        if ($.browser.msie && document.documentMode >= 9) {
+	            event.button = 1;
+	        };
+	        inner.apply(this, [ event ]);
+		};
+	});
+})(jQuery);
