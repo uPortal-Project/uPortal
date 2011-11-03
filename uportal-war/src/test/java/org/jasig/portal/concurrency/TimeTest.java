f5a4f301fece5db4603c88f2cf606afeb9518fbd
edalquist <eric.dalquist@gmail.com>
2011-11-03 10:34:23 -0500

    Update license headers for release

--- uportal-war/src/test/java/org/jasig/portal/concurrency/TimeTest.java
+++ uportal-war/src/test/java/org/jasig/portal/concurrency/TimeTest.java
@@ -5,9 +5,9 @@
  * Jasig licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a
- * copy of the License at_
+ * copy of the License at:
  *
- * http_//www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.jasig.portal.concurrency;
 
 import static org.junit.Assert.assertEquals;
