f73624d350ed73fc03fed2cd6b389b154cf9b9b5
Jen Bourey <jennifer.bourey@gmail.com>
2012-02-14 11:39:39 -0800

    UP-3343 Updating new profile evaluator to work with IProfileMapper service to avoid circular spring references

--- uportal-war/src/main/java/org/jasig/portal/layout/dlm/providers/ProfileEvaluator.java
+++ uportal-war/src/main/java/org/jasig/portal/layout/dlm/providers/ProfileEvaluator.java
@@ -1,44 +1,45 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.jasig.portal.layout.dlm.providers;
 
 import javax.persistence.Cacheable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Transient;
 import javax.servlet.http.HttpServletRequest;
 
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.jasig.portal.IUserProfile;
+import org.jasig.portal.layout.IProfileMapper;
 import org.jasig.portal.layout.dlm.Evaluator;
 import org.jasig.portal.layout.dlm.EvaluatorFactory;
 import org.jasig.portal.security.IPerson;
+import org.jasig.portal.spring.locator.ApplicationContextLocator;
 import org.jasig.portal.url.IPortalRequestUtils;
-import org.jasig.portal.user.IUserInstance;
-import org.jasig.portal.user.IUserInstanceManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 @Entity
 @Cacheable
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class ProfileEvaluator extends Evaluator {
-
-    @Transient
-    private IPortalRequestUtils portalRequestUtils;
-    
-    @Autowired(required = true)
-    public void setPortalRequestUtils(IPortalRequestUtils portalRequestUtils) {
-        this.portalRequestUtils = portalRequestUtils;
-    }
-    
-    @Transient
-    private IUserInstanceManager userInstanceManager;
-    
-    @Autowired(required = true)
-    public void setUserInstanceManager(IUserInstanceManager userInstanceManager) {
-        this.userInstanceManager = userInstanceManager;
-    }
 
     @Column(name = "PROFILE_FNAME")
     protected String profileFname;
@@ -51,12 +52,15 @@ public class ProfileEvaluator extends Evaluator {
     
     @Override
     public boolean isApplicable(IPerson person) {
-
-        final HttpServletRequest request = portalRequestUtils.getCurrentPortalRequest();
-        final IUserInstance userInstance = userInstanceManager.getUserInstance(request);
-        final IUserProfile profile = userInstance.getPreferencesManager().getUserProfile();
         
-        return profileFname.equals(profile.getProfileFname());
+        final ApplicationContext applicationContext = ApplicationContextLocator.getApplicationContext();
+        final IPortalRequestUtils portalRequestUtils = applicationContext.getBean(IPortalRequestUtils.class);
+        final IProfileMapper profileMapper = applicationContext.getBean(IProfileMapper.class);
+        
+        final HttpServletRequest request = portalRequestUtils.getCurrentPortalRequest();
+        final String currentFname = profileMapper.getProfileFname(person, request);
+        
+        return profileFname.equals(currentFname);
     }
 
     @Override
