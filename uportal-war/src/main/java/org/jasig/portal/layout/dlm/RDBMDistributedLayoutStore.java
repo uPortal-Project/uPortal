35133ed68c9ac7fb9bcfaa077b26b1eb395b299f
Eric Dalquist <eric.dalquist@gmail.com>
2012-03-05 16:50:04 -0600

    UP-3368 Work on not failing for missing portlets

--- uportal-war/src/main/java/org/jasig/portal/layout/dlm/RDBMDistributedLayoutStore.java
+++ uportal-war/src/main/java/org/jasig/portal/layout/dlm/RDBMDistributedLayoutStore.java
@@ -86,6 +86,7 @@ import org.jasig.portal.xml.XmlUtilitiesImpl;
 import org.jasig.portal.xml.xpath.XPathOperations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.transaction.annotation.Transactional;
@@ -122,6 +123,8 @@ public class RDBMDistributedLayoutStore extends RDBMUserLayoutStore {
     private FragmentActivator fragmentActivator;
 
     private Ehcache fragmentNodeInfoCache;
+    
+    private boolean errorOnMissingPortlet = true;
 
     static final String TEMPLATE_USER_NAME = "org.jasig.portal.services.Authentication.defaultTemplateUserName";
 
@@ -182,6 +185,10 @@ public class RDBMDistributedLayoutStore extends RDBMUserLayoutStore {
         this.fragmentNodeInfoCache = fragmentNodeInfoCache;
     }
 
+    @Value("${org.jasig.portal.io.layout.errorOnMissingPortlet}")
+    public void setErrorOnMissingPortlet(boolean errorOnMissingPortlet) {
+        this.errorOnMissingPortlet = errorOnMissingPortlet;
+    }
 
     /**
      * Method for acquiring copies of fragment layouts to assist in debugging.
@@ -790,8 +797,13 @@ public class RDBMDistributedLayoutStore extends RDBMUserLayoutStore {
             final String fname = c.valueOf("@fname");
             final IPortletDefinition cd = this.portletDefinitionRegistry.getPortletDefinitionByFname(fname);
             if (cd == null) {
-                throw new IllegalArgumentException("No published channel for fname=" + fname
-                        + " referenced by layout for " + ownerUsername);
+                if (errorOnMissingPortlet) {
+                    throw new IllegalArgumentException("No published channel for fname=" + fname
+                            + " referenced by layout for " + ownerUsername);
+                }
+                else {
+                    log.warn("");
+                }
             }
             c.addAttribute("chanID", String.valueOf(cd.getPortletDefinitionId().getStringId()));
         }
