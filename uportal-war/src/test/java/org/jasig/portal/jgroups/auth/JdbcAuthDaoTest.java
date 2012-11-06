3c60936d37393f91e62a01f8c304f583a58bc210
Eric Dalquist <eric.dalquist@gmail.com>
2012-11-06 14:23:37 -0600

    UP-3607 Enable AUTH and ENCRYPT in jGroups
    Add custom auth token API that picks a random string and stores it in the DB to
    be used as the base for the hashed authentication key

---
+++ uportal-war/src/test/java/org/jasig/portal/jgroups/auth/JdbcAuthDaoTest.java
@@ -0,0 +1,82 @@
+package org.jasig.portal.jgroups.auth;
+
+import static junit.framework.Assert.assertEquals;
+
+import java.net.UnknownHostException;
+import java.util.concurrent.Callable;
+
+import org.jasig.portal.concurrency.CallableWithoutResult;
+import org.jasig.portal.test.BasePortalJpaDaoTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:jpaPortalTestApplicationContext.xml")
+public class JdbcAuthDaoTest extends BasePortalJpaDaoTest {
+
+    @Autowired
+    private JdbcAuthDao authDao;
+    
+    @Autowired
+    private JdbcOperations jdbcOperations;
+    
+    @Test
+    public void testSimpleGetCreate() throws UnknownHostException {
+        final String service = "foo";
+        
+        //Create & Return
+        final String token = this.execute(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                final String t = authDao.getAuthToken(service);
+                assertEquals(100, t.length());
+                return t;
+            }
+        });
+        
+        
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                final String t = authDao.getAuthToken(service);
+                assertEquals(token, t);
+            }
+        });
+   }
+    
+
+    @Test
+    public void testConcurrentCreate() throws UnknownHostException {
+        final String service = "foo";
+        
+        //Create & Return
+        final String token = this.execute(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                final String t = authDao.getAuthToken(service);
+                assertEquals(100, t.length());
+                return t;
+            }
+        });
+        
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                authDao.createToken(service);
+            }
+        });
+        
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                final String t = authDao.getAuthToken(service);
+                assertEquals(token, t);
+            }
+        });
+   }
+}
