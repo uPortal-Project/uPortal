a17a3485acf56157f58fc9e1d1ced0561eab5473
Eric Dalquist <eric.dalquist@gmail.com>
2012-11-06 11:25:38 -0600

    UP-3607 Switch to TCP and database ping for jGroups
    Add DAO_PING discovery protocol that delegates to a PingDao impl
    Add Spring JDBC impl of PingDao
    Switch jGroups config to use TCP transport and DAO_PING for discovery

---
+++ uportal-war/src/test/java/org/jasig/portal/jgroups/protocols/JdbcPingDaoTest.java
@@ -0,0 +1,118 @@
+package org.jasig.portal.jgroups.protocols;
+
+import static junit.framework.Assert.assertEquals;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.jasig.portal.concurrency.CallableWithoutResult;
+import org.jasig.portal.test.BasePortalJpaDaoTest;
+import org.jgroups.Address;
+import org.jgroups.PhysicalAddress;
+import org.jgroups.stack.IpAddress;
+import org.jgroups.util.UUID;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:jpaPortalTestApplicationContext.xml")
+public class JdbcPingDaoTest extends BasePortalJpaDaoTest {
+
+    @Autowired
+    private PingDao pingDao;
+    
+    @Test
+    public void testPingLifecycle() throws UnknownHostException {
+        final String cluster = "cluster";
+        final UUID uuid = UUID.randomUUID();
+        final IpAddress ipAddress = new IpAddress("127.0.0.1", 1337);
+        final IpAddress ipAddress2 = new IpAddress("127.0.0.2", 7331);
+        
+        //Doesn't exist
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                final Map<Address, PhysicalAddress> addresses = pingDao.getAddresses(cluster);
+                assertEquals(0, addresses.size());
+            }
+        });
+
+        //Delete nothing
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                final Collection<Address> addresses = Arrays.<Address>asList(ipAddress, ipAddress2);
+                pingDao.purgeOtherAddresses(cluster, addresses);
+            }
+        });
+
+        //Create
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                pingDao.addAddress(cluster, uuid, ipAddress);
+            }
+        });
+
+        //verify
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                final Map<Address, PhysicalAddress> addresses = pingDao.getAddresses(cluster);
+                
+                assertEquals(1, addresses.size());
+                
+                final Entry<Address, PhysicalAddress> entry = addresses.entrySet().iterator().next();
+                assertEquals(uuid, entry.getKey());
+                assertEquals(ipAddress, entry.getValue());
+            }
+        });
+
+        //Update
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                pingDao.addAddress(cluster, uuid, ipAddress2);
+            }
+        });
+
+        //verify
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                final Map<Address, PhysicalAddress> addresses = pingDao.getAddresses(cluster);
+                
+                assertEquals(1, addresses.size());
+                
+                final Entry<Address, PhysicalAddress> entry = addresses.entrySet().iterator().next();
+                assertEquals(uuid, entry.getKey());
+                assertEquals(ipAddress2, entry.getValue());
+            }
+        });
+
+        //Delete
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                final Collection<Address> addresses = Arrays.<Address>asList(ipAddress);
+                pingDao.purgeOtherAddresses(cluster, addresses);
+            }
+        });
+        
+        //Doesn't exist
+        this.execute(new CallableWithoutResult() {
+            @Override
+            protected void callWithoutResult() {
+                final Map<Address, PhysicalAddress> addresses = pingDao.getAddresses(cluster);
+                assertEquals(0, addresses.size());
+            }
+        });
+    }
+}
