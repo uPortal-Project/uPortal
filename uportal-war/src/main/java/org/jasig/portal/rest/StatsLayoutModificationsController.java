7260726d8f8714f16ddbfdcd62f0f763ec05277f
Chris Waymire <chris@waymire.net>
2013-01-02 16:55:11 -0700

    UP-3632 Refactor the "Popular Portlets" controller logic to use the new aggregation methods/data.

--- uportal-war/src/main/java/org/jasig/portal/rest/StatsLayoutModificationsController.java
+++ uportal-war/src/main/java/org/jasig/portal/rest/StatsLayoutModificationsController.java
@@ -19,20 +19,16 @@
 
 package org.jasig.portal.rest;
 
-import java.io.Serializable;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.regex.Pattern;
 
-import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -41,18 +37,27 @@ import javax.sql.DataSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jasig.portal.EntityIdentifier;
-import org.jasig.portal.jpa.BaseRawEventsJpaDao;
+import org.jasig.portal.events.aggr.AggregationInterval;
+import org.jasig.portal.events.aggr.BaseAggregationDateTimeComparator;
+import org.jasig.portal.events.aggr.groups.AggregatedGroupLookupDao;
+import org.jasig.portal.events.aggr.groups.AggregatedGroupMapping;
+import org.jasig.portal.events.aggr.portletlayout.PortletLayoutAggregation;
+import org.jasig.portal.events.aggr.portletlayout.PortletLayoutAggregationDao;
+import org.jasig.portal.events.aggr.portletlayout.PortletLayoutAggregationKey;
+import org.jasig.portal.events.aggr.portletlayout.PortletLayoutAggregationKeyImpl;
+import org.jasig.portal.events.aggr.portlets.AggregatedPortletLookupDao;
+import org.jasig.portal.events.aggr.portlets.AggregatedPortletMapping;
+import org.jasig.portal.portlet.dao.IPortletDefinitionDao;
+import org.jasig.portal.portlet.om.IPortletDefinition;
 import org.jasig.portal.security.AdminEvaluator;
 import org.jasig.portal.security.IAuthorizationPrincipal;
 import org.jasig.portal.security.IPerson;
 import org.jasig.portal.security.IPersonManager;
 import org.jasig.portal.services.AuthorizationService;
 import org.jasig.portal.utils.cache.CacheFactory;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -71,26 +76,32 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class StatsLayoutModificationsController implements InitializingBean {
-    
+
+    public static final AggregationInterval AGGREGATION_INTERVAL = AggregationInterval.DAY;
+    public static final String GROUP_SERVICE = "local";
+    public static final String GROUP_NAME = "Everyone";
+
     private static final int MIN_DAYS = 0;
     
     private DataSource dataSource;
     private EventCountFactory factory;
-    private int maxDays = 365;  // default
     private final Pattern datePattern = Pattern.compile("\\d+/\\d+/\\d+");
     private final DateFormat format = DateFormat.getDateInstance(DateFormat.SHORT);
     private IPersonManager personManager;
     private CacheFactory cacheFactory;
     private final Log log = LogFactory.getLog(getClass());
-    
-    @Resource(name=BaseRawEventsJpaDao.PERSISTENCE_UNIT_NAME)
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
-    public void setMaxDays(int maxDays) {
-        this.maxDays = maxDays;
-    }
+    @Autowired
+    protected PortletLayoutAggregationDao<PortletLayoutAggregation> portletLayoutDao;
+
+    @Autowired
+    protected AggregatedPortletLookupDao aggregatedPortletLookupDao;
+
+    @Autowired
+    protected AggregatedGroupLookupDao aggregatedGroupLookupDao;
+
+    @Autowired
+    protected IPortletDefinitionDao portletDefinitionDao;
 
     @Autowired
     public void setPersonManager(IPersonManager personManager) {
@@ -104,68 +115,23 @@ public class StatsLayoutModificationsController implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        this.factory = new EventCountFactory(dataSource, cacheFactory);
+        this.factory = new EventCountFactory(portletLayoutDao, aggregatedPortletLookupDao,aggregatedGroupLookupDao,portletDefinitionDao,cacheFactory);
     }
 
     @RequestMapping(value="/userLayoutModificationsCounts")
     public ModelAndView getEventCounts(HttpServletRequest req, HttpServletResponse res) throws ServletException {
-        
-        // Days parameter
-        int days = 30;  // default
-        if (req.getParameter("days") != null) {
-            String daysParam = req.getParameter("days").trim();
-            try {
-                days = Integer.parseInt(daysParam);
-            } catch (NumberFormatException nfe) {
-                String msg = "Unrecognizable days parameter (must be a valid integer): " + daysParam;
-                log.warn(msg, nfe);
-                throw new ServletException(msg, nfe);
-            }
-        }
-        
-        // fromDate parameter
-        Calendar dateOnOrBefore = Calendar.getInstance();  
-        dateOnOrBefore.set(Calendar.HOUR_OF_DAY, 0);
-        dateOnOrBefore.set(Calendar.MINUTE, 0);
-        dateOnOrBefore.set(Calendar.SECOND, 0);
-        dateOnOrBefore.set(Calendar.MILLISECOND, 0);
-        dateOnOrBefore.roll(Calendar.DATE, true); // default is tomorrow with time fields cleared
-        if (req.getParameter("fromDate") != null) {
-            String fromDateParam = req.getParameter("fromDate").trim();
-            // If the user doesn't enter a date, the UI sends "today" (or other 
-            // string), so ignore anything that's not even close...
-            if (datePattern.matcher(fromDateParam).matches()) {
-                try {
-                    Date fromDate = format.parse(fromDateParam);
-                    // Need to add one day, since the report is inclusive
-                    Calendar cal = Calendar.getInstance();
-                    cal.setTime(fromDate);
-                    cal.add(Calendar.DATE, 1);
-                    dateOnOrBefore = cal;
-                } catch (ParseException pe) {
-                    // Passing a bad date is ok, it just results in the default
-                    if (log.isInfoEnabled()) {
-                        String msg = "Unrecognizable fromDate parameter (format 'mm/dd/yyyy'): " + fromDateParam;
-                        log.info(msg, pe);
-                    }
-                }
-            }
-        }
+        Map<String, Object> model = new HashMap<String, Object>();
+        String daysParam = req.getParameter("days");
+        Integer days = (daysParam == null ? 1 : Integer.valueOf(daysParam));
+        model.put("counts",getCounts(req,days));
+        return new ModelAndView("jsonView",model);
+    }
 
-        // Be certain days is within prescribed limits
-        if (days < MIN_DAYS) {
-            days = MIN_DAYS;
-        } else if (days > maxDays) {
-            days = maxDays;
-        }
-        
-        List<CountingTuple> completeList = factory.getEventCounts(dateOnOrBefore.getTime(), days);
-
+    private final List<CountingTuple> getCounts(HttpServletRequest req,Integer days) {
+        List<CountingTuple> completeList = factory.getEventCounts(days);
         IPerson user = personManager.getPerson(req);
         List<CountingTuple> filteredList = filterByPermissions(user, completeList);
-        
-        return new ModelAndView("jsonView", "counts", filteredList);
-
+        return filteredList;
     }
 
     /*
@@ -267,27 +233,36 @@ public class StatsLayoutModificationsController implements InitializingBean {
     
     private static final class EventCountFactory {
         
-        private static final String ADDED_PORTLETS_SQL = 
-            "SELECT uppd.portlet_def_id, uppd.portlet_fname, uppd.portlet_title, uppd.portlet_desc, (SELECT COUNT(*) " +
-                "FROM stats_event ste, stats_event_type stet, stats_channel stc " +
-                "WHERE ste.type_id = stet.id " +
-                "AND stet.type = 'LAYOUT_CHANNEL_ADDED' " +
-                "AND ste.id = stc.event_id " +
-                "AND stc.definition_id = uppd.portlet_def_id " +
-                "AND ste.act_date >= ? AND ste.act_date <= ?) " +
-            "FROM up_portlet_def uppd";
-        private static final String PORTLETS_ADDED_CACHE_KEY = 
+        private static final String PORTLETS_ADDED_CACHE_KEY =
             "PortalStats.org.jasig.portal.rest.StatsLayoutModificationsController.portletsAdded";
 
-        private final SimpleJdbcTemplate simpleJdbcTemplate;
-        private final RowMapperImpl rowMapper = new RowMapperImpl();
-        private final Map<CacheTuple,List<CountingTuple>> cache;
+        private final PortletLayoutAggregationDao<PortletLayoutAggregation> portletLayoutDao;
+        private final AggregatedPortletLookupDao aggregatedPortletLookupDao;
+        private final AggregatedGroupLookupDao aggregatedGroupLookupDao;
+        private final IPortletDefinitionDao portletDefinitionDao;
+        private final Map<Integer,List<CountingTuple>> cache;
         
-        public EventCountFactory(DataSource dataSource, CacheFactory cacheFactory) {
+        public EventCountFactory(PortletLayoutAggregationDao<PortletLayoutAggregation> portletLayoutDao,
+                                 AggregatedPortletLookupDao aggregatedPortletLookupDao,
+                                 AggregatedGroupLookupDao aggregatedGroupLookupDao,
+                                 IPortletDefinitionDao portletDefinitionDao,
+                                 CacheFactory cacheFactory) {
 
             // Assertions
-            if (dataSource == null) {
-                String msg = "Argument 'dataSource' cannot be null";
+            if (portletLayoutDao == null) {
+                String msg = "Argument 'portletLayoutDao' cannot be null";
+                throw new IllegalArgumentException(msg);
+            }
+            if (aggregatedPortletLookupDao == null) {
+                String msg = "Argument 'aggregatedPortletLookupDao' cannot be null";
+                throw new IllegalArgumentException(msg);
+            }
+            if (aggregatedGroupLookupDao == null) {
+                String msg = "Argument 'aggregatedGroupLookupDao' cannot be null";
+                throw new IllegalArgumentException(msg);
+            }
+            if (portletDefinitionDao == null) {
+                String msg = "Argument 'portletDefinitionDao' cannot be null";
                 throw new IllegalArgumentException(msg);
             }
             if (cacheFactory == null) {
@@ -295,85 +270,60 @@ public class StatsLayoutModificationsController implements InitializingBean {
                 throw new IllegalArgumentException(msg);
             }
 
-            this.simpleJdbcTemplate = new SimpleJdbcTemplate(dataSource);
+            this.portletLayoutDao = portletLayoutDao;
+            this.aggregatedPortletLookupDao = aggregatedPortletLookupDao;
+            this.aggregatedGroupLookupDao = aggregatedGroupLookupDao;
+            this.portletDefinitionDao = portletDefinitionDao;
             this.cache = cacheFactory.getCache(PORTLETS_ADDED_CACHE_KEY);
 
         }
 
-        public List<CountingTuple> getEventCounts(Date dateOnOrBefore, int days) {
-            CacheTuple tuple = new CacheTuple(dateOnOrBefore.getTime(), days);
-            List<CountingTuple> rslt = (List<CountingTuple>) cache.get(tuple);
+        public List<CountingTuple> getEventCounts(Integer days) {
+            List<CountingTuple> rslt = (List<CountingTuple>) cache.get(days);
             if(rslt == null) {
-                rslt = buildEventCounts(dateOnOrBefore, days);
-                cache.put(tuple, rslt);
+                rslt = buildEventCounts(days);
+                cache.put(days, rslt);
             }
             return rslt;
         }
-        
-        private List<CountingTuple> buildEventCounts(Date dateOnOrBefore, int days) {
 
-            Calendar cal = Calendar.getInstance();
-            cal.add(Calendar.DATE, -days);
-            Date dateOnOrAfter = cal.getTime();
-            
-            List<CountingTuple> rslt = simpleJdbcTemplate.query(
-                        ADDED_PORTLETS_SQL, 
-                        rowMapper, 
-                        dateOnOrAfter, dateOnOrBefore);
-            // Remove rows with zero count... not interested in those
-            int index = 0;
-            while (rslt.size() > index) {
-                if (rslt.get(index).getCount() == 0) {
-                    rslt.remove(index);
-                } else {
-                    index += 1;
+        /*
+        This is probably far from the most efficient way of doing this but given our time constraints
+        we will use this and see how it performs.
+         */
+        private List<CountingTuple> buildEventCounts(Integer days) {
+            DateTime end = new DateTime();
+            DateTime begin = end.minusDays(days);
+
+            Set<AggregatedPortletMapping> portletMappings = aggregatedPortletLookupDao.getPortletMappings();
+            AggregatedGroupMapping group = aggregatedGroupLookupDao.getGroupMapping(GROUP_SERVICE,GROUP_NAME);
+
+            List<CountingTuple> completeList =  new ArrayList<CountingTuple>();
+            for(AggregatedPortletMapping portletMapping : portletMappings) {
+                IPortletDefinition def = portletDefinitionDao.getPortletDefinitionByFname(portletMapping.getFname());
+                PortletLayoutAggregationKey key = new PortletLayoutAggregationKeyImpl(AGGREGATION_INTERVAL,group,portletMapping);
+                List<PortletLayoutAggregation> aggregations = portletLayoutDao.getAggregations(begin,end,key,group);
+
+                if((aggregations != null) && (!aggregations.isEmpty())) {
+                    Collections.sort(aggregations,Collections.reverseOrder(BaseAggregationDateTimeComparator.INSTANCE));
+                    int portletId = (int)def.getPortletDefinitionId().getLongId();
+                    int maxCount = 0;
+                    DateTime compval = new DateTime().minusDays(days);
+
+                    for(PortletLayoutAggregation aggregation : aggregations) {
+                        if(!aggregation.getDateTime().isAfter(compval) && !aggregation.getDateTime().equals(compval)) {
+                            break;
+                        }
+
+                        int count = aggregation.getAddCount();// - aggregation.getDeleteCount();
+                        if(count > 0) {
+                            maxCount += count;
+                        }
+                    }
+                    completeList.add(new CountingTuple(portletId, def.getFName(), def.getTitle(), def.getDescription(), maxCount));
                 }
             }
-            // We're interested in descending order 
-            Collections.sort(rslt, Collections.reverseOrder());
-
-            return rslt;
-
+            return completeList;
         }
-
     }
-    
-    private static final class RowMapperImpl implements ParameterizedRowMapper<CountingTuple> {
-
-        @Override
-        public CountingTuple mapRow(ResultSet rs, int index) throws SQLException {
-            return new CountingTuple(
-                    rs.getInt("portlet_def_id"), 
-                    rs.getString("portlet_fname"), 
-                    rs.getString("portlet_title"), 
-                    rs.getString("portlet_desc"), 
-                    rs.getInt(5));
-        }
-        
-    }
-    
-    private static final class CacheTuple implements Serializable {
-        
-        private static final long serialVersionUID = 1L;
-
-        private final long time;
-        private final int days;
-        
-        public CacheTuple(long time, int days) {
-            this.time = time;
-            this.days = days;
-        }
-        
-        @Override
-        public boolean equals(Object o) {
-            boolean rslt = false;
-            if (o instanceof CacheTuple) {
-                CacheTuple p = (CacheTuple) o;
-                rslt = p.time == time && p.days == days;
-            }
-            return rslt;
-        }
-        
-    }
-
 }
