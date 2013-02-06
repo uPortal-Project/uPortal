002317ba00ab84daad63823a37c7e37a48fe21b1
Eric Dalquist <eric.dalquist@gmail.com>
2013-02-06 08:59:44 -0600

    UP-3655 Special handling of DOUBLE for MySQL
    On MySQL -Infinity is converted to MIN_VALUE, +Infinity is converted
    to MAX_VALUE, and NaN is converted to 0

--- uportal-war/src/main/java/org/jasig/portal/utils/MySQL5InnoDBCompressedDialect.java
+++ uportal-war/src/main/java/org/jasig/portal/utils/MySQL5InnoDBCompressedDialect.java
@@ -18,7 +18,19 @@
  */
 package org.jasig.portal.utils;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
 import org.hibernate.dialect.MySQL5InnoDBDialect;
+import org.hibernate.type.descriptor.ValueBinder;
+import org.hibernate.type.descriptor.WrapperOptions;
+import org.hibernate.type.descriptor.java.JavaTypeDescriptor;
+import org.hibernate.type.descriptor.sql.BasicBinder;
+import org.hibernate.type.descriptor.sql.DoubleTypeDescriptor;
+import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Uses the COMPRESSED row format in an InnoDB engine, needed for long index support with UTF-8 
@@ -26,9 +38,53 @@ import org.hibernate.dialect.MySQL5InnoDBDialect;
  * @author Raymond Bourges
  */
 public class MySQL5InnoDBCompressedDialect extends MySQL5InnoDBDialect {
+    
+    public MySQL5InnoDBCompressedDialect() {
+        super();
+    }
 
 	public String getTableTypeString() {
 		return " ENGINE=InnoDB ROW_FORMAT=COMPRESSED";
 	}
-	
+
+    @Override
+    protected SqlTypeDescriptor getSqlTypeDescriptorOverride(int sqlCode) {
+        switch (sqlCode) {
+            case Types.DOUBLE: {
+                return MySqlDoubleTypeDescriptor.INSTANCE;
+            }
+            default: {
+                return super.getSqlTypeDescriptorOverride(sqlCode);
+            }
+        }
+    }
+
+    private static class MySqlDoubleTypeDescriptor extends DoubleTypeDescriptor {
+	    private static final Logger LOGGER = LoggerFactory.getLogger(MySqlDoubleTypeDescriptor.class);
+        private static final long serialVersionUID = 1L;
+        
+        public static final MySqlDoubleTypeDescriptor INSTANCE = new MySqlDoubleTypeDescriptor();
+
+	    public <X> ValueBinder<X> getBinder(final JavaTypeDescriptor<X> javaTypeDescriptor) {
+	        return new BasicBinder<X>( javaTypeDescriptor, this ) {
+	            @Override
+	            protected void doBind(PreparedStatement st, X value, int index, WrapperOptions options) throws SQLException {
+	                Double unwrappedValue = javaTypeDescriptor.unwrap( value, Double.class, options );
+	                if (unwrappedValue == Double.NEGATIVE_INFINITY) {
+	                    LOGGER.debug("Converting double from NEGATIVE_INFINITY to MIN_VALUE");
+	                    unwrappedValue = Double.MIN_VALUE;
+	                }
+	                else if (unwrappedValue == Double.POSITIVE_INFINITY) {
+	                    LOGGER.debug("Converting double from POSITIVE_INFINITY to MAX_VALUE");
+                        unwrappedValue = Double.MAX_VALUE;
+                    }
+                    else if (unwrappedValue == Double.NaN) {
+                        LOGGER.debug("Converting double from NaN to 0");
+                        unwrappedValue = 0d;
+                    }
+                    st.setDouble( index, unwrappedValue );
+	            }
+	        };
+	    }
+	}
 }
