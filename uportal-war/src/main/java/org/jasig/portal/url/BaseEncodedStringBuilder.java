fe6df8b3a1420355132b229308910cae43669142
Eric Dalquist <eric.dalquist@gmail.com>
2013-06-24 22:34:20 -0500

    UP-3723 Work on portal event to tincan statement
    Fix Locale serialization
    Use URN for LRS_Object ID
    Setup LrsActor lookup cache

---
+++ uportal-war/src/main/java/org/jasig/portal/url/BaseEncodedStringBuilder.java
@@ -0,0 +1,68 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.jasig.portal.url;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.commons.lang.Validate;
+
+/**
+ * Base class to handle encoding strings
+ * 
+ * @author Eric Dalquist
+ * @version $Revision$
+ */
+public abstract class BaseEncodedStringBuilder implements Serializable, Cloneable {
+    private static final long serialVersionUID = 1L;
+
+    private final String encoding;
+    
+    
+    public BaseEncodedStringBuilder(String encoding) {
+        Validate.notNull(encoding, "encoding can not be null");
+        this.checkEncoding(encoding);
+
+        this.encoding = encoding;
+    }
+
+    protected final void checkEncoding(String encoding) {
+        try {
+            URLEncoder.encode("", encoding);
+        }
+        catch (UnsupportedEncodingException e) {
+            throw new IllegalArgumentException("Encoding '" + encoding + "' is not supported", e);
+        }
+    }
+    
+    protected final String encode(String s) {
+        try {
+            return URLEncoder.encode(s, this.encoding);
+        }
+        catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException("Encoding '" + this.encoding + "' is not supported. This should have been caught in the consructor.", e);
+        }
+    }
+
+    public String getEncoding() {
+        return encoding;
+    }
+}
--- uportal-war/src/test/java/org/jasig/portal/events/tincan/json/LrsDataModelSerializeTest.java
+++ uportal-war/src/test/java/org/jasig/portal/events/tincan/json/LrsDataModelSerializeTest.java
@@ -2,15 +2,16 @@ package org.jasig.portal.events.tincan.json;
 
 import static org.junit.Assert.assertEquals;
 
+import java.net.URI;
 import java.util.Locale;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.jasig.portal.events.tincan.UrnBuilder;
 import org.jasig.portal.events.tincan.om.LocalizedString;
 import org.jasig.portal.events.tincan.om.LrsActor;
 import org.jasig.portal.events.tincan.om.LrsObject;
 import org.jasig.portal.events.tincan.om.LrsStatement;
 import org.jasig.portal.events.tincan.om.LrsVerb;
-import org.jasig.portal.url.UrlStringBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -41,28 +42,26 @@ public class LrsDataModelSerializeTest {
     
     @Test
     public void testLrsObjectSerialize() throws Exception {
-        final UrlStringBuilder idBuilder = new UrlStringBuilder("UTF-8", "http", "www.example.com");
-        idBuilder.addPath("portlets", "fname");
+        final UrnBuilder idBuilder = new UrnBuilder("UTF-8", "tincan", "uportal", "activities", "portlet", "fname");
+        final URI id = idBuilder.getUri();
         
-        final LrsObject lrsObject = new LrsObject(idBuilder, "Activity", 
+        final LrsObject lrsObject = new LrsObject(id, "Activity", 
                 ImmutableMap.of("name", new LocalizedString(new Locale("en", "us"), "Portlet Name"),
                                 "description", new LocalizedString(new Locale("en", "us"), "Portlet Description")));
         
         final String result = this.objectMapper.writeValueAsString(lrsObject);
         
-        assertEquals("{\"id\":\"http://www.example.com/portlets/fname\",\"objectType\":\"Activity\",\"definition\":{\"name\":{\"en_US\":\"Portlet Name\"},\"description\":{\"en_US\":\"Portlet Description\"}}}", result);
+        assertEquals("{\"id\":\"urn:tincan:uportal:activities:portlet:fname\",\"objectType\":\"Activity\",\"definition\":{\"name\":{\"en-US\":\"Portlet Name\"},\"description\":{\"en-US\":\"Portlet Description\"}}}", result);
     }
-    
-
     
     @Test
     public void testLrsStatementSerialize() throws Exception {
         final LrsActor lrsActor = new LrsActor("user@example.com", "John Doe");
         
-        final UrlStringBuilder idBuilder = new UrlStringBuilder("UTF-8", "http", "www.example.com");
-        idBuilder.addPath("portlets", "fname");
+        final UrnBuilder idBuilder = new UrnBuilder("UTF-8", "tincan", "uportal", "activities", "portlet", "fname");
+        final URI id = idBuilder.getUri();
         
-        final LrsObject lrsObject = new LrsObject(idBuilder, "Activity", 
+        final LrsObject lrsObject = new LrsObject(id, "Activity", 
                 ImmutableMap.of("name", new LocalizedString(new Locale("en", "us"), "Portlet Name"),
                                 "description", new LocalizedString(new Locale("en", "us"), "Portlet Description")));
 
@@ -70,6 +69,6 @@ public class LrsDataModelSerializeTest {
         
         final String result = this.objectMapper.writeValueAsString(lrsStatement);
         
-        assertEquals("{\"actor\":{\"mbox\":\"user@example.com\",\"name\":\"John Doe\",\"objectType\":\"Agent\"},\"verb\":{\"id\":\"http://adlnet.gov/expapi/verbs/initialized\",\"display\":{\"en-us\":\"initialized\"}},\"object\":{\"id\":\"http://www.example.com/portlets/fname\",\"objectType\":\"Activity\",\"definition\":{\"name\":{\"en_US\":\"Portlet Name\"},\"description\":{\"en_US\":\"Portlet Description\"}}}}", result);
+        assertEquals("{\"actor\":{\"mbox\":\"user@example.com\",\"name\":\"John Doe\",\"objectType\":\"Agent\"},\"verb\":{\"id\":\"http://adlnet.gov/expapi/verbs/initialized\",\"display\":{\"en-us\":\"initialized\"}},\"object\":{\"id\":\"urn:tincan:uportal:activities:portlet:fname\",\"objectType\":\"Activity\",\"definition\":{\"name\":{\"en-US\":\"Portlet Name\"},\"description\":{\"en-US\":\"Portlet Description\"}}}}", result);
     }
 }
