e58b2a2a4e306a09df810011e7ceb2bffb742cfc
Tim Levett <levett@wisc.edu>
2014-03-20 13:04:02 -0500

    Added license header

--- uportal-war/src/main/webapp/media/skins/common/javascript/less/less-1.6.2.js
+++ uportal-war/src/main/webapp/media/skins/common/javascript/less/less-1.6.2.js
@@ -1,12 +1,21 @@
-/*! 
- * LESS - Leaner CSS v1.6.2 
- * http://lesscss.org 
- * 
- * Copyright (c) 2009-2014, Alexis Sellier <self@cloudhead.net> 
- * Licensed under the Apache v2 License. 
- * 
- */ 
-
+/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
  /** * @license Apache v2
  */ 
 
--- uportal-war/src/test/java/org/jasig/portal/groups/pags/testers/EagerRegexTesterTest.java
+++ uportal-war/src/test/java/org/jasig/portal/groups/pags/testers/EagerRegexTesterTest.java
@@ -15,8 +15,7 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
- */package org.jasig.portal.groups.pags.testers;
-
+ */
 import org.junit.Assert;
 import org.junit.Test;
 
