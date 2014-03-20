941b4ddce9542a052a5b605ee408b54807f2bb49
Tim Levett <levett@wisc.edu>
2014-03-20 13:17:17 -0500

    cleanup license header generation

--- uportal-war/src/test/java/org/jasig/portal/groups/pags/testers/EagerRegexTesterTest.java
+++ uportal-war/src/test/java/org/jasig/portal/groups/pags/testers/EagerRegexTesterTest.java
@@ -16,6 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+package org.jasig.portal.groups.pags.testers;
+
 import org.junit.Assert;
 import org.junit.Test;
 
