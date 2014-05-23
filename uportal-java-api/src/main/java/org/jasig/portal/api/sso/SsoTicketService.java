9560bbc65463938eafff858dfa0a5af34eee247d
Dan McCallum <dan.mccallum@gmail.com>
2014-05-23 08:35:30 -0700

    SSP-2461 Refactor javadoc

--- uportal-java-api/src/main/java/org/jasig/portal/api/sso/SsoTicketService.java
+++ uportal-java-api/src/main/java/org/jasig/portal/api/sso/SsoTicketService.java
@@ -18,6 +18,34 @@
  */
 package org.jasig.portal.api.sso;
 
+/**
+ * Cross-context (i.e. "shared") API for JVM-locally deployed code to interact
+ * with "SSO Tickets", i.e. trusted login tokens.
+ *
+ * <p>Design notes:</p>
+ *
+ * <p>{@link #issueTicket(String,String)} issues a {@link SsoTicket} to any caller
+ * who can produce the correct shared secret (the {@code secret} arg in
+ * {@link #issueTicket(String,String)}). The implementation will typically
+ * not be able to execute a portal permission check because under typical usage
+ * there isn't an actual, authenticated user when these tickets are issued. So the
+ * implementation will typically just trust the calling context to have verified
+ * <em>its</em> caller in some way (a la
+ * {@link org.jasig.portal.security.sso.mvc.SsoController}) and require that the
+ * caller be in possession of the secret mentioned above. This mechanism is not terribly
+ * dissimilar to the {@code key} used by SpringSecurity's {@code RunAsManagerImpl}.</p>
+ *
+ * <p>The shared secret isn't hugely secure by itself since not-even-very-clever use
+ * of reflection can get at the expected value (though a SecurityManager could likely
+ * help prevent this). But we do need <em>something</em> because this service is
+ * specifically intended to expose ticket issuance functionality outside of the portal
+ * classloader and thus the audience is much more broad than the now-legacy
+ * {@link org.jasig.portal.security.sso.mvc.SsoController}. But the
+ * calling code still effectively needs to be deployed to the same VM, which
+ * is protection of a sort. And we're specifically <em>not</em> exposing this
+ * service as a REST endpoint. So we believe the shared secret is good enough for now</p>
+ *
+ */
 public interface SsoTicketService {
 
 	/**
@@ -48,9 +76,18 @@ public interface SsoTicketService {
 	static final SsoTicketServiceAccessor IMPL = new SsoTicketServiceAccessor();
 
 	/**
+	 * Create a new SSO ticket for the given username and shared secret. The resulting
+	 * ticket is essentially a short-lived token that can be passed to the portal
+	 * login service as a HTTP query param to assert a trusted login request. See the
+	 * portal's {@link org.jasig.portal.security.sso.mvc.SsoController} for a usage
+	 * example, abeit one that relies on lower-level implementation details. See
+	 * additional design notes in class comments above.
 	 *
-	 * @param username
-	 * @param secret
+	 * @param username The end-user logical identifier to associate with the ticket.
+	 *                 Do not depend on the implementation to validate this value
+	 *                 in any way.
+	 * @param secret A shared secret that entitles the calling code to request a new
+	 *               ticket.
 	 * @return
 	 */
 	SsoTicket issueTicket(String username, String secret) throws SecurityException;
--- uportal-war/src/main/java/org/jasig/portal/api/sso/ApiSsoTicketService.java
+++ uportal-war/src/main/java/org/jasig/portal/api/sso/ApiSsoTicketService.java
@@ -30,24 +30,8 @@ import org.springframework.util.Assert;
 import java.util.Calendar;
 
 /**
- * Issues a {@link SsoTicket} to any caller who can produce the correct shared
- * secret (The {@code secret} arg in {@link #issueTicket(String)}). There is no permission
- * check involved because under typical usage there isn't an actual, authenticated user
- * when these tickets are issued. So we just trust the calling context to have verified
- * <em>its</em> caller in some way (a la {@link org.jasig.portal.security.sso.mvc.SsoController})
- * and require that it be in possession of the secret mentioned above. This
- * mechanism is not terribly dissimilar to the {@code key} used by SpringSecurity's
- * {@code RunAsManagerImpl}.
- *
- * <p>The shared secret isn't hugely secure by itself since not-even-very-clever use
- * of reflection can get at the expected value (though a SecurityManager could likely
- * help prevent this). But we do need <em>something</em> because this service is
- * specifically intended to expose ticket issuance functionality outside of the portal
- * classloader and thus the audience is much more broad than the now-legacy
- * {@link org.jasig.portal.security.sso.mvc.SsoController}. But the
- * calling code still effectively needs to be deployed to the same VM, which
- * is protection of a sort. And we're specifically <em>not</em> exposing this
- * service as a REST endpoint. So we believe the shared secret is good enough for now</p>
+ * See design notes in {@link SsoTicketService}, esp re the {@code secret}
+ * field/arg.
  */
 @Service
 public class ApiSsoTicketService implements SsoTicketService, InitializingBean {
