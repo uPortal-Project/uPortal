4a9df899292ff2d9362a5fe6460c1a77077b6680
Josh Helmer <jhelmer@unicon.net>
2014-07-08 20:57:54 -0700

    Initial work.  First chunk of code that will actually submit to cloudscorm correctly. (cherry picked from commit 1f39f2d)

---
+++ uportal-war/src/main/java/org/springframework/util/DynamicPropertyResolver.java
@@ -0,0 +1,7 @@
+package org.springframework.util;
+
+/**
+ * @author Josh Helmer, jhelmer@unicon.net
+ */
+public class DynamicPropertyResolver {
+}
---
+++ uportal-war/src/main/java/org/springframework/web/client/interceptors/Headers.java
@@ -0,0 +1,8 @@
+package org.springframework.web.client.interceptors;
+
+/**
+ * @author Josh Helmer, jhelmer@unicon.net
+ */
+public enum Headers {
+    Authorization
+}
