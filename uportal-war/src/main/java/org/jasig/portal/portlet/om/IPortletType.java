824ceab020495e69029db53a39623a5f7ff07b95
James Wennmacher <jwennmacher@unicon.net>
2014-10-20 09:45:53 -0700

    NOJIRA documentation update

--- uportal-war/src/main/java/org/jasig/portal/portlet/om/IPortletType.java
+++ uportal-war/src/main/java/org/jasig/portal/portlet/om/IPortletType.java
@@ -79,7 +79,7 @@ public interface IPortletType extends IPortalData {
 	 * type.  This CPD will be used to determine configuration options for 
 	 * channels of this type.
 	 * 
-	 * @param cpdUri URI
+	 * @param cpdUri Channel publishing definition URI
 	 */
 	public void setCpdUri(String cpdUri);
 
