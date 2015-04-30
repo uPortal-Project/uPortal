593e48c76141d28ac15e2e75e58e431c60fd9d2a
Benito J. Gonzalez <bgonzalez@unicon.net>
2015-04-30 14:23:33 -0700

    Switched design to use isDeepMemberOf() and cleaned up the class. Also enabled debug logging

--- uportal-war/src/main/java/org/jasig/portal/groups/pags/testers/AdHocGroupTester.java
+++ uportal-war/src/main/java/org/jasig/portal/groups/pags/testers/AdHocGroupTester.java
@@ -18,20 +18,23 @@
  */
 package org.jasig.portal.groups.pags.testers;
 
+import org.jasig.portal.EntityIdentifier;
+import org.jasig.portal.groups.IEntityGroup;
 import org.jasig.portal.groups.IGroupMember;
 import org.jasig.portal.groups.pags.IPersonTester;
+import org.jasig.portal.portlets.groupselector.EntityEnum;
 import org.jasig.portal.security.IPerson;
 import org.jasig.portal.services.GroupService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
- * PAGS Tester for inclusive/exclusive membership in a sets of groups.
+ * PAGS Tester for inclusive/exclusive membership in sets of groups.
  *
  * @author Benito J. Gonzalez <bgonzalez@unicon.net>
  */
@@ -39,54 +42,89 @@ public final class AdHocGroupTester implements IPersonTester {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private static final Set<String> currentPersons = new ConcurrentSkipListSet<String>();
-    /* public since they are final and set in the constructor. */
-    private final String groupName;
-    private final boolean exclude;
+    private static final Set<IEntityGroup> includes = new HashSet<>();
+    private static final Set<IEntityGroup> excludes = new HashSet<>();
 
+    /**
+     * Temporary constructor for testing group test before tackling import/export changes.
+     *
+     * @param attribute not used
+     * @param value     group name
+     */
     public AdHocGroupTester(String attribute, String value) {
-        logger.debug("constructor attribute: ", attribute);
-        logger.debug("constructor value: ", value);
-        String attributeLower = attribute.toLowerCase();
-        this.exclude = "non-member".equals(attributeLower);
-        if (!this.exclude && !"member".equals(attributeLower))
-            logger.warn("Group member test attribute should be either 'member' or 'non-member'");
-        //if (groupExists(value))
-            this.groupName = value;
-        //else
-        //    throw new IllegalArgumentException("Unknown group name specified in PAG Group Member Tester: " + value);
+        logger.debug("constructor attribute: {}", attribute);
+        logger.debug("constructor value: {}", value);
+
+        // Get IEntityGroup for group value
+        IEntityGroup entityGroup = findGroupByName(value);
+        if (entityGroup != null) {
+            includes.add(entityGroup);
+        } else {
+            logger.warn("Could not find group {}", value);
+        }
     }
 
     @Override
     public boolean test(IPerson person) {
-        logger.warn("Person {}'s ID is {}", person.getUserName(), person.getEntityIdentifier().getKey());
+        logger.debug("ID of Person \"{}\" is {}", person.getUserName(), person.getEntityIdentifier().getKey());
         if (currentPersons.contains(person.getEntityIdentifier().getKey()))
-            return false;
+            return false; // stop recursing
         currentPersons.add(person.getEntityIdentifier().getKey());
-        Set<String> personGroups = getPersonGroupMembership(person);
+
+        IGroupMember gmPerson = findPersonAsGroupMember(person);
+        boolean pass = true;
+
+        Iterator<IEntityGroup> groups = includes.iterator();
+        while (pass && groups.hasNext()) {
+            IEntityGroup group = groups.next();
+            if (!gmPerson.isDeepMemberOf(group)) {
+                logger.debug("!! {} is not a member of {}", person.getUserName(), group.getDescription());
+                pass = false;
+            } else {
+                logger.debug("{} is a member of {}", person.getUserName(), group.getDescription());
+            }
+        }
+        groups = excludes.iterator();
+        while (pass && groups.hasNext()) {
+            IEntityGroup group = groups.next();
+            if (gmPerson.isDeepMemberOf(group)) {
+                logger.debug("!! {} is a member of {}", person.getUserName(), group.getDescription());
+                pass = false;
+            } else {
+                logger.debug("{} is a not member of {}", person.getUserName(), group.getDescription());
+            }
+        }
+
         currentPersons.remove(person.getEntityIdentifier().getKey());
-        logger.warn(personGroups.toString());
-        return personGroups.contains(groupName) ^ exclude;
+        return pass;
     }
 
-    @SuppressWarnings("unchecked")
-    protected Set<String> getPersonGroupMembership(IPerson person) {
+    /**
+     * Find <code>IEntityGroup</code> from group name.
+     *
+     * @param groupName name of group
+     * @return IEntityGroup group object with given name
+     */
+    private IEntityGroup findGroupByName(String groupName) {
+        EntityIdentifier[] identifiers = GroupService.searchForGroups(groupName, GroupService.IS, EntityEnum.GROUP.getClazz());
+        logger.debug("Found {} indentifier(s) for group name {}", identifiers.length, groupName);
+        for (EntityIdentifier entityIdentifier : identifiers) {
+            logger.debug(entityIdentifier.toString());
+            if (entityIdentifier.getType().equals(IEntityGroup.class)) {
+                return GroupService.findGroup(entityIdentifier.getKey());
+            }
+        }
+        return null;
+    }
+    /**
+     * Find <code>IPerson</code> as <code>IGroupMember</code>.
+     *
+     * @param person <code>IPerson</code> to be tested
+     * @return IGroupMember person as <code>IGroupMember</code>
+     */
+    private IGroupMember findPersonAsGroupMember(IPerson person) {
         String personKey = person.getEntityIdentifier().getKey();
         logger.warn(personKey);
-        IGroupMember member = GroupService.getEntity(personKey, IPerson.class);
-        logger.warn(member.toString());
-        Iterator<IGroupMember> iterator = (Iterator<IGroupMember>) member.getAllContainingGroups();
-        Set<String> groupNames = new HashSet<String>();
-        while (iterator.hasNext()) {
-            IGroupMember group = iterator.next();
-            String groupKey = group.getKey();
-            logger.warn("Person {}/{} is a member of group {}/{}", person.getUserName(), personKey, groupKey, group.toString());
-            groupNames.add(groupKey);
-        }
-        return groupNames;
-
-    }
-
-    protected boolean groupExists(String groupName) {
-        return (GroupService.getDistinguishedGroup(groupName) != null);
+        return GroupService.getEntity(personKey, IPerson.class);
     }
 }
