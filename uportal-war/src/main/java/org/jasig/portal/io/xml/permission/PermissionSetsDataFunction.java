8054d0a1579ca481225e875037c9f8ebf180ff03
Drew Wills <wills.drew@gmail.com>
2016-04-21 13:56:55 -0700

    UP-4626:  Prevent BROWSE permissions that target portlets from appearing in permission-set.xml files on export

--- uportal-war/src/main/java/org/jasig/portal/io/xml/permission/PermissionSetsDataFunction.java
+++ uportal-war/src/main/java/org/jasig/portal/io/xml/permission/PermissionSetsDataFunction.java
@@ -26,6 +26,7 @@ import javax.sql.DataSource;
 import org.jasig.portal.io.xml.IPortalData;
 import org.jasig.portal.io.xml.IPortalDataType;
 import org.jasig.portal.io.xml.SimpleStringPortalData;
+import org.jasig.portal.security.IPermission;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -34,18 +35,24 @@ import org.springframework.jdbc.core.RowMapper;
 import com.google.common.base.Function;
 
 /**
- * Lists each Permission Set in the portal
+ * Lists each Permission Set in the portal during the export process.
  * 
  * @author Eric Dalquist
- * @version $Revision$
  */
 public class PermissionSetsDataFunction implements Function<IPortalDataType, Iterable<? extends IPortalData>> {
     private JdbcOperations jdbcOperations;
+
+    /**
+     * NOTE:  Both SUBSCRIBE and BROWSE are handled inside the portlet-definition.xml
+     * file whenever they apply to <em>portlets</em>.  (They are handled here, per
+     * normal, when they apply to categories.)
+     */
     private static final String QUERY =
-        "SELECT DISTINCT UPP.OWNER, UPET.ENTITY_TYPE_NAME, UPP.PRINCIPAL_KEY, UPP.ACTIVITY, UPP.PRINCIPAL_TYPE\n" + 
-        "FROM UP_PERMISSION UPP\n" + 
-        "  LEFT JOIN UP_ENTITY_TYPE UPET ON UPP.PRINCIPAL_TYPE = UPET.ENTITY_TYPE_ID\n" + 
-        "WHERE UPP.ACTIVITY != 'SUBSCRIBE'";
+        "SELECT DISTINCT UPP.OWNER, UPET.ENTITY_TYPE_NAME, UPP.PRINCIPAL_KEY, UPP.ACTIVITY, UPP.PRINCIPAL_TYPE\n " + 
+        "FROM UP_PERMISSION UPP\n " + 
+        "LEFT JOIN UP_ENTITY_TYPE UPET ON UPP.PRINCIPAL_TYPE = UPET.ENTITY_TYPE_ID\n " + 
+        "WHERE NOT (UPP.ACTIVITY = 'SUBSCRIBE' AND UPP.TARGET LIKE '" + IPermission.PORTLET_PREFIX + "%')\n " +
+        "AND NOT (UPP.ACTIVITY = 'BROWSE' AND UPP.TARGET LIKE '" + IPermission.PORTLET_PREFIX + "%')\n ";
 
     @Required
     public void setDataSource(DataSource dataSource) {
@@ -53,7 +60,6 @@ public class PermissionSetsDataFunction implements Function<IPortalDataType, Ite
         jdbcTemplate.afterPropertiesSet();
         this.jdbcOperations = jdbcTemplate;
     }
-    
 
     @Override
     public Iterable<? extends IPortalData> apply(IPortalDataType input) {
@@ -61,13 +67,13 @@ public class PermissionSetsDataFunction implements Function<IPortalDataType, Ite
             @Override
             public IPortalData mapRow(ResultSet rs, int rowNum) throws SQLException {
                 final StringBuilder key = new StringBuilder();
-                
+
                 key.append(rs.getString("OWNER")).append("|");
                 key.append(rs.getString("ENTITY_TYPE_NAME")).append("|");
                 key.append(rs.getString("PRINCIPAL_KEY")).append("|");
                 key.append(rs.getString("ACTIVITY")).append("|");
                 key.append(rs.getString("PRINCIPAL_TYPE"));
-                
+
                 return new SimpleStringPortalData(key.toString(), null, null);
             }
         });
