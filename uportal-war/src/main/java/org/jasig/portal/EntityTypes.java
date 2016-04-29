7837754e33ea8c33c8b2ce1ed2a3a680c6c5a453
Drew Wills <wills.drew@gmail.com>
2016-04-29 16:39:48 -0700

    UP-4672:  Prevent a missing EntityType class from crashing the whole portal (log an ERROR instead)

--- uportal-war/src/main/java/org/jasig/portal/EntityTypes.java
+++ uportal-war/src/main/java/org/jasig/portal/EntityTypes.java
@@ -20,14 +20,18 @@ package org.jasig.portal;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.jasig.portal.groups.IEntity;
 import org.jasig.portal.groups.IEntityGroup;
 import org.jasig.portal.jpa.BasePortalJpaDao;
 import org.jasig.portal.spring.locator.EntityTypesLocator;
 import org.jasig.portal.utils.ICounterStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.support.DataAccessUtils;
@@ -50,26 +54,33 @@ import com.googlecode.ehcache.annotations.key.ListCacheKeyGenerator;
  * <code>Integer</code> and <code>Class</code> values.
  *
  * @author Dan Ellentuck
- * @version $Revision$
  * @see org.jasig.portal.IBasicEntity
  */
 @Repository("entityTypes")
 public class EntityTypes {
-    private static final RowMapper<Class> CLASS_ROW_MAPPER = new RowMapper<Class>() {
+
+    private static final RowMapper<Class<?>> CLASS_ROW_MAPPER = new RowMapper<Class<?>>() {
+
+        private final Logger logger = LoggerFactory.getLogger(getClass());
+
         @Override
-        public Class mapRow(ResultSet rs, int rowNum) throws SQLException {
+        public Class<?> mapRow(ResultSet rs, int rowNum) throws SQLException {
             final String className = rs.getString("ENTITY_TYPE_NAME");
             try {
                 return Class.forName(className);
-            }
-            catch (ClassNotFoundException e) {
-                throw new RuntimeException("Failed to find Class for: " + className, e);
+            } catch (ClassNotFoundException e) {
+                logger.error("Failed to find the specified EntityType class '{}';  "
+                        + "this is either a catastrophic problem (is the portal working "
+                        + "at all?) or an artifact of orphaned data.", className);
+                // These will be removed from the allEntityTypes collection
+                return null;
             }
         }
     };
 
     public static final Class<IEntityGroup> GROUP_ENTITY_TYPE = org.jasig.portal.groups.IEntityGroup.class;
     public static final Class<IEntity> LEAF_ENTITY_TYPE = org.jasig.portal.groups.IEntity.class;
+
 
     @Deprecated
     public static Class<? extends IBasicEntity> getEntityType(Integer typeID) {
@@ -85,10 +96,9 @@ public class EntityTypes {
     public static EntityTypes singleton() {
         return EntityTypesLocator.getEntityTypes();
     }
-    
+
     private JdbcOperations jdbcOperations;
     private ICounterStore counterStore;
-    
 
     @Autowired
     public void setJdbcOperations(@Qualifier(BasePortalJpaDao.PERSISTENCE_UNIT_NAME) JdbcOperations jdbcOperations) {
@@ -102,9 +112,10 @@ public class EntityTypes {
 
     @Cacheable(cacheName = "org.jasig.portal.EntityTypes.CLASS_BY_ID", keyGeneratorName = ListCacheKeyGenerator.DEFAULT_BEAN_NAME)
     public Class<? extends IBasicEntity> getEntityTypeFromID(Integer id) {
-        final List<Class> result = this.jdbcOperations.query("SELECT ENTITY_TYPE_NAME FROM UP_ENTITY_TYPE WHERE ENTITY_TYPE_ID = ?", CLASS_ROW_MAPPER, id);
-        
-        return DataAccessUtils.singleResult(result);
+        final List<Class<?>> result = this.jdbcOperations.query("SELECT ENTITY_TYPE_NAME FROM UP_ENTITY_TYPE WHERE ENTITY_TYPE_ID = ?", CLASS_ROW_MAPPER, id);
+        @SuppressWarnings("unchecked") // There is an unused(?) row for java.lang.Object that looks as though it will fail here
+        Class<? extends IBasicEntity> rslt = (Class<? extends IBasicEntity>) DataAccessUtils.singleResult(result);
+        return rslt;
     }
 
     @Cacheable(cacheName = "org.jasig.portal.EntityTypes.ID_BY_CLASS", keyGeneratorName = ListCacheKeyGenerator.DEFAULT_BEAN_NAME)
@@ -114,8 +125,16 @@ public class EntityTypes {
     }
 
     @Cacheable(cacheName = "org.jasig.portal.EntityTypes.ALL", keyGeneratorName = ListCacheKeyGenerator.DEFAULT_BEAN_NAME)
-    public Iterator<Class> getAllEntityTypes() {
-        return this.jdbcOperations.query("SELECT ENTITY_TYPE_NAME FROM UP_ENTITY_TYPE", CLASS_ROW_MAPPER).iterator();
+    public Iterator<Class<?>> getAllEntityTypes() {
+        final List<Class<?>> entityTypes =  this.jdbcOperations.query("SELECT ENTITY_TYPE_NAME FROM UP_ENTITY_TYPE", CLASS_ROW_MAPPER);
+        // Filter null values
+        final Set<Class<?>> rslt = new HashSet<>();
+        for (Class<?> clazz : entityTypes) {
+            if (clazz != null) {
+                rslt.add(clazz);
+            }
+        }
+        return rslt.iterator();
     }
 
     @TriggersRemove(cacheName = { "org.jasig.portal.EntityTypes.ALL" }, removeAll=true)
@@ -126,7 +145,7 @@ public class EntityTypes {
             //Entity type already exists, ignore call
             return;
         }
-        
+
         final int nextId = counterStore.getNextId("UP_ENTITY_TYPE");
         this.jdbcOperations.update("INSERT INTO UP_ENTITY_TYPE (ENTITY_TYPE_ID, ENTITY_TYPE_NAME, DESCRIPTIVE_NAME) VALUES (?, ?, ?)", 
                 nextId, newType.getName(), description);
@@ -134,7 +153,7 @@ public class EntityTypes {
 
     @TriggersRemove(cacheName = { "org.jasig.portal.EntityTypes.CLASS_BY_ID", "org.jasig.portal.EntityTypes.ID_BY_CLASS", "org.jasig.portal.EntityTypes.ALL"}, removeAll=true)
     @Transactional
-    public void deleteEntityType(Class type) throws SQLException {
+    public void deleteEntityType(Class<?> type) throws SQLException {
         this.jdbcOperations.update("DELETE FROM UP_ENTITY_TYPE WHERE ENTITY_TYPE_NAME = ?", type.getName());
     }
 }
