d37a7904b846d52ed5739d6be8cb9d3c6450e551
Drew Wills <wills.drew@gmail.com>
2016-07-09 11:17:55 -0700

    UP-4705:  o.j.p.layout.dlm.NodeInfo overrides equals() but does not override hashCode()

--- uportal-war/src/main/java/org/jasig/portal/layout/dlm/NodeInfo.java
+++ uportal-war/src/main/java/org/jasig/portal/layout/dlm/NodeInfo.java
@@ -21,17 +21,18 @@ package org.jasig.portal.layout.dlm;
 import org.w3c.dom.Element;
 
 /**
+ * Simple tuple that holds onto metadata about a layout node while the Composite Layout is being calculated.
  * @since uPortal 2.5
  */
-public class NodeInfo
-{
-    String id = null;
-    Element node = null;
-    boolean differentParent = false;
-    int indexInCVP = -1; // CVP = Composite View Parent
-    Precedence precedence = null;
-    Element positionDirective = null;
-    
+public class NodeInfo {
+
+    private final Element node;
+    private final String id;
+    private final Precedence precedence;
+    private boolean differentParent = false;
+    private int indexInCVP = -1; // CVP = Composite View Parent
+    private Element positionDirective = null;
+
     NodeInfo( Element node )
     {
         this.node = node;
@@ -39,30 +40,80 @@ public class NodeInfo
         .newInstance( node.getAttribute( Constants.ATT_FRAGMENT ) );
         id = node.getAttribute( Constants.ATT_ID );
     }
-    
+
     NodeInfo( Element node, int indexInCVP )
     {
         this( node );
         this.indexInCVP = indexInCVP;
     }
-    
-    public boolean equals( Object o )
-    {
-        if ( o != null &&
-             o instanceof NodeInfo &&
-             ((NodeInfo) o).id.equals( id ) )
-            return true;
-        if ( o == this )
-            return true;
-        return false;
+
+    public boolean isDifferentParent() {
+        return differentParent;
     }
 
-    public String toString()
-    {
-        return "ni[ id:" + id +
-        ", diffPrnt:" + differentParent +
-        ", idxInCVP:" + indexInCVP +
-        ", prec:" + precedence +
-        " ]";
+    public void setDifferentParent(boolean differentParent) {
+        this.differentParent = differentParent;
     }
+
+    public int getIndexInCVP() {
+        return indexInCVP;
+    }
+
+    public void setIndexInCVP(int indexInCVP) {
+        this.indexInCVP = indexInCVP;
+    }
+
+    public Element getPositionDirective() {
+        return positionDirective;
+    }
+
+    public void setPositionDirective(Element positionDirective) {
+        this.positionDirective = positionDirective;
+    }
+
+    public Element getNode() {
+        return node;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Precedence getPrecedence() {
+        return precedence;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        NodeInfo other = (NodeInfo) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "NodeInfo [node=" + node + ", id=" + id + ", precedence=" + precedence + ", differentParent="
+                + differentParent + ", indexInCVP=" + indexInCVP + ", positionDirective=" + positionDirective + "]";
+    }
+
+
+
 }
