86ca33a4ee27e2b26dad52a83babf8f9a47194d6
Drew Wills <wills.drew@gmail.com>
2016-09-01 15:29:02 -0700

    UP-4728: Add submodule uPortal-soffit-connector ported from https://github.com/drewwills/Soffit

---
+++ uPortal-soffit-connector/src/main/java/org/apereo/portal/soffit/connector/AbstractHeaderProvider.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to Apereo under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Apereo licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apereo.portal.soffit.connector;
+
+import java.util.Date;
+
+import javax.portlet.PortletSession;
+import javax.portlet.RenderRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+public abstract class AbstractHeaderProvider implements IHeaderProvider {
+
+    @Value("${org.jasig.portal.security.PersonFactory.guest_user_name:guest}")
+    private String guestUserName;
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    protected final String getUsername(RenderRequest renderRequest) {
+        final String rslt = renderRequest.getRemoteUser() != null
+                ? renderRequest.getRemoteUser()
+                : guestUserName;
+        return rslt;
+    }
+
+    /**
+     * Point at which the JWT expires
+     */
+    protected final Date getExpiration(RenderRequest renderRequest) {
+        // Expiration of the JWT
+        final PortletSession portletSession = renderRequest.getPortletSession();
+        final Date rslt = new Date(
+                    portletSession.getLastAccessedTime() + ((long) portletSession.getMaxInactiveInterval() * 1000L)
+                );
+        return rslt;
+    }
+
+}
---
+++ uPortal-soffit-connector/src/main/java/org/apereo/portal/soffit/connector/SoffitConnectorConfiguration.java
@@ -0,0 +1,81 @@
+/**
+ * Licensed to Apereo under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Apereo licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apereo.portal.soffit.connector;
+
+import org.apereo.portal.soffit.service.BearerService;
+import org.apereo.portal.soffit.service.DefinitionService;
+import org.apereo.portal.soffit.service.PortalRequestService;
+import org.apereo.portal.soffit.service.PreferencesService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Defines beans for the Soffit Connector, which runs inside uPortal
+ *
+ * @author drewwills
+ */
+@Configuration
+public class SoffitConnectorConfiguration {
+
+    /*
+     * Services
+     *
+     * These beans know how to produce & consume the JWTs that represent the
+     * data model of Soffit.
+     */
+
+    @Bean
+    public BearerService bearerService() {
+        return new BearerService();
+    }
+
+    @Bean
+    public PortalRequestService portalRequestService() {
+        return new PortalRequestService();
+    }
+
+    @Bean
+    public PreferencesService preferencesService() {
+        return new PreferencesService();
+    }
+
+    @Bean
+    public DefinitionService definitionService() {
+        return new DefinitionService();
+    }
+
+    /*
+     * Header Providers
+     *
+     * These beans know how to produce HTTP headers based on JWTs.
+     * (NOTE:  there are more of these in uPortal.)
+     */
+
+    @Bean
+    public PortalRequestHeaderProvider portalRequestHeaderProvider() {
+        return new PortalRequestHeaderProvider();
+    }
+
+    @Bean
+    public PreferencesHeaderProvider preferencesHeaderProvider() {
+        return new PreferencesHeaderProvider();
+    }
+
+}
