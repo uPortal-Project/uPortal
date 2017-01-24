7af4733efeea05f5414aef0913c968e2e0bfe4dd
Benito J. Gonzalez <bgonzalez@unicon.net>
2017-01-23 17:53:09 -0800

    Add groovy support for deployments via Tomcat Manager.

---
+++ deployables.groovy
@@ -0,0 +1,48 @@
+"Announcements" {
+    file = "uportal-portlets-overlay/AnnouncementsPortlet/target/Announcements.war"
+}
+"basiclti-portlet" {
+    file = "uportal-portlets-overlay/basiclti-portlet/target/basiclti-portlet.war"
+}
+"BookmarksPortlet" {
+    file = "uportal-portlets-overlay/BookmarksPortlet/target/BookmarksPortlet.war"
+}
+"CalendarPortlet" {
+    file = "uportal-portlets-overlay/CalendarPortlet/target/CalendarPortlet.war"
+}
+"cas-proxy-test-portlet" {
+    file = "uportal-portlets-overlay/cas-proxy-test-portlet/target/cas-proxy-test-portlet.war"
+}
+"email-preview" {
+    file = "uportal-portlets-overlay/email-preview/target/email-preview.war"
+}
+"FunctionalTestsPortlet" {
+    file = "uportal-portlets-overlay/FunctionalTestsPortlet/target/FunctionalTestsPortlet.war"
+}
+"jasig-widget-portlets" {
+    file = "uportal-portlets-overlay/jasig-widget-portlets/target/jasig-widget-portlets.war"
+}
+"NewsReaderPortlet" {
+    file = "uportal-portlets-overlay/NewsReaderPortlet/target/NewsReaderPortlet.war"
+}
+"NotificationPortlet" {
+    file = "uportal-portlets-overlay/NotificationPortlet/target/NotificationPortlet.war"
+}
+"pluto-testsuite" {
+    file = "uportal-portlets-overlay/pluto-testsuite/target/pluto-testsuite.war"
+}
+"sakai-connector" {
+    file = "uportal-portlets-overlay/sakai-connector-portlet/target/sakai-connector.war"
+}
+"SimpleContentPortlet" {
+    file = "uportal-portlets-overlay/SimpleContentPortlet/target/SimpleContentPortlet.war"
+}
+"WeatherPortlet" {
+    file = "uportal-portlets-overlay/WeatherPortlet/target/WeatherPortlet.war"
+}
+"WebProxyPortlet" {
+    file = "uportal-portlets-overlay/WebProxyPortlet/target/WebProxyPortlet.war"
+}
+"ROOT" {
+    file = "uportal-war/target/uPortal.war"
+}
