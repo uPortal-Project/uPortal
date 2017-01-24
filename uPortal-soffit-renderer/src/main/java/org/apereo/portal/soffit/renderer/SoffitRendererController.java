608df05c6dd647d7eda634981708e8756e19f8b9
Drew Wills <wills.drew@gmail.com>
2017-01-23 19:11:14 -0700

    UP-4792:  Provide an annotation-based mechanism for soffits based on the SoffitRendererController to inject model attributes

--- uPortal-soffit-renderer/src/main/java/org/apereo/portal/soffit/renderer/SoffitRendererController.java
+++ uPortal-soffit-renderer/src/main/java/org/apereo/portal/soffit/renderer/SoffitRendererController.java
@@ -16,13 +16,21 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apereo.portal.soffit.renderer;
 
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -39,11 +47,12 @@ import org.apereo.portal.soffit.service.PortalRequestService;
 import org.apereo.portal.soffit.service.PreferencesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -83,9 +92,15 @@ public class SoffitRendererController {
     public static final String CACHE_MAXAGE_PROPERTY_FORMAT = PROPERTY_PREFIX + "%s.cache.max-age";
 
     private static final String PORTAL_REQUEST_MODEL_NAME = "portalRequest";
+    private static final String BEARER_MODEL_NAME = "bearer";
+    private static final String PREFERENCES_MODEL_NAME = "preferences";
+    private static final String DEFINITION_MODEL_NAME = "definition";
 
     private static final String DEFAULT_MODE = "view";
     private static final String DEFAULT_WINDOW_STATE = "normal";
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Autowired
     private Environment environment;
@@ -106,70 +121,88 @@ public class SoffitRendererController {
     private String viewsLocation;
     private final Map<ViewTuple,String> availableViews = new HashMap<>();
 
+    private Map<AnnotatedElement,Object> modelAttributes;
+
     protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @PostConstruct
+    public void init() {
+        /*
+         * Gather classes & methods that reference @SoffitMoldelAttribute
+         */
+        final Map<AnnotatedElement,Object> map = new HashMap<>();
+        final String[] beanNames = applicationContext.getBeanDefinitionNames();
+        for (String name : beanNames) {
+            final Object bean = applicationContext.getBean(name);
+            final Class clazz = AopUtils.isAopProxy(bean)
+                    ? AopUtils.getTargetClass(bean)
+                    : bean.getClass();
+            if (clazz.isAnnotationPresent(SoffitModelAttribute.class)) {
+                // The bean itself is the model attribute
+                map.put(clazz, bean);
+            } else {
+                // Check the bean for annotated methods...
+                for (Method m : clazz.getMethods()) {
+                    if (m.isAnnotationPresent(SoffitModelAttribute.class)) {
+                        map.put(m, bean);
+                    }
+                }
+            }
+        }
+        logger.debug("Found {} beans and/or methods referencing @SoffitModelAttribute", map.size());
+        modelAttributes = Collections.unmodifiableMap(map);
+    }
 
     @RequestMapping(value="/{module}", method=RequestMethod.GET)
     public ModelAndView render(final HttpServletRequest req, final HttpServletResponse res, final @PathVariable String module) {
 
         logger.debug("Rendering for request URI '{}'", req.getRequestURI());
 
-        // PortalRequest
-        final String portalRequestToken = req.getHeader(Headers.PORTAL_REQUEST.getName());
-        final PortalRequest portalRequest = portalRequestService.parsePortalRequest(portalRequestToken);
+        // Soffit Object Model
+        final PortalRequest portalRequest = getPortalRequest(req);
+        final Bearer bearer = getBearer(req);
+        final Preferences preferences = getPreferences(req);
+        final Definition definition = getDefinition(req);
 
         // Select a view
         final String viewName = selectView(req, module, portalRequest);
 
+        final Map<String,Object> model = gatherModelAttributes(viewName, req, res, portalRequest, bearer, preferences, definition);
+        model.put(PORTAL_REQUEST_MODEL_NAME, portalRequest);
+        model.put(BEARER_MODEL_NAME, bearer);
+        model.put(PREFERENCES_MODEL_NAME, preferences);
+        model.put(DEFINITION_MODEL_NAME, definition);
+
         // Set up cache headers appropriately
         configureCacheHeaders(res, module);
 
-        return new ModelAndView(viewName.toString(), PORTAL_REQUEST_MODEL_NAME, portalRequest);
+        return new ModelAndView(viewName.toString(), model);
 
-    }
-
-    @ModelAttribute("bearer")
-    public Bearer getBearer(final HttpServletRequest req) {
-        final String authorizationHeader = req.getHeader(Headers.AUTHORIZATION.getName());
-        final String bearerToken = authorizationHeader.substring(Headers.BEARER_TOKEN_PREFIX.length());
-        return bearerService.parseBearerToken(bearerToken);
-    }
-
-    @ModelAttribute("preferences")
-    public Preferences getPreferences(final HttpServletRequest req) {
-        final String preferencesToken = req.getHeader(Headers.PREFERECES.getName());
-        return preferencesService.parsePreferences(preferencesToken);
-    }
-
-    @ModelAttribute("definition")
-    public Definition getDefinition(final HttpServletRequest req) {
-        final String definitionToken = req.getHeader(Headers.DEFINITION.getName());
-        return definitionService.parseDefinition(definitionToken);
     }
 
     /*
      * Implementation
      */
 
-    private void configureCacheHeaders(final HttpServletResponse res, final String module) {
+    private PortalRequest getPortalRequest(final HttpServletRequest req) {
+        final String portalRequestToken = req.getHeader(Headers.PORTAL_REQUEST.getName());
+        return portalRequestService.parsePortalRequest(portalRequestToken);
+    }
 
-        final String cacheScopeProperty = String.format(CACHE_SCOPE_PROPERTY_FORMAT, module);
-        final String cacheScopeValue = environment.getProperty(cacheScopeProperty);
-        logger.debug("Selecting cacheScopeValue='{}' for property '{}'", cacheScopeValue, cacheScopeProperty);
+    private Bearer getBearer(final HttpServletRequest req) {
+        final String authorizationHeader = req.getHeader(Headers.AUTHORIZATION.getName());
+        final String bearerToken = authorizationHeader.substring(Headers.BEARER_TOKEN_PREFIX.length());
+        return bearerService.parseBearerToken(bearerToken);
+    }
 
-        final String cacheMaxAgeProperty = String.format(CACHE_MAXAGE_PROPERTY_FORMAT, module);
-        final String cacheMaxAgeValue = environment.getProperty(cacheMaxAgeProperty);
-        logger.debug("Selecting cacheMaxAgeValue='{}' for property '{}'", cacheMaxAgeValue, cacheMaxAgeProperty);
+    private Preferences getPreferences(final HttpServletRequest req) {
+        final String preferencesToken = req.getHeader(Headers.PREFERECES.getName());
+        return preferencesService.parsePreferences(preferencesToken);
+    }
 
-        // Both must be specified, else we just use the default...
-        final String cacheControl = (StringUtils.isNotEmpty(cacheScopeValue) && StringUtils.isNotEmpty(cacheMaxAgeValue))
-                ? cacheScopeValue + ", max-age=" + cacheMaxAgeValue
-                : CACHE_CONTROL_NOSTORE;
-        logger.debug("Setting cache-control='{}' for module '{}'", cacheControl, module);
-
-        // TODO: support validation caching
-
-        res.setHeader(Headers.CACHE_CONTROL.getName(), cacheControl);
-
+    private Definition getDefinition(final HttpServletRequest req) {
+        final String definitionToken = req.getHeader(Headers.DEFINITION.getName());
+        return definitionService.parseDefinition(definitionToken);
     }
 
     private String selectView(final HttpServletRequest req, final String module, final PortalRequest portalRequest) {
@@ -246,6 +279,102 @@ public class SoffitRendererController {
         logger.debug("Calculated path '{}' for parts={}", rslt, parts);
 
         return rslt.toString();
+
+    }
+
+    private Map<String,Object> gatherModelAttributes(String viewName, HttpServletRequest req, HttpServletResponse res,
+                        PortalRequest portalRequest, Bearer bearer, Preferences preferences, Definition definition) {
+
+        final Map<String,Object> rslt = new HashMap<>();
+
+        logger.debug("Processing model attributes for viewName='{}'", viewName);
+
+        for (Map.Entry<AnnotatedElement,Object> y : modelAttributes.entrySet()) {
+            final AnnotatedElement annotatedElement = y.getKey();
+            final Object bean = y.getValue();
+            final SoffitModelAttribute sma = annotatedElement.getAnnotation(SoffitModelAttribute.class);
+            if (attributeAppliesToView(sma, viewName)) {
+                logger.debug("The following  SoffitModelAttribute applies to viewName='{}':  {}", viewName, sma);
+                final String modelAttributeName = sma.value();
+                // Are we looking at a class or a method?
+                if (annotatedElement instanceof Class) {
+                    // The bean itself is the model attribute
+                    rslt.put(modelAttributeName, bean);
+                } else if (annotatedElement instanceof Method) {
+                    Method m = (Method) annotatedElement;
+                    // This Method must NOT have a void return type...
+                    if (m.getReturnType().equals(Void.TYPE)) {
+                        final String msg = "Methods annotated with SoffitModelAttribute must not specify a void return type;  " + m.getName();
+                        throw new IllegalStateException(msg);
+                    }
+                    // Examine the parameters this Method declares and try to match them.
+                    final Class<?>[] parameterTypes = m.getParameterTypes();
+                    final Object[] parameters = new Object[parameterTypes.length];
+                    for (int i=0; i < parameters.length; i++) {
+                        final Class<?> pType = parameterTypes[i];
+                        // At present, these are the parameter types we support...
+                        if (HttpServletRequest.class.equals(pType)) {
+                            parameters[i] = req;
+                        } else if (HttpServletResponse.class.equals(pType)) {
+                            parameters[i] = res;
+                        } else if (PortalRequest.class.equals(pType)) {
+                            parameters[i] = portalRequest;
+                        } else if (Bearer.class.equals(pType)) {
+                            parameters[i] = bearer;
+                        } else if (Preferences.class.equals(pType)) {
+                            parameters[i] = preferences;
+                        } else if (Definition.class.equals(pType)) {
+                            parameters[i] = definition;
+                        } else {
+                            final String msg = "Unsupported parameter type for SoffitModelAttribute method:  " + pType;
+                            throw new UnsupportedOperationException(msg);
+                        }
+                    }
+                    try {
+                        final Object value = m.invoke(bean, parameters);
+                        rslt.put(modelAttributeName, value);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        final String msg = "Failed to evaluate the specified model attribute:  " + sma.value();
+                        throw new RuntimeException(msg);
+                    }
+                } else {
+                    final String msg = "Unsuppored AnnotatedElement type:  " + AnnotatedElement.class.getName();
+                    throw new UnsupportedOperationException(msg);
+                }
+            }
+        }
+
+        logger.debug("Calculated the following model attributes for viewName='{}':  {}", viewName, rslt);
+
+        return rslt;
+
+    }
+
+    private boolean attributeAppliesToView(SoffitModelAttribute attributeAnnotation, String viewName) {
+        final Pattern pattern = Pattern.compile(attributeAnnotation.viewRegex());
+        final Matcher matcher = pattern.matcher(viewName);
+        return matcher.matches();
+    }
+
+    private void configureCacheHeaders(final HttpServletResponse res, final String module) {
+
+        final String cacheScopeProperty = String.format(CACHE_SCOPE_PROPERTY_FORMAT, module);
+        final String cacheScopeValue = environment.getProperty(cacheScopeProperty);
+        logger.debug("Selecting cacheScopeValue='{}' for property '{}'", cacheScopeValue, cacheScopeProperty);
+
+        final String cacheMaxAgeProperty = String.format(CACHE_MAXAGE_PROPERTY_FORMAT, module);
+        final String cacheMaxAgeValue = environment.getProperty(cacheMaxAgeProperty);
+        logger.debug("Selecting cacheMaxAgeValue='{}' for property '{}'", cacheMaxAgeValue, cacheMaxAgeProperty);
+
+        // Both must be specified, else we just use the default...
+        final String cacheControl = (StringUtils.isNotEmpty(cacheScopeValue) && StringUtils.isNotEmpty(cacheMaxAgeValue))
+                ? cacheScopeValue + ", max-age=" + cacheMaxAgeValue
+                : CACHE_CONTROL_NOSTORE;
+        logger.debug("Setting cache-control='{}' for module '{}'", cacheControl, module);
+
+        // TODO: support validation caching
+
+        res.setHeader(Headers.CACHE_CONTROL.getName(), cacheControl);
 
     }
 
