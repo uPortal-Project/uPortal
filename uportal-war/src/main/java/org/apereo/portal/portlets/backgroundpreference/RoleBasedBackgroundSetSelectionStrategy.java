da5bee5dd07e1f17127a4862869e885db238e39d
Corey Rowe <coreyr1196@gmail.com>
2017-02-21 11:03:16 -0500

    UP-4802: Convert preference name to public field

--- uportal-war/src/main/java/org/apereo/portal/portlets/backgroundpreference/RoleBasedBackgroundSetSelectionStrategy.java
+++ uportal-war/src/main/java/org/apereo/portal/portlets/backgroundpreference/RoleBasedBackgroundSetSelectionStrategy.java
@@ -57,6 +57,7 @@ public class RoleBasedBackgroundSetSelectionStrategy implements BackgroundSetSel
         public static PreferenceNames getInstance(PortletRequest req) {
             return req.isUserInRole(MOBILE_DEVICE_ROLE_NAME) ? MOBILE : DEFAULT;
         }
+
         private final String prefix;
 
         private PreferenceNames(String prefix) {
@@ -79,9 +80,11 @@ public class RoleBasedBackgroundSetSelectionStrategy implements BackgroundSetSel
             return prefix + "BackgroundContainerSelector";
         }
 
-        public String getImageCaptionsName() {
-            return "backgroundImageCaptions";
-        }
+        /**
+         * The portlet-preference containing the image captions.
+         * This preference name will not change.
+         */
+        public static final String IMAGE_CAPTIONS_PREFERENCE_NAME = "backgroundImageCaptions";
     }
 
     private static final String[] EMPTY_STRING_ARRAY = new String[0];
@@ -126,7 +129,7 @@ public class RoleBasedBackgroundSetSelectionStrategy implements BackgroundSetSel
     public String[] getImageCaptions(PortletRequest req) {
         PreferenceNames names = PreferenceNames.getInstance(req);
         PortletPreferences prefs = req.getPreferences();
-        return prefs.getValues(names.getImageCaptionsName(), null);
+        return prefs.getValues(names.IMAGE_CAPTIONS_PREFERENCE_NAME, null);
     }
 
     @Override
