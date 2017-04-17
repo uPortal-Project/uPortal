febe59a0272158a8582c19fcf9efd5bdbbe3aac5
Drew Wills <wills.drew@gmail.com>
2017-04-17 11:47:20 -0700

    Clean up a few unused import statements

--- uportal-war/src/main/java/org/apereo/portal/layout/profile/ChainingProfileMapperImpl.java
+++ uportal-war/src/main/java/org/apereo/portal/layout/profile/ChainingProfileMapperImpl.java
@@ -19,11 +19,9 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-import org.apereo.portal.layout.IUserLayoutStore;
 import org.apereo.portal.security.IPerson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * This concrete implementation of {@link IProfileMapper} decorates one or more enclosed profile
