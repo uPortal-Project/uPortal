d5fd75be621a5a03fb6285964426dd155164cb99
Seema <stalele@unicon.net>
2017-07-07 14:36:03 -0700

    Removed the apply:idea from build.gradle and removed the * from imports of EntitiesRESTController.java

--- uPortal-api/uPortal-api-rest/src/main/java/org/apereo/portal/rest/EntitiesRESTController.java
+++ uPortal-api/uPortal-api-rest/src/main/java/org/apereo/portal/rest/EntitiesRESTController.java
@@ -25,7 +25,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /** EntitiesRESTController */
 @Controller
