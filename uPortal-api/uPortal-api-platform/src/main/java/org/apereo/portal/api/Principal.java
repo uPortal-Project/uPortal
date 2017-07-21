f2cf501bb9617ddcbe6aa67d5f91eac11817bd53
Seema <stalele@unicon.net>
2017-07-21 09:30:36 -0700

    UP-4900 - Removed the public modifier

--- uPortal-api/uPortal-api-platform/src/main/java/org/apereo/portal/api/Principal.java
+++ uPortal-api/uPortal-api-platform/src/main/java/org/apereo/portal/api/Principal.java
@@ -18,7 +18,7 @@ import java.io.Serializable;
 
 public interface Principal extends Serializable {
 
-    public String getKey();
+    String getKey();
 
-    public  String getName();
+    String getName();
 }
