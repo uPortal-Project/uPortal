2efb8258498b3b44b31d031ea6d6078490fffd8a
Benito J. Gonzalez <bgonzalez@unicon.net>
2017-10-19 13:48:29 -0700

    UP-4957: Replace new CORS filter enable flag with OptionalFilterDecorator

--- uPortal-security/uPortal-security-mvc/src/main/java/org/apereo/portal/security/filter/CorsFilter.java
+++ uPortal-security/uPortal-security-mvc/src/main/java/org/apereo/portal/security/filter/CorsFilter.java
@@ -122,22 +122,12 @@ public class CorsFilter implements Filter {
     /** Determines if the request should be decorated or not. */
     private boolean decorateRequest;
 
-
-    /** Determines if the filter should be enabled or bypassed. */
-    private boolean enabled;
-
     @Override
     public void doFilter(
             final ServletRequest servletRequest,
             final ServletResponse servletResponse,
             final FilterChain filterChain)
             throws IOException, ServletException {
-        if (!enabled) {
-            // Forward the request down the filter chain.
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-
         if (!(servletRequest instanceof HttpServletRequest)
                 || !(servletResponse instanceof HttpServletResponse)) {
             throw new ServletException("CORS doesn't support non-HTTP request or response");
@@ -189,7 +179,6 @@ public class CorsFilter implements Filter {
     public CorsFilter() throws ServletException {
         // set defaults
         parseAndStore(
-                DEFAULT_ENABLED,
                 DEFAULT_ALLOWED_ORIGINS,
                 DEFAULT_ALLOWED_HTTP_METHODS,
                 DEFAULT_ALLOWED_HTTP_HEADERS,
@@ -207,7 +196,6 @@ public class CorsFilter implements Filter {
 
     public void init() throws ServletException {
         parseAndStore(
-                getInitParameter(PARAM_CORS_ENABLED, DEFAULT_ENABLED),
                 getInitParameter(PARAM_CORS_ALLOWED_ORIGINS, DEFAULT_ALLOWED_ORIGINS),
                 getInitParameter(PARAM_CORS_ALLOWED_METHODS, DEFAULT_ALLOWED_HTTP_METHODS),
                 getInitParameter(PARAM_CORS_ALLOWED_HEADERS, DEFAULT_ALLOWED_HTTP_HEADERS),
@@ -718,7 +706,6 @@ public class CorsFilter implements Filter {
      * @throws ServletException
      */
     private void parseAndStore(
-            final String enabled,
             final String allowedOrigins,
             final String allowedHttpMethods,
             final String allowedHttpHeaders,
@@ -727,8 +714,6 @@ public class CorsFilter implements Filter {
             final String preflightMaxAge,
             final String decorateRequest)
             throws ServletException {
-
-        setEnabled(enabled);
 
         setAllowedOrigins(allowedOrigins);
 
@@ -743,12 +728,6 @@ public class CorsFilter implements Filter {
         setPreflightMaxAge(preflightMaxAge);
 
         setDecorateRequest(decorateRequest);
-    }
-
-    public void setEnabled(String enabled) {
-        log.debug("setEnabled set to {}", enabled);
-        // For any value other then 'true' this will be false.
-        this.enabled = Boolean.parseBoolean(enabled);
     }
 
     public void setDecorateRequest(String decorateRequest) {
@@ -1074,9 +1053,6 @@ public class CorsFilter implements Filter {
                             "text/plain"));
 
     // ------------------------------------------------ Configuration Defaults
-    /** By default, enabled is turned off. */
-    public static final String DEFAULT_ENABLED = "false";
-
     /** By default, all origins are allowed to make requests. */
     public static final String DEFAULT_ALLOWED_ORIGINS = "*";
 
@@ -1104,9 +1080,6 @@ public class CorsFilter implements Filter {
     public static final String DEFAULT_DECORATE_REQUEST = "true";
 
     // ----------------------------------------Filter Config Init param-name(s)
-    /** Key to determine if CORS filter is enabled or bypassed. */
-    public static final String PARAM_CORS_ENABLED = "cors.enabled";
-
     /** Key to retrieve allowed origins from {@link javax.servlet.FilterConfig}. */
     public static final String PARAM_CORS_ALLOWED_ORIGINS = "cors.allowed.origins";
 
