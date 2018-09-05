6de6c56b632fb669d626736950384d32d9590ac1
Mairi Fraser <Mairi.Fraser@ed.ac.uk>
2018-09-05 18:11:46 +0100

    fix(#1322): Handle strings over 4000 characters as CLOB type in Oracle 12

---
+++ uPortal-hibernate/uPortal-hibernate4-dialects/src/main/java/org/apereo/portal/utils/hibernate4/dialects/Oracle12ForceClobDialect.java
@@ -0,0 +1,23 @@
+package org.apereo.portal.utils.hibernate4.dialects;
+
+import org.hibernate.dialect.Oracle10gDialect;
+import java.sql.Types;
+
+/**
+ * When using Oracle 10g dialect with Oracle 12, ensure strings over
+ * 4000 characters are handled correctly.
+ *
+ * See also https://groups.google.com/a/apereo.org/forum/#!topic/uportal-user/j1Opk6Knv_k
+ *
+ * @since 5.3
+ */
+public class Oracle12ForceClobDialect extends Oracle10gDialect {
+
+    public Oracle12ForceClobDialect() {
+
+        registerColumnType( Types.CHAR, "char(1 char)" );
+        registerColumnType( Types.VARCHAR, 4000, "varchar2($l char)" );
+        registerColumnType( Types.VARCHAR, "clob" );
+    }
+
+}
