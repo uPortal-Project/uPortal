3f3f5cb9550ea7140bcd6aa488dd42e6358fb104
Mairi Fraser <Mairi.Fraser@ed.ac.uk>
2018-09-06 17:37:44 +0100

    fix(#1322): Correct formatting

--- uPortal-hibernate/uPortal-hibernate4-dialects/src/main/java/org/apereo/portal/utils/hibernate4/dialects/Oracle12ForceClobDialect.java
+++ uPortal-hibernate/uPortal-hibernate4-dialects/src/main/java/org/apereo/portal/utils/hibernate4/dialects/Oracle12ForceClobDialect.java
@@ -1,13 +1,13 @@
 package org.apereo.portal.utils.hibernate4.dialects;
 
-import org.hibernate.dialect.Oracle10gDialect;
 import java.sql.Types;
+import org.hibernate.dialect.Oracle10gDialect;
 
 /**
- * When using Oracle 10g dialect with Oracle 12, ensure strings over
- * 4000 characters are handled correctly.
+ * When using Oracle 10g dialect with Oracle 12, ensure strings over 4000 characters are handled
+ * correctly.
  *
- * See also https://groups.google.com/a/apereo.org/forum/#!topic/uportal-user/j1Opk6Knv_k
+ * <p>See also https://groups.google.com/a/apereo.org/forum/#!topic/uportal-user/j1Opk6Knv_k
  *
  * @since 5.3
  */
@@ -15,9 +15,8 @@ public class Oracle12ForceClobDialect extends Oracle10gDialect {
 
     public Oracle12ForceClobDialect() {
 
-        registerColumnType( Types.CHAR, "char(1 char)" );
-        registerColumnType( Types.VARCHAR, 4000, "varchar2($l char)" );
-        registerColumnType( Types.VARCHAR, "clob" );
+        registerColumnType(Types.CHAR, "char(1 char)");
+        registerColumnType(Types.VARCHAR, 4000, "varchar2($l char)");
+        registerColumnType(Types.VARCHAR, "clob");
     }
-
 }
