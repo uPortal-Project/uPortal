db72d2a38397248ddf54a7d8c9f35ade9bb03b78
Lauren Anderson <laurenra@byu.edu>
2018-09-21 14:06:41 -0600

    Added uPortal/health-check endpoint

---
+++ uPortal-health/src/main/java/org/apereo/portal/health/HealthCheckController.java
@@ -0,0 +1,25 @@
+package org.apereo.portal.health;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class HealthCheckController {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public void healthCheck(HttpServletRequest request, HttpServletResponse response) {
+        // Do nothing, just return HTTP 200, OK
+        logger.debug("Doing a health check. Returning HTTP 200, OK");
+    }
+
+}
