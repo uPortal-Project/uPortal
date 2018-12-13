f4f4b713c3e115da7d8deded7a090ddb8add6d8c
Benito J. Gonzalez <bgonzalez@unicon.net>
2018-12-13 10:18:03 -0800

    fix(1384): update logging to a sane level.

--- uPortal-groups/uPortal-groups-smartldap/src/main/java/org/apereo/portal/groups/smartldap/SmartLdapGroupStore.java
+++ uPortal-groups/uPortal-groups-smartldap/src/main/java/org/apereo/portal/groups/smartldap/SmartLdapGroupStore.java
@@ -296,7 +296,7 @@ public final class SmartLdapGroupStore implements IEntityGroupStore {
             if (parent == null) {
                 log.warn("Group tree inconsistent -- missing parent: {}", parentKey);
             } else if (groups.contains(parent)) {
-                log.warn("Parent found multiple times, could be recursion group tree!");
+                log.warn("Parent found multiple times, could be recursion group tree! key: {}, parent key: {}", key, parentKey);
             } else {
                 groups.add(parent);
                 getParentGroups(parentKey, groups);
