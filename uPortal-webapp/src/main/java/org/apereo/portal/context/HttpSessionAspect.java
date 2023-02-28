8d24ea65a37fef10f3ec45260f3679ff3265ab58
Michael Gillian <mgillian-contractor@scholastic.com>
2023-02-28 08:50:41 -0700

    eat/SessionReplicationAop functional version

--- uPortal-webapp/src/main/java/org/apereo/portal/context/HttpSessionAspect.java
+++ uPortal-webapp/src/main/java/org/apereo/portal/context/HttpSessionAspect.java
@@ -1,59 +1,62 @@
+/**
+ * Licensed to Apereo under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright ownership. Apereo
+ * licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the License at the
+ * following location:
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apereo.portal.context;
 
-import java.util.Arrays;
-
-import javax.servlet.http.HttpSession;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.stereotype.Component;
 
-//import javax.servlet.http.HttpSession;
-
+/**
+ * HttpSessionAspect is responsible for instrumenting classes that implement HttpSession
+ * with logging data, to determine if the instance of HttpSession is trying to add
+ * a class that is not serialzable to the session.  HttpSessionAspect is not needed
+ * at this point for any production functionality; it is only used to identify
+ * classes that need to be modified to be serializable.
+ *
+ * This is class is being included in the uPortal-webapp package so that as part
+ * of the uPortal gradle build, the needed classes are available as part of the
+ * uPortal packaging.  uPortal-start will then pull needed instrumented classes
+ * and include them in the appropriate place within the tomcat instance.  This assumes
+ * that uPortal-start is being used to manage the installation into the appropriate
+ * tomcat server.
+ * @author mgillian
+ *
+ */
 @Aspect
-@Component
-//@EnableAspectJAutoProxy(proxyTargetClass = true)
-public class HttpSessionAspect {    
-	private static final Log log = LogFactory.getLog(HttpSessionAspect.class);
-	
-	public HttpSessionAspect() {
-		log.error("initializing HttpSessionAspect");
-	}
+public class HttpSessionAspect {
+    private static final Logger log = Logger.getLogger(HttpSessionAspect.class.getName());
 
+    @Before("execution(* javax.servlet.http.HttpSession+.setAttribute(..))")
+    public void loggingAdvice(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        if (args.length < 2) {
+            log.log(Level.SEVERE, "HttpSession.setAttribute does not have at least 2 parameters");
+            return;
+        }
 
-//    @Pointcut(value = "execution(public * *(..))")
-//    public void anyPublicMethod() {}
-
-//	HttpSession session;
-//	@Before("execution(public void com.journaldev.spring.model..set*(*))")
-//	@Before("execution(public void javax.servlet.http.HttpSession.setAttribute(*))")
-//	@Before("execution(* javax.servlet.http.HttpSession.setAttribute(..))")
-//	public void loggingAdvice(JoinPoint joinPoint){
-	
-	
-//    @Pointcut(value = "execution(public * java.servlet.http.HttpSession.setAttribute(..))")
-//    public void anyPublicMethod() {}
-
-//  @Around("anyPublicMethod()")
-//    @Around("anyPublicMethod() && @annotation(openEntityManager)")
-//    public Object openEntityManager(ProceedingJoinPoint pjp, OpenEntityManager openEntityManager)
-//            throws Throwable {
-	@Around("execution(* javax.servlet.http.HttpSession.setAttribute(..))")
-	public void loggingAdvice(JoinPoint joinPoint){
-//	  @Before("args(httpSession,..)")
-//	  public void logHttpSession(HttpSession httpSession) {
-	  log.error("##############################################");
-//		log.error("Before running loggingAdvice on method="+joinPoint.toString());		
-//		log.error("Agruments Passed=" + Arrays.toString(joinPoint.getArgs()));
-		log.error("in loggingAdvice");
-		log.error("##############################################");
-//		return null;
-	}
+        // HttpSession.setAttribute takes two parameters:
+        //   key: a string name for the parameter, not important for this use case
+        //   value: the object to be stored, which must be serializable
+        Object value = args[1];
+        if (!(value instanceof Serializable)) {
+            log.log(Level.INFO, "value [" + value.getClass().getName() + "] is NOT serializable");
+        } else {
+            log.log(Level.FINE, "value [" + value.getClass().getName() + "] is serializable");
+        }
+    }
 }
