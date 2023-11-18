ed5a3fdd5a604b24121252661cabdfc4d9a6f07a
Snehit Roda <sn584121@dal.ca>
2023-11-18 15:18:37 -0400

    chore: pull method and variable refactor for PortletPlaceholderEventImpl

--- uPortal-rendering/src/main/java/org/apereo/portal/character/stream/events/PortletContentPlaceholderEventImpl.java
+++ uPortal-rendering/src/main/java/org/apereo/portal/character/stream/events/PortletContentPlaceholderEventImpl.java
@@ -21,8 +21,6 @@ public final class PortletContentPlaceholderEventImpl extends PortletPlaceholder
         implements PortletContentPlaceholderEvent {
     private static final long serialVersionUID = 1L;
 
-    private int hash = 0;
-
     public PortletContentPlaceholderEventImpl(IPortletWindowId portletWindowId) {
         super(portletWindowId);
     }
@@ -33,30 +31,6 @@ public final class PortletContentPlaceholderEventImpl extends PortletPlaceholder
     @Override
     public CharacterEventTypes getEventType() {
         return CharacterEventTypes.PORTLET_CONTENT;
-    }
-
-    @Override
-    public int hashCode() {
-        int h = hash;
-        if (h == 0) {
-            h = internalHashCode();
-            hash = h;
-        }
-        return h;
-    }
-
-    private int internalHashCode() {
-        final int prime = 31;
-        int result = 1;
-        result =
-                prime * result
-                        + ((this.getPortletWindowId() == null)
-                                ? 0
-                                : this.getPortletWindowId().hashCode());
-        result =
-                prime * result
-                        + ((this.getEventType() == null) ? 0 : this.getEventType().hashCode());
-        return result;
     }
 
     @Override
@@ -72,13 +46,5 @@ public final class PortletContentPlaceholderEventImpl extends PortletPlaceholder
             if (other.getPortletWindowId() != null) return false;
         } else if (!this.getPortletWindowId().equals(other.getPortletWindowId())) return false;
         return true;
-    }
-
-    @Override
-    public String toString() {
-        return "PortletContentPlaceholderEvent ["
-                + "portletWindowId="
-                + this.getPortletWindowId()
-                + "]";
     }
 }
