58ec456f528a18b0b6758daaa34ca54286afe240
himanshi <hm771904@dal.ca>
2024-03-20 16:40:58 -0300

    Formatting in google java format

--- uPortal-url/src/test/java/org/apereo/portal/url/ResourceUrlProviderImplTest.java
+++ uPortal-url/src/test/java/org/apereo/portal/url/ResourceUrlProviderImplTest.java
@@ -1,26 +1,21 @@
 package org.apereo.portal.url;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import javax.servlet.http.HttpServletRequest;
 import org.apereo.portal.portlet.om.IPortletWindow;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import javax.servlet.http.HttpServletRequest;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-
-/**
- * Test class for {@link ResourceUrlProviderImpl}.
- */
+/** Test class for {@link ResourceUrlProviderImpl}. */
 public class ResourceUrlProviderImplTest {
 
-    @Mock
-    private IPortletWindow portletWindowMock;
+    @Mock private IPortletWindow portletWindowMock;
 
-    @Mock
-    private HttpServletRequest requestMock;
+    @Mock private HttpServletRequest requestMock;
 
     @Before
     public void setup() {
@@ -28,35 +23,36 @@ public class ResourceUrlProviderImplTest {
         requestMock = mock(HttpServletRequest.class);
     }
 
-    /**
-     * Test method for {@link ResourceUrlProviderImpl#setAbsoluteURL(String)}.
-     */
+    /** Test method for {@link ResourceUrlProviderImpl#setAbsoluteURL(String)}. */
     @Test
     public void testSetAbsoluteURL() {
-        ResourceUrlProviderImpl urlProvider = new ResourceUrlProviderImpl(portletWindowMock, requestMock);
+        ResourceUrlProviderImpl urlProvider =
+                new ResourceUrlProviderImpl(portletWindowMock, requestMock);
         String absolutePath = "http://test.com/test";
         urlProvider.setAbsoluteURL(absolutePath);
         assertEquals(absolutePath, urlProvider.toString());
     }
 
-    /**
-     * Test method for {@link ResourceUrlProviderImpl#setFullPath(String)}.
-     */
+    /** Test method for {@link ResourceUrlProviderImpl#setFullPath(String)}. */
     @Test
     public void testSetFullPath() {
-        ResourceUrlProviderImpl urlProvider = new ResourceUrlProviderImpl(portletWindowMock, requestMock);
+        ResourceUrlProviderImpl urlProvider =
+                new ResourceUrlProviderImpl(portletWindowMock, requestMock);
         String fullPath = "/portlet/resource";
         urlProvider.setFullPath(fullPath);
         assertEquals(fullPath, urlProvider.toString());
     }
 
     /**
-     * Test method for {@link ResourceUrlProviderImpl#equals(Object)} and {@link ResourceUrlProviderImpl#hashCode()}.
+     * Test method for {@link ResourceUrlProviderImpl#equals(Object)} and {@link
+     * ResourceUrlProviderImpl#hashCode()}.
      */
     @Test
     public void testEqualsAndHashCode() {
-        ResourceUrlProviderImpl urlProvider1 = new ResourceUrlProviderImpl(portletWindowMock, requestMock);
-        ResourceUrlProviderImpl urlProvider2 = new ResourceUrlProviderImpl(portletWindowMock, requestMock);
+        ResourceUrlProviderImpl urlProvider1 =
+                new ResourceUrlProviderImpl(portletWindowMock, requestMock);
+        ResourceUrlProviderImpl urlProvider2 =
+                new ResourceUrlProviderImpl(portletWindowMock, requestMock);
 
         String path = "/portlet/resource";
         urlProvider1.setFullPath(path);
